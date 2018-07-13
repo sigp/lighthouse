3ef88be04bbc8a02db58bb5b6cae89893fdb2762
Paul Hauner <paul@paulhauner.com>
2018-07-13 15:34:05 +1000

    Add params to config

--- src/state/config.rs
+++ src/state/config.rs
@@ -1,13 +1,17 @@
 pub struct Config {
     pub attester_count: u64,
-    pub max_validators: u64
+    pub max_validators: u64,
+    pub shard_count: u16,
+    pub notaries_per_crosslink: u16
 }
 
 impl Config {
     pub fn standard() -> Self {
         Self {
             attester_count: 32,
-            max_validators: 2u64.pow(24)
+            max_validators: 2u64.pow(24),
+            shard_count: 20,
+            notaries_per_crosslink: 100
         }
     }
 }
