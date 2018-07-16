068ed4255f77764dec1a08349949b39f914651c1
Paul Hauner <paul@paulhauner.com>
2018-07-16 11:56:29 +1000

    Add param to config

--- src/state/config.rs
+++ src/state/config.rs
@@ -1,8 +1,13 @@
+use super::utils::types::U256;
+
 pub struct Config {
     pub attester_count: u64,
     pub max_validators: u64,
     pub shard_count: u16,
-    pub notaries_per_crosslink: u16
+    pub notaries_per_crosslink: u16,
+    pub default_balance: U256,
+    pub eject_balance: U256
+    
 }
 
 impl Config {
@@ -11,7 +16,9 @@ impl Config {
             attester_count: 32,
             max_validators: 2u64.pow(24),
             shard_count: 20,
-            notaries_per_crosslink: 100
+            notaries_per_crosslink: 100,
+            default_balance: U256::from(32000),
+            eject_balance: U256::from(16000)
         }
     }
 }
