f7c5625a621585d0d345a7a2d3b45eeee0086a5e
Paul Hauner <paul@paulhauner.com>
2018-09-11 13:26:45 +0200

    Fix bug in decoding, add more tests

--- ssz/src/impls.rs
+++ ssz/src/impls.rs
@@ -16,17 +16,17 @@ macro_rules! impl_decodable_for_uint {
             fn ssz_decode(bytes: &[u8])
                 -> Result<Self, DecodeError>
             {
-                assert!(0 < $bit_size &&
-                       $bit_size <= 64 &&
-                       $bit_size % 8 == 0);
-                let bytes_required = $bit_size / 8;
-                if bytes_required <= bytes.len() {
-                    let mut result = 0;
+                assert!((0 < $bit_size) &
+                        ($bit_size <= 64) &
+                        ($bit_size % 8 == 0));
+                let max_bytes = $bit_size / 8;
+                if bytes.len() <= max_bytes {
+                    let mut result: $type = 0;
                     for i in 0..bytes.len() {
                         let offset = (bytes.len() - i - 1) * 8;
-                        result = (bytes[i] << offset) | result;
+                        result = ((bytes[i] as $type) << offset) | result;
                     };
-                    Ok(result.into())
+                    Ok(result)
                 } else {
                     Err(DecodeError::TooLong)
                 }
@@ -35,8 +35,10 @@ macro_rules! impl_decodable_for_uint {
     }
 }
 
-impl_decodable_for_uint!(u64, 64 / 8);
-impl_decodable_for_uint!(u16, 16 / 8);
+impl_decodable_for_uint!(u16, 16);
+impl_decodable_for_uint!(u32, 32);
+impl_decodable_for_uint!(u64, 64);
+impl_decodable_for_uint!(usize, 64);
 
 impl Encodable for u8 {
     fn ssz_append(&self, s: &mut SszStream) {
@@ -79,5 +81,86 @@ impl Encodable for U256 {
         let mut a = [0; 32];
         self.to_big_endian(&mut a);
         s.append_encoded_val(&a.to_vec());
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::super::{
+        DecodeError,
+        decode_ssz_list_element,
+    };
+
+    #[test]
+    fn test_ssz_decode_u16() {
+        let ssz = vec![0, 0, 0, 1, 0];
+        let result: u16 = decode_ssz_list_element(&ssz, 0).unwrap();
+        assert_eq!(result, 0);
+
+        let ssz = vec![0, 0, 0, 1, 16];
+        let result: u16 = decode_ssz_list_element(&ssz, 0).unwrap();
+        assert_eq!(result, 16);
+
+        let ssz = vec![0, 0, 0, 2, 1, 0];
+        let result: u16 = decode_ssz_list_element(&ssz, 0).unwrap();
+        assert_eq!(result, 256);
+
+        let ssz = vec![0, 0, 0, 2, 255, 255];
+        let result: u16 = decode_ssz_list_element(&ssz, 0).unwrap();
+        assert_eq!(result, 65535);
+
+        let ssz = vec![0, 0, 0, 3, 0, 0, 1];
+        let result: Result<u16, DecodeError> =
+            decode_ssz_list_element(&ssz, 0);
+        assert_eq!(result, Err(DecodeError::TooLong));
+    }
+
+    #[test]
+    fn test_ssz_decode_u32() {
+        let ssz = vec![0, 0, 0, 1, 0];
+        let result: u32 = decode_ssz_list_element(&ssz, 0).unwrap();
+        assert_eq!(result, 0);
+
+        let ssz = vec![0, 0, 0, 4, 255, 255, 255, 255];
+        let result: u32 = decode_ssz_list_element(&ssz, 0).unwrap();
+        assert_eq!(result, 4294967295);
+
+        let ssz = vec![0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+        let result: Result<u32, DecodeError> =
+            decode_ssz_list_element(&ssz, 0);
+        assert_eq!(result, Err(DecodeError::TooLong));
+    }
+
+    #[test]
+    fn test_ssz_decode_u64() {
+        let ssz = vec![0, 0, 0, 1, 0];
+        let result: u64 = decode_ssz_list_element(&ssz, 0).unwrap();
+        assert_eq!(result, 0);
+
+        let ssz = vec![0, 0, 0, 8, 255, 255, 255, 255, 255, 255, 255, 255];
+        let result: u64 = decode_ssz_list_element(&ssz, 0).unwrap();
+        assert_eq!(result, 18446744073709551615);
+
+        let ssz = vec![0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+        let result: Result<u64, DecodeError> =
+            decode_ssz_list_element(&ssz, 0);
+        assert_eq!(result, Err(DecodeError::TooLong));
+    }
+
+    #[test]
+    fn test_ssz_decode_usize() {
+        let ssz = vec![0, 0, 0, 1, 0];
+        let result: usize = decode_ssz_list_element(&ssz, 0).unwrap();
+        assert_eq!(result, 0);
+
+        let ssz = vec![0, 0, 0, 8, 255, 255, 255, 255, 255, 255, 255, 255];
+        let result: usize = decode_ssz_list_element(&ssz, 0).unwrap();
+        assert_eq!(result, 18446744073709551615);
+
+        let ssz = vec![0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+        let result: Result<usize, DecodeError> =
+            decode_ssz_list_element(&ssz, 0);
+        assert_eq!(result, Err(DecodeError::TooLong));
     }
 }
