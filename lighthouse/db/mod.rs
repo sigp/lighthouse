cf2add475a93facbf406b735a1c570049c31c680
Paul Hauner <paul@paulhauner.com>
2018-09-18 10:27:29 +1000

    Implement get and put on ClientDB trait

--- lighthouse/db/mod.rs
+++ lighthouse/db/mod.rs
@@ -4,18 +4,23 @@ mod disk_db;
 
 pub use self::disk_db::DiskDB;
 
+type DBValue = Vec<u8>;
+
 #[derive(Debug)]
 pub struct DBError {
     message: String
 }
 
 impl DBError {
-    fn new(message: String) -> Self {
+    pub fn new(message: String) -> Self {
         Self { message }
     }
 }
 
 pub trait ClientDB: Sync + Send {
     fn get(&self, col: &str, key: &[u8])
-        -> Result<Option<&[u8]>, DBError>;
+        -> Result<Option<DBValue>, DBError>;
+
+    fn put(&self, col: &str, key: &[u8], val: &[u8])
+        -> Result<(), DBError>;
 }
