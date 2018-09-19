815180d88c338394d2e9af455a9fac9e388f6d85
Age <Age@AgeManning.com>
2018-09-19 14:00:35 +1000

    Adds assert_error macro

--- lighthouse/utils/mod.rs
+++ lighthouse/utils/mod.rs
@@ -3,8 +3,12 @@ extern crate blake2;
 extern crate crypto_mac;
 extern crate boolean_bitfield;
 
+#[macro_use]
+pub mod macros;
 pub mod types;
 pub mod bls;
 pub mod test_helpers;
 pub mod logging;
 pub mod errors;
+
+
