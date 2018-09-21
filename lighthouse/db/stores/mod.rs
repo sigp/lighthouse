1fbe6d3c1d7c25294059aaa3534cb66592d0dbd9
Paul Hauner <paul@paulhauner.com>
2018-09-21 11:13:07 +1000

    Add basic PoW chain db store

--- lighthouse/db/stores/mod.rs
+++ lighthouse/db/stores/mod.rs
@@ -4,7 +4,10 @@ use super::{
 };
 
 mod block_store;
+mod pow_chain_store;
 
 pub use self::block_store::BlockStore;
+pub use self::pow_chain_store::PoWChainStore;
 
 const BLOCKS_DB_COLUMN: &str = "blocks";
+const POW_CHAIN_DB_COLUMN: &str = "powchain";
