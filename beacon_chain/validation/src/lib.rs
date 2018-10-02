117a207d495244377e56f2c2fd308726ed7ef1bd
Paul Hauner <paul@paulhauner.com>
2018-10-02 17:35:03 +1000

    Add pre-written validation code
    This adds block and attestation validation code that was written
    previously. There were many non-validation specific changes made whilst
    building these functions (e.g., db, hashing, etc) -- these changes have
    already been merged into master and this branch has been created just to
    make it easy to review this code.

---
+++ beacon_chain/validation/src/lib.rs
@@ -0,0 +1,12 @@
+extern crate db;
+extern crate bls;
+extern crate hashing;
+extern crate ssz;
+extern crate ssz_helpers;
+extern crate types;
+
+pub mod attestation_validation;
+mod attestation_parent_hashes;
+pub mod block_validation;
+mod message_generation;
+mod signature_verification;
---
+++ beacon_chain/validation/tests/attestation_validation/mod.rs
@@ -0,0 +1,9 @@
+pub mod helpers;
+mod tests;
+
+use super::bls;
+use super::db;
+use super::ssz;
+use super::types;
+use super::hashing;
+use super::validation;
