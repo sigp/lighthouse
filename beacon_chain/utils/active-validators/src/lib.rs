e7679563e3bb7a4de03a7abda921cd6157355f04
Alex Stokes <r.alex.stokes@gmail.com>
2018-12-11 15:16:25 -0800

    add method to determine validator status

--- beacon_chain/utils/active-validators/src/lib.rs
+++ beacon_chain/utils/active-validators/src/lib.rs
@@ -2,27 +2,28 @@ extern crate types;
 
 use types::{ValidatorRecord, ValidatorStatus};
 
-pub fn validator_is_active(v: &ValidatorRecord) -> bool {
-    v.status == ValidatorStatus::Active as u8
-}
-
 /// Returns the indicies of each active validator in a given vec of validators.
 pub fn active_validator_indices(validators: &[ValidatorRecord]) -> Vec<usize> {
     validators
         .iter()
         .enumerate()
         .filter_map(|(i, validator)| {
-            if validator_is_active(&validator) {
+            if validator.status_is(ValidatorStatus::Active) {
                 Some(i)
             } else {
                 None
             }
-        }).collect()
+        })
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    pub fn validator_is_active(v: &ValidatorRecord) -> bool {
+        v.status_is(ValidatorStatus::Active)
+    }
 
     #[test]
     fn test_active_validator() {
