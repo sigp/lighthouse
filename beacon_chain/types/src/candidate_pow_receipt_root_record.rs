eb9de4386bbee5cce3ac8e127f384831d03f5e2f
Paul Hauner <paul@paulhauner.com>
2018-12-13 17:32:17 +1100

    Add clone method to some `types`

--- beacon_chain/types/src/candidate_pow_receipt_root_record.rs
+++ beacon_chain/types/src/candidate_pow_receipt_root_record.rs
@@ -1,6 +1,6 @@
 use super::Hash256;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct CandidatePoWReceiptRootRecord {
     pub candidate_pow_receipt_root: Hash256,
     pub votes: u64,
