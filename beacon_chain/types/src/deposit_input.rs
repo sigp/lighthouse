b920dbe4db07b81180295412a428c5a8ebd6206a
Grant Wuerker <gwuerker@googlemail.com>
2018-12-26 12:10:01 -0600

    deleted deposit types

--- beacon_chain/types/src/deposit_input.rs
+++
@@ -1,9 +0,0 @@
-use super::bls::{PublicKey, Signature};
-use super::{Hash256};
-
-pub struct DepositInput {
-    pub pubkey: PublicKey,
-    pub withdrawal_credentials: Hash256,
-    pub randao_commitment: Hash256,
-    pub proof_of_possession: Signature
-}
