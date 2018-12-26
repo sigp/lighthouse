2b818d0c70e9131a71fc88f95421f917d00d4331
Grant Wuerker <gwuerker@googlemail.com>
2018-12-26 12:10:01 -0600

    deleted deposit types

--- beacon_chain/types/src/deposit.rs
+++
@@ -1,8 +0,0 @@
-use super::{Hash256};
-use super::deposit_data::DepositData;
-
-pub struct Deposit {
-    pub merkle_branch: Vec<Hash256>,
-    pub merkle_tree_index: u64,
-    pub deposit_data: DepositData
-}
--- beacon_chain/types/src/deposit_data.rs
+++
@@ -1,7 +0,0 @@
-use super::deposit_input::DepositInput;
-
-pub struct DepositData {
-    pub deposit_input: DepositInput,
-    pub value: u64,
-    pub timestamp: u64
-}
