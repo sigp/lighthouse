f4c3e8f7fcb9c263988dc3316d2cdb212620ef61
Paul Hauner <paul@paulhauner.com>
2018-12-29 14:39:56 +1100

    Add unfinished chain code

---
+++ lighthouse/db/src/stores/beacon_state_store.rs
@@ -0,0 +1,33 @@
+use super::STATES_DB_COLUMN as DB_COLUMN;
+use super::{ClientDB, DBError};
+use std::sync::Arc;
+use types::Hash256;
+
+pub struct BeaconStateStore<T>
+where
+    T: ClientDB,
+{
+    db: Arc<T>,
+}
+
+impl<T: ClientDB> BeaconStateStore<T> {
+    pub fn new(db: Arc<T>) -> Self {
+        Self { db }
+    }
+
+    pub fn put(&self, hash: &Hash256, ssz: &[u8]) -> Result<(), DBError> {
+        self.db.put(DB_COLUMN, hash, ssz)
+    }
+
+    pub fn get(&self, hash: &[u8]) -> Result<Option<Vec<u8>>, DBError> {
+        self.db.get(DB_COLUMN, hash)
+    }
+
+    pub fn exists(&self, hash: &[u8]) -> Result<bool, DBError> {
+        self.db.exists(DB_COLUMN, hash)
+    }
+
+    pub fn delete(&self, hash: &[u8]) -> Result<(), DBError> {
+        self.db.delete(DB_COLUMN, hash)
+    }
+}
