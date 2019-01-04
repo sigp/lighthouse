ad91648f30248e0e62146de14112e9fd50987cd4
Paul Hauner <paul@paulhauner.com>
2019-01-04 18:10:14 +1100

    Tidy up SSZ testing on types crate

--- beacon_chain/types/src/beacon_state.rs
+++ beacon_chain/types/src/beacon_state.rs
@@ -166,3 +166,21 @@ impl<T: RngCore> TestRandom<T> for BeaconState {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::ssz::ssz_encode;
+    use super::*;
+    use crate::test_utils::{SeedableRng, TestRandom, XorShiftRng};
+
+    #[test]
+    pub fn test_ssz_round_trip() {
+        let mut rng = XorShiftRng::from_seed([42; 16]);
+        let original = BeaconState::random_for_test(&mut rng);
+
+        let bytes = ssz_encode(&original);
+        let (decoded, _) = <_>::ssz_decode(&bytes, 0).unwrap();
+
+        assert_eq!(original, decoded);
+    }
+}
