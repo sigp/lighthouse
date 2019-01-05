d1744e8a59488069011499a46d73eaae45c7a759
Victor Lopez <vhrlopes@gmail.com>
2019-01-06 02:51:53 +0300

    Fork ethereum-types, ethbloom and serialize, remove deprecated macros

---
+++ beacon_chain/ethereum-types/src/lib.rs
@@ -0,0 +1,32 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(feature = "std")]
+extern crate core;
+#[macro_use]
+extern crate crunchy;
+#[macro_use]
+extern crate uint as uint_crate;
+#[macro_use]
+extern crate fixed_hash;
+extern crate ethbloom;
+
+#[cfg(feature = "serialize")]
+extern crate ethereum_types_serialize;
+#[cfg(feature = "serialize")]
+extern crate serde;
+
+#[cfg(test)]
+extern crate serde_json;
+
+mod hash;
+mod uint;
+
+pub use ethbloom::{Bloom, BloomRef, Input as BloomInput};
+pub use fixed_hash::clean_0x;
+pub use hash::{H128, H160, H256, H264, H32, H512, H520, H64};
+pub use uint::{U128, U256, U512, U64};
+
+pub type Address = H160;
+pub type Secret = H256;
+pub type Public = H512;
+pub type Signature = H520;
