a2b23d6ae2051ee7d3ad0a1bf167ee4861907d99
Alex Stokes <r.alex.stokes@gmail.com>
2019-01-10 14:57:34 -0600

    run cargo fmt

--- beacon_chain/genesis/src/lib.rs
+++ beacon_chain/genesis/src/lib.rs
@@ -3,8 +3,8 @@ extern crate types;
 extern crate validator_induction;
 extern crate validator_shuffling;
 
-mod beacon_state;
 mod beacon_block;
+mod beacon_state;
 
 pub use crate::beacon_block::genesis_beacon_block;
 pub use crate::beacon_state::{genesis_beacon_state, Error as GenesisError};
