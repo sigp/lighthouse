33425f6290a679bbc87d606a37299ded5dd278a1
Feng94 <8751189+Feng94@users.noreply.github.com>
2019-01-24 23:48:52 +1100

    Fix hardcoded string to use system constant for directory separator

--- protos/build.rs
+++ protos/build.rs
@@ -1,7 +1,7 @@
 extern crate protoc_grpcio;
 
 fn main() {
-    let proto_root = "src/";
+    let proto_root = &format!("{}{}", "src", std::path::MAIN_SEPARATOR);
     println!("cargo:rerun-if-changed={}", proto_root);
     protoc_grpcio::compile_grpc_protos(&["services.proto"], &[proto_root], &proto_root)
         .expect("Failed to compile gRPC definitions!");
