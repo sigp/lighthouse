f76081fc6197fb64fe3002c10dcf99fb534876b3
Kirk Baird <baird.k@outlook.com>
2019-01-25 16:01:52 +1100

    Minor chagnes

--- eth2/slashing/src/verify_slashable_data.rs
+++ eth2/slashing/src/verify_slashable_data.rs
@@ -1,5 +1,17 @@
 use bls::AggregatePublicKey;
-use types::SlashableVoteData;
+use types::{Fork, SlashableVoteData};
+
+pu fn get_fork_version(fork: Fork, slot: u64) -> u64 {
+    if slot < fork.slot {
+        fork.previous_version
+    } else {
+        fork.current_version
+    }
+}
+
+pub fn get_domain(fork: Fork, slot: u64, domain_type: u64) -> u64 {
+    get_fork_version(fork, slot) * u64::pow(2, 32) + domain_type
+}
 
 pub fn verify_slashable_vote_data(
     state: BeaconState,
@@ -9,7 +21,7 @@ pub fn verify_slashable_vote_data(
     if (vote_data.custody_bit_0_indicies.len() + vote_data.custody_bit_1_indicies.len() > spec.max_casper_votes) {
         return false;
     }
-    
+
     // Generate aggregate public key for custody bit 0 indicies
     let mut pubkeys_custody_bit_0: AggregatePublicKey = AggregatePublicKey::new();
     for i in vote_data.custody_bit_0_indicies.iter() {
@@ -21,12 +33,21 @@ pub fn verify_slashable_vote_data(
     for i in vote_data.custody_bit_1_indicies.iter() {
          pubkeys_custody_bit_1.add(&state.validator_registry[i].pubkey);
     }
-   
+
+    // Convert messages to hashes
+    let mut messages: Vec<u8>;
+    messages.append((AttestationDataAndCustodyBit {vote_data.data, false}).hash_tree_root());
+    messages.append((AttestationDataAndCustodyBit {vote_data.data, true}).hash_tree_root());
+
+
     vote_data.aggregate_signature.verify_multiple (
-        // parse messages, https://github.com/sigp/lighthouse/issues/70
-        &vec![publickeys_custody_bit_0, publickeys_custody_bit_1], 
-        // parse domain, https://github.com/sigp/lighthouse/issues/91
-        // For domain likely just add 8 bytes to message
+        &messages,
+        &vec![publickeys_custody_bit_0, publickeys_custody_bit_1],
+        get_domain(
+            state.fork,
+            vote_data.data.slot,
+            1, // DOMAIN_ATTESTATION
+        ),
     )
 }
 
