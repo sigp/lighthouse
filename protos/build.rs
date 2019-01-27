f218ab8a2383ff5e536ff70e5ebb8137098a8810
Feng94 <8751189+Feng94@users.noreply.github.com>
2019-01-28 02:19:53 +1100

    Change build root folder String to a Path

--- protos/build.rs
+++ protos/build.rs
@@ -1,8 +1,10 @@
 extern crate protoc_grpcio;
 
+use std::path::Path;
+
 fn main() {
-    let proto_root = &format!("{}{}", "src", std::path::MAIN_SEPARATOR);
-    println!("cargo:rerun-if-changed={}", proto_root);
+    let proto_root = Path::new("src");
+    println!("cargo:rerun-if-changed={}", proto_root.display());
     protoc_grpcio::compile_grpc_protos(&["services.proto"], &[proto_root], &proto_root)
         .expect("Failed to compile gRPC definitions!");
 }
