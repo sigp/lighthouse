4b9b10458056372fbc685e2e0146e23a92378655
Paul Hauner <paul@paulhauner.com>
2019-02-13 14:15:53 +1100

    Strip out old code
    All of these files have been moved to either:
    
    - https://github.com/sigp/lighthouse-beacon
    - https://github.com/sigp/lighthouse-validator
    - https://github.com/sigp/lighthouse-common
    
    For rationale, see: https://github.com/sigp/lighthouse/issues/197

--- beacon_node/beacon_chain/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod attestation_aggregator;
-mod attestation_targets;
-mod beacon_chain;
-mod block_graph;
-mod checkpoint;
-mod lmd_ghost;
-
-pub use self::beacon_chain::{BeaconChain, Error};
-pub use self::checkpoint::CheckPoint;
