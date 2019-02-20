6fc74cff5487ddba4e651c62f3ade05b57d21e6b
Kirk Baird <baird.k@outlook.com>
2019-02-20 15:34:15 +1100

    Fuzz test decodes from u8 to u64

---
+++ eth2/utils/ssz/fuzz/fuzz_targets/fuzz_target_u64_decode.rs
@@ -0,0 +1,31 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate ssz;
+
+use ssz::{DecodeError, Decodable};
+
+// Fuzz ssz_decode()
+fuzz_target!(|data: &[u8]| {
+    let result: Result<(u64, usize), DecodeError> = Decodable::ssz_decode(data, 0);
+    if data.len() >= 8 {
+        // Valid result
+        let (number_u64, index) = result.unwrap();
+        assert_eq!(index, 8);
+        // TODO: change to little endian bytes
+        // https://github.com/sigp/lighthouse/issues/215
+        let val = u64::from_be_bytes([
+            data[0],
+            data[1],
+            data[2],
+            data[3],
+            data[4],
+            data[5],
+            data[6],
+            data[7],
+            ]);
+        assert_eq!(number_u64, val);
+    } else {
+        // Length less then 4 should return error
+        assert_eq!(result, Err(DecodeError::TooShort));
+    }
+});
--- eth2/utils/ssz/fuzz/fuzz_targets/fuzz_target_u8_decode.rs
+++ eth2/utils/ssz/fuzz/fuzz_targets/fuzz_target_u8_decode.rs
@@ -2,16 +2,18 @@
 #[macro_use] extern crate libfuzzer_sys;
 extern crate ssz;
 
-use ssz::{DecodeError, Decodable, Encodable};
+use ssz::{DecodeError, Decodable};
 
-// Fuzz ssz_decode(u8)
+// Fuzz ssz_decode()
 fuzz_target!(|data: &[u8]| {
     let result: Result<(u8, usize), DecodeError> = Decodable::ssz_decode(data, 0);
-    if data.len() > 0 {
+    if data.len() >= 1 {
         // Should have valid result
         let (number_u8, index) = result.unwrap();
+        // TODO: change to little endian bytes
+        // https://github.com/sigp/lighthouse/issues/215
         assert_eq!(number_u8, data[0]);
-        assert_eq!(index, 2);
+        assert_eq!(index, 1);
     } else {
         // Length of 0 should return error
         assert_eq!(result, Err(DecodeError::TooShort));
