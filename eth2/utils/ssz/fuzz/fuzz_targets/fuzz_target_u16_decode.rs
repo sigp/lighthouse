d5c4771f0a3810d50334cadcaef75606065974f3
Kirk Baird <baird.k@outlook.com>
2019-02-20 15:34:15 +1100

    Fuzz test decodes from u8 to u64

--- eth2/utils/ssz/fuzz/fuzz_targets/fuzz_target_u16_decode.rs
+++ eth2/utils/ssz/fuzz/fuzz_targets/fuzz_target_u16_decode.rs
@@ -2,16 +2,19 @@
 #[macro_use] extern crate libfuzzer_sys;
 extern crate ssz;
 
-use ssz::{DecodeError, Decodable, Encodable};
+use ssz::{DecodeError, Decodable};
 
-// Fuzz ssz_decode(u8)
+// Fuzz ssz_decode()
 fuzz_target!(|data: &[u8]| {
     let result: Result<(u16, usize), DecodeError> = Decodable::ssz_decode(data, 0);
-    if data.len() > 1 {
+    if data.len() >= 2 {
         // Valid result
         let (number_u16, index) = result.unwrap();
         assert_eq!(index, 2);
-        // TODO: add test for number?
+        // TODO: change to little endian bytes
+        // https://github.com/sigp/lighthouse/issues/215
+        let val = u16::from_be_bytes([data[0], data[1]]);
+        assert_eq!(number_u16, val);
     } else {
         // Length of 0 or 1 should return error
         assert_eq!(result, Err(DecodeError::TooShort));
