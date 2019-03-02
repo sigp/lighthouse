0efebee43a1e20c2d234b8f94e1561ed9008bdd1
Paul Hauner <paul@paulhauner.com>
2019-03-02 20:17:14 +1100

    Add state-checks to test_harness YAML
    Runs tests against a state at some slot

--- beacon_node/beacon_chain/test_harness/src/manifest/results.rs
+++ beacon_node/beacon_chain/test_harness/src/manifest/results.rs
@@ -1,18 +1,28 @@
-use super::yaml_helpers::{as_usize, as_vec_u64};
+use super::state_check::StateCheck;
+use super::yaml_helpers::as_usize;
 use yaml_rust::Yaml;
 
+#[derive(Debug)]
 pub struct Results {
-    pub num_validators: Option<usize>,
-    pub slashed_validators: Option<Vec<u64>>,
-    pub exited_validators: Option<Vec<u64>>,
+    pub num_skipped_slots: Option<usize>,
+    pub state_checks: Option<Vec<StateCheck>>,
 }
 
 impl Results {
     pub fn from_yaml(yaml: &Yaml) -> Self {
         Self {
-            num_validators: as_usize(&yaml, "num_validators"),
-            slashed_validators: as_vec_u64(&yaml, "slashed_validators"),
-            exited_validators: as_vec_u64(&yaml, "exited_validators"),
+            num_skipped_slots: as_usize(yaml, "num_skipped_slots"),
+            state_checks: parse_state_checks(yaml),
         }
     }
+}
+
+fn parse_state_checks(yaml: &Yaml) -> Option<Vec<StateCheck>> {
+    let mut states = vec![];
+
+    for state_yaml in yaml["states"].as_vec()? {
+        states.push(StateCheck::from_yaml(state_yaml));
+    }
+
+    Some(states)
 }
