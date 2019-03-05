33a3161905561c9ae52914ab0b95775f49fc4051
Michael Sproul <michael@sigmaprime.io>
2019-03-05 17:28:51 +1100

    Remove SSZ round-trip test duplication
    Closes #244

--- eth2/types/src/beacon_state/tests.rs
+++ eth2/types/src/beacon_state/tests.rs
@@ -3,7 +3,6 @@
 use super::*;
 use crate::test_utils::{SeedableRng, TestRandom, XorShiftRng};
 use crate::{BeaconState, ChainSpec};
-use ssz::{ssz_encode, Decodable};
 
 #[test]
 pub fn can_produce_genesis_block() {
@@ -60,25 +59,4 @@ pub fn get_attestation_participants_consistency() {
     }
 }
 
-#[test]
-pub fn test_ssz_round_trip() {
-    let mut rng = XorShiftRng::from_seed([42; 16]);
-    let original = BeaconState::random_for_test(&mut rng);
-
-    let bytes = ssz_encode(&original);
-    let (decoded, _) = <_>::ssz_decode(&bytes, 0).unwrap();
-
-    assert_eq!(original, decoded);
-}
-
-#[test]
-pub fn test_hash_tree_root_internal() {
-    let mut rng = XorShiftRng::from_seed([42; 16]);
-    let original = BeaconState::random_for_test(&mut rng);
-
-    let result = original.hash_tree_root_internal();
-
-    assert_eq!(result.len(), 32);
-    // TODO: Add further tests
-    // https://github.com/sigp/lighthouse/issues/170
-}
+ssz_tests!(BeaconState);
