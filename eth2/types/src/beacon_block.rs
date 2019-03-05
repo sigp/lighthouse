53890aa93313f6c36c4a0ce2d331df3292a20487
Michael Sproul <michael@sigmaprime.io>
2019-03-05 17:28:51 +1100

    Re-apply removal of duplicated ssz_tests
    Also, re-apply: "use cfg(test) for test macros"

--- eth2/types/src/beacon_block.rs
+++ eth2/types/src/beacon_block.rs
@@ -70,29 +70,6 @@ impl BeaconBlock {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::test_utils::{SeedableRng, TestRandom, XorShiftRng};
-    use ssz::{ssz_encode, Decodable, TreeHash};
 
-    #[test]
-    pub fn test_ssz_round_trip() {
-        let mut rng = XorShiftRng::from_seed([42; 16]);
-        let original = BeaconBlock::random_for_test(&mut rng);
-
-        let bytes = ssz_encode(&original);
-        let (decoded, _) = <_>::ssz_decode(&bytes, 0).unwrap();
-
-        assert_eq!(original, decoded);
-    }
-
-    #[test]
-    pub fn test_hash_tree_root() {
-        let mut rng = XorShiftRng::from_seed([42; 16]);
-        let original = BeaconBlock::random_for_test(&mut rng);
-
-        let result = original.hash_tree_root();
-
-        assert_eq!(result.len(), 32);
-        // TODO: Add further tests
-        // https://github.com/sigp/lighthouse/issues/170
-    }
+    ssz_tests!(BeaconBlock);
 }
--- eth2/types/src/lib.rs
+++ eth2/types/src/lib.rs
@@ -1,3 +1,4 @@
+#[macro_use]
 pub mod test_utils;
 
 pub mod attestation;
