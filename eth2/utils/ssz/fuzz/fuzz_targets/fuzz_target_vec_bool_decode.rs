08b803b6e7d24ddb85eb4e6678647509eb562af0
Kirk Baird <kirk@sigmaprime.io>
2019-03-07 17:50:00 +1100

    Modifications to fuzz tests

--- eth2/utils/ssz/fuzz/fuzz_targets/fuzz_target_vec_bool_decode.rs
+++ eth2/utils/ssz/fuzz/fuzz_targets/fuzz_target_vec_bool_decode.rs
@@ -1,12 +1,10 @@
 #![no_main]
 #[macro_use] extern crate libfuzzer_sys;
-extern crate ethereum_types;
 extern crate ssz;
 
-use ethereum_types::{Address, H256};
 use ssz::{DecodeError, Decodable};
 
 // Fuzz ssz_decode()
 fuzz_target!(|data: &[u8]| {
-    let _result: Result<(Vec<u8>, usize), DecodeError> = Decodable::ssz_decode(data, 0);
+    let _result: Result<(Vec<bool>, usize), DecodeError> = Decodable::ssz_decode(data, 0);
 });
