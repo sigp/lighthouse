5c2c611e84c669673aaaf069687781f4a6b4dbd5
Paul Hauner <paul@paulhauner.com>
2019-03-12 14:39:16 +1100

    Improve DX for loading validator keys from file

--- eth2/fork_choice/tests/tests.rs
+++ eth2/fork_choice/tests/tests.rs
@@ -224,7 +224,8 @@ fn setup_inital_state(
 
     let spec = ChainSpec::foundation();
 
-    let state_builder = TestingBeaconStateBuilder::new(no_validators, None, &spec);
+    let state_builder =
+        TestingBeaconStateBuilder::from_deterministic_keypairs(no_validators, &spec);
     let (state, _keypairs) = state_builder.build();
 
     let state_root = state.canonical_root();
