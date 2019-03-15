527538ec0945f9628d7366304eed53bfe27037e5
Paul Hauner <paul@paulhauner.com>
2019-03-15 16:30:05 +1100

    Update "block" family types to 0.5.0
    - Removes Proposal
    - Removes "readers" as they aren't actually being used anywhere.

--- eth2/types/src/beacon_block.rs
+++ eth2/types/src/beacon_block.rs
@@ -1,15 +1,15 @@
 use crate::test_utils::TestRandom;
-use crate::{BeaconBlockBody, ChainSpec, Eth1Data, Hash256, Proposal, Slot};
+use crate::{BeaconBlockBody, ChainSpec, Eth1Data, Hash256, Slot};
 use bls::Signature;
 use rand::RngCore;
 use serde_derive::{Deserialize, Serialize};
-use ssz::{SignedRoot, TreeHash};
+use ssz::TreeHash;
 use ssz_derive::{Decode, Encode, SignedRoot, TreeHash};
 use test_random_derive::TestRandom;
 
 /// A block of the `BeaconChain`.
 ///
-/// Spec v0.4.0
+/// Spec v0.5.0
 #[derive(
     Debug,
     PartialEq,
@@ -24,29 +24,27 @@ use test_random_derive::TestRandom;
 )]
 pub struct BeaconBlock {
     pub slot: Slot,
-    pub parent_root: Hash256,
+    pub previous_block_root: Hash256,
     pub state_root: Hash256,
-    pub randao_reveal: Signature,
-    pub eth1_data: Eth1Data,
     pub body: BeaconBlockBody,
     pub signature: Signature,
 }
 
 impl BeaconBlock {
-    /// Produce the first block of the Beacon Chain.
+    /// The first block of the Beacon Chain.
     ///
-    /// Spec v0.4.0
+    /// Spec v0.5.0
     pub fn genesis(state_root: Hash256, spec: &ChainSpec) -> BeaconBlock {
         BeaconBlock {
             slot: spec.genesis_slot,
-            parent_root: spec.zero_hash,
+            previous_block_root: spec.zero_hash,
             state_root,
-            randao_reveal: spec.empty_signature.clone(),
-            eth1_data: Eth1Data {
-                deposit_root: spec.zero_hash,
-                block_hash: spec.zero_hash,
-            },
             body: BeaconBlockBody {
+                randao_reveal: spec.empty_signature.clone(),
+                eth1_data: Eth1Data {
+                    deposit_root: spec.zero_hash,
+                    block_hash: spec.zero_hash,
+                },
                 proposer_slashings: vec![],
                 attester_slashings: vec![],
                 attestations: vec![],
@@ -60,21 +58,9 @@ impl BeaconBlock {
 
     /// Returns the `hash_tree_root` of the block.
     ///
-    /// Spec v0.4.0
+    /// Spec v0.5.0
     pub fn canonical_root(&self) -> Hash256 {
         Hash256::from_slice(&self.hash_tree_root()[..])
-    }
-
-    /// Returns an unsigned proposal for block.
-    ///
-    /// Spec v0.4.0
-    pub fn proposal(&self, spec: &ChainSpec) -> Proposal {
-        Proposal {
-            slot: self.slot,
-            shard: spec.beacon_chain_shard_number,
-            block_root: Hash256::from_slice(&self.signed_root()),
-            signature: spec.empty_signature.clone(),
-        }
     }
 }
 
--- eth2/types/src/readers/mod.rs
+++
@@ -1,5 +0,0 @@
-mod block_reader;
-mod state_reader;
-
-pub use self::block_reader::BeaconBlockReader;
-pub use self::state_reader::BeaconStateReader;
