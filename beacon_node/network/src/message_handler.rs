17c5a29ebfc171fac6f1db91e907519210f3ab56
Paul Hauner <paul@paulhauner.com>
2019-03-25 16:48:44 +1100

    Introduced `RequestId` newtype

--- beacon_node/network/src/message_handler.rs
+++ beacon_node/network/src/message_handler.rs
@@ -4,7 +4,7 @@ use crate::service::{NetworkMessage, OutgoingMessage};
 use crate::sync::SimpleSync;
 use crossbeam_channel::{unbounded as channel, Sender};
 use eth2_libp2p::{
-    rpc::{methods::GoodbyeReason, IncomingGossip, RPCRequest, RPCResponse},
+    rpc::{methods::GoodbyeReason, IncomingGossip, RPCRequest, RPCResponse, RequestId},
     PeerId, RPCEvent,
 };
 use futures::future;
@@ -111,25 +111,31 @@ impl MessageHandler {
     }
 
     /// A new RPC request has been received from the network.
-    fn handle_rpc_request(&mut self, peer_id: PeerId, _id: u64, request: RPCRequest) {
+    fn handle_rpc_request(&mut self, peer_id: PeerId, request_id: RequestId, request: RPCRequest) {
         // TODO: process the `id`.
         match request {
-            RPCRequest::Hello(hello_message) => {
-                self.sync
-                    .on_hello_request(peer_id, hello_message, &mut self.network_context)
-            }
+            RPCRequest::Hello(hello_message) => self.sync.on_hello_request(
+                peer_id,
+                request_id,
+                hello_message,
+                &mut self.network_context,
+            ),
             RPCRequest::Goodbye(goodbye_reason) => self.sync.on_goodbye(peer_id, goodbye_reason),
-            RPCRequest::BeaconBlockRoots(request) => {
-                self.sync
-                    .on_beacon_block_roots_request(peer_id, request, &mut self.network_context)
-            }
+            RPCRequest::BeaconBlockRoots(request) => self.sync.on_beacon_block_roots_request(
+                peer_id,
+                request_id,
+                request,
+                &mut self.network_context,
+            ),
             RPCRequest::BeaconBlockHeaders(request) => self.sync.on_beacon_block_headers_request(
                 peer_id,
+                request_id,
                 request,
                 &mut self.network_context,
             ),
             RPCRequest::BeaconBlockBodies(request) => self.sync.on_beacon_block_bodies_request(
                 peer_id,
+                request_id,
                 request,
                 &mut self.network_context,
             ),
@@ -143,17 +149,23 @@ impl MessageHandler {
 
     /// An RPC response has been received from the network.
     // we match on id and ignore responses past the timeout.
-    fn handle_rpc_response(&mut self, peer_id: PeerId, id: u64, response: RPCResponse) {
-        // if response id is related to a request, ignore (likely RPC timeout)
+    fn handle_rpc_response(&mut self, peer_id: PeerId, id: RequestId, response: RPCResponse) {
+        // if response id is not related to a request, ignore (likely RPC timeout)
         if self
             .network_context
-            .requests
-            .remove(&(peer_id.clone(), id))
+            .outstanding_outgoing_request_ids
+            .remove(&(peer_id.clone(), id.clone()))
             .is_none()
         {
-            debug!(self.log, "Unrecognised response from peer: {:?}", peer_id);
+            warn!(
+                self.log,
+                "Unknown ResponseId for incoming RPCRequest";
+                "peer" => format!("{:?}", peer_id),
+                "request_id" => format!("{:?}", id)
+            );
             return;
         }
+
         match response {
             RPCResponse::Hello(hello_message) => {
                 self.sync
@@ -210,9 +222,9 @@ pub struct NetworkContext {
     /// The network channel to relay messages to the Network service.
     network_send: crossbeam_channel::Sender<NetworkMessage>,
     /// A mapping of peers and the RPC id we have sent an RPC request to.
-    requests: HashMap<(PeerId, u64), Instant>,
-    /// A counter of request id for each peer.
-    request_ids: HashMap<PeerId, u64>,
+    outstanding_outgoing_request_ids: HashMap<(PeerId, RequestId), Instant>,
+    /// Stores the next `RequestId` we should include on an outgoing `RPCRequest` to a `PeerId`.
+    outgoing_request_ids: HashMap<PeerId, RequestId>,
     /// The `MessageHandler` logger.
     log: slog::Logger,
 }
@@ -221,8 +233,8 @@ impl NetworkContext {
     pub fn new(network_send: crossbeam_channel::Sender<NetworkMessage>, log: slog::Logger) -> Self {
         Self {
             network_send,
-            requests: HashMap::new(),
-            request_ids: HashMap::new(),
+            outstanding_outgoing_request_ids: HashMap::new(),
+            outgoing_request_ids: HashMap::new(),
             log,
         }
     }
@@ -234,6 +246,10 @@ impl NetworkContext {
 
     pub fn send_rpc_request(&mut self, peer_id: PeerId, rpc_request: RPCRequest) {
         let id = self.generate_request_id(&peer_id);
+
+        self.outstanding_outgoing_request_ids
+            .insert((peer_id.clone(), id.clone()), Instant::now());
+
         self.send_rpc_event(
             peer_id,
             RPCEvent::Request {
@@ -244,12 +260,16 @@ impl NetworkContext {
         );
     }
 
-    pub fn send_rpc_response(&mut self, peer_id: PeerId, rpc_response: RPCResponse) {
-        let id = self.generate_request_id(&peer_id);
+    pub fn send_rpc_response(
+        &mut self,
+        peer_id: PeerId,
+        request_id: RequestId,
+        rpc_response: RPCResponse,
+    ) {
         self.send_rpc_event(
             peer_id,
             RPCEvent::Response {
-                id,
+                id: request_id,
                 method_id: rpc_response.method_id(),
                 result: rpc_response,
             },
@@ -272,18 +292,14 @@ impl NetworkContext {
         //
     }
 
-    /// Generates a new request id for a peer.
-    fn generate_request_id(&mut self, peer_id: &PeerId) -> u64 {
-        // generate a unique id for the peer
-        let id = {
-            let borrowed_id = self.request_ids.entry(peer_id.clone()).or_insert_with(|| 0);
-            let id = borrowed_id.clone();
-            //increment the counter
-            *borrowed_id += 1;
-            id
-        };
-        // register RPC request
-        self.requests.insert((peer_id.clone(), id), Instant::now());
-        id
+    /// Returns the next `RequestId` for sending an `RPCRequest` to the `peer_id`.
+    fn generate_request_id(&mut self, peer_id: &PeerId) -> RequestId {
+        let next_id = self
+            .outgoing_request_ids
+            .entry(peer_id.clone())
+            .and_modify(|id| id.increment())
+            .or_insert_with(|| RequestId::from(1));
+
+        next_id.previous()
     }
 }
