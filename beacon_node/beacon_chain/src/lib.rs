935c64deef97431d71e20272c9411e99ecfd2722
Paul Hauner <paul@paulhauner.com>
2019-03-30 19:11:52 +1100

    Adds attestation validation to `SimpleSync`

--- beacon_node/beacon_chain/src/lib.rs
+++ beacon_node/beacon_chain/src/lib.rs
@@ -13,4 +13,8 @@ pub use db;
 pub use fork_choice;
 pub use parking_lot;
 pub use slot_clock;
+pub use state_processing::per_block_processing::errors::{
+    AttestationValidationError, AttesterSlashingValidationError, DepositValidationError,
+    ExitValidationError, ProposerSlashingValidationError, TransferValidationError,
+};
 pub use types;
