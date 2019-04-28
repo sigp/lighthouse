ba60a8fbc2bc7f0f9d66fce1b66225859ea6e639
Paul Hauner <paul@paulhauner.com>
2019-04-28 11:38:32 +1000

    Add benches, examples for cached hashing.
    Ignore flamegraph files

---
+++ eth2/utils/cached_tree_hash/examples/8k_hashes_cached.rs
@@ -0,0 +1,21 @@
+use cached_tree_hash::TreeHashCache;
+use ethereum_types::H256 as Hash256;
+
+fn run(vec: &Vec<Hash256>, modified_vec: &Vec<Hash256>) {
+    let mut cache = TreeHashCache::new(vec, 0).unwrap();
+
+    cache.update(modified_vec).unwrap();
+}
+
+fn main() {
+    let n = 2048;
+
+    let vec: Vec<Hash256> = (0..n).map(|_| Hash256::random()).collect();
+
+    let mut modified_vec = vec.clone();
+    modified_vec[n - 1] = Hash256::random();
+
+    for _ in 0..100 {
+        run(&vec, &modified_vec);
+    }
+}
