3f9430ddffb4722604c791b31db38e0aa7baedf9
Paul Hauner <paul@paulhauner.com>
2019-05-03 11:05:51 +1000

    Add partial progress on sos

--- eth2/utils/ssz2/src/lib.rs
+++ eth2/utils/ssz2/src/lib.rs
@@ -13,8 +13,10 @@ extern crate ethereum_types;
 
 pub mod decode;
 */
+mod decode;
 mod encode;
 
+pub use decode::{Decodable, DecodeError};
 pub use encode::{Encodable, SszStream};
 
 pub const BYTES_PER_LENGTH_OFFSET: usize = 4;
