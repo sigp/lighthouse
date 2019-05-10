1e2bf03d5e0f05468f9354090b267276c5f7df51
Paul Hauner <paul@paulhauner.com>
2019-05-10 16:37:56 +1000

    Update ssz impl for `fixed_len_vec`

--- eth2/utils/fixed_len_vec/src/impls.rs
+++ eth2/utils/fixed_len_vec/src/impls.rs
@@ -55,8 +55,26 @@ impl<T, N: Unsigned> ssz::Encodable for FixedLenVec<T, N>
 where
     T: ssz::Encodable,
 {
-    fn ssz_append(&self, s: &mut ssz::SszStream) {
-        s.append_vec(&self.vec)
+    fn is_ssz_fixed_len() -> bool {
+        true
+    }
+
+    fn ssz_append(&self, buf: &mut Vec<u8>) {
+        if T::is_ssz_fixed_len() {
+            buf.reserve(T::ssz_fixed_len() * self.len());
+
+            for item in &self.vec {
+                item.ssz_append(buf);
+            }
+        } else {
+            let mut encoder = ssz::SszEncoder::list(buf, self.len() * ssz::BYTES_PER_LENGTH_OFFSET);
+
+            for item in &self.vec {
+                encoder.append(item);
+            }
+
+            encoder.finalize();
+        }
     }
 }
 
@@ -64,7 +82,29 @@ impl<T, N: Unsigned> ssz::Decodable for FixedLenVec<T, N>
 where
     T: ssz::Decodable + Default,
 {
-    fn ssz_decode(bytes: &[u8], index: usize) -> Result<(Self, usize), ssz::DecodeError> {
-        ssz::decode_ssz_list(bytes, index).and_then(|(vec, i)| Ok((vec.into(), i)))
+    fn is_ssz_fixed_len() -> bool {
+        T::is_ssz_fixed_len()
+    }
+
+    fn ssz_fixed_len() -> usize {
+        if <Self as ssz::Decodable>::is_ssz_fixed_len() {
+            T::ssz_fixed_len() * N::to_usize()
+        } else {
+            ssz::BYTES_PER_LENGTH_OFFSET
+        }
+    }
+
+    fn from_ssz_bytes(bytes: &[u8]) -> Result<Self, ssz::DecodeError> {
+        if bytes.len() == 0 {
+            Ok(FixedLenVec::from(vec![]))
+        } else if T::is_ssz_fixed_len() {
+            bytes
+                .chunks(T::ssz_fixed_len())
+                .map(|chunk| T::from_ssz_bytes(chunk))
+                .collect::<Result<Vec<T>, _>>()
+                .and_then(|vec| Ok(vec.into()))
+        } else {
+            ssz::decode_list_of_variable_length_items(bytes).and_then(|vec| Ok(vec.into()))
+        }
     }
 }
