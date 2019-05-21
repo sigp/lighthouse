0b2aa26f2d884e404eb6ac87527fc8b78befaa77
Michael Sproul <michael@sigmaprime.io>
2019-05-21 16:38:16 +1000

    spec v0.6.1: attestation processing/verif

--- eth2/state_processing/src/per_block_processing/verify_indexed_attestation.rs
+++ eth2/state_processing/src/per_block_processing/verify_indexed_attestation.rs
@@ -1,75 +1,118 @@
 use super::errors::{
     IndexedAttestationInvalid as Invalid, IndexedAttestationValidationError as Error,
 };
-use crate::common::verify_bitfield_length;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 use tree_hash::TreeHash;
 use types::*;
 
-/// Indicates if a `IndexedAttestation` is valid to be included in a block in the current epoch of the given
-/// state.
+/// Verify an `IndexedAttestation`.
 ///
-/// Returns `Ok(())` if the `IndexedAttestation` is valid, otherwise indicates the reason for invalidity.
-///
-/// Spec v0.5.1
+/// Spec v0.6.1
 pub fn verify_indexed_attestation<T: EthSpec>(
     state: &BeaconState<T>,
     indexed_attestation: &IndexedAttestation,
     spec: &ChainSpec,
 ) -> Result<(), Error> {
-    if indexed_attestation.custody_bitfield.num_set_bits() > 0 {
+    verify_indexed_attestation_parametric(state, indexed_attestation, spec, true)
+}
+
+/// Verify but don't check the signature.
+///
+/// Spec v0.6.1
+pub fn verify_indexed_attestation_without_signature<T: EthSpec>(
+    state: &BeaconState<T>,
+    indexed_attestation: &IndexedAttestation,
+    spec: &ChainSpec,
+) -> Result<(), Error> {
+    verify_indexed_attestation_parametric(state, indexed_attestation, spec, false)
+}
+
+/// Optionally check the signature.
+///
+/// Spec v0.6.1
+fn verify_indexed_attestation_parametric<T: EthSpec>(
+    state: &BeaconState<T>,
+    indexed_attestation: &IndexedAttestation,
+    spec: &ChainSpec,
+    verify_signature: bool,
+) -> Result<(), Error> {
+    let custody_bit_0_indices = &indexed_attestation.custody_bit_0_indices;
+    let custody_bit_1_indices = &indexed_attestation.custody_bit_1_indices;
+
+    // Ensure no duplicate indices across custody bits
+    let custody_bit_intersection =
+        &HashSet::from_iter(custody_bit_0_indices) & &HashSet::from_iter(custody_bit_1_indices);
+    verify!(
+        custody_bit_intersection.is_empty(),
+        Invalid::CustodyBitValidatorsIntersect
+    );
+
+    // Check that nobody signed with custody bit 1 (to be removed in phase 1)
+    if custody_bit_1_indices.len() > 0 {
         invalid!(Invalid::CustodyBitfieldHasSetBits);
     }
 
-    if indexed_attestation.validator_indices.is_empty() {
-        invalid!(Invalid::NoValidatorIndices);
-    }
+    let total_indices = custody_bit_0_indices.len() + custody_bit_1_indices.len();
+    verify!(1 <= total_indices, Invalid::NoValidatorIndices);
+    verify!(
+        total_indices as u64 <= spec.max_indices_per_attestation,
+        Invalid::MaxIndicesExceed(spec.max_indices_per_attestation, total_indices)
+    );
 
-    for i in 0..(indexed_attestation.validator_indices.len() - 1) {
-        if indexed_attestation.validator_indices[i] >= indexed_attestation.validator_indices[i + 1]
-        {
-            invalid!(Invalid::BadValidatorIndicesOrdering(i));
-        }
-    }
-
-    if !verify_bitfield_length(
-        &indexed_attestation.custody_bitfield,
-        indexed_attestation.validator_indices.len(),
-    ) {
-        invalid!(Invalid::BadCustodyBitfieldLength(
-            indexed_attestation.validator_indices.len(),
-            indexed_attestation.custody_bitfield.len()
-        ));
-    }
-
-    if indexed_attestation.validator_indices.len() > spec.max_indices_per_indexed_vote as usize {
-        invalid!(Invalid::MaxIndicesExceed(
-            spec.max_indices_per_indexed_vote as usize,
-            indexed_attestation.validator_indices.len()
-        ));
-    }
-
-    // TODO: this signature verification could likely be replaced with:
-    //
-    // super::validate_attestation::validate_attestation_signature(..)
-
-    let mut aggregate_pubs = vec![AggregatePublicKey::new(); 2];
-    let mut message_exists = vec![false; 2];
-
-    for (i, v) in indexed_attestation.validator_indices.iter().enumerate() {
-        let custody_bit = match indexed_attestation.custody_bitfield.get(i) {
-            Ok(bit) => bit,
-            Err(_) => unreachable!(),
-        };
-
-        message_exists[custody_bit as usize] = true;
-
-        match state.validator_registry.get(*v as usize) {
-            Some(validator) => {
-                aggregate_pubs[custody_bit as usize].add(&validator.pubkey);
+    // Check that both vectors of indices are sorted
+    let check_sorted = |list: &Vec<u64>| {
+        for i in 0..list.len() - 1 {
+            if list[i] >= list[i + 1] {
+                invalid!(Invalid::BadValidatorIndicesOrdering(i));
             }
-            None => invalid!(Invalid::UnknownValidator(*v)),
-        };
+        }
+        Ok(())
+    };
+    check_sorted(custody_bit_0_indices)?;
+    check_sorted(custody_bit_1_indices)?;
+
+    if verify_signature {
+        verify_indexed_attestation_signature(state, indexed_attestation, spec)?;
     }
+
+    Ok(())
+}
+
+/// Create an aggregate public key for a list of validators, failing if any key can't be found.
+fn create_aggregate_pubkey<'a, T, I>(
+    state: &BeaconState<T>,
+    validator_indices: I,
+) -> Result<AggregatePublicKey, Error>
+where
+    I: IntoIterator<Item = &'a u64>,
+    T: EthSpec,
+{
+    validator_indices.into_iter().try_fold(
+        AggregatePublicKey::new(),
+        |mut aggregate_pubkey, &validator_idx| {
+            state
+                .validator_registry
+                .get(validator_idx as usize)
+                .ok_or(Error::Invalid(Invalid::UnknownValidator(validator_idx)))
+                .map(|validator| {
+                    aggregate_pubkey.add(&validator.pubkey);
+                    aggregate_pubkey
+                })
+        },
+    )
+}
+
+/// Verify the signature of an IndexedAttestation.
+///
+/// Spec v0.6.1
+fn verify_indexed_attestation_signature<T: EthSpec>(
+    state: &BeaconState<T>,
+    indexed_attestation: &IndexedAttestation,
+    spec: &ChainSpec,
+) -> Result<(), Error> {
+    let bit_0_pubkey = create_aggregate_pubkey(state, &indexed_attestation.custody_bit_0_indices)?;
+    let bit_1_pubkey = create_aggregate_pubkey(state, &indexed_attestation.custody_bit_1_indices)?;
 
     let message_0 = AttestationDataAndCustodyBit {
         data: indexed_attestation.data.clone(),
@@ -85,23 +128,24 @@ pub fn verify_indexed_attestation<T: EthSpec>(
     let mut messages = vec![];
     let mut keys = vec![];
 
-    if message_exists[0] {
+    if !indexed_attestation.custody_bit_0_indices.is_empty() {
         messages.push(&message_0[..]);
-        keys.push(&aggregate_pubs[0]);
+        keys.push(&bit_0_pubkey);
     }
-    if message_exists[1] {
+    if !indexed_attestation.custody_bit_1_indices.is_empty() {
         messages.push(&message_1[..]);
-        keys.push(&aggregate_pubs[1]);
+        keys.push(&bit_1_pubkey);
     }
 
-    let domain = {
-        let epoch = indexed_attestation.data.slot.epoch(spec.slots_per_epoch);
-        spec.get_domain(epoch, Domain::Attestation, &state.fork)
-    };
+    let domain = spec.get_domain(
+        indexed_attestation.data.target_epoch,
+        Domain::Attestation,
+        &state.fork,
+    );
 
     verify!(
         indexed_attestation
-            .aggregate_signature
+            .signature
             .verify_multiple(&messages[..], domain, &keys[..]),
         Invalid::BadSignature
     );
