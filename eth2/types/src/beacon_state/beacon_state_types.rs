48c5681b027a18faa8008e8ff00bb6e699fd8ee9
Paul Hauner <paul@paulhauner.com>
2019-05-22 13:03:51 +1000

    Update various v0.5.1 tags, delete old file

--- eth2/types/src/beacon_state/beacon_state_types.rs
+++ eth2/types/src/beacon_state/beacon_state_types.rs
@@ -56,35 +56,35 @@ pub trait EthSpec:
 
     /// Returns the `SHARD_COUNT` constant for this specification.
     ///
-    /// Spec v0.5.1
+    /// Spec v0.6.1
     fn shard_count() -> usize {
         Self::ShardCount::to_usize()
     }
 
     /// Returns the `SLOTS_PER_HISTORICAL_ROOT` constant for this specification.
     ///
-    /// Spec v0.5.1
+    /// Spec v0.6.1
     fn slots_per_historical_root() -> usize {
         Self::SlotsPerHistoricalRoot::to_usize()
     }
 
     /// Returns the `LATEST_RANDAO_MIXES_LENGTH` constant for this specification.
     ///
-    /// Spec v0.5.1
+    /// Spec v0.6.1
     fn latest_randao_mixes_length() -> usize {
         Self::LatestRandaoMixesLength::to_usize()
     }
 
     /// Returns the `LATEST_ACTIVE_INDEX_ROOTS` constant for this specification.
     ///
-    /// Spec v0.5.1
+    /// Spec v0.6.1
     fn latest_active_index_roots() -> usize {
         Self::LatestActiveIndexRootsLength::to_usize()
     }
 
     /// Returns the `LATEST_SLASHED_EXIT_LENGTH` constant for this specification.
     ///
-    /// Spec v0.5.1
+    /// Spec v0.6.1
     fn latest_slashed_exit_length() -> usize {
         Self::LatestSlashedExitLength::to_usize()
     }
@@ -92,7 +92,7 @@ pub trait EthSpec:
 
 /// Ethereum Foundation specifications.
 ///
-/// Spec v0.5.1
+/// Spec v0.6.1
 #[derive(Clone, PartialEq, Debug, Default, Serialize, Deserialize)]
 pub struct FoundationEthSpec;
 
@@ -111,8 +111,6 @@ impl EthSpec for FoundationEthSpec {
 pub type FoundationBeaconState = BeaconState<FoundationEthSpec>;
 
 /// Ethereum Foundation specifications, modified to be suitable for < 1000 validators.
-///
-/// Spec v0.5.1
 #[derive(Clone, PartialEq, Debug, Default, Serialize, Deserialize)]
 pub struct FewValidatorsEthSpec;
 
@@ -131,8 +129,6 @@ impl EthSpec for FewValidatorsEthSpec {
 pub type FewValidatorsBeaconState = BeaconState<FewValidatorsEthSpec>;
 
 /// Specifications suitable for a small-scale (< 1000 validators) lighthouse testnet.
-///
-/// Spec v0.5.1
 #[derive(Clone, PartialEq, Debug, Default, Serialize, Deserialize)]
 pub struct LighthouseTestnetEthSpec;
 
