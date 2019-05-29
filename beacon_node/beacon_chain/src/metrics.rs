9f1039a35007713832a9c7e447996cbcab7bd391
Paul Hauner <paul@paulhauner.com>
2019-05-29 17:52:43 +1000

    Add metrics to `BeaconChain`, tidy fork choice

--- beacon_node/beacon_chain/src/metrics.rs
+++ beacon_node/beacon_chain/src/metrics.rs
@@ -4,13 +4,20 @@ use prometheus::{Histogram, HistogramOpts, IntCounter, Opts, Registry};
 pub struct Metrics {
     pub block_processing_requests: IntCounter,
     pub block_processing_successes: IntCounter,
-    pub block_processing_historgram: Histogram,
+    pub block_processing_times: Histogram,
     pub block_production_requests: IntCounter,
     pub block_production_successes: IntCounter,
-    pub block_production_historgram: Histogram,
+    pub block_production_times: Histogram,
     pub attestation_production_requests: IntCounter,
     pub attestation_production_successes: IntCounter,
-    pub attestation_production_histogram: Histogram,
+    pub attestation_production_times: Histogram,
+    pub attestation_processing_requests: IntCounter,
+    pub attestation_processing_successes: IntCounter,
+    pub attestation_processing_times: Histogram,
+    pub fork_choice_requests: IntCounter,
+    pub fork_choice_changed_head: IntCounter,
+    pub fork_choice_reorg_count: IntCounter,
+    pub fork_choice_times: Histogram,
 }
 
 impl Metrics {
@@ -24,9 +31,8 @@ impl Metrics {
                 let opts = Opts::new("valid_blocks_processed", "total_valid_blocks_processed");
                 IntCounter::with_opts(opts)?
             },
-            block_processing_historgram: {
-                let opts =
-                    HistogramOpts::new("block_processing_historgram", "block_processing_time");
+            block_processing_times: {
+                let opts = HistogramOpts::new("block_processing_times", "block_processing_time");
                 Histogram::with_opts(opts)?
             },
             block_production_requests: {
@@ -37,9 +43,8 @@ impl Metrics {
                 let opts = Opts::new("block_production_successes", "blocks_successfully_produced");
                 IntCounter::with_opts(opts)?
             },
-            block_production_historgram: {
-                let opts =
-                    HistogramOpts::new("block_production_historgram", "block_production_time");
+            block_production_times: {
+                let opts = HistogramOpts::new("block_production_times", "block_production_time");
                 Histogram::with_opts(opts)?
             },
             attestation_production_requests: {
@@ -56,11 +61,51 @@ impl Metrics {
                 );
                 IntCounter::with_opts(opts)?
             },
-            attestation_production_histogram: {
+            attestation_production_times: {
                 let opts = HistogramOpts::new(
-                    "attestation_production_histogram",
+                    "attestation_production_times",
                     "attestation_production_time",
                 );
+                Histogram::with_opts(opts)?
+            },
+            attestation_processing_requests: {
+                let opts = Opts::new(
+                    "attestation_processing_requests",
+                    "total_attestation_processing_requests",
+                );
+                IntCounter::with_opts(opts)?
+            },
+            attestation_processing_successes: {
+                let opts = Opts::new(
+                    "attestation_processing_successes",
+                    "total_attestation_processing_successes",
+                );
+                IntCounter::with_opts(opts)?
+            },
+            attestation_processing_times: {
+                let opts = HistogramOpts::new(
+                    "attestation_processing_times",
+                    "attestation_processing_time",
+                );
+                Histogram::with_opts(opts)?
+            },
+            fork_choice_requests: {
+                let opts = Opts::new("fork_choice_requests", "total_times_fork_choice_called");
+                IntCounter::with_opts(opts)?
+            },
+            fork_choice_changed_head: {
+                let opts = Opts::new(
+                    "fork_choice_changed_head",
+                    "total_times_fork_choice_chose_a_new_head",
+                );
+                IntCounter::with_opts(opts)?
+            },
+            fork_choice_reorg_count: {
+                let opts = Opts::new("fork_choice_reorg_depth", "depth_of_reorg");
+                IntCounter::with_opts(opts)?
+            },
+            fork_choice_times: {
+                let opts = HistogramOpts::new("fork_choice_time", "total_time_to_run_fork_choice");
                 Histogram::with_opts(opts)?
             },
         })
@@ -69,13 +114,20 @@ impl Metrics {
     pub fn register(&self, registry: &Registry) -> Result<(), Error> {
         registry.register(Box::new(self.block_processing_requests.clone()))?;
         registry.register(Box::new(self.block_processing_successes.clone()))?;
-        registry.register(Box::new(self.block_processing_historgram.clone()))?;
+        registry.register(Box::new(self.block_processing_times.clone()))?;
         registry.register(Box::new(self.block_production_requests.clone()))?;
         registry.register(Box::new(self.block_production_successes.clone()))?;
-        registry.register(Box::new(self.block_production_historgram.clone()))?;
+        registry.register(Box::new(self.block_production_times.clone()))?;
         registry.register(Box::new(self.attestation_production_requests.clone()))?;
         registry.register(Box::new(self.attestation_production_successes.clone()))?;
-        registry.register(Box::new(self.attestation_production_histogram.clone()))?;
+        registry.register(Box::new(self.attestation_production_times.clone()))?;
+        registry.register(Box::new(self.attestation_processing_requests.clone()))?;
+        registry.register(Box::new(self.attestation_processing_successes.clone()))?;
+        registry.register(Box::new(self.attestation_processing_times.clone()))?;
+        registry.register(Box::new(self.fork_choice_requests.clone()))?;
+        registry.register(Box::new(self.fork_choice_changed_head.clone()))?;
+        registry.register(Box::new(self.fork_choice_reorg_count.clone()))?;
+        registry.register(Box::new(self.fork_choice_times.clone()))?;
 
         Ok(())
     }
