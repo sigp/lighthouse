23bc122af1796d23618dada9a8901b85db401582
Paul Hauner <paul@paulhauner.com>
2019-05-29 15:45:09 +1000

    Add additional metrics to `BeaconChain`

--- beacon_node/beacon_chain/src/metrics.rs
+++ beacon_node/beacon_chain/src/metrics.rs
@@ -1,25 +1,33 @@
 pub use prometheus::Error;
-use prometheus::{IntCounter, Opts, Registry};
+use prometheus::{Histogram, HistogramOpts, IntCounter, Opts, Registry};
 
 pub struct Metrics {
-    pub blocks_processed: IntCounter,
-    pub valid_blocks_processed: IntCounter,
+    pub block_processing_requests: IntCounter,
+    pub block_processing_successes: IntCounter,
+    pub block_processing_historgram: Histogram,
     pub block_production_requests: IntCounter,
     pub block_production_successes: IntCounter,
+    pub block_production_historgram: Histogram,
     pub attestation_production_requests: IntCounter,
     pub attestation_production_successes: IntCounter,
+    pub attestation_production_histogram: Histogram,
 }
 
 impl Metrics {
     pub fn new() -> Result<Self, Error> {
         Ok(Self {
-            blocks_processed: {
+            block_processing_requests: {
                 let opts = Opts::new("blocks_processed", "total_blocks_processed");
                 IntCounter::with_opts(opts)?
             },
-            valid_blocks_processed: {
+            block_processing_successes: {
                 let opts = Opts::new("valid_blocks_processed", "total_valid_blocks_processed");
                 IntCounter::with_opts(opts)?
+            },
+            block_processing_historgram: {
+                let opts =
+                    HistogramOpts::new("block_processing_historgram", "block_processing_time");
+                Histogram::with_opts(opts)?
             },
             block_production_requests: {
                 let opts = Opts::new("block_production_requests", "attempts_to_produce_new_block");
@@ -28,6 +36,11 @@ impl Metrics {
             block_production_successes: {
                 let opts = Opts::new("block_production_successes", "blocks_successfully_produced");
                 IntCounter::with_opts(opts)?
+            },
+            block_production_historgram: {
+                let opts =
+                    HistogramOpts::new("block_production_historgram", "block_production_time");
+                Histogram::with_opts(opts)?
             },
             attestation_production_requests: {
                 let opts = Opts::new(
@@ -43,16 +56,26 @@ impl Metrics {
                 );
                 IntCounter::with_opts(opts)?
             },
+            attestation_production_histogram: {
+                let opts = HistogramOpts::new(
+                    "attestation_production_histogram",
+                    "attestation_production_time",
+                );
+                Histogram::with_opts(opts)?
+            },
         })
     }
 
     pub fn register(&self, registry: &Registry) -> Result<(), Error> {
-        registry.register(Box::new(self.blocks_processed.clone()))?;
-        registry.register(Box::new(self.valid_blocks_processed.clone()))?;
+        registry.register(Box::new(self.block_processing_requests.clone()))?;
+        registry.register(Box::new(self.block_processing_successes.clone()))?;
+        registry.register(Box::new(self.block_processing_historgram.clone()))?;
         registry.register(Box::new(self.block_production_requests.clone()))?;
         registry.register(Box::new(self.block_production_successes.clone()))?;
+        registry.register(Box::new(self.block_production_historgram.clone()))?;
         registry.register(Box::new(self.attestation_production_requests.clone()))?;
         registry.register(Box::new(self.attestation_production_successes.clone()))?;
+        registry.register(Box::new(self.attestation_production_histogram.clone()))?;
 
         Ok(())
     }
