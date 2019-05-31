91f77006f45611984f19ae5ec5e4c9a4b277ca4d
Paul Hauner <paul@paulhauner.com>
2019-05-31 18:13:35 +1000

    Add additional metrics for `BeaconChain`

--- beacon_node/http_server/src/metrics.rs
+++ beacon_node/http_server/src/metrics.rs
@@ -6,10 +6,12 @@ use beacon_chain::{BeaconChain, BeaconChainTypes};
 use iron::prelude::*;
 use iron::{status::Status, Handler, IronResult, Request, Response};
 use persistent::Read;
-use prometheus::{Encoder, IntGauge, Opts, Registry, TextEncoder};
-use slot_clock::SlotClock;
+use prometheus::{Encoder, Registry, TextEncoder};
 use std::sync::Arc;
-use types::Slot;
+
+pub use local_metrics::LocalMetrics;
+
+mod local_metrics;
 
 /// Yields a handler for the metrics endpoint.
 pub fn build_handler<T: BeaconChainTypes + 'static>(
@@ -28,39 +30,6 @@ pub fn build_handler<T: BeaconChainTypes + 'static>(
     chain
 }
 
-pub struct LocalMetrics {
-    present_slot: IntGauge,
-    best_slot: IntGauge,
-    validator_count: IntGauge,
-}
-
-impl LocalMetrics {
-    pub fn new() -> Result<Self, prometheus::Error> {
-        Ok(Self {
-            present_slot: {
-                let opts = Opts::new("present_slot", "slot_at_time_of_scrape");
-                IntGauge::with_opts(opts)?
-            },
-            best_slot: {
-                let opts = Opts::new("best_slot", "slot_of_block_at_chain_head");
-                IntGauge::with_opts(opts)?
-            },
-            validator_count: {
-                let opts = Opts::new("validator_count", "number_of_validators");
-                IntGauge::with_opts(opts)?
-            },
-        })
-    }
-
-    pub fn register(&self, registry: &Registry) -> Result<(), prometheus::Error> {
-        registry.register(Box::new(self.present_slot.clone()))?;
-        registry.register(Box::new(self.best_slot.clone()))?;
-        registry.register(Box::new(self.validator_count.clone()))?;
-
-        Ok(())
-    }
-}
-
 /// Handle a request for Prometheus metrics.
 ///
 /// Returns a text string containing all metrics.
@@ -77,18 +46,8 @@ fn handle_metrics<T: BeaconChainTypes + 'static>(req: &mut Request) -> IronResul
         .get::<Read<LocalMetricsKey>>()
         .map_err(map_persistent_err_to_500)?;
 
-    let present_slot = beacon_chain
-        .slot_clock
-        .present_slot()
-        .unwrap_or_else(|_| None)
-        .unwrap_or_else(|| Slot::new(0));
-    local_metrics.present_slot.set(present_slot.as_u64() as i64);
-
-    let best_slot = beacon_chain.head().beacon_block.slot;
-    local_metrics.best_slot.set(best_slot.as_u64() as i64);
-
-    let validator_count = beacon_chain.head().beacon_state.validator_registry.len();
-    local_metrics.validator_count.set(validator_count as i64);
+    // Update metrics that are calculated on each scrape.
+    local_metrics.update(&beacon_chain);
 
     let mut buffer = vec![];
     let encoder = TextEncoder::new();
