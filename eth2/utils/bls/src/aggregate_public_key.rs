b262535e7f28e3101a3b6351a805a3b515336684
Michael Sproul <michael@sigmaprime.io>
2019-06-11 18:00:05 +1000

    bls: as_hex_string for aggregate signatures/pubkeys

--- eth2/utils/bls/src/aggregate_public_key.rs
+++ eth2/utils/bls/src/aggregate_public_key.rs
@@ -1,5 +1,6 @@
 use super::PublicKey;
 use milagro_bls::AggregatePublicKey as RawAggregatePublicKey;
+use serde_hex::encode as hex_encode;
 
 /// A BLS aggregate public key.
 ///
@@ -20,5 +21,11 @@ impl AggregatePublicKey {
     /// Returns the underlying public key.
     pub fn as_raw(&self) -> &RawAggregatePublicKey {
         &self.0
+    }
+
+    /// Return a hex string representation of this key's bytes.
+    #[cfg(test)]
+    pub fn as_hex_string(&self) -> String {
+        hex_encode(self.as_raw().as_bytes())
     }
 }
