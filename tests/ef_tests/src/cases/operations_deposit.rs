8a9f7ff32bb308338caa9df8ed0e1e6be98068e4
Michael Sproul <michael@sigmaprime.io>
2019-06-12 16:30:18 +1000

    ef_tests: use BlsSetting in operations_deposit

--- tests/ef_tests/src/cases/operations_deposit.rs
+++ tests/ef_tests/src/cases/operations_deposit.rs
@@ -1,4 +1,5 @@
 use super::*;
+use crate::bls_setting::BlsSetting;
 use crate::case_result::compare_beacon_state_results_without_caches;
 use serde_derive::Deserialize;
 use state_processing::per_block_processing::process_deposits;
@@ -6,8 +7,8 @@ use types::{BeaconState, Deposit, EthSpec};
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct OperationsDeposit<E: EthSpec> {
-    pub bls_setting: Option<u8>,
     pub description: String,
+    pub bls_setting: Option<BlsSetting>,
     #[serde(bound = "E: EthSpec")]
     pub pre: BeaconState<E>,
     pub deposit: Deposit,
@@ -27,9 +28,8 @@ impl<E: EthSpec> Case for OperationsDeposit<E> {
     }
 
     fn result(&self, _case_index: usize) -> Result<(), Error> {
-        if self.bls_setting == Some(cfg!(feature = "fake_crypto") as u8) {
-            return Ok(());
-        }
+        self.bls_setting.unwrap_or_default().check()?;
+
         let mut state = self.pre.clone();
         let deposit = self.deposit.clone();
         let mut expected = self.post.clone();
