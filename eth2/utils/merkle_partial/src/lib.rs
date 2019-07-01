dff1191b4912b94924c25ccb423f268c453374ae
Matt Garnett <14004106+c-o-l-o-r@users.noreply.github.com>
2019-07-01 17:49:45 -0400

    reorganize into a single trait

--- eth2/utils/merkle_partial/src/lib.rs
+++ eth2/utils/merkle_partial/src/lib.rs
@@ -1,17 +1,17 @@
 use hashing::hash;
 use ssz::BYTES_PER_LENGTH_OFFSET;
-use std::collections::HashMap;
+
 use tree_hash::BYTES_PER_CHUNK;
 
 #[derive(Debug, PartialEq)]
-pub enum ParsePartialError {
+pub enum Error {
     // The node is not equal to h(left, right)
     InvalidNode(u64),
     // The partial is incomplete
     MissingNode(u64),
 }
 
-pub type Result<T> = std::result::Result<T, ParsePartialError>;
+pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, Default)]
 pub struct SerializedPartial {
@@ -19,32 +19,52 @@ pub struct SerializedPartial {
     chunks: Vec<u8>, // vec<bytes32>
 }
 
-pub struct Partial {
-    cache: HashMap<u64, Vec<u8>>,
-}
+pub trait Partial {
+    fn get(&self, index: u64) -> Option<&Vec<u8>>;
+    fn insert(&mut self, index: u64, chunk: Vec<u8>) -> Option<Vec<u8>>;
+    fn nodes(&self) -> Vec<u64>;
+    fn contains_node(&self, index: u64) -> bool;
 
-impl Partial {
-    pub fn is_valid(&self) -> bool {
-        for (key, _) in &self.cache {
-            let key = *key;
-            let parent = key / 2;
-            let left = key & (-2_i64 as u64);
-            let right = key | 1;
+    fn get_partial(&self, path: &str) -> SerializedPartial {
+        unimplemented!()
+    }
 
-            if key > 1 {
-                let h = hash_children(
-                    match self.cache.get(&left) {
-                        Some(n) => n,
-                        None => return false,
-                    },
-                    match self.cache.get(&right) {
-                        Some(n) => n,
-                        None => return false,
-                    },
-                );
+    fn load_partial(&mut self, partial: SerializedPartial) -> Result<()> {
+        for (i, index) in partial.indicies.iter().enumerate() {
+            let begin = i * BYTES_PER_CHUNK;
+            let end = begin + BYTES_PER_CHUNK;
+            self.insert(*index, partial.chunks[begin..end].into());
+        }
+
+        Ok(())
+    }
+
+    fn is_valid(&self) -> bool {
+        for node in self.nodes() {
+            let parent = node / 2;
+            let left = node & (-2_i64 as u64);
+            let right = node | 1;
+
+            if node > 1 {
+                let left = match self.get(left) {
+                    Some(n) => n,
+                    None => return false,
+                };
+
+                let right = match self.get(right) {
+                    Some(n) => n,
+                    None => return false,
+                };
+
+                let parent = match self.get(parent) {
+                    Some(n) => n,
+                    None => return false,
+                };
+
+                let h = hash_children(&left, &right);
 
                 // Child nodes should always hash to their parent
-                if self.cache[&parent] != h {
+                if h != *parent {
                     return false;
                 }
             }
@@ -53,36 +73,31 @@ impl Partial {
         true
     }
 
-    pub fn fill(&mut self) -> Result<()> {
-        let mut keys: Vec<u64> = self.cache.keys().clone().map(|x| x.to_owned()).collect();
-        keys.sort_by(|a, b| b.cmp(a));
+    fn fill(&mut self) -> Result<()> {
+        let mut nodes: Vec<u64> = self.nodes();
+        nodes.sort_by(|a, b| b.cmp(a));
 
         let mut position = 0;
 
-        while position < keys.len() {
-            let key = keys[position];
+        while position < nodes.len() {
+            let node = nodes[position];
 
-            if self.cache.contains_key(&key)
-                && self.cache.contains_key(&(key ^ 1))
-                && !self.cache.contains_key(&(key / 2))
+            // Calculate the parent node if both children are present
+            if self.contains_node(node)
+                && self.contains_node(node ^ 1)
+                && !self.contains_node(node / 2)
             {
-                let parent = key / 2;
-                let left = key & (-2_i64 as u64);
-                let right = key | 1;
+                let parent = node / 2;
+                let left = node & (-2_i64 as u64);
+                let right = node | 1;
 
                 let h = hash_children(
-                    match self.cache.get(&left) {
-                        Some(n) => n,
-                        None => return Err(ParsePartialError::MissingNode(left)),
-                    },
-                    match self.cache.get(&right) {
-                        Some(n) => n,
-                        None => return Err(ParsePartialError::MissingNode(right)),
-                    },
+                    &self.get(left).ok_or(Error::MissingNode(left))?,
+                    &self.get(right).ok_or(Error::MissingNode(right))?,
                 );
 
-                self.cache.insert(parent, h);
-                keys.push(parent);
+                self.insert(parent, h);
+                nodes.push(parent);
             }
 
             position += 1;
@@ -92,45 +107,38 @@ impl Partial {
     }
 }
 
-trait PartialObject {
-    fn get_partial(&self, path: &str) -> Vec<u8> {
-        unimplemented!()
-    }
-
-    fn load_partial(&mut self, partial: Partial) -> Result<()> {
-        unimplemented!()
-    }
-}
-
 fn hash_children(left: &[u8], right: &[u8]) -> Vec<u8> {
     let children: Vec<u8> = left.iter().cloned().chain(right.iter().cloned()).collect();
     hash(&children)
 }
 
-impl From<&SerializedPartial> for Partial {
-    fn from(partial: &SerializedPartial) -> Self {
-        let mut cache: HashMap<u64, Vec<u8>> = HashMap::new();
-
-        for (i, index) in partial.indicies.iter().enumerate() {
-            let begin = i * BYTES_PER_CHUNK;
-            let end = begin + BYTES_PER_CHUNK;
-            cache.insert(*index, partial.chunks[begin..end].into());
-        }
-
-        Self { cache }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashMap;
 
     struct A {
-        a: usize,
-        b: usize,
+        // Should be added by derive macro
+        cache: HashMap<u64, Vec<u8>>,
     }
 
-    impl PartialObject for A {}
+    impl Partial for A {
+        fn get(&self, index: u64) -> Option<&Vec<u8>> {
+            self.cache.get(&index)
+        }
+
+        fn insert(&mut self, index: u64, chunk: Vec<u8>) -> Option<Vec<u8>> {
+            self.cache.insert(index, chunk)
+        }
+
+        fn nodes(&self) -> Vec<u64> {
+            self.cache.keys().clone().map(|x| x.to_owned()).collect()
+        }
+
+        fn contains_node(&self, index: u64) -> bool {
+            self.cache.contains_key(&index)
+        }
+    }
 
     #[test]
     fn is_valid_partial_bigger() {
@@ -149,7 +157,7 @@ mod tests {
         // root node
         cache.insert(1, hash_children(&cache[&2], &cache[&3]));
 
-        let p = Partial { cache };
+        let p = A { cache };
 
         assert_eq!(p.is_valid(), true);
     }
@@ -164,7 +172,7 @@ mod tests {
         cache.insert(5, vec![5; BYTES_PER_CHUNK]);
         cache.insert(4, vec![4; BYTES_PER_CHUNK]);
 
-        let mut p = Partial { cache };
+        let mut p = A { cache };
         assert_eq!(p.fill(), Ok(()));
         assert_eq!(p.is_valid(), true);
     }
