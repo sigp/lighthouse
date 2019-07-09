2ab4bee0f82a52dd0d76612bea5591e0b60ccc4a
Paul Hauner <paul@paulhauner.com>
2019-07-09 17:01:37 +1000

    Satisfy clippy lint in SSZ

--- eth2/utils/ssz/src/lib.rs
+++ eth2/utils/ssz/src/lib.rs
@@ -47,9 +47,9 @@ pub use encode::{Encode, SszEncoder};
 pub const BYTES_PER_LENGTH_OFFSET: usize = 4;
 /// The maximum value that can be represented using `BYTES_PER_LENGTH_OFFSET`.
 #[cfg(target_pointer_width = "32")]
-pub const MAX_LENGTH_VALUE: usize = (std::u32::MAX >> 8 * (4 - BYTES_PER_LENGTH_OFFSET)) as usize;
+pub const MAX_LENGTH_VALUE: usize = (std::u32::MAX >> (8 * (4 - BYTES_PER_LENGTH_OFFSET))) as usize;
 #[cfg(target_pointer_width = "64")]
-pub const MAX_LENGTH_VALUE: usize = (std::u64::MAX >> 8 * (8 - BYTES_PER_LENGTH_OFFSET)) as usize;
+pub const MAX_LENGTH_VALUE: usize = (std::u64::MAX >> (8 * (8 - BYTES_PER_LENGTH_OFFSET))) as usize;
 
 /// Convenience function to SSZ encode an object supporting ssz::Encode.
 ///
