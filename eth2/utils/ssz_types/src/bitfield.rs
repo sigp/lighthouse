e4ef0fc9d4fa322d4b3356d025f5526aa2805a52
Paul Hauner <paul@paulhauner.com>
2019-07-09 11:00:40 +1000

    Add crate-level docs to ssz_types

--- eth2/utils/ssz_types/src/bitfield.rs
+++ eth2/utils/ssz_types/src/bitfield.rs
@@ -471,7 +471,82 @@ impl<N: Unsigned + Clone> tree_hash::TreeHash for Bitfield<BitVector<N>> {
     }
 }
 
-// TODO: test ssz decode a zero-length bitlist.
+impl<N: Unsigned + Clone> cached_tree_hash::CachedTreeHash for Bitfield<BitList<N>> {
+    fn new_tree_hash_cache(
+        &self,
+        depth: usize,
+    ) -> Result<cached_tree_hash::TreeHashCache, cached_tree_hash::Error> {
+        let bytes = self.to_bytes();
+
+        let (mut cache, schema) = cached_tree_hash::vec::new_tree_hash_cache(&bytes, depth)?;
+
+        cache.add_length_nodes(schema.into_overlay(0).chunk_range(), bytes.len())?;
+
+        Ok(cache)
+    }
+
+    fn num_tree_hash_cache_chunks(&self) -> usize {
+        // Add two extra nodes to cater for the node before and after to allow mixing-in length.
+        cached_tree_hash::BTreeOverlay::new(self, 0, 0).num_chunks() + 2
+    }
+
+    fn tree_hash_cache_schema(&self, depth: usize) -> cached_tree_hash::BTreeSchema {
+        let bytes = self.to_bytes();
+        cached_tree_hash::vec::produce_schema(&bytes, depth)
+    }
+
+    fn update_tree_hash_cache(
+        &self,
+        cache: &mut cached_tree_hash::TreeHashCache,
+    ) -> Result<(), cached_tree_hash::Error> {
+        let bytes = self.to_bytes();
+
+        // Skip the length-mixed-in root node.
+        cache.chunk_index += 1;
+
+        // Update the cache, returning the new overlay.
+        let new_overlay = cached_tree_hash::vec::update_tree_hash_cache(&bytes, cache)?;
+
+        // Mix in length
+        cache.mix_in_length(new_overlay.chunk_range(), bytes.len())?;
+
+        // Skip an extra node to clear the length node.
+        cache.chunk_index += 1;
+
+        Ok(())
+    }
+}
+
+impl<N: Unsigned + Clone> cached_tree_hash::CachedTreeHash for Bitfield<BitVector<N>> {
+    fn new_tree_hash_cache(
+        &self,
+        depth: usize,
+    ) -> Result<cached_tree_hash::TreeHashCache, cached_tree_hash::Error> {
+        let (cache, _schema) =
+            cached_tree_hash::vec::new_tree_hash_cache(&ssz::ssz_encode(self), depth)?;
+
+        Ok(cache)
+    }
+
+    fn tree_hash_cache_schema(&self, depth: usize) -> cached_tree_hash::BTreeSchema {
+        let lengths = vec![
+            1;
+            cached_tree_hash::merkleize::num_unsanitized_leaves(bytes_for_bit_len(
+                N::to_usize()
+            ))
+        ];
+        cached_tree_hash::BTreeSchema::from_lengths(depth, lengths)
+    }
+
+    fn update_tree_hash_cache(
+        &self,
+        cache: &mut cached_tree_hash::TreeHashCache,
+    ) -> Result<(), cached_tree_hash::Error> {
+        cached_tree_hash::vec::update_tree_hash_cache(&ssz::ssz_encode(self), cache)?;
+
+        Ok(())
+    }
+}
 
 #[cfg(test)]
 mod test {
