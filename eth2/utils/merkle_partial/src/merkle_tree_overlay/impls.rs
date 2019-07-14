3b8c66a0bde925b634230ca2fe431370d2c6d3d5
Matt Garnett <14004106+c-o-l-o-r@users.noreply.github.com>
2019-07-14 16:46:07 -0400

    add docs to merkle tree overlay impls

--- eth2/utils/merkle_partial/src/merkle_tree_overlay/impls.rs
+++ eth2/utils/merkle_partial/src/merkle_tree_overlay/impls.rs
@@ -46,17 +46,29 @@ impl_merkle_overlay_for_basic_type!(u128, 128);
 impl_merkle_overlay_for_basic_type!(U256, 256);
 impl_merkle_overlay_for_basic_type!(usize, std::mem::size_of::<usize>());
 
-// VariableList merkle tree
-//
-//         root(0)
-//       /         \
-//  data_root(1) b_len(2)
-//    /   \
-//  . . . . .
+/// Implements the `MerkleTreeOverlay` trait for `ssz_types::VariableList`.
+///
+/// The full specification of the merkle tree structure can be found in the SSZ documentation:
+/// https://github.com/ethereum/eth2.0-specs/blob/dev/specs/simple-serialize.md#merkleization
+///
+/// Below is a visual representation of the structure:
+///
+///             root(0)
+///           /         \
+///      data_root(1)  len(2)
+///        /   \
+///      . . . . .  <= intermediate nodes
+///      / \   / \
+///     x   x x   x <= leaf nodes
+///
 
 impl<T: MerkleTreeOverlay, N: Unsigned> MerkleTreeOverlay for VariableList<T, N> {
     fn height() -> u8 {
-        1 + log_base_two(next_power_of_two(N::to_u64())) as u8
+        let num_leaves = next_power_of_two(N::to_u64());
+        let data_tree_height = log_base_two(num_leaves);
+
+        // Add one to account for the data root and the length of the list.
+        (data_tree_height as u8) + 1
     }
 
     fn first_leaf() -> NodeIndex {
@@ -64,16 +76,24 @@ impl<T: MerkleTreeOverlay, N: Unsigned> MerkleTreeOverlay for VariableList<T, N>
     }
 
     fn last_leaf() -> NodeIndex {
-        (1_u64 << Self::height()) + (1_u64 << Self::height()) / 2 - 2
+        // last_leaf = 2^h + (2^h / 2) - 2
+        //
+        // The last leaf in the data tree would be first leaf, plus half the total number of leaves,
+        // because the data tree is only in the left side of the full tree. The leaves afterwards
+        // are children of the length node and are unattached.
+        (1_u64 << Self::height()) + (1_u64 << (Self::height() - 1)) - 2
     }
 
     fn get_node(index: NodeIndex) -> Node {
+        let first_leaf = Self::first_leaf();
         let first_internal = 3;
         let last_internal = (1_u64 << Self::height()) - 2;
-
-        let first_leaf = Self::first_leaf();
         let last_leaf = Self::last_leaf();
 
+        // The `index` can either i) exist within the current object and directly match a local
+        // index in `0..=Self::last_leaf()` or ii) is a child of one of the leaves in the current
+        // tree. If `index` is a child, call `get_node` on the child object `T` with an index
+        // translated to the child object's index space.
         if index == 0 {
             Node::Composite(Composite {
                 ident: "".to_owned(),
@@ -104,8 +124,7 @@ impl<T: MerkleTreeOverlay, N: Unsigned> MerkleTreeOverlay for VariableList<T, N>
                             .iter()
                             .enumerate()
                             .map(|(i, _)| Basic {
-                                ident: (((index + 1) % 2_u64.pow(Self::height() as u32 - 1))
-                                    * items_per_chunk as u64
+                                ident: ((index - Self::first_leaf()) * items_per_chunk as u64
                                     + i as u64)
                                     .to_string(),
                                 index: index,
@@ -116,17 +135,21 @@ impl<T: MerkleTreeOverlay, N: Unsigned> MerkleTreeOverlay for VariableList<T, N>
                     ))
                 }
                 Node::Composite(c) => Node::Composite(Composite {
-                    ident: ((index + 1) % 2_u64.pow(Self::height() as u32 - 1)).to_string(),
+                    ident: (index - Self::first_leaf()).to_string(),
                     index,
                     height: c.height,
                 }),
                 _ => unreachable!("Leaf should either be composite or basic value"),
             }
         } else {
+            // If no match at this point, the node must be in one of `T`'s subtrees or it's not
+            // attached to the current tree anywhere.
+
             let subtree_root = root_from_depth(index, relative_depth(first_leaf, index));
             let subtree_index = general_index_to_subtree(subtree_root, index);
 
             if (first_leaf..=last_leaf).contains(&subtree_root) {
+                // Call `T::get_node` & replace the node's index with the current known index.
                 replace_index(T::get_node(subtree_index), index)
             } else {
                 Node::Unattached(index)
@@ -135,6 +158,7 @@ impl<T: MerkleTreeOverlay, N: Unsigned> MerkleTreeOverlay for VariableList<T, N>
     }
 }
 
+/// Returns a copy of `node` with all its index values changed to `index`.
 fn replace_index(node: Node, index: NodeIndex) -> Node {
     match node {
         Node::Composite(c) => Node::Composite(Composite {
@@ -224,6 +248,7 @@ mod tests {
         );
 
         assert_eq!(T::get_node(23), Node::Unattached(23));
+        assert_eq!(T::get_node(31), Node::Unattached(31));
         assert_eq!(T::get_node(2_u64.pow(32)), Node::Unattached(2_u64.pow(32)));
     }
 
