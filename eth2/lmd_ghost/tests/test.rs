c904559e4a50edfeb191a72a148e0f65e0a0063f
Paul Hauner <paul@paulhauner.com>
2019-07-20 13:48:15 +1000

    Add failing lmd_ghost fork choice tests

--- eth2/lmd_ghost/tests/test.rs
+++ eth2/lmd_ghost/tests/test.rs
@@ -1,5 +1,7 @@
-use beacon_chain::test_utils::{AttestationStrategy, BeaconChainHarness, BlockStrategy};
-use lmd_ghost::ThreadSafeReducedTree;
+use beacon_chain::test_utils::{
+    AttestationStrategy, BeaconChainHarness as BaseBeaconChainHarness, BlockStrategy,
+};
+use lmd_ghost::{LmdGhost, ThreadSafeReducedTree as BaseThreadSafeReducedTree};
 use std::sync::Arc;
 use store::{
     iter::{AncestorIter, BestBlockRootsIterator},
@@ -10,9 +12,11 @@ use types::{BeaconBlock, BeaconState, EthSpec, Hash256, MinimalEthSpec, Slot};
 // Should ideally be divisible by 3.
 pub const VALIDATOR_COUNT: usize = 24;
 
-type TestForkChoice = ThreadSafeReducedTree<MemoryStore, MinimalEthSpec>;
+type TestEthSpec = MinimalEthSpec;
+type ThreadSafeReducedTree = BaseThreadSafeReducedTree<MemoryStore, TestEthSpec>;
+type BeaconChainHarness = BaseBeaconChainHarness<ThreadSafeReducedTree, TestEthSpec>;
 
-fn get_harness(validator_count: usize) -> BeaconChainHarness<TestForkChoice, MinimalEthSpec> {
+fn get_harness(validator_count: usize) -> BeaconChainHarness {
     let harness = BeaconChainHarness::new(validator_count);
 
     // Move past the zero slot.
@@ -21,7 +25,10 @@ fn get_harness(validator_count: usize) -> BeaconChainHarness<TestForkChoice, Min
     harness
 }
 
-fn get_ancestor_roots<E: EthSpec, U: Store>(store: Arc<U>, block_root: Hash256) -> Vec<Hash256> {
+fn get_ancestor_roots<E: EthSpec, U: Store>(
+    store: Arc<U>,
+    block_root: Hash256,
+) -> Vec<(Hash256, Slot)> {
     let block = store
         .get::<BeaconBlock>(&block_root)
         .expect("block should exist")
@@ -30,15 +37,27 @@ fn get_ancestor_roots<E: EthSpec, U: Store>(store: Arc<U>, block_root: Hash256) 
     <BeaconBlock as AncestorIter<_, BestBlockRootsIterator<E, _>>>::iter_ancestor_roots(
         &block, store,
     )
-    .map(|(root, _slot)| root)
     .collect()
 }
 
-#[test]
-fn chooses_fork() {
+fn get_slot_for_block_root(harness: &BeaconChainHarness, block_root: Hash256) -> Slot {
+    harness
+        .chain
+        .store
+        .get::<BeaconBlock>(&block_root)
+        .expect("head block should exist")
+        .expect("DB should not error")
+        .slot
+}
+
+fn get_harness_containing_two_forks() -> (
+    BeaconChainHarness,
+    Vec<(Hash256, Slot)>,
+    Vec<(Hash256, Slot)>,
+) {
     let harness = get_harness(VALIDATOR_COUNT);
 
-    let delay = MinimalEthSpec::default_spec().min_attestation_inclusion_delay as usize;
+    let delay = TestEthSpec::default_spec().min_attestation_inclusion_delay as usize;
 
     let initial_blocks = delay + 1;
 
@@ -62,17 +81,42 @@ fn chooses_fork() {
         faulty_fork_blocks,
     );
 
-    let state = &harness.chain.head().beacon_state;
-
     let mut honest_roots =
-        get_ancestor_roots::<MinimalEthSpec, _>(harness.chain.store.clone(), honest_head);
-    honest_roots.push(honest_head);
+        get_ancestor_roots::<TestEthSpec, _>(harness.chain.store.clone(), honest_head);
+
+    honest_roots.push((honest_head, get_slot_for_block_root(&harness, honest_head)));
+
     let mut faulty_roots =
-        get_ancestor_roots::<MinimalEthSpec, _>(harness.chain.store.clone(), faulty_head);
-    faulty_roots.push(faulty_head);
+        get_ancestor_roots::<TestEthSpec, _>(harness.chain.store.clone(), faulty_head);
 
-    dbg!(honest_roots);
-    dbg!(faulty_roots);
+    faulty_roots.push((faulty_head, get_slot_for_block_root(&harness, faulty_head)));
 
-    panic!();
+    dbg!(&honest_roots);
+    dbg!(&faulty_roots);
+
+    (harness, honest_roots, faulty_roots)
+}
+
+#[test]
+fn unnamed() {
+    let (harness, honest_roots, _faulty_roots) = get_harness_containing_two_forks();
+
+    let genesis_block_root = harness.chain.genesis_block_root;
+    let genesis_block = harness
+        .chain
+        .store
+        .get::<BeaconBlock>(&genesis_block_root)
+        .expect("Genesis block should exist")
+        .expect("DB should not error");
+
+    let lmd = ThreadSafeReducedTree::new(
+        harness.chain.store.clone(),
+        &genesis_block,
+        genesis_block_root,
+    );
+
+    for (root, slot) in &honest_roots {
+        lmd.process_attestation(0, *root, *slot)
+            .expect("fork choice should accept attestations to honest roots");
+    }
 }
