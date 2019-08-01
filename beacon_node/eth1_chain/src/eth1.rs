00340521b1a94c62a1bd828ae7927322351df028
pawan <pawandhananjay@gmail.com>
2019-08-01 18:18:14 +0530

    Add parsing for deposit contract logs and get_eth1_data function

--- beacon_node/eth1_chain/src/eth1.rs
+++ beacon_node/eth1_chain/src/eth1.rs
@@ -1,6 +1,7 @@
+use types::Eth1Data;
 use web3::types::*;
 
-/// nterface for getting Eth1 chain data.
+/// Interface for getting Eth1 chain data.
 pub trait Eth1DataFetcher {
     /// Get block_header of the head of the chain.
     fn get_current_block_number(&self) -> Option<U256>;
@@ -8,11 +9,11 @@ pub trait Eth1DataFetcher {
     /// Get block_hash at given height.
     fn get_block_hash_by_height(&self, height: u64) -> Option<H256>;
 
-    /// Get deposit contract root.
-    fn get_deposit_root(&self) -> Option<Vec<u8>>;
+    /// Get deposit contract root at given eth1 block-number.
+    fn get_deposit_root(&self, block_number: Option<BlockNumber>) -> Option<H256>;
 
-    /// Get `deposit_count` from DepositContract.
-    fn get_deposit_count(&self) -> Option<Vec<u8>>;
+    /// Get `deposit_count` from DepositContract at given eth1 block_number.
+    fn get_deposit_count(&self, block_number: Option<BlockNumber>) -> Option<u64>;
 
     /// Get `DepositEvent` events in given range.
     fn get_deposit_logs_in_range(
@@ -20,4 +21,17 @@ pub trait Eth1DataFetcher {
         start_block: BlockNumber,
         end_block: BlockNumber,
     ) -> Option<Vec<Log>>;
+}
+
+/// Get `Eth1Data` object at a distance of `distance` from the perceived head of the currrent Eth1 chain.
+pub fn get_eth1_data<T: Eth1DataFetcher>(distance: u64, eth1_fetcher: &T) -> Option<Eth1Data> {
+    let current_block_number: U256 = eth1_fetcher.get_current_block_number()?;
+    let block_number: U256 = current_block_number.checked_sub(distance.into())?;
+    Some(Eth1Data {
+        deposit_root: eth1_fetcher
+            .get_deposit_root(Some(BlockNumber::Number(block_number.as_u64())))?,
+        deposit_count: eth1_fetcher
+            .get_deposit_count(Some(BlockNumber::Number(block_number.as_u64())))?,
+        block_hash: eth1_fetcher.get_block_hash_by_height(block_number.as_u64())?,
+    })
 }
