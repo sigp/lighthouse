63f40b081092f2321297f2e719de0a19a1e06a46
pawan <pawandhananjay@gmail.com>
2019-08-01 18:18:14 +0530

    Add parsing for deposit contract logs and get_eth1_data function

--- beacon_node/eth1_chain/src/web3.rs
+++ beacon_node/eth1_chain/src/web3.rs
@@ -1,3 +1,6 @@
+use bls::{PublicKey, Signature};
+use ethabi::{decode, ParamType, Token};
+use types::DepositData;
 use web3::contract::{Contract, Options};
 use web3::futures::Future;
 use web3::transports::WebSocket;
@@ -55,22 +58,34 @@ impl Eth1DataFetcher for Web3DataFetcher {
         block.ok().and_then(|x| x).and_then(|b| b.hash)
     }
 
-    /// Get `deposit_count` from deposit contract.
-    fn get_deposit_count(&self) -> Option<Vec<u8>> {
+    /// Get `deposit_count` from deposit contract at given eth1 block number.
+    fn get_deposit_count(&self, block_number: Option<BlockNumber>) -> Option<u64> {
         let contract =
             Contract::from_json(self.web3.eth(), self.contract.address, &self.contract.abi).ok()?;
-        let data = contract.query("get_deposit_count", (), None, Options::default(), None);
+        let data = contract.query(
+            "get_deposit_count",
+            (),
+            None,
+            Options::default(),
+            block_number,
+        );
         let deposit_count: Vec<u8> = data.wait().ok()?;
-        Some(deposit_count)
+        vec_to_u64_le(&deposit_count)
     }
 
-    /// Get `deposit_root` from deposit contract.
-    fn get_deposit_root(&self) -> Option<Vec<u8>> {
+    /// Get `deposit_root` from deposit contract at given eth1 block number.
+    fn get_deposit_root(&self, block_number: Option<BlockNumber>) -> Option<H256> {
         let contract =
             Contract::from_json(self.web3.eth(), self.contract.address, &self.contract.abi).ok()?;
-        let data = contract.query("get_hash_tree_root", (), None, Options::default(), None);
+        let data = contract.query(
+            "get_hash_tree_root",
+            (),
+            None,
+            Options::default(),
+            block_number,
+        );
         let deposit_root: Vec<u8> = data.wait().ok()?;
-        Some(deposit_root)
+        Some(H256::from_slice(&deposit_root))
     }
 
     /// Get `DepositEvent` events in given range.
@@ -81,7 +96,7 @@ impl Eth1DataFetcher for Web3DataFetcher {
     ) -> Option<Vec<Log>> {
         /// Keccak256 hash of "DepositEvent" in bytes for passing to log filter.
         const DEPOSIT_CONTRACT_HASH: &str =
-            "0x649bbc62d0e31342afea4e5cd82d4049e7e1ee912fc0889aa790803be39038c5";
+            "649bbc62d0e31342afea4e5cd82d4049e7e1ee912fc0889aa790803be39038c5";
         let filter = FilterBuilder::default()
             .address(vec![self.contract.address])
             .topics(
@@ -97,11 +112,61 @@ impl Eth1DataFetcher for Web3DataFetcher {
     }
 }
 
+// Converts a valid vector to a u64.
+fn vec_to_u64_le(bytes: &[u8]) -> Option<u64> {
+    let mut array = [0; 8];
+    if bytes.len() == 8 {
+        let bytes = &bytes[..array.len()];
+        array.copy_from_slice(bytes);
+        Some(u64::from_le_bytes(array))
+    } else {
+        None
+    }
+}
+
+/// Parse contract logs.
+fn parse_logs(log: Log, types: &[ParamType]) -> Option<Vec<Token>> {
+    decode(types, &log.data.0).ok()
+}
+
+/// Parse logs from deposit contract.
+pub fn parse_deposit_logs(log: Log) -> Option<DepositData> {
+    let deposit_event_params = &[
+        ParamType::FixedBytes(48), // pubkey
+        ParamType::FixedBytes(32), // withdrawal_credentials
+        ParamType::FixedBytes(8),  // amount
+        ParamType::FixedBytes(96), // signature
+        ParamType::FixedBytes(8),  // index
+    ];
+    let parsed_logs = parse_logs(log, deposit_event_params).unwrap();
+    // Convert from tokens to Vec<u8>.
+    let params = parsed_logs
+        .into_iter()
+        .map(|x| match x {
+            Token::FixedBytes(v) => Some(v),
+            _ => None,
+        })
+        .collect::<Option<Vec<_>>>()?;
+
+    // Event should have exactly 5 parameters.
+    if params.len() == 5 {
+        Some(DepositData {
+            pubkey: PublicKey::from_bytes(&params[0]).unwrap(),
+            withdrawal_credentials: H256::from_slice(&params[1]),
+            amount: vec_to_u64_le(&params[2])?,
+            signature: Signature::from_bytes(&params[3]).ok()?,
+            // How is index used?
+        })
+    } else {
+        None
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // Note: Running tests using ganache-cli instance with config 
+    // Note: Running tests using ganache-cli instance with config
     // from https://github.com/ChainSafe/lodestar#starting-private-eth1-chain
 
     #[test]
@@ -139,8 +204,8 @@ mod tests {
             abi: include_bytes!("deposit_contract.json").to_vec(),
         };
         let w3 = Web3DataFetcher::new("ws://localhost:8545", deposit_contract);
-        let deposit_count = w3.get_deposit_count();
-        assert_eq!(deposit_count, Some(vec![0; 8]));
+        let deposit_count = w3.get_deposit_count(None);
+        assert_eq!(deposit_count, Some(0));
     }
 
     #[test]
@@ -152,12 +217,31 @@ mod tests {
             abi: include_bytes!("deposit_contract.json").to_vec(),
         };
         let w3 = Web3DataFetcher::new("ws://localhost:8545", deposit_contract);
-        let expected = vec![
+        let expected: H256 = [
             215, 10, 35, 71, 49, 40, 92, 104, 4, 194, 164, 245, 103, 17, 221, 184, 200, 44, 153,
             116, 15, 32, 120, 84, 137, 16, 40, 175, 52, 226, 126, 94,
-        ];
-        let deposit_root = w3.get_deposit_root();
+        ]
+        .into();
+        let deposit_root = w3.get_deposit_root(None);
         assert_eq!(deposit_root, Some(expected));
+    }
+
+    #[test]
+    fn test_deposit_contract_logs() {
+        let deposit_contract_address: Address =
+            "8c594691C0E592FFA21F153a16aE41db5beFcaaa".parse().unwrap();
+        let deposit_contract = ContractConfig {
+            address: deposit_contract_address,
+            abi: include_bytes!("deposit_contract.json").to_vec(),
+        };
+        let w3 = Web3DataFetcher::new("ws://localhost:8545", deposit_contract);
+        let logs = w3
+            .get_deposit_logs_in_range(BlockNumber::Earliest, BlockNumber::Latest)
+            .unwrap();
+
+        for log in logs {
+            assert!(parse_deposit_logs(log).is_some());
+        }
     }
 
 }
