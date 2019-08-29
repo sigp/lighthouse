aaea2b36fe47e481a5ed172e4dfd2835e302c74d
Paul Hauner <paul@paulhauner.com>
2019-08-29 11:34:25 +1000

    Bulk signature verification (#507)
    * Add basic block processing benches
    
    * Start reviving state processing benches
    
    * Fix old block builders
    
    * Add optimization for faster pubkey add
    
    * Tidy benches, add another
    
    * Add extra block processing bench
    
    * Start working on faster BLS scheme
    
    * Add partially complete sig verify optimization
    
    * Add .gitignore to state processing
    
    * Add progress on faster signature verification
    
    * Fix SignatureSet for fake_crypto
    
    * Tidy attester slashings sig set
    
    * Tidy bulk signature verifier
    
    * Refactor signature sets to be cleaner
    
    * Start threading SignatureStrategy through code
    
    * Add (empty) test dir
    
    * Move BenchingBlockBuilder
    
    * Add initial block signature verification tests
    
    * Add tests for bulk signature verification
    
    * Start threading SignatureStrategy in block proc.
    
    * Refactor per_block_processing errors
    
    * Use sig set tuples instead of lists of two
    
    * Remove dead code
    
    * Thread VerifySignatures through per_block_processing
    
    * Add bulk signature verification
    
    * Introduce parallel bulk signature verification
    
    * Expand state processing benches
    
    * Fix additional compile errors
    
    * Fix issue where par iter chunks is 0
    
    * Update milagro_bls dep
    
    * Remove debugs, code fragment in beacon chain
    
    * Tidy, add comments to block sig verifier
    
    * Fix various PR comments
    
    * Add block_root option to per_block_processing
    
    * Fix comment in block signature verifier
    
    * Fix comments from PR review
    
    * Remove old comment
    
    * Fix comment

--- eth2/state_processing/src/per_block_processing/verify_exit.rs
+++ eth2/state_processing/src/per_block_processing/verify_exit.rs
@@ -1,6 +1,12 @@
-use super::errors::{ExitInvalid as Invalid, ExitValidationError as Error};
-use tree_hash::SignedRoot;
+use super::errors::{BlockOperationError, ExitInvalid};
+use crate::per_block_processing::{signature_sets::exit_signature_set, VerifySignatures};
 use types::*;
+
+type Result<T> = std::result::Result<T, BlockOperationError<ExitInvalid>>;
+
+fn error(reason: ExitInvalid) -> BlockOperationError<ExitInvalid> {
+    BlockOperationError::invalid(reason)
+}
 
 /// Indicates if an `Exit` is valid to be included in a block in the current epoch of the given
 /// state.
@@ -11,9 +17,10 @@ use types::*;
 pub fn verify_exit<T: EthSpec>(
     state: &BeaconState<T>,
     exit: &VoluntaryExit,
+    verify_signatures: VerifySignatures,
     spec: &ChainSpec,
-) -> Result<(), Error> {
-    verify_exit_parametric(state, exit, spec, false)
+) -> Result<()> {
+    verify_exit_parametric(state, exit, verify_signatures, spec, false)
 }
 
 /// Like `verify_exit` but doesn't run checks which may become true in future states.
@@ -22,9 +29,10 @@ pub fn verify_exit<T: EthSpec>(
 pub fn verify_exit_time_independent_only<T: EthSpec>(
     state: &BeaconState<T>,
     exit: &VoluntaryExit,
+    verify_signatures: VerifySignatures,
     spec: &ChainSpec,
-) -> Result<(), Error> {
-    verify_exit_parametric(state, exit, spec, true)
+) -> Result<()> {
+    verify_exit_parametric(state, exit, verify_signatures, spec, true)
 }
 
 /// Parametric version of `verify_exit` that skips some checks if `time_independent_only` is true.
@@ -33,30 +41,31 @@ pub fn verify_exit_time_independent_only<T: EthSpec>(
 fn verify_exit_parametric<T: EthSpec>(
     state: &BeaconState<T>,
     exit: &VoluntaryExit,
+    verify_signatures: VerifySignatures,
     spec: &ChainSpec,
     time_independent_only: bool,
-) -> Result<(), Error> {
+) -> Result<()> {
     let validator = state
         .validators
         .get(exit.validator_index as usize)
-        .ok_or_else(|| Error::Invalid(Invalid::ValidatorUnknown(exit.validator_index)))?;
+        .ok_or_else(|| error(ExitInvalid::ValidatorUnknown(exit.validator_index)))?;
 
     // Verify the validator is active.
     verify!(
         validator.is_active_at(state.current_epoch()),
-        Invalid::NotActive(exit.validator_index)
+        ExitInvalid::NotActive(exit.validator_index)
     );
 
     // Verify that the validator has not yet exited.
     verify!(
         validator.exit_epoch == spec.far_future_epoch,
-        Invalid::AlreadyExited(exit.validator_index)
+        ExitInvalid::AlreadyExited(exit.validator_index)
     );
 
     // Exits must specify an epoch when they become valid; they are not valid before then.
     verify!(
         time_independent_only || state.current_epoch() >= exit.epoch,
-        Invalid::FutureEpoch {
+        ExitInvalid::FutureEpoch {
             state: state.current_epoch(),
             exit: exit.epoch
         }
@@ -65,20 +74,18 @@ fn verify_exit_parametric<T: EthSpec>(
     // Verify the validator has been active long enough.
     verify!(
         state.current_epoch() >= validator.activation_epoch + spec.persistent_committee_period,
-        Invalid::TooYoungToExit {
+        ExitInvalid::TooYoungToExit {
             current_epoch: state.current_epoch(),
             earliest_exit_epoch: validator.activation_epoch + spec.persistent_committee_period,
         }
     );
 
-    // Verify signature.
-    let message = exit.signed_root();
-    let domain = spec.get_domain(exit.epoch, Domain::VoluntaryExit, &state.fork);
-    verify!(
-        exit.signature
-            .verify(&message[..], domain, &validator.pubkey),
-        Invalid::BadSignature
-    );
+    if verify_signatures.is_true() {
+        verify!(
+            exit_signature_set(state, exit, spec)?.is_valid(),
+            ExitInvalid::BadSignature
+        );
+    }
 
     Ok(())
 }
