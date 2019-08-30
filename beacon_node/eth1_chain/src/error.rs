cd49f2a7c837d97e49d90cdf2399d5622de5ff72
pawan <pawandhananjay@gmail.com>
2019-08-29 22:36:21 -0500

    Rename Eth1Error and change to Result<T>

--- beacon_node/eth1_chain/src/error.rs
+++ beacon_node/eth1_chain/src/error.rs
@@ -3,7 +3,7 @@ use ssz;
 use web3;
 
 #[derive(Debug)]
-pub enum Eth1Error {
+pub enum Error {
     /// Errors that occur on interaction with an Eth1 node.
     Web3Error(web3::error::Error),
     /// Web3 request timeout.
@@ -14,26 +14,28 @@ pub enum Eth1Error {
     DecodingError,
 }
 
-impl From<web3::error::Error> for Eth1Error {
+impl From<web3::error::Error> for Error {
     fn from(err: web3::error::Error) -> Self {
-        Eth1Error::Web3Error(err)
+        Error::Web3Error(err)
     }
 }
 
-impl From<web3::contract::Error> for Eth1Error {
+impl From<web3::contract::Error> for Error {
     fn from(err: web3::contract::Error) -> Self {
-        Eth1Error::ContractError(err)
+        Error::ContractError(err)
     }
 }
 
-impl From<AbiError> for Eth1Error {
+impl From<AbiError> for Error {
     fn from(err: AbiError) -> Self {
-        Eth1Error::ContractError(err.into())
+        Error::ContractError(err.into())
     }
 }
 
-impl From<ssz::DecodeError> for Eth1Error {
+impl From<ssz::DecodeError> for Error {
     fn from(_err: ssz::DecodeError) -> Self {
-        Eth1Error::DecodingError
+        Error::DecodingError
     }
 }
+
+pub type Result<T> = std::result::Result<T, Error>;
