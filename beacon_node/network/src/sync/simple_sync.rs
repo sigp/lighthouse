1845a5e98281f54e387d7fb3c0b601bc82063009
Paul Hauner <paul@paulhauner.com>
2019-08-30 17:06:59 +1000

    Add warning when disconnecting peer

--- beacon_node/network/src/sync/simple_sync.rs
+++ beacon_node/network/src/sync/simple_sync.rs
@@ -525,6 +525,13 @@ impl NetworkContext {
     }
 
     pub fn disconnect(&mut self, peer_id: PeerId, reason: GoodbyeReason) {
+        warn!(
+            &self.log,
+            "Disconnecting peer";
+            "reason" => format!("{:?}", reason),
+            "peer_id" => format!("{:?}", peer_id),
+        );
+
         self.send_rpc_request(None, peer_id, RPCRequest::Goodbye(reason))
         // TODO: disconnect peers.
     }
