9096295c46eb1209580441c00a3b662cd1fe887c
pscott <scottpiriou@gmail.com>
2019-09-26 11:17:23 +0200

    Remove complex signature

--- beacon_node/beacon_chain/src/beacon_chain.rs
+++ beacon_node/beacon_chain/src/beacon_chain.rs
@@ -125,6 +125,8 @@ pub struct BeaconChain<T: BeaconChainTypes> {
     log: Logger,
 }
 
+type BeaconInfo<T> = (BeaconBlock<T>, BeaconState<T>);
+
 impl<T: BeaconChainTypes> BeaconChain<T> {
     /// Instantiate a new Beacon Chain, from genesis.
     pub fn from_genesis(
@@ -1258,7 +1260,7 @@ impl<T: BeaconChainTypes> BeaconChain<T> {
         &self,
         randao_reveal: Signature,
         slot: Slot,
-    ) -> Result<(BeaconBlock<T::EthSpec>, BeaconState<T::EthSpec>), BlockProductionError> {
+    ) -> Result<BeaconInfo<T::EthSpec>, BlockProductionError> {
         let state = self
             .state_at_slot(slot - 1)
             .map_err(|_| BlockProductionError::UnableToProduceAtSlot(slot))?;
@@ -1279,7 +1281,7 @@ impl<T: BeaconChainTypes> BeaconChain<T> {
         mut state: BeaconState<T::EthSpec>,
         produce_at_slot: Slot,
         randao_reveal: Signature,
-    ) -> Result<(BeaconBlock<T::EthSpec>, BeaconState<T::EthSpec>), BlockProductionError> {
+    ) -> Result<BeaconInfo<T::EthSpec>, BlockProductionError> {
         metrics::inc_counter(&metrics::BLOCK_PRODUCTION_REQUESTS);
         let timer = metrics::start_timer(&metrics::BLOCK_PRODUCTION_TIMES);
 
