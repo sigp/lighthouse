3c0d30b683722643d5d17848196dd5706aa41a09
Paul Hauner <paul@paulhauner.com>
2019-10-02 10:09:17 +1000

    Add script to start ganache

---
+++ scripts/ganache_test_node.sh
@@ -0,0 +1,6 @@
+ganache-cli \
+	--defaultBalanceEther 1000000000 \
+	--gasLimit 1000000000 \
+	--accounts 10 \
+	--seed "vast thought differ pull jewel broom cook wrist tribe word before omit" \
+	--port 8545 \
