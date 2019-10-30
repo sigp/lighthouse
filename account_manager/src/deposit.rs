0e37bf742871d5d6bc7e698e0c2f38e4b14a0a05
pawan <pawandhananjay@gmail.com>
2019-10-30 15:41:33 +0530

    Add test

--- account_manager/src/deposit.rs
+++ account_manager/src/deposit.rs
@@ -3,10 +3,10 @@ use ssz::{ssz_encode, Decode};
 use types::{ChainSpec, DepositData, Hash256};
 
 pub fn generate_deposit_params(
-    validator_key: Keypair,
-    withdrawal_key: Keypair,
+    validator_key: &Keypair,
+    withdrawal_key: &Keypair,
     deposit_amount: u64,
-    spec: ChainSpec,
+    spec: &ChainSpec,
 ) -> Result<DepositData, String> {
     let pubkey = PublicKeyBytes::from_ssz_bytes(&ssz_encode(&validator_key.pk))
         .map_err(|e| format!("Invalid validator pubkey ssz: {:?}", e))?;
@@ -23,7 +23,42 @@ pub fn generate_deposit_params(
         amount: deposit_amount,
         signature: fake_signature,
     };
-    let signature = deposit.create_signature(&validator_key.sk, &spec);
+    let signature = deposit.create_signature(&validator_key.sk, spec);
     deposit.signature = signature;
     Ok(deposit)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bls::Signature;
+    use tree_hash::SignedRoot;
+
+    #[test]
+    fn test_deposit_params() {
+        let vkp = Keypair::random();
+        let wkp = Keypair::random();
+        let amount: u64 = 0;
+        let spec = ChainSpec::default();
+        let deposit_data = generate_deposit_params(&vkp, &wkp, amount, &spec).unwrap();
+
+        assert_eq!(
+            deposit_data.pubkey,
+            PublicKeyBytes::from_ssz_bytes(&ssz_encode(&vkp.pk)).unwrap()
+        );
+
+        assert_eq!(
+            deposit_data.withdrawal_credentials,
+            Hash256::from_slice(&get_withdrawal_credentials(
+                &wkp.pk,
+                spec.bls_withdrawal_prefix_byte,
+            ))
+        );
+
+        assert_eq!(deposit_data.amount, amount);
+
+        let signature = Signature::from_bytes(&deposit_data.signature.as_bytes()).unwrap();
+        let hashed_root = deposit_data.signed_root();
+        assert!(signature.verify(&hashed_root, spec.get_deposit_domain(), &vkp.pk));
+    }
 }
