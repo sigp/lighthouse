9599182f3aebbb875cee949f63e90cbb3a9a5cc3
pscott <scottpiriou@gmail.com>
2019-10-30 19:31:00 +0100

    rename main to histori_info.rs; move enums to lib.rs

--- validator_client/slashing_protection/src/lib.rs
+++ validator_client/slashing_protection/src/lib.rs
@@ -1,4 +1,30 @@
 pub mod attester_slashings;
-pub mod enums;
 pub mod proposer_slashings;
 mod tests;
+
+pub mod enums {
+    use crate::attester_slashings::InvalidAttestation;
+    use crate::proposer_slashings::InvalidBlock;
+    use std::io::ErrorKind;
+
+    #[derive(PartialEq, Debug)]
+    pub enum NotSafe {
+        InvalidAttestation(InvalidAttestation),
+        InvalidBlock(InvalidBlock),
+        PruningError,
+        IOError(ErrorKind),
+    }
+
+    #[derive(PartialEq, Debug)]
+    pub enum ValidityReason {
+        EmptyHistory,
+        SameVote,
+        Valid,
+    }
+
+    #[derive(PartialEq, Debug)]
+    pub struct Safe {
+        pub insert_index: usize,
+        pub reason: ValidityReason,
+    }
+}
