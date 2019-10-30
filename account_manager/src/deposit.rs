d260bead526df517ca6c916fdcebc451d07718ca
pawan <pawandhananjay@gmail.com>
2019-10-30 17:03:03 +0530

    Clean up and add docs

--- account_manager/src/deposit.rs
+++ account_manager/src/deposit.rs
@@ -1,37 +1,32 @@
-use bls::{get_withdrawal_credentials, Keypair, PublicKeyBytes, SignatureBytes, BLS_SIG_BYTE_SIZE};
-use ssz::{ssz_encode, Decode};
+use bls::{get_withdrawal_credentials, Keypair, Signature};
 use types::{ChainSpec, DepositData, Hash256};
 
+/// Generate deposit parameters given validator and withdrawal keypairs and deposit amount.
 pub fn generate_deposit_params(
-    validator_key: &Keypair,
+    validator_key: Keypair,
     withdrawal_key: &Keypair,
-    deposit_amount: u64,
+    amount: u64,
     spec: &ChainSpec,
-) -> Result<DepositData, String> {
-    let pubkey = PublicKeyBytes::from_ssz_bytes(&ssz_encode(&validator_key.pk))
-        .map_err(|e| format!("Invalid validator pubkey ssz: {:?}", e))?;
+) -> DepositData {
     let withdrawal_credentials = Hash256::from_slice(&get_withdrawal_credentials(
         &withdrawal_key.pk,
         spec.bls_withdrawal_prefix_byte,
     ));
-    // Creating a fake signature to construct the deposit struct
-    let fake_signature = SignatureBytes::from_ssz_bytes(&[0; BLS_SIG_BYTE_SIZE])
-        .map_err(|e| format!("Invalid signature ssz: {:?}", e))?;
     let mut deposit = DepositData {
-        pubkey,
+        pubkey: validator_key.pk.into(),
         withdrawal_credentials,
-        amount: deposit_amount,
-        signature: fake_signature,
+        amount,
+        signature: Signature::empty_signature().into(),
     };
-    let signature = deposit.create_signature(&validator_key.sk, spec);
-    deposit.signature = signature;
-    Ok(deposit)
+    deposit.signature = deposit.create_signature(&validator_key.sk, spec);
+    deposit
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use bls::Signature;
+    use bls::PublicKeyBytes;
+    use ssz::{ssz_encode, Decode};
     use tree_hash::SignedRoot;
 
     #[test]
@@ -40,7 +35,7 @@ mod tests {
         let wkp = Keypair::random();
         let amount: u64 = 0;
         let spec = ChainSpec::default();
-        let deposit_data = generate_deposit_params(&vkp, &wkp, amount, &spec).unwrap();
+        let deposit_data = generate_deposit_params(vkp.clone(), &wkp, amount, &spec);
 
         assert_eq!(
             deposit_data.pubkey,
