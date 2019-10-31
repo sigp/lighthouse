eb5a9888f6af72d6ee213833250d40c403100e9a
pawan <pawandhananjay@gmail.com>
2019-11-01 00:59:26 +0530

    First commit

--- account_manager/src/keystore.rs
+++ account_manager/src/keystore.rs
@@ -1,89 +1,95 @@
-#[cfg(test)]
-mod tests {
-    use parity_crypto::aes::{decrypt_128_ctr, encrypt_128_ctr};
-    use parity_crypto::digest;
-    use parity_crypto::pbkdf2::{sha256, Salt, Secret};
-    use rand::prelude::*;
-    use types::Keypair;
+use crate::cipher::{Aes128Ctr, Cipher, CipherMessage};
+use crate::kdf::{Kdf, Pbkdf2, Prf};
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
 
-    struct Keystore {
-        // Cipher stuff
-        pub cipher_message: Vec<u8>,
-        pub iv: Vec<u8>,
-        // Checksum stuff
-        pub checksum: Vec<u8>,
-        // kdf stuff
-        pub iterations: u32,
-        pub dk_len: u32,
-        pub salt: Vec<u8>,
+use rand::prelude::*;
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Checksum(String);
+
+/// Crypto
+pub struct Crypto<K: Kdf, C: Cipher> {
+    /// Key derivation function parameters
+    pub kdf: K,
+    /// Checksum for password verification
+    pub checksum: Checksum,
+    /// CipherParams parameters
+    pub cipher: CipherMessage<C>,
+}
+
+impl<K: Kdf, C: Cipher> Crypto<K, C> {
+    pub fn encrypt(password: String, secret: &[u8], kdf: K, cipher: C) -> Self {
+        // Generate derived key
+        let derived_key = kdf.derive_key(&password);
+        // Encrypt secret
+        let cipher_message = cipher.encrypt(&derived_key[0..16], secret);
+
+        // Generate checksum
+        let mut pre_image: Vec<u8> = derived_key[16..32].to_owned(); // last 16 bytes of decryption key
+        pre_image.append(&mut cipher_message.clone());
+        // create a Sha256 object
+        let mut hasher = Sha256::new();
+        hasher.input(&pre_image);
+        // read hash digest
+        let checksum = hasher.result_str();
+        Crypto {
+            kdf,
+            checksum: Checksum(checksum),
+            cipher: CipherMessage {
+                cipher,
+                message: cipher_message,
+            },
+        }
     }
 
+    pub fn decrypt(&self, password: String) -> Vec<u8> {
+        // Genrate derived key
+        let derived_key = self.kdf.derive_key(&password);
+        // Regenerate checksum
+        let mut pre_image: Vec<u8> = derived_key[16..32].to_owned();
+        pre_image.append(&mut self.cipher.message.clone());
+        // create a Sha256 object
+        let mut hasher = Sha256::new();
+        hasher.input(&pre_image);
+        // read hash digest
+        let checksum = hasher.result_str();
+        debug_assert_eq!(checksum, self.checksum.0);
+        let secret = self
+            .cipher
+            .cipher
+            .decrypt(&derived_key[0..16], &self.cipher.message);
+        return secret;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
     #[test]
-    fn test_keystore() {
-        let keypair = Keypair::random();
-        let password = "bythepowerofgrayskull";
-        // Derive decryption key from password
-        let iterations = 1000;
-        let dk_len = 32;
-        let salt = rand::thread_rng().gen::<[u8; 32]>();
-        let mut decryption_key = [0; 32];
+    fn test_vector() {
+        let secret_str = "7a28b5ba57c53603b0b07b56bba752f7784bf506fa95edc395f5cf6c7514fe9d";
+        let salt_str = "d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3";
+        let iv_str = "264daa3f303d7259501c93d997d84fe6";
+        let password = "testpassword".to_string();
 
-        // Run the kdf on the password to derive decryption key
-        sha256(
-            iterations,
-            Salt(&salt),
-            Secret(password.as_bytes()),
-            &mut decryption_key,
-        );
-        // Encryption params
-        let iv = rand::thread_rng().gen::<[u8; 16]>();
-        let mut cipher_message = vec![0; 48];
-        // Encrypt bls secret key with first 16 bytes as aes key
-        encrypt_128_ctr(
-            &decryption_key[0..16],
-            &iv,
-            &keypair.sk.as_raw().as_bytes(),
-            &mut cipher_message,
-        )
-        .unwrap();
-        // Generate checksum
-        let mut pre_image: Vec<u8> = decryption_key[16..32].to_owned(); // last 16 bytes of decryption key
-        pre_image.append(&mut cipher_message.clone());
-        let checksum_message: Vec<u8> = digest::sha256(&pre_image).to_owned();
-
-        // Generate keystore
-        let keystore = Keystore {
-            cipher_message,
-            iv: iv.to_owned().to_vec(),
-            checksum: checksum_message,
-            iterations,
-            dk_len,
-            salt: salt.to_owned().to_vec(),
+        let salt = hex::decode(salt_str).unwrap();
+        let iv = hex::decode(iv_str).unwrap();
+        let kdf = Pbkdf2 {
+            c: 262144,
+            dklen: 32,
+            prf: Prf::HmacSha256,
+            salt: salt.to_vec(),
         };
-
-        // Regnerate decryption key
-        let mut decryption_key1 = [0; 32];
-        sha256(
-            keystore.iterations,
-            Salt(&keystore.salt),
-            Secret(password.as_bytes()),
-            &mut decryption_key1,
-        );
-        // Verify checksum
-        let mut dk_slice = decryption_key1[16..32].to_owned();
-        dk_slice.append(&mut keystore.cipher_message.clone());
-        let checksum: Vec<u8> = digest::sha256(&dk_slice).to_owned();
-        assert_eq!(checksum, keystore.checksum);
-
-        // Verify receovered sk
-        let mut sk = vec![0; 48];
-        decrypt_128_ctr(
-            &decryption_key[0..16],
-            &keystore.iv,
-            &keystore.cipher_message,
-            &mut sk,
-        )
-        .unwrap();
-        assert_eq!(sk, keypair.sk.as_raw().as_bytes());
+        let cipher = Aes128Ctr { iv: iv };
+        let secret = hex::decode(secret_str).unwrap();
+        let crypto = Crypto::encrypt(password.clone(), &secret, kdf, cipher);
+        println!("Cipher is {:?}", hex::encode(crypto.cipher.message.clone()));
+        println!("Checksum is {:?}", crypto.checksum);
+        let recovered_secret = crypto.decrypt(password);
+        let recovered_secret_str = hex::encode(recovered_secret);
+        println!("Secret is {:?}", recovered_secret_str);
+        assert_eq!(recovered_secret_str, secret_str);
     }
 }
