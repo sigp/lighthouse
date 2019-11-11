d0249a5518c7b271f9ab3ae8cbbf6e1b3a7b8043
Michael Sproul <michael@sigmaprime.io>
2019-11-11 16:02:22 +1100

    Formatting, fix CI failures

--- eth2/operation_pool/src/lib.rs
+++ eth2/operation_pool/src/lib.rs
@@ -578,7 +578,7 @@ mod tests {
             let mut builder = TestingAttestationBuilder::new(state, committee, slot, index);
             let signers = &committee[signing_range];
             let committee_keys = signers.iter().map(|&i| &keypairs[i].sk).collect::<Vec<_>>();
-            builder.sign(signers, &committee_keys, &state.fork, spec, false);
+            builder.sign(signers, &committee_keys, &state.fork, spec);
             extra_signer.map(|c_idx| {
                 let validator_index = committee[c_idx];
                 builder.sign(
@@ -586,7 +586,6 @@ mod tests {
                     &[&keypairs[validator_index].sk],
                     &state.fork,
                     spec,
-                    false,
                 )
             });
             builder.build()
