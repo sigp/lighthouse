9f14df3b2896065eaa13fda84e72c87c28d5b3ce
pawan <pawandhananjay@gmail.com>
2019-12-16 17:46:19 +0530

    Truncate required_blocks in block_cache upto latest_processed_block in deposit cache

--- beacon_node/eth1/src/service.rs
+++ beacon_node/eth1/src/service.rs
@@ -252,11 +252,12 @@ impl Service {
             .map_err(|e| format!("Failed to update eth1 cache: {:?}", e))
             .then(move |result| {
                 match &result {
-                    Ok(DepositCacheUpdateOutcome::Success { logs_imported }) => trace!(
+                    Ok(DepositCacheUpdateOutcome::Success { logs_imported }) => debug!(
                         log_a,
                         "Updated eth1 deposit cache";
                         "cached_deposits" => inner_1.deposit_cache.read().cache.len(),
                         "logs_imported" => logs_imported,
+                        "latest_block" => inner_1.deposit_cache.read().last_processed_block,
                     ),
                     Err(e) => error!(
                         log_a,
@@ -492,6 +493,10 @@ impl Service {
         let cache_4 = self.inner.clone();
         let cache_5 = self.inner.clone();
         let cache_6 = self.inner.clone();
+        let log1 = self.log.clone();
+        let log2 = self.log.clone();
+        let log3 = self.log.clone();
+        let log4 = self.log.clone();
 
         let block_cache_truncation = self.config().block_cache_truncation;
         let max_blocks_per_update = self
@@ -505,7 +510,9 @@ impl Service {
             .highest_block_number()
             .map(|n| n + 1)
             .unwrap_or_else(|| self.config().lowest_cached_block_number);
-
+        debug!(log1, "Next required block";
+            "block" => next_required_block,
+        );
         get_new_block_numbers(
             &self.config().endpoint,
             next_required_block,
@@ -528,7 +535,9 @@ impl Service {
                         let max_size = block_cache_truncation
                             .map(|n| n as u64)
                             .unwrap_or_else(u64::max_value);
-
+                        debug!(log2, "Block cache truncation";
+                               "value" => max_size,
+                        );
                         if range_size > max_size {
                             // If the range of required blocks is larger than `max_size`, drop all
                             // existing blocks and download `max_size` count of blocks.
@@ -544,14 +553,21 @@ impl Service {
         })
         // Download the range of blocks and sequentially import them into the cache.
         .and_then(move |required_block_numbers| {
+            debug!(log3.clone(), "Required block numbers";
+                "block_numbers" => format!("{:?}", required_block_numbers),
+            );
             // Last processed block in deposit cache
             let latest_in_cache = cache_6
                 .deposit_cache
                 .read()
                 .last_processed_block
                 .unwrap_or(0);
-            if let Some(last) = required_block_numbers.last() {
-                if latest_in_cache < *last {
+            if let Some(first_block_num) = required_block_numbers.first() {
+                debug!(log3,
+                    "Values in cache";
+                    "Latest in cache" =>  latest_in_cache,
+                );
+                if latest_in_cache < *first_block_num {
                     return Either::A(ok::<BlockCacheUpdateOutcome, Error>(
                         BlockCacheUpdateOutcome::WaitingForDepositCache,
                     ));
@@ -560,22 +576,30 @@ impl Service {
             let required_block_numbers = required_block_numbers
                 .into_iter()
                 .take(max_blocks_per_update)
+                .filter(|x| *x <= latest_in_cache)
                 .collect::<Vec<_>>();
             Either::B(
                 ok::<Vec<_>, Error>(required_block_numbers)
                     .and_then(|required_block_numbers| {
                         // Produce a stream from the list of required block numbers and return a future that
                         // consumes the it.
-                        stream::unfold(required_block_numbers, move |block_numbers| {
-                            match block_numbers.iter().next() {
-                                Some(block_number) => Some(
-                                    download_eth1_block(cache_2.clone(), *block_number)
-                                        .map(|v| (v, block_numbers)),
-                                ),
+                        stream::unfold(
+                            required_block_numbers.into_iter(),
+                            move |mut block_numbers| match block_numbers.next() {
+                                Some(block_number) => {
+                                    debug!(log4, "Requesting block number";
+                                        "block_number" => block_number,
+                                    );
+                                    Some(
+                                        download_eth1_block(cache_2.clone(), block_number)
+                                            .map(|v| (v, block_numbers)),
+                                    )
+                                }
                                 None => None,
-                            }
-                        })
+                            },
+                        )
                         .fold(0, move |sum, eth1_block| {
+                            // println!("Eth1 block downloaded: {:?}", eth1_block);
                             cache_3
                                 .block_cache
                                 .write()
