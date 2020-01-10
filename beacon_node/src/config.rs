2d19aa05fe2a78d4e6b71568251e7b48133dc1c7
Michael Sproul <michael@sigmaprime.io>
2020-01-10 13:59:20 +1100

    Increase default slots per restore point to 2048
    This should reduce disk usage by 32x while keeping historical state queries to
    less than 10s. If historical states are required quickly, the minimum SPRP of 32
    can be set on the CLI.

--- beacon_node/src/config.rs
+++ beacon_node/src/config.rs
@@ -258,6 +258,11 @@ pub fn get_configs<E: EthSpec>(
         client_config.store.slots_per_restore_point = slots_per_restore_point
             .parse()
             .map_err(|_| "slots-per-restore-point is not a valid integer".to_string())?;
+    } else {
+        client_config.store.slots_per_restore_point = std::cmp::min(
+            E::slots_per_historical_root() as u64,
+            store::config::DEFAULT_SLOTS_PER_RESTORE_POINT,
+        );
     }
 
     if eth2_config.spec_constants != client_config.spec_constants {
