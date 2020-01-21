e571034d882401a61aa8a4e0559d496be1a2f800
pawan <pawandhananjay@gmail.com>
2020-01-21 17:13:14 +0530

    Add sign method to `VoluntaryExit`

--- eth2/types/src/voluntary_exit.rs
+++ eth2/types/src/voluntary_exit.rs
@@ -1,10 +1,10 @@
-use crate::{test_utils::TestRandom, Epoch};
-use bls::Signature;
+use crate::{test_utils::TestRandom, ChainSpec, Domain, Epoch, Fork};
+use bls::{SecretKey, Signature};
 
 use serde_derive::{Deserialize, Serialize};
 use ssz_derive::{Decode, Encode};
 use test_random_derive::TestRandom;
-use tree_hash::TreeHash;
+use tree_hash::{SignedRoot, TreeHash};
 use tree_hash_derive::{SignedRoot, TreeHash};
 
 /// An exit voluntarily submitted a validator who wishes to withdraw.
@@ -28,6 +28,15 @@ pub struct VoluntaryExit {
     pub validator_index: u64,
     #[signed_root(skip_hashing)]
     pub signature: Signature,
+}
+
+impl VoluntaryExit {
+    /// Signs `self`.
+    pub fn sign(&mut self, secret_key: &SecretKey, fork: &Fork, spec: &ChainSpec) {
+        let message = self.signed_root();
+        let domain = spec.get_domain(self.epoch, Domain::VoluntaryExit, &fork);
+        self.signature = Signature::new(&message, domain, &secret_key);
+    }
 }
 
 #[cfg(test)]
