20e155d4266a5d6ae6df3b4f04258b08dcd1afc9
Paul Hauner <paul@paulhauner.com>
2020-01-30 08:23:54 +1100

    Add endpoint to dump operation pool

--- beacon_node/rest_api/src/advanced.rs
+++ beacon_node/rest_api/src/advanced.rs
@@ -2,6 +2,7 @@ use crate::response_builder::ResponseBuilder;
 use crate::ApiResult;
 use beacon_chain::{BeaconChain, BeaconChainTypes};
 use hyper::{Body, Request};
+use operation_pool::PersistedOperationPool;
 use std::sync::Arc;
 
 /// Returns the `proto_array` fork choice struct, encoded as JSON.
@@ -12,4 +13,16 @@ pub fn get_fork_choice<T: BeaconChainTypes>(
     beacon_chain: Arc<BeaconChain<T>>,
 ) -> ApiResult {
     ResponseBuilder::new(&req)?.body_no_ssz(&*beacon_chain.fork_choice.core_proto_array())
+}
+
+/// Returns the `PersistedOperationPool` struct.
+///
+/// Useful for debugging or advanced inspection of the stored operations.
+pub fn get_operation_pool<T: BeaconChainTypes>(
+    req: Request<Body>,
+    beacon_chain: Arc<BeaconChain<T>>,
+) -> ApiResult {
+    ResponseBuilder::new(&req)?.body(&PersistedOperationPool::from_operation_pool(
+        &beacon_chain.op_pool,
+    ))
 }
