b7f2ff2fde2d621e8e55252ee8b345c342c58cb0
pawan <pawandhananjay@gmail.com>
2020-02-20 18:57:00 +0530

    All encode/decode functions use snappy frame format

--- beacon_node/eth2-libp2p/src/rpc/codec/ssz_snappy.rs
+++ beacon_node/eth2-libp2p/src/rpc/codec/ssz_snappy.rs
@@ -21,9 +21,9 @@ use unsigned_varint::{decode, encode};
 /* Inbound Codec */
 
 pub struct SSZSnappyInboundCodec<TSpec: EthSpec> {
-    decoder: snap::raw::Decoder,
     protocol: ProtocolId,
     phantom: PhantomData<TSpec>,
+    len: Option<usize>,
     /// Maximum bytes that can be sent in one req/resp chunked responses.
     max_packet_size: usize,
 }
@@ -34,8 +34,8 @@ impl<T: EthSpec> SSZSnappyInboundCodec<T> {
         debug_assert!(protocol.encoding.as_str() == "ssz_snappy");
 
         SSZSnappyInboundCodec {
-            decoder: snap::raw::Decoder::new(),
             protocol,
+            len: None,
             phantom: PhantomData,
             max_packet_size,
         }
@@ -85,43 +85,90 @@ impl<TSpec: EthSpec> Decoder for SSZSnappyInboundCodec<TSpec> {
     type Error = RPCError;
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        match self.decoder.decompress_vec(src).map_err(RPCError::from) {
-            Ok(packet) => match self.protocol.message_name.as_str() {
-                RPC_STATUS => match self.protocol.version.as_str() {
-                    "1" => Ok(Some(RPCRequest::Status(StatusMessage::from_ssz_bytes(
-                        &packet,
-                    )?))),
-                    _ => unreachable!("Cannot negotiate an unknown version"),
-                },
-                RPC_GOODBYE => match self.protocol.version.as_str() {
-                    "1" => Ok(Some(RPCRequest::Goodbye(GoodbyeReason::from_ssz_bytes(
-                        &packet,
-                    )?))),
-                    _ => unreachable!("Cannot negotiate an unknown version"),
-                },
-                RPC_BLOCKS_BY_RANGE => match self.protocol.version.as_str() {
-                    "1" => Ok(Some(RPCRequest::BlocksByRange(
-                        BlocksByRangeRequest::from_ssz_bytes(&packet)?,
-                    ))),
-                    _ => unreachable!("Cannot negotiate an unknown version"),
-                },
-                RPC_BLOCKS_BY_ROOT => match self.protocol.version.as_str() {
-                    "1" => Ok(Some(RPCRequest::BlocksByRoot(BlocksByRootRequest {
-                        block_roots: Vec::from_ssz_bytes(&packet)?,
-                    }))),
-                    _ => unreachable!("Cannot negotiate an unknown version"),
-                },
-                _ => unreachable!("Cannot negotiate an unknown protocol"),
+        // Decode the length of the uncompressed bytes
+        if self.len.is_none() {
+            match decode::u64(src) {
+                Err(decode::Error::Insufficient) => return Ok(None), // Need more bytes to read len
+                Err(decode::Error::Overflow) => {
+                    return Err(RPCError::Custom(
+                        "Overflow while reading uncompressed length from snappy frame".into(),
+                    ))
+                }
+                Err(_) => {
+                    return Err(RPCError::Custom(
+                        "Failed to read length from snappy frame".into(),
+                    ))
+                }
+                Ok((length, remaining)) => {
+                    // split the incoming buffer to remove the read length bytes
+                    let input_len = src.len();
+                    let remaining_len = remaining.len();
+                    src.split_to(input_len - remaining_len);
+                    self.len = Some(length as usize);
+                }
+            }
+        };
+
+        // TODO: Double check that this never panics
+        let length = self.len.expect("length should be Some");
+
+        // Should not attempt to decode rpc chunks with length > max_packet_size
+        if length > self.max_packet_size {
+            return Err(RPCError::Custom(
+                "attempting to decode data > max_packet_size".into(),
+            ));
+        }
+        let mut reader = FrameDecoder::new(Cursor::new(&src));
+        let mut decoded_buffer = vec![0; length];
+
+        match reader.read_exact(&mut decoded_buffer) {
+            Ok(()) => {
+                // `n` is how many bytes the reader read in the compressed stream
+                let n = reader.get_ref().position();
+                self.len = None;
+                src.split_to(n as usize);
+                match self.protocol.message_name.as_str() {
+                    RPC_STATUS => match self.protocol.version.as_str() {
+                        "1" => Ok(Some(RPCRequest::Status(StatusMessage::from_ssz_bytes(
+                            &decoded_buffer,
+                        )?))),
+                        _ => unreachable!("Cannot negotiate an unknown version"),
+                    },
+                    RPC_GOODBYE => match self.protocol.version.as_str() {
+                        "1" => Ok(Some(RPCRequest::Goodbye(GoodbyeReason::from_ssz_bytes(
+                            &decoded_buffer,
+                        )?))),
+                        _ => unreachable!("Cannot negotiate an unknown version"),
+                    },
+                    RPC_BLOCKS_BY_RANGE => match self.protocol.version.as_str() {
+                        "1" => Ok(Some(RPCRequest::BlocksByRange(
+                            BlocksByRangeRequest::from_ssz_bytes(&decoded_buffer)?,
+                        ))),
+                        _ => unreachable!("Cannot negotiate an unknown version"),
+                    },
+                    RPC_BLOCKS_BY_ROOT => match self.protocol.version.as_str() {
+                        "1" => Ok(Some(RPCRequest::BlocksByRoot(BlocksByRootRequest {
+                            block_roots: Vec::from_ssz_bytes(&decoded_buffer)?,
+                        }))),
+                        _ => unreachable!("Cannot negotiate an unknown version"),
+                    },
+                    _ => unreachable!("Cannot negotiate an unknown protocol"),
+                }
+            }
+            Err(e) => match e.kind() {
+                // Haven't received enough bytes to decode yet
+                // TODO: check if this is the only Error variant where we return `Ok(None)`
+                ErrorKind::UnexpectedEof => {
+                    return Ok(None);
+                }
+                _ => return Err(e).map_err(RPCError::from),
             },
-            Err(e) => Err(e),
         }
     }
 }
 
 /* Outbound Codec: Codec for initiating RPC requests */
 pub struct SSZSnappyOutboundCodec<TSpec: EthSpec> {
-    encoder: snap::raw::Encoder,
-    decoder: snap::raw::Decoder,
     len: Option<usize>,
     protocol: ProtocolId,
     /// Maximum bytes that can be sent in one req/resp chunked responses.
@@ -135,8 +182,6 @@ impl<TSpec: EthSpec> SSZSnappyOutboundCodec<TSpec> {
         debug_assert!(protocol.encoding.as_str() == "ssz_snappy");
 
         SSZSnappyOutboundCodec {
-            encoder: snap::raw::Encoder::new(),
-            decoder: snap::raw::Decoder::new(),
             protocol,
             max_packet_size,
             len: None,
@@ -158,10 +203,20 @@ impl<TSpec: EthSpec> Encoder for SSZSnappyOutboundCodec<TSpec> {
             RPCRequest::BlocksByRoot(req) => req.block_roots.as_ssz_bytes(),
             RPCRequest::Phantom(_) => unreachable!("Never encode phantom data"),
         };
-        // Compressed RpcRequests are not UVI encoded since they don't have chunks.
-        // Correspondingly on the receiving end, they are directly snappy uncompressed.
-        let compressed_bytes = self.encoder.compress_vec(&bytes).map_err(RPCError::from)?;
-        dst.extend_from_slice(&compressed_bytes);
+        // SSZ encoded bytes should be within `max_packet_size`
+        if bytes.len() > self.max_packet_size {
+            return Err(RPCError::Custom(
+                "attempting to encode data > max_packet_size".into(),
+            ));
+        }
+        let mut writer = FrameEncoder::new(Vec::new());
+        writer.write_all(&bytes).map_err(RPCError::from)?;
+        writer.flush().map_err(RPCError::from)?;
+
+        // Length prefix uncompressed bytes
+        dst.extend_from_slice(encode::u64(bytes.len() as u64, &mut encode::u64_buffer()));
+        // Write compressed bytes to `dst`
+        dst.extend_from_slice(writer.get_ref());
         Ok(())
     }
 }
@@ -258,9 +313,57 @@ impl<TSpec: EthSpec> OutboundCodec for SSZSnappyOutboundCodec<TSpec> {
     type ErrorType = ErrorMessage;
 
     fn decode_error(&mut self, src: &mut BytesMut) -> Result<Option<Self::ErrorType>, RPCError> {
-        match self.decoder.decompress_vec(src).map_err(RPCError::from) {
-            Ok(packet) => Ok(Some(ErrorMessage::from_ssz_bytes(&packet)?)),
-            Err(e) => Err(e),
+        // Decode the length of the uncompressed bytes
+        if self.len.is_none() {
+            match decode::u64(src) {
+                Err(decode::Error::Insufficient) => return Ok(None), // Need more bytes to read len
+                Err(decode::Error::Overflow) => {
+                    return Err(RPCError::Custom(
+                        "Overflow while reading uncompressed length from snappy frame".into(),
+                    ))
+                }
+                Err(_) => {
+                    return Err(RPCError::Custom(
+                        "Failed to read length from snappy frame".into(),
+                    ))
+                }
+                Ok((length, remaining)) => {
+                    // split the incoming buffer to remove the read length bytes
+                    let input_len = src.len();
+                    let remaining_len = remaining.len();
+                    src.split_to(input_len - remaining_len);
+                    self.len = Some(length as usize);
+                }
+            }
+        };
+
+        // TODO: Double check that this never panics
+        let length = self.len.expect("length should be Some");
+
+        // Should not attempt to decode rpc chunks with length > max_packet_size
+        if length > self.max_packet_size {
+            return Err(RPCError::Custom(
+                "attempting to decode data > max_packet_size".into(),
+            ));
+        }
+        let mut reader = FrameDecoder::new(Cursor::new(&src));
+        let mut decoded_buffer = vec![0; length];
+        match reader.read_exact(&mut decoded_buffer) {
+            Ok(()) => {
+                // `n` is how many bytes the reader read in the compressed stream
+                let n = reader.get_ref().position();
+                self.len = None;
+                src.split_to(n as usize);
+                Ok(Some(ErrorMessage::from_ssz_bytes(&decoded_buffer)?))
+            }
+            Err(e) => match e.kind() {
+                // Haven't received enough bytes to decode yet
+                // TODO: check if this is the only Error variant where we return `Ok(None)`
+                ErrorKind::UnexpectedEof => {
+                    return Ok(None);
+                }
+                _ => return Err(e).map_err(RPCError::from),
+            },
         }
     }
 }
