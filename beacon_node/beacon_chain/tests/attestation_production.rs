08ca9504aab46a850ae0de31bf4dcf7ffdcaf9da
Paul Hauner <paul@paulhauner.com>
2020-03-05 10:35:39 +1100

    Fix compile issue (#893)

--- beacon_node/beacon_chain/tests/attestation_production.rs
+++ beacon_node/beacon_chain/tests/attestation_production.rs
@@ -8,7 +8,7 @@ use beacon_chain::{
     StateSkipConfig,
 };
 use tree_hash::TreeHash;
-use types::{AggregateSignature, EthSpec, Hash256, Keypair, MainnetEthSpec, RelativeEpoch, Slot};
+use types::{AggregateSignature, EthSpec, Keypair, MainnetEthSpec, RelativeEpoch, Slot};
 
 pub const VALIDATOR_COUNT: usize = 16;
 
@@ -63,7 +63,7 @@ fn produces_attestations() {
             .block_at_slot(block_slot)
             .expect("should get block")
             .expect("block should not be skipped");
-        let block_root = Hash256::from_slice(&block.message.tree_hash_root());
+        let block_root = block.message.tree_hash_root();
 
         let epoch_boundary_slot = state
             .current_epoch()
