7f42267d1bb5af04123fcaf5fe97b39353e280db
Age Manning <Age@AgeManning.com>
2020-03-17 11:07:52 +1100

    Fix master failing test (#915)
    * Use a valid secret key in ssz test
    
    * Correct test_random secret key tests

--- eth2/types/src/test_utils/test_random/secret_key.rs
+++ eth2/types/src/test_utils/test_random/secret_key.rs
@@ -2,17 +2,7 @@ use super::*;
 use bls::SecretKey;
 
 impl TestRandom for SecretKey {
-    fn random_for_test(rng: &mut impl RngCore) -> Self {
-        let mut key_bytes = vec![0; 48];
-        rng.fill_bytes(&mut key_bytes);
-        /*
-         * An `unreachable!` is used here as there's no reason why you cannot construct a key from a
-         * fixed-length byte slice. Also, this should only be used during testing so a panic is
-         * acceptable.
-         */
-        match SecretKey::from_bytes(&key_bytes) {
-            Ok(key) => key,
-            Err(_) => unreachable!(),
-        }
+    fn random_for_test(_rng: &mut impl RngCore) -> Self {
+        SecretKey::random()
     }
 }
