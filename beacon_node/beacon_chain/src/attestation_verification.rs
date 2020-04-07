3f7c9b9abef8fab72ceb80fcfdcba32c196abb8e
Paul Hauner <paul@paulhauner.com>
2020-04-07 16:49:07 +1000

    Add more attestation verification

--- beacon_node/beacon_chain/src/attestation_verification.rs
+++ beacon_node/beacon_chain/src/attestation_verification.rs
@@ -4,10 +4,11 @@ use crate::{
         VALIDATOR_PUBKEY_CACHE_LOCK_TIMEOUT,
     },
     metrics,
+    naive_aggregation_pool::Error as NaiveAggregationError,
     observed_attestations::{Error as AttestationObservationError, ObserveOutcome},
     BeaconChain, BeaconChainError, BeaconChainTypes,
 };
-use slog::debug;
+use slog::{debug, error, trace};
 use slot_clock::SlotClock;
 use state_processing::{
     common::get_indexed_attestation, per_block_processing::errors::AttestationValidationError,
@@ -16,7 +17,8 @@ use state_processing::{
 use std::borrow::Cow;
 use tree_hash::TreeHash;
 use types::{
-    Attestation, CommitteeIndex, Epoch, EthSpec, Hash256, IndexedAttestation, RelativeEpoch, Slot,
+    Attestation, CommitteeIndex, Epoch, EthSpec, Hash256, IndexedAttestation, RelativeEpoch,
+    SignedAggregateAndProof, Slot,
 };
 
 pub enum Error {
@@ -26,6 +28,7 @@ pub enum Error {
         attestation_slot: Slot,
         latest_permissible_slot: Slot,
     },
+    EmptyAggregationBitfield,
     /// The attestation is from a slot that is prior to the earliest permissible slolt (with
     /// respect to the gossip clock disparity).
     PastSlot {
@@ -51,6 +54,20 @@ pub enum Error {
         validator_index: u64,
         epoch: Epoch,
     },
+    FutureEpoch {
+        attestation_epoch: Epoch,
+        current_epoch: Epoch,
+    },
+    PastEpoch {
+        attestation_epoch: Epoch,
+        current_epoch: Epoch,
+    },
+    /// The attestation is attesting to a state that is later than itself. (Viz., attesting to the
+    /// future).
+    AttestsToFutureBlock {
+        block: Slot,
+        attestation: Slot,
+    },
     Invalid(AttestationValidationError),
     /// There was an error whilst processing the attestation. It is not known if it is valid or invalid.
     BeaconChainError(BeaconChainError),
@@ -62,31 +79,16 @@ impl From<BeaconChainError> for Error {
     }
 }
 
-#[derive(Debug, PartialEq)]
-pub enum AttestationType {
-    /// An attestation with a single-signature that has been published in accordance with the naive
-    /// aggregation strategy.
-    ///
-    /// These attestations may have come from a `committee_index{subnet_id}_beacon_attestation`
-    /// gossip subnet or they have have come directly from a validator attached to our API.
-    ///
-    /// If `should_store == true`, the attestation will be added to the `NaiveAggregationPool`.
-    Unaggregated { should_store: bool },
-    /// An attestation with one more more signatures that has passed through the aggregation phase
-    /// of the naive aggregation scheme.
-    ///
-    /// These attestations must have come from the `beacon_aggregate_and_proof` gossip subnet.
-    Aggregated,
+pub struct VerifiedAggregateAttestation<T: BeaconChainTypes> {
+    signed_aggregate: SignedAggregateAndProof<T::EthSpec>,
 }
 
-pub struct GossipVerifiedAttestation<T: BeaconChainTypes> {
+pub struct VerifiedUnaggregateAttestation<T: BeaconChainTypes> {
     attestation: Attestation<T::EthSpec>,
-    attestation_type: AttestationType,
 }
 
 pub struct FullyVerifiedAttestation<T: BeaconChainTypes> {
     attestation: Attestation<T::EthSpec>,
-    attestation_type: AttestationType,
 }
 
 // TODO: aggregates:
@@ -118,21 +120,12 @@ The aggregator signature, signed_aggregate_and_proof.signature, is valid.
 */
 
 /*
-       let attestation_root = attestation.tree_hash_root();
-
-       if let ObserveOutcome::AlreadyKnown = chain
-           .observed_attestations
-           .observe_attestation(&attestation, Some(attestation_root))
-           .map_err(|e| Error::BeaconChainError(e.into()))?
-       {
-           return Err(Error::AttestationAlreadyKnown);
-       }
 */
 
-impl<T: BeaconChainTypes> GossipVerifiedAttestation<T> {
+/*
+impl<T: BeaconChainTypes> VerifiedAggregateAttestation<T> {
     pub fn new(
-        attestation: Attestation<T::EthSpec>,
-        attestation_type: AttestationType,
+        signed_aggregate: SignedAggregateAndProof<T::EthSpec>,
         chain: &BeaconChain<T>,
     ) -> Result<Self, Error> {
         // Ensure attestation is within the last ATTESTATION_PROPAGATION_SLOT_RANGE slots (within a
@@ -165,6 +158,54 @@ impl<T: BeaconChainTypes> GossipVerifiedAttestation<T> {
             })?;
 
         let indexed_attestation = obtain_indexed_attestation(chain, &attestation, block_slot)?;
+    }
+}
+*/
+
+impl<T: BeaconChainTypes> VerifiedUnaggregateAttestation<T> {
+    pub fn new(
+        attestation: Attestation<T::EthSpec>,
+        chain: &BeaconChain<T>,
+    ) -> Result<Self, Error> {
+        // Ensure attestation is within the last ATTESTATION_PROPAGATION_SLOT_RANGE slots (within a
+        // MAXIMUM_GOSSIP_CLOCK_DISPARITY allowance).
+        //
+        // We do not queue future attestations for later processing.
+        verify_propagation_slot_range(chain, &attestation)?;
+
+        // Check to ensure that the attestation is "unaggregated". I.e., it has exactly one
+        // aggregation bit set.
+        let num_aggreagtion_bits = attestation.aggregation_bits.num_set_bits();
+        if num_aggreagtion_bits != 1 {
+            return Err(Error::NotExactlyOneAggregationBitSet(num_aggreagtion_bits));
+        }
+
+        // The block being voted for (attestation.data.beacon_block_root) passes validation.
+        //
+        // This indirectly checks to see if the `attestation.data.beacon_block_root` is in our fork
+        // choice. Any known, non-finalized, processed block should be in fork choice, so this
+        // check immediately filters out attestations that attest to a block that has not been
+        // processed.
+        //
+        // Attestations must be for a known block. If the block is unknown, we simply drop the
+        // attestation and do not delay consideration for later.
+        let (block_slot, _state_root) = chain
+            .fork_choice
+            .block_slot_and_state_root(&attestation.data.beacon_block_root)
+            .ok_or_else(|| Error::UnknownHeadBlock {
+                beacon_block_root: attestation.data.beacon_block_root,
+            })?;
+
+        let attestation_root = attestation.tree_hash_root();
+        if let ObserveOutcome::AlreadyKnown = chain
+            .observed_attestations
+            .observe_attestation(&attestation, Some(attestation_root))
+            .map_err(|e| Error::BeaconChainError(e.into()))?
+        {
+            return Err(Error::AttestationAlreadyKnown);
+        }
+
+        let indexed_attestation = obtain_indexed_attestation(chain, &attestation, block_slot)?;
 
         let validator_index = *indexed_attestation
             .attesting_indices
@@ -189,10 +230,108 @@ impl<T: BeaconChainTypes> GossipVerifiedAttestation<T> {
         // The signature of attestation is valid.
         verify_attestation_signature(chain, &indexed_attestation, block_slot)?;
 
-        Ok(Self {
-            attestation,
-            attestation_type,
-        })
+        match chain.naive_aggregation_pool.insert(&attestation) {
+            Ok(outcome) => trace!(
+                chain.log,
+                "Stored unaggregated attestation";
+                "outcome" => format!("{:?}", outcome),
+                "index" => attestation.data.index,
+                "slot" => attestation.data.slot.as_u64(),
+            ),
+            Err(NaiveAggregationError::SlotTooLow {
+                slot,
+                lowest_permissible_slot,
+            }) => {
+                trace!(
+                    chain.log,
+                    "Refused to store unaggregated attestation";
+                    "lowest_permissible_slot" => lowest_permissible_slot.as_u64(),
+                    "slot" => slot.as_u64(),
+                );
+            }
+            Err(e) => error!(
+                    chain.log,
+                    "Failed to store unaggregated attestation";
+                    "error" => format!("{:?}", e),
+                    "index" => attestation.data.index,
+                    "slot" => attestation.data.slot.as_u64(),
+            ),
+        }
+
+        Ok(Self { attestation })
+    }
+}
+
+impl<T: BeaconChainTypes> FullyVerifiedAttestation<T> {
+    fn from_signature_verified_components(
+        attestation: Attestation<T::EthSpec>,
+        chain: &BeaconChain<T>,
+    ) -> Result<Self, Error> {
+        // There is no point in processing an attestation with an empty bitfield. Reject
+        // it immediately.
+        if attestation.aggregation_bits.num_set_bits() == 0 {
+            return Err(Error::EmptyAggregationBitfield);
+        }
+
+        let attestation_epoch = attestation.data.slot.epoch(T::EthSpec::slots_per_epoch());
+        let epoch_now = chain.epoch()?;
+        let target = attestation.data.target.clone();
+
+        // Attestation must be from the current or previous epoch.
+        if attestation_epoch > epoch_now {
+            return Err(Error::FutureEpoch {
+                attestation_epoch,
+                current_epoch: epoch_now,
+            });
+        } else if attestation_epoch + 1 < epoch_now {
+            return Err(Error::PastEpoch {
+                attestation_epoch,
+                current_epoch: epoch_now,
+            });
+        }
+
+        if target.epoch != attestation.data.slot.epoch(T::EthSpec::slots_per_epoch()) {
+            return Err(Error::BadTargetEpoch);
+        }
+
+        // Load the slot and state root for `attestation.data.beacon_block_root`.
+        //
+        // This indirectly checks to see if the `attestation.data.beacon_block_root` is in our fork
+        // choice. Any known, non-finalized block should be in fork choice, so this check
+        // immediately filters out attestations that attest to a block that has not been processed.
+        //
+        // Attestations must be for a known block. If the block is unknown, we simply drop the
+        // attestation and do not delay consideration for later.
+        let block_slot = if let Some((slot, _state_root)) = chain
+            .fork_choice
+            .block_slot_and_state_root(&attestation.data.beacon_block_root)
+        {
+            slot
+        } else {
+            return Err(Error::UnknownHeadBlock {
+                beacon_block_root: attestation.data.beacon_block_root,
+            });
+        };
+
+        // TODO: currently we do not check the FFG source/target. This is what the spec dictates
+        // but it seems wrong.
+        //
+        // I have opened an issue on the specs repo for this:
+        //
+        // https://github.com/ethereum/eth2.0-specs/issues/1636
+        //
+        // We should revisit this code once that issue has been resolved.
+
+        // Attestations must not be for blocks in the future. If this is the case, the attestation
+        // should not be considered.
+        if block_slot > attestation.data.slot {
+            return Err(Error::AttestsToFutureBlock {
+                block: block_slot,
+                attestation: attestation.data.slot,
+            });
+        }
+
+        Ok(Self { attestation })
     }
 }
 
