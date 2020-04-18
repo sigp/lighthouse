18d85eca03efb096bbdd0df39ec783de40c52579
Age Manning <Age@AgeManning.com>
2020-04-18 10:05:47 +1000

    Correct port issues with tests

--- beacon_node/eth2-libp2p/tests/gossipsub_tests.rs
+++ beacon_node/eth2-libp2p/tests/gossipsub_tests.rs
@@ -25,8 +25,7 @@ fn test_gossipsub_forward() {
     let log = common::build_log(Level::Info, false);
 
     let num_nodes = 20;
-    let base_port = 54236;
-    let mut nodes = common::build_linear(log.clone(), num_nodes, Some(base_port));
+    let mut nodes = common::build_linear(log.clone(), num_nodes);
     let mut received_count = 0;
     let spec = E::default_spec();
     let empty_block = BeaconBlock::empty(&spec);
@@ -99,8 +98,7 @@ fn test_gossipsub_full_mesh_publish() {
     // as nodes may get pruned out of the mesh before the gossipsub message
     // is published to them.
     let num_nodes = 12;
-    let base_port = 5264;
-    let mut nodes = common::build_full_mesh(log, num_nodes, Some(base_port));
+    let mut nodes = common::build_full_mesh(log, num_nodes);
     let mut publishing_node = nodes.pop().unwrap();
     let spec = E::default_spec();
     let empty_block = BeaconBlock::empty(&spec);
