ce8786e470dd164de622a6ecca9176d9779435fc
Paul Hauner <paul@paulhauner.com>
2020-04-23 14:36:41 +1000

    Add gossip attn tests

--- beacon_node/beacon_chain/tests/attestation_tests.rs
+++ beacon_node/beacon_chain/tests/attestation_tests.rs
@@ -1,19 +1,21 @@
 // #![cfg(not(debug_assertions))]
 
-/*
 #[macro_use]
 extern crate lazy_static;
 
-use beacon_chain::test_utils::{
-    AttestationError, AttestationStrategy, BeaconChain, BeaconChainHarness, BlockStrategy,
-    HarnessType,
+use beacon_chain::AttestationProcessingOutcome;
+use beacon_chain::{
+    attestation_verification::{Error as AttnError, VerifiedUnaggregatedAttestation},
+    test_utils::{AttestationStrategy, BeaconChainHarness, BlockStrategy, HarnessType},
+    BeaconChain,
 };
-use beacon_chain::{AttestationProcessingOutcome, AttestationType};
 use state_processing::per_slot_processing;
 use types::{
     test_utils::generate_deterministic_keypair, AggregateSignature, Attestation, BitList, EthSpec,
-    Hash256, Keypair, MainnetEthSpec, Signature,
+    Hash256, Keypair, MainnetEthSpec, Signature, Slot,
 };
+
+pub type E = MainnetEthSpec;
 
 pub const VALIDATOR_COUNT: usize = 128;
 
@@ -22,7 +24,7 @@ lazy_static! {
     static ref KEYPAIRS: Vec<Keypair> = types::test_utils::generate_deterministic_keypairs(VALIDATOR_COUNT);
 }
 
-fn get_harness(validator_count: usize) -> BeaconChainHarness<HarnessType<MainnetEthSpec>> {
+fn get_harness(validator_count: usize) -> BeaconChainHarness<HarnessType<E>> {
     let harness = BeaconChainHarness::new(MainnetEthSpec, KEYPAIRS[0..validator_count].to_vec());
 
     harness.advance_slot();
@@ -30,43 +32,228 @@ fn get_harness(validator_count: usize) -> BeaconChainHarness<HarnessType<Mainnet
     harness
 }
 
-fn process_gossip_attestation<T: BeaconChainTypes>(
-    chain: &BeaconChain<T>,
-    attestation: &Attestation<T::EthSpec>,
-) -> Result<(), AttestationError> {
-    let gossip_attestation = chain.verify_unaggregated_attestation_for_gossip()?;
-    let fc_attestation =
-}
-
+/// This test is "general" because it tests several things to avoid the overhead of spinning up
+/// lots of beacon chain harnesses.
 #[test]
-fn attestation_validity() {
+fn unaggregated_gossip_verification() {
     let harness = get_harness(VALIDATOR_COUNT);
     let chain = &harness.chain;
 
     // Extend the chain out a few epochs so we have some chain depth to play with.
     harness.extend_chain(
-        MainnetEthSpec::slots_per_epoch() as usize * 3 + 1,
+        MainnetEthSpec::slots_per_epoch() as usize * 3 - 1,
         BlockStrategy::OnCanonicalHead,
         AttestationStrategy::AllValidators,
     );
+
+    // Advance into a slot where there have not been blocks or attestations produced.
+    harness.advance_slot();
 
     let head = chain.head().expect("should get head");
     let current_slot = chain.slot().expect("should get slot");
     let current_epoch = chain.epoch().expect("should get epoch");
 
-    let valid_attestation = harness
-        .get_unaggregated_attestations(
-            &AttestationStrategy::AllValidators,
-            &head.beacon_state,
-            head.beacon_block_root,
-            head.beacon_block.slot(),
+    assert_eq!(
+        current_slot % E::slots_per_epoch(),
+        0,
+        "the test requires a new epoch to avoid already-seen errors"
+    );
+
+    let mut valid_attestation = harness
+        .chain
+        .produce_unaggregated_attestation(current_slot, 0)
+        .expect("should not error while producing attestation");
+
+    let validator_committee_index = 0;
+    let validator_index = *head
+        .beacon_state
+        .get_beacon_committee(current_slot, valid_attestation.data.index)
+        .expect("should get committees")
+        .committee
+        .get(validator_committee_index)
+        .expect("there should be an attesting validator");
+
+    let validator_sk = generate_deterministic_keypair(validator_index).sk;
+
+    valid_attestation
+        .sign(
+            &validator_sk,
+            validator_committee_index,
+            &head.beacon_state.fork,
+            harness.chain.genesis_validators_root,
+            &harness.chain.spec,
         )
-        .first()
-        .cloned()
-        .expect("should get at least one attestation");
+        .expect("should sign attestation");
+
+    macro_rules! assert_invalid {
+        ($desc: tt, $attn_getter: expr, $error: expr) => {
+            assert_eq!(
+                harness
+                    .chain
+                    .verify_unaggregated_attestation_for_gossip($attn_getter)
+                    .err()
+                    .expect(&format!(
+                        "{} should error during VerifiedUnaggregatedAttestation::verify",
+                        $desc
+                    )),
+                $error,
+                "case: {}",
+                $desc,
+            );
+        };
+    }
+
+    let future_slot = current_slot + 1;
+    assert_invalid!(
+        "attestation from future slot",
+        {
+            let mut a = valid_attestation.clone();
+            a.data.slot = future_slot;
+            a
+        },
+        AttnError::FutureSlot {
+            attestation_slot: future_slot,
+            latest_permissible_slot: current_slot,
+        }
+    );
+
+    let early_slot = current_slot
+        .as_u64()
+        .checked_sub(E::slots_per_epoch() + 2)
+        .expect("chain is not sufficiently deep for test")
+        .into();
+    assert_invalid!(
+        "attestation from past slot",
+        {
+            let mut a = valid_attestation.clone();
+            a.data.slot = early_slot;
+            a
+        },
+        AttnError::PastSlot {
+            attestation_slot: early_slot,
+            // Subtract an additional slot since the harness will be exactly on the start of the
+            // slot and the propagation tolerance will allow an extra slot.
+            earliest_permissible_slot: current_slot - E::slots_per_epoch() - 1,
+        }
+    );
+
+    assert_invalid!(
+        "attestation without any aggregation bits set",
+        {
+            let mut a = valid_attestation.clone();
+            a.aggregation_bits
+                .set(validator_committee_index, false)
+                .expect("should unset aggregation bit");
+            assert_eq!(
+                a.aggregation_bits.num_set_bits(),
+                0,
+                "test requires no set bits"
+            );
+            a
+        },
+        AttnError::NotExactlyOneAggregationBitSet(0)
+    );
+
+    assert_invalid!(
+        "attestation with two aggregation bits set",
+        {
+            let mut a = valid_attestation.clone();
+            a.aggregation_bits
+                .set(validator_committee_index + 1, true)
+                .expect("should set second aggregation bit");
+            a
+        },
+        AttnError::NotExactlyOneAggregationBitSet(2)
+    );
+
+    let awkward_root = Hash256::from_low_u64_le(424242); // No one wants one of these
+    assert_invalid!(
+        "attestation with unknown head block",
+        {
+            let mut a = valid_attestation.clone();
+            a.data.beacon_block_root = awkward_root;
+            a
+        },
+        AttnError::UnknownHeadBlock {
+            beacon_block_root: awkward_root
+        }
+    );
+
+    let awkward_root = Hash256::from_low_u64_le(424242); // No one wants one of these
+    assert_invalid!(
+        "attestation with unknown head block",
+        {
+            let mut a = valid_attestation.clone();
+            a.data.beacon_block_root = awkward_root;
+            a
+        },
+        AttnError::UnknownHeadBlock {
+            beacon_block_root: awkward_root
+        }
+    );
+
+    assert_invalid!(
+        "attestation with bad signature",
+        {
+            let mut a = valid_attestation.clone();
+
+            let mut agg_sig = AggregateSignature::new();
+            agg_sig.add(&Signature::new(&[42, 42], &validator_sk));
+            a.signature = agg_sig;
+
+            a
+        },
+        AttnError::InvalidSignature
+    );
+
+    assert!(
+        harness
+            .chain
+            .verify_unaggregated_attestation_for_gossip(valid_attestation.clone())
+            .is_ok(),
+        "valid attestation should be verified"
+    );
+
+    assert_invalid!(
+        "attestation that has already been seen",
+        valid_attestation.clone(),
+        AttnError::PriorAttestationKnown {
+            validator_index: validator_index as u64,
+            epoch: current_epoch
+        }
+    );
+
+    // TODO: double check first valid attestation thing.
+
+    /*
+    assert_invalid(
+        &harness,
+        || invalidate_as_future_slot(valid_attestation.clone(), current_slot),
+        AttnError::FutureSlot {
+            attestation_slot: current_slot + 1,
+            latest_permissible_slot: current_slot,
+        },
+        "future slot",
+    );
 
     assert_eq!(
-        chain.process_attestation(valid_attestation.clone(), AttestationType::Aggregated),
+        try_verify(&harness, || invalidate_as_future_slot(
+            valid_attestation.clone(),
+            current_slot
+        ))
+        .err()
+        .expect("should error"),
+        AttnError::FutureSlot {
+            attestation_slot: current_slot + 1,
+            latest_permissible_slot: current_slot
+        },
+        "should refuse future slot"
+    );
+    */
+
+    /*
+    assert_eq!(
+        chain.process_attestation(valid_attestation.clone()),
         Ok(AttestationProcessingOutcome::Processed),
         "should accept valid attestation"
     );
@@ -81,7 +268,7 @@ fn attestation_validity() {
     assert_eq!(
         harness
             .chain
-            .process_attestation(epoch_mismatch_attestation, AttestationType::Aggregated),
+            .process_attestation(epoch_mismatch_attestation),
         Ok(AttestationProcessingOutcome::BadTargetEpoch),
         "should not accept attestation where the slot is not in the same epoch as the target"
     );
@@ -95,9 +282,7 @@ fn attestation_validity() {
     early_attestation.data.slot = (current_epoch + 1).start_slot(MainnetEthSpec::slots_per_epoch());
 
     assert_eq!(
-        harness
-            .chain
-            .process_attestation(early_attestation, AttestationType::Aggregated),
+        harness.chain.process_attestation(early_attestation),
         Ok(AttestationProcessingOutcome::FutureEpoch {
             attestation_epoch: current_epoch + 1,
             current_epoch
@@ -130,9 +315,7 @@ fn attestation_validity() {
         .expect("should get at least one late attestation");
 
     assert_eq!(
-        harness
-            .chain
-            .process_attestation(late_attestation, AttestationType::Aggregated),
+        harness.chain.process_attestation(late_attestation),
         Ok(AttestationProcessingOutcome::PastEpoch {
             attestation_epoch: current_epoch - 2,
             current_epoch
@@ -148,9 +331,7 @@ fn attestation_validity() {
     bad_target_attestation.data.target.root = Hash256::from_low_u64_be(42);
 
     assert_eq!(
-        harness
-            .chain
-            .process_attestation(bad_target_attestation, AttestationType::Aggregated),
+        harness.chain.process_attestation(bad_target_attestation),
         Ok(AttestationProcessingOutcome::UnknownTargetRoot(
             Hash256::from_low_u64_be(42)
         )),
@@ -165,9 +346,7 @@ fn attestation_validity() {
     future_block_attestation.data.slot -= 1;
 
     assert_eq!(
-        harness
-            .chain
-            .process_attestation(future_block_attestation, AttestationType::Aggregated),
+        harness.chain.process_attestation(future_block_attestation),
         Ok(AttestationProcessingOutcome::AttestsToFutureBlock {
             block: current_slot,
             attestation: current_slot - 1
@@ -183,9 +362,7 @@ fn attestation_validity() {
     bad_head_attestation.data.beacon_block_root = Hash256::from_low_u64_be(42);
 
     assert_eq!(
-        harness
-            .chain
-            .process_attestation(bad_head_attestation, AttestationType::Aggregated),
+        harness.chain.process_attestation(bad_head_attestation),
         Ok(AttestationProcessingOutcome::UnknownHeadBlock {
             beacon_block_root: Hash256::from_low_u64_be(42)
         }),
@@ -203,9 +380,7 @@ fn attestation_validity() {
     bad_signature_attestation.signature = agg_sig;
 
     assert_eq!(
-        harness
-            .chain
-            .process_attestation(bad_signature_attestation, AttestationType::Aggregated),
+        harness.chain.process_attestation(bad_signature_attestation),
         Ok(AttestationProcessingOutcome::InvalidSignature),
         "should not accept bad_signature attestation"
     );
@@ -221,12 +396,14 @@ fn attestation_validity() {
     assert_eq!(
         harness
             .chain
-            .process_attestation(empty_bitfield_attestation, AttestationType::Aggregated),
+            .process_attestation(empty_bitfield_attestation),
         Ok(AttestationProcessingOutcome::EmptyAggregationBitfield),
         "should not accept empty_bitfield attestation"
     );
+    */
 }
 
+/*
 #[test]
 fn attestation_that_skips_epochs() {
     let harness = get_harness(VALIDATOR_COUNT);
@@ -269,9 +446,7 @@ fn attestation_that_skips_epochs() {
         .expect("should get at least one attestation");
 
     assert_eq!(
-        harness
-            .chain
-            .process_attestation(attestation, AttestationType::Aggregated),
+        harness.chain.process_attestation(attestation),
         Ok(AttestationProcessingOutcome::Processed),
         "should process attestation that skips slots"
     );
