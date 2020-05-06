f5ccaa2b62995af8808055c9a128cad8f5fc6800
Paul Hauner <paul@paulhauner.com>
2020-05-06 18:21:27 +1000

    Move key derivation into own crate

--- eth2/utils/eth2_keystore/src/lib.rs
+++ eth2/utils/eth2_keystore/src/lib.rs
@@ -3,14 +3,11 @@
 
 mod derived_key;
 mod keystore;
-mod lamport_secret_key;
 mod password;
-mod path;
 mod plain_text;
 
 pub mod json_keystore;
 
 pub use keystore::{decrypt, encrypt, Error, Keystore, KeystoreBuilder};
 pub use password::Password;
-pub use path::MasterKey;
 pub use uuid::Uuid;
