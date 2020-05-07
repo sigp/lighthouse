aed5ca6581a6fdd8187e12ae5dad9434f9acc3c7
Paul Hauner <paul@paulhauner.com>
2020-05-07 15:55:30 +1000

    Tidy

--- eth2/utils/eth2_key_derivation/src/lamport_secret_key.rs
+++ eth2/utils/eth2_key_derivation/src/lamport_secret_key.rs
@@ -2,6 +2,9 @@ use crate::derived_key::{HASH_SIZE, LAMPORT_ARRAY_SIZE};
 use std::iter::Iterator;
 use zeroize::Zeroize;
 
+/// A Lamport secret key as specified in [EIP-2333](https://eips.ethereum.org/EIPS/eip-2333).
+///
+/// Implements `Zeroize` on `Drop`.
 #[derive(Zeroize)]
 #[zeroize(drop)]
 pub struct LamportSecretKey(Vec<[u8; HASH_SIZE]>);
@@ -35,10 +38,12 @@ impl LamportSecretKey {
         this
     }
 
+    /// Returns a reference to the `i`th `HASH_SIZE` chunk of `self`.
     pub fn get_mut_chunk(&mut self, i: u8) -> &mut [u8] {
         &mut self.0[i as usize]
     }
 
+    /// Returns an iterator over `LAMPORT_ARRAY_SIZE` chunks of `HASH_SIZE` bytes.
     pub fn iter_chunks(&self) -> impl Iterator<Item = &[u8; HASH_SIZE]> {
         self.0.iter()
     }
