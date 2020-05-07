b02951e7340e9122af629b08f158c7c59e16c0c8
Paul Hauner <paul@paulhauner.com>
2020-05-07 16:13:17 +1000

    Expose json_keystore mod

--- eth2/utils/eth2_keystore/src/lib.rs
+++ eth2/utils/eth2_keystore/src/lib.rs
@@ -2,10 +2,11 @@
 //! [EIP-2335](https://eips.ethereum.org/EIPS/eip-2335).
 
 mod derived_key;
-mod json_keystore;
 mod keystore;
 mod password;
 mod plain_text;
+
+pub mod json_keystore;
 
 pub use keystore::{Error, Keystore, KeystoreBuilder};
 pub use password::Password;
