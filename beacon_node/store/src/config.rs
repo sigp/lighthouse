67a6d81f972b47d988db99f6aa05dc0286392cb5
Thor Kamphefner <thor314@gmail.com>
2020-05-08 11:40:26 +0200

    removed state-cache-size related lines from store/src/config.rs

--- beacon_node/store/src/config.rs
+++ beacon_node/store/src/config.rs
@@ -3,7 +3,6 @@ use types::{EthSpec, MinimalEthSpec};
 
 pub const DEFAULT_SLOTS_PER_RESTORE_POINT: u64 = 2048;
 pub const DEFAULT_BLOCK_CACHE_SIZE: usize = 5;
-pub const DEFAULT_STATE_CACHE_SIZE: usize = 5;
 
 /// Database configuration parameters.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -12,8 +11,6 @@ pub struct StoreConfig {
     pub slots_per_restore_point: u64,
     /// Maximum number of blocks to store in the in-memory block cache.
     pub block_cache_size: usize,
-    /// Maximum number of states to store in the in-memory state cache.
-    pub state_cache_size: usize,
 }
 
 impl Default for StoreConfig {
@@ -22,7 +19,6 @@ impl Default for StoreConfig {
             // Safe default for tests, shouldn't ever be read by a CLI node.
             slots_per_restore_point: MinimalEthSpec::slots_per_historical_root() as u64,
             block_cache_size: DEFAULT_BLOCK_CACHE_SIZE,
-            state_cache_size: DEFAULT_STATE_CACHE_SIZE,
         }
     }
 }
