acc52004a86580bcc26e3ce93ad813f1a68358f5
Paul Hauner <paul@paulhauner.com>
2020-05-12 08:47:15 +1000

    Shift LockedWallet into own file

--- eth2/utils/eth2_wallet_manager/src/wallet_manager.rs
+++ eth2/utils/eth2_wallet_manager/src/wallet_manager.rs
@@ -1,14 +1,13 @@
-use crate::filesystem::{create, read, update, Error as FilesystemError};
-use eth2_wallet::{
-    bip39::Mnemonic, Error as WalletError, Uuid, ValidatorKeystores, Wallet, WalletBuilder,
+use crate::{
+    filesystem::{create, Error as FilesystemError},
+    LockedWallet,
 };
+use eth2_wallet::{bip39::Mnemonic, Error as WalletError, Uuid, Wallet, WalletBuilder};
 use std::collections::HashMap;
 use std::ffi::OsString;
-use std::fs::{create_dir_all, read_dir, remove_file, File, OpenOptions};
+use std::fs::{create_dir_all, read_dir, OpenOptions};
 use std::io;
 use std::path::{Path, PathBuf};
-
-const LOCK_FILE: &str = ".lock";
 
 #[derive(Debug)]
 pub enum Error {
@@ -52,104 +51,6 @@ impl From<FilesystemError> for Error {
 pub enum WalletType {
     /// Hierarchical-deterministic.
     Hd,
-}
-
-/// Represents a `Wallet` in a `wallet_dir`.
-///
-/// For example:
-///
-/// ```ignore
-/// <wallet_dir>
-/// └── .lock
-/// └── <wallet-json>
-/// ```
-///
-/// Provides the following functionality:
-///
-/// - Control over the `.lock` file to prevent concurrent access.
-/// - A `next_validator` function which wraps `Wallet::next_validator`, ensuring that the wallet is
-///     persisted to disk (as JSON) between each consecutive call.
-pub struct LockedWallet {
-    wallet_dir: PathBuf,
-    wallet: Wallet,
-}
-
-impl LockedWallet {
-    /// Opens a wallet with the `uuid` from a `base_dir`.
-    ///
-    /// ```ignore
-    /// <base-dir>
-    /// ├── <uuid (directory)>
-    ///     └── <uuid (json file)>
-    /// ```
-    ///
-    /// ## Errors
-    ///
-    /// - If the wallet does not exist.
-    /// - There is file-system or parsing error.
-    /// - The lock-file already exists.
-    fn open<P: AsRef<Path>>(base_dir: P, uuid: &Uuid) -> Result<Self, Error> {
-        let wallet_dir = base_dir.as_ref().join(format!("{}", uuid));
-
-        if !wallet_dir.exists() {
-            return Err(Error::MissingWalletDir(wallet_dir));
-        }
-
-        let lockfile = wallet_dir.join(LOCK_FILE);
-        if lockfile.exists() {
-            return Err(Error::WalletIsLocked(wallet_dir));
-        } else {
-            File::create(lockfile).map_err(Error::UnableToCreateLockfile)?;
-        }
-
-        Ok(Self {
-            wallet: read(&wallet_dir, uuid)?,
-            wallet_dir,
-        })
-    }
-
-    /// Returns a reference to the underlying wallet.
-    ///
-    /// Note: this does not read from the file-system on each call. It assumes that the wallet does
-    /// not change due to the use of a lock-file.
-    pub fn wallet(&self) -> &Wallet {
-        &self.wallet
-    }
-
-    /// Calls `Wallet::next_validator` on the underlying `wallet`.
-    ///
-    /// Ensures that the wallet JSON file is updated after each call.
-    ///
-    /// ## Errors
-    ///
-    /// - If there is an error generating the validator keys.
-    /// - If there is a file-system error.
-    pub fn next_validator(
-        &mut self,
-        wallet_password: &[u8],
-        voting_keystore_password: &[u8],
-        withdrawal_keystore_password: &[u8],
-    ) -> Result<ValidatorKeystores, Error> {
-        let keystores = self.wallet.next_validator(
-            wallet_password,
-            voting_keystore_password,
-            withdrawal_keystore_password,
-        )?;
-
-        update(&self.wallet_dir, &self.wallet)?;
-
-        Ok(keystores)
-    }
-}
-
-impl Drop for LockedWallet {
-    /// Clean-up the lockfile.
-    fn drop(&mut self) {
-        let lockfile = self.wallet_dir.clone().join(LOCK_FILE);
-        if let Err(e) = remove_file(&lockfile) {
-            panic!("Unable to remove {:?}: {:?}", lockfile, e);
-        }
-    }
 }
 
 /// Manages a directory containing EIP-2386 wallets.
@@ -290,6 +191,7 @@ impl WalletManager {
 #[cfg(not(debug_assertions))]
 mod tests {
     use super::*;
+    use crate::{filesystem::read, locked_wallet::LOCK_FILE};
     use eth2_wallet::bip39::{Language, Mnemonic};
     use tempfile::tempdir;
 
@@ -312,15 +214,15 @@ mod tests {
             .expect("should create wallet");
 
         assert!(
-            wallet_dir_path(&mgr.dir, wallet.wallet.uuid()).exists(),
+            wallet_dir_path(&mgr.dir, wallet.wallet().uuid()).exists(),
             "should have created wallet dir"
         );
         assert!(
-            json_path(&mgr.dir, wallet.wallet.uuid()).exists(),
+            json_path(&mgr.dir, wallet.wallet().uuid()).exists(),
             "should have created json file"
         );
         assert!(
-            lockfile_path(&mgr.dir, wallet.wallet.uuid()).exists(),
+            lockfile_path(&mgr.dir, wallet.wallet().uuid()).exists(),
             "should have created lockfile"
         );
 
