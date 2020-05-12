4703ece497dfd1ef86e6599b33ded597cd128a8e
Age Manning <Age@AgeManning.com>
2020-05-12 15:28:21 +1000

    Clean up RPC protocol

--- beacon_node/eth2-libp2p/src/rpc/protocol.rs
+++ beacon_node/eth2-libp2p/src/rpc/protocol.rs
@@ -469,35 +469,3 @@ impl<TSpec: EthSpec> std::fmt::Display for RPCRequest<TSpec> {
         }
     }
 }
-
-/*
-/// Converts a futures AsyncRead + AsyncWrite object to a tokio::AsyncRead + tokio::AsyncWrite
-/// object.
-struct TokioNegotiatedStream<T: AsyncRead + AsyncWrite + Unpin>(T);
-
-impl<T: AsyncRead + AsyncWrite + Unpin> tokio::io::AsyncRead for TokioNegotiatedStream<T> {
-    fn poll_read(
-        self: Pin<&mut Self>,
-        cx: &mut Context,
-        buf: &mut [u8],
-    ) -> Poll<Result<usize, io::Error>> {
-        Pin::new(&mut self.0).poll_read(cx, buf)
-    }
-}
-
-impl<T: AsyncRead + AsyncWrite + Unpin> tokio::io::AsyncWrite for TokioNegotiatedStream<T> {
-    fn poll_write(
-        self: Pin<&mut Self>,
-        cx: &mut Context,
-        buf: &[u8],
-    ) -> Poll<Result<usize, io::Error>> {
-        Pin::new(&mut self.0).poll_write(cx, buf)
-    }
-    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), io::Error>> {
-        Pin::new(&mut self.0).poll_flush(cx)
-    }
-    fn poll_shutdown(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), io::Error>> {
-        Pin::new(&mut self.0).poll_close(cx)
-    }
-}
-*/
