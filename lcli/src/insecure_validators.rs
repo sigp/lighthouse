aad286bdac1d129fcec805e6f2d756bc37af9c0c
Paul Hauner <paul@paulhauner.com>
2020-05-22 15:22:40 +1000

    Remove checks for existing validators

--- lcli/src/insecure_validators.rs
+++ lcli/src/insecure_validators.rs
@@ -1,7 +1,7 @@
 use clap::ArgMatches;
 use std::fs;
 use std::path::PathBuf;
-use validator_dir::{Builder as ValidatorBuilder, Manager as ValidatorManager};
+use validator_dir::Builder as ValidatorBuilder;
 
 pub fn run(matches: &ArgMatches) -> Result<(), String> {
     let validator_count: usize = clap_utils::parse_required(matches, "count")?;
@@ -18,27 +18,8 @@ pub fn run(matches: &ArgMatches) -> Result<(), String> {
             .map_err(|e| format!("Unable to create secrets dir: {:?}", e))?;
     }
 
-    let mgr = ValidatorManager::open(&validators_dir)
-        .map_err(|e| format!("Unable to create validator manager: {:?}", e))?;
-
-    let num_existing_validators = mgr
-        .directory_names()
-        .map_err(|e| format!("Unable to list existing validators: {:?}", e))?
-        .len();
-
-    let n = validator_count.saturating_sub(num_existing_validators);
-
-    eprintln!(
-        "Requested {} validators when {} already exist. Creating {}.",
-        validator_count, num_existing_validators, n
-    );
-
-    // Second, write all the builders to file.
-    //
-    // Doing this part separate to the concurrent generation ensures that the validators are
-    // written to disk in order. This gives more confidence when assuming that if there are `n`
-    // validators in a directory then those are validators `0..n`, not some random assortment.
-    for i in num_existing_validators..validator_count {
+    // Note: only the difference between the number of existing validators is
+    for i in 0..validator_count {
         println!("Validator {}/{}", i + 1, validator_count);
 
         ValidatorBuilder::new(validators_dir.clone(), secrets_dir.clone())
