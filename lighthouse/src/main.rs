01fdcf93ca0d0431965d4309ac1830600d1b3e4a
Age Manning <Age@AgeManning.com>
2020-05-26 14:10:05 +1000

    Drop runtime after task completion

--- lighthouse/src/main.rs
+++ lighthouse/src/main.rs
@@ -241,5 +241,9 @@ fn run<E: EthSpec>(
     drop(validator_client);
 
     // Shutdown the environment once all tasks have completed.
-    Ok(environment.shutdown_on_idle())
+    // Due to a bug in tokio: https://github.com/tokio-rs/tokio/issues/2314
+    // the shutdown on idle wait until the timeout. For the time-being, we shutdown as soon as all
+    // threads have completed, by dropping the runtime.
+    //Ok(environment.shutdown_on_idle())
+    Ok(())
 }
