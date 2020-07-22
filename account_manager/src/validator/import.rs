eedc834cab743f8f03bf26154ca576da92006f98
Paul Hauner <paul@paulhauner.com>
2020-07-22 12:04:13 +1000

    Add password reading prompts

--- account_manager/src/validator/import.rs
+++ account_manager/src/validator/import.rs
@@ -1,10 +1,17 @@
 use crate::{common::ensure_dir_exists, VALIDATOR_DIR_FLAG};
-use account_utils::validator_definitions::{
-    recursively_find_voting_keystores, ValidatorDefinitions, CONFIG_FILENAME,
+use account_utils::{
+    eth2_keystore::Keystore,
+    validator_definitions::{
+        recursively_find_voting_keystores, ValidatorDefinitions, CONFIG_FILENAME,
+    },
+    ZeroizeString,
 };
 use clap::{App, Arg, ArgMatches};
 use environment::Environment;
+use std::io::{self, BufRead, Stdin};
 use std::path::PathBuf;
+use std::thread::sleep;
+use std::time::Duration;
 use types::EthSpec;
 
 pub const CMD: &str = "import";
@@ -60,13 +67,14 @@ pub fn cli_run(matches: &ArgMatches) -> Result<(), String> {
         VALIDATOR_DIR_FLAG,
         PathBuf::new().join(".lighthouse").join("validators"),
     )?;
+    let stdin = io::stdin();
 
     ensure_dir_exists(&validator_dir)?;
 
     let defs = ValidatorDefinitions::open_or_create(&validator_dir)
         .map_err(|e| format!("Unable to open {}: {:?}", CONFIG_FILENAME, e))?;
 
-    let keystores = match (keystore, keystores_dir) {
+    let keystore_paths = match (keystore, keystores_dir) {
         (Some(keystore), None) => vec![keystore],
         (None, Some(keystores_dir)) => {
             let mut keystores = vec![];
@@ -89,8 +97,58 @@ pub fn cli_run(matches: &ArgMatches) -> Result<(), String> {
         }
     };
 
-    for keystore in keystores {
-        dbg!(keystore);
+    for keystore_path in &keystore_paths {
+        let keystore = Keystore::from_json_file(keystore_path)
+            .map_err(|e| format!("Unable to read keystore JSON {:?}: {:?}", keystore_path, e))?;
+
+        eprintln!("");
+        eprintln!("Keystore found at {:?}:", keystore_path);
+        eprintln!("");
+        eprintln!(" - Description: {}", "TODO");
+        eprintln!(" - Public key: 0x{}", keystore.pubkey());
+        eprintln!(" - UUID: {}", keystore.uuid());
+        eprintln!("");
+        eprintln!("You may enter a password so the validator is decrypted automatically");
+        eprintln!("whenever the validator client starts. Whilst this is favourable");
+        eprintln!("for validator uptime, it means that the password to the keystore");
+        eprintln!(
+            "is saved on-disk in the {} file. If you choose not to enter a password",
+            CONFIG_FILENAME
+        );
+        eprintln!("you will be prompted to enter the password each time the validator client");
+        eprintln!("starts.");
+        eprintln!("");
+        eprintln!("Enter a password, or press enter to omit a password:");
+
+        let password_opt = loop {
+            let password = stdin
+                .lock()
+                .lines()
+                .next()
+                .ok_or_else(|| "Failed to read from stdin".to_string())?
+                .map_err(|e| format!("Error reading from stdin: {}", e))
+                .map(ZeroizeString::from)?;
+
+            if password.as_ref().is_empty() {
+                eprintln!("Continuing without password.");
+                sleep(Duration::from_secs(1)); // Provides nicer UX.
+                break None;
+            }
+
+            eprintln!("");
+
+            match keystore.decrypt_keypair(password.as_ref()) {
+                Ok(_) => {
+                    eprintln!("Password is correct.");
+                    sleep(Duration::from_secs(1)); // Provides nicer UX.
+                    break Some(password);
+                }
+                Err(eth2_keystore::Error::InvalidPassword) => {
+                    eprintln!("Invalid password, try again (or press Ctrl+c to exit):");
+                }
+                Err(e) => return Err(format!("Error whilst decrypting keypair: {:?}", e)),
+            }
+        };
     }
 
     Ok(())
