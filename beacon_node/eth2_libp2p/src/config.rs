445d49fb394dbc6dea1b49dff8a9e65d06da9e25
Diva <divma@protonmail.com>
2020-08-03 17:21:24 -0500

    add explicit params to gossipsub

--- beacon_node/eth2_libp2p/src/config.rs
+++ beacon_node/eth2_libp2p/src/config.rs
@@ -96,6 +96,13 @@ impl Default for Config {
         let gs_config = GossipsubConfigBuilder::new()
             .max_transmit_size(GOSSIP_MAX_SIZE)
             .heartbeat_interval(Duration::from_millis(700))
+            .mesh_n(6)
+            .mesh_n_low(5)
+            .mesh_n_high(12)
+            .gossip_lazy(6)
+            .fanout_ttl(Duration::from_secs(60))
+            .history_length(6)
+            .history_gossip(3)
             .validate_messages() // require validation before propagation
             .validation_mode(ValidationMode::Permissive)
             // Prevent duplicates by caching messages from an epoch + 1 slot amount of time (33*12)
