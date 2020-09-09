aeb009ce2cd871eaa4f08e82561e5efaa9efa95d
Daniel Schonfeld <daniel@schonfeld.org>
2020-09-09 17:47:27 -0400

    static testnet config files and unpacking during build

--- common/eth2_testnet_config/build.rs
+++ common/eth2_testnet_config/build.rs
@@ -1,37 +1,47 @@
 //! Downloads a testnet configuration from Github.
 
-use eth2_config::{altona, medalla, Eth2NetDirectory};
+use eth2_config::{altona, medalla, Eth2NetArchiveAndDirectory};
 use handlebars::Handlebars;
 use serde_json::json;
 use std::fs::File;
 use std::io::Write;
+use zip::ZipArchive;
+use std::fs;
+use std::io;
 
-const ETH2_NET_DIRS: &[Eth2NetDirectory<'static>] = &[altona::ETH2_NET_DIR, medalla::ETH2_NET_DIR];
+const ETH2_NET_DIRS: &[Eth2NetArchiveAndDirectory<'static>] = &[altona::ETH2_NET_DIR, medalla::ETH2_NET_DIR];
 
 fn main() {
     for testnet in ETH2_NET_DIRS {
         let testnet_dir = testnet.dir();
+        let archive_fullpath = testnet.archive_fullpath();
 
-        if !testnet_dir.exists() {
-            std::fs::create_dir_all(&testnet_dir)
-                .unwrap_or_else(|_| panic!("Unable to create {:?}", testnet_dir));
-
-            match get_all_files(testnet) {
-                Ok(()) => (),
-                Err(e) => {
-                    std::fs::remove_dir_all(&testnet_dir).unwrap_or_else(|_| panic!(
-                        "{}. Failed to remove {:?}, please remove the directory manually because it may contains incomplete testnet data.",
-                        e,
-                        testnet_dir,
-                    ));
-                    panic!(e);
-                }
-            }
+        if !testnet_dir.exists() && archive_fullpath.exists() {
+            //uncompress archive and continue            
+            let archive_file = File::open(&archive_fullpath).unwrap();
+            uncompress(archive_file);
         }
     }
 }
 
-fn get_all_files(testnet: &Eth2NetDirectory<'static>) -> Result<(), String> {
+fn uncompress(archive_file: File) {
+    let mut archive = ZipArchive::new(archive_file).unwrap();
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+        let outpath = file.sanitized_name();
+
+        if let Some(p) = outpath.parent() {
+            if !p.exists() {
+                fs::create_dir_all(&p).unwrap();
+            }            
+        }
+
+        let mut outfile = File::create(&outpath).unwrap();
+        io::copy(&mut file, &mut outfile).unwrap();
+    }
+}
+
+fn get_all_files(testnet: &Eth2NetArchiveAndDirectory<'static>) -> Result<(), String> {
     get_file(testnet, "boot_enr.yaml")?;
     get_file(testnet, "config.yaml")?;
     get_file(testnet, "deploy_block.txt")?;
@@ -46,7 +56,7 @@ fn get_all_files(testnet: &Eth2NetDirectory<'static>) -> Result<(), String> {
     Ok(())
 }
 
-fn get_file(testnet: &Eth2NetDirectory, filename: &str) -> Result<(), String> {
+fn get_file(testnet: &Eth2NetArchiveAndDirectory, filename: &str) -> Result<(), String> {
     let url = Handlebars::new()
         .render_template(
             testnet.url_template,
