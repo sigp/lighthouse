0d228da767c0e2434d39a769a12a246432cadb0c
Daniel Schonfeld <daniel@schonfeld.org>
2020-09-10 13:01:47 -0400

    fmt

--- common/eth2_testnet_config/build.rs
+++ common/eth2_testnet_config/build.rs
@@ -22,43 +22,48 @@ fn main() {
                 //uncompress archive and continue
                 let archive_file = match File::open(&archive_fullpath) {
                     Ok(f) => f,
-                    Err(e) => panic!("Problem opening archive file: {}", e)
+                    Err(e) => panic!("Problem opening archive file: {}", e),
                 };
 
                 match uncompress(archive_file) {
                     Ok(_) => (),
-                    Err(e) => panic!(e)
-                };               
+                    Err(e) => panic!(e),
+                };
             } else {
-                panic!("Couldn't find testnet archive at this location: {:?}", archive_fullpath);
+                panic!(
+                    "Couldn't find testnet archive at this location: {:?}",
+                    archive_fullpath
+                );
             }
         }
     }
 }
 
 fn uncompress(archive_file: File) -> Result<(), String> {
-    let mut archive = ZipArchive::new(archive_file).map_err(|e| format!("Error with zip file: {}", e))?;
+    let mut archive =
+        ZipArchive::new(archive_file).map_err(|e| format!("Error with zip file: {}", e))?;
     for i in 0..archive.len() {
-        let mut file = archive.by_index(i)
-        .map_err(|e| format!("Error retrieving file {} inside zip: {}", i, e))?;
+        let mut file = archive
+            .by_index(i)
+            .map_err(|e| format!("Error retrieving file {} inside zip: {}", i, e))?;
 
         let outpath = file.sanitized_name();
 
         if file.name().ends_with('/') {
             fs::create_dir_all(&outpath)
-            .map_err(|e| format!("Error creating testnet directories: {}", e))?;
+                .map_err(|e| format!("Error creating testnet directories: {}", e))?;
         } else {
             if let Some(p) = outpath.parent() {
                 if !p.exists() {
                     fs::create_dir_all(&p)
-                    .map_err(|e| format!("Error creating testnet directories: {}", e))?;
+                        .map_err(|e| format!("Error creating testnet directories: {}", e))?;
                 }
             }
 
             let mut outfile = File::create(&outpath)
-            .map_err(|e| format!("Error while creating file {:?}: {}", outpath, e))?;
+                .map_err(|e| format!("Error while creating file {:?}: {}", outpath, e))?;
             io::copy(&mut file, &mut outfile)
-            .map_err(|e| format!("Error writing file {:?}: {}", outpath, e))?;
+                .map_err(|e| format!("Error writing file {:?}: {}", outpath, e))?;
         }
     }
 
