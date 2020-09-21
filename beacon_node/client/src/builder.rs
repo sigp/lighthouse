810b1c5b1d261a8412e4a4ce9d903c523f6576ac
Paul Hauner <paul@paulhauner.com>
2020-09-21 16:05:10 +1000

    Apply hotfix for inconsistent head

--- beacon_node/client/src/builder.rs
+++ beacon_node/client/src/builder.rs
@@ -713,7 +713,7 @@ where
             .ok_or_else(|| "system_time_slot_clock requires a beacon_chain_builder")?;
 
         let genesis_time = beacon_chain_builder
-            .finalized_snapshot
+            .canonical_head
             .as_ref()
             .ok_or_else(|| "system_time_slot_clock requires an initialized beacon state")?
             .beacon_state
