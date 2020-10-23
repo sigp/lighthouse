e797bf9167b3dfa5f35d0916a29a8454f85976f5
Age Manning <Age@AgeManning.com>
2020-10-23 12:48:06 +1100

    Add coloured help to subcommands

--- beacon_node/src/cli.rs
+++ beacon_node/src/cli.rs
@@ -5,6 +5,7 @@ pub fn cli_app<'a, 'b>() -> App<'a, 'b> {
         .visible_aliases(&["b", "bn", "beacon"])
         .version(crate_version!())
         .author("Sigma Prime <contact@sigmaprime.io>")
+        .setting(clap::AppSettings::ColoredHelp)
         .about("The primary component which connects to the Ethereum 2.0 P2P network and \
                 downloads, verifies and stores blocks. Provides a HTTP API for querying \
                 the beacon chain and publishing messages to the network.")
