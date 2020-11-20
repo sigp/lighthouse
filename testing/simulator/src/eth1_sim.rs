cdbe84fff7f7c51d0c1f6115f71d09ae5ad94b67
Age Manning <Age@AgeManning.com>
2020-11-20 16:50:16 +1100

    Add tokio compatibility to the simulator

--- testing/simulator/src/eth1_sim.rs
+++ testing/simulator/src/eth1_sim.rs
@@ -194,6 +194,8 @@ pub fn run_eth1_sim(matches: &ArgMatches) -> Result<(), String> {
         Ok::<(), String>(())
     };
 
-    env.runtime().block_on(main_future).unwrap();
+    env.runtime()
+        .block_on(tokio_compat_02::FutureExt::compat(main_future))
+        .unwrap();
     Ok(())
 }
