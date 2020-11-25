1dcb48389d006be685f05129f5c62e94c2fb8b2c
Paul Hauner <paul@paulhauner.com>
2020-11-25 14:51:01 +1100

    Add genesis distance, tidy

--- validator_client/src/http_metrics/metrics.rs
+++ validator_client/src/http_metrics/metrics.rs
@@ -1,9 +1,13 @@
 use super::Context;
-use crate::ProductionValidatorClient;
-use eth2::lighthouse::Health;
-use lazy_static::lazy_static;
-use lighthouse_metrics::{Encoder, TextEncoder};
+use std::time::{SystemTime, UNIX_EPOCH};
 use types::EthSpec;
+
+lazy_static::lazy_static! {
+    pub static ref GENESIS_DISTANCE: Result<Gauge> = try_create_float_gauge(
+        "vc_genesis_distance_seconds",
+        "Distance between now and genesis time"
+    );
+}
 
 pub use lighthouse_metrics::*;
 
@@ -12,6 +16,17 @@ pub fn gather_prometheus_metrics<T: EthSpec>(
 ) -> std::result::Result<String, String> {
     let mut buffer = vec![];
     let encoder = TextEncoder::new();
+
+    {
+        let shared = ctx.shared.read();
+
+        if let Some(genesis_time) = shared.genesis_time {
+            if let Some(now) = SystemTime::now().duration_since(UNIX_EPOCH).ok() {
+                let distance = now.as_secs() as f64 - genesis_time as f64;
+                set_float_gauge(&GENESIS_DISTANCE, distance);
+            }
+        }
+    }
 
     warp_utils::metrics::scrape_health_metrics();
 
