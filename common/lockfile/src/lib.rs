36cb7de57894ca5c367a0d3b1202cb59bef5d339
Michael Sproul <michael@sigmaprime.io>
2020-11-26 12:00:10 +1100

    Add lockfile tests

--- common/lockfile/src/lib.rs
+++ common/lockfile/src/lib.rs
@@ -1,6 +1,6 @@
 use fs2::FileExt;
 use std::fs::{self, File};
-use std::io;
+use std::io::{self, ErrorKind};
 use std::path::PathBuf;
 
 /// Cross-platform file lock that auto-deletes on drop.
@@ -18,15 +18,19 @@ pub struct Lockfile {
 #[derive(Debug)]
 pub enum LockfileError {
     FileLocked(PathBuf, io::Error),
-    UnableToCreateFile(io::Error),
+    IoError(PathBuf, io::Error),
+    UnableToOpenFile(PathBuf, io::Error),
 }
 
 impl Lockfile {
     /// Obtain an exclusive lock on the file at `path`, creating it if it doesn't exist.
     pub fn new(path: PathBuf) -> Result<Self, LockfileError> {
-        let file = File::create(&path).map_err(LockfileError::UnableToCreateFile)?;
-        file.try_lock_exclusive()
-            .map_err(|e| LockfileError::FileLocked(path.clone(), e))?;
+        let file =
+            File::create(&path).map_err(|e| LockfileError::UnableToOpenFile(path.clone(), e))?;
+        file.try_lock_exclusive().map_err(|e| match e.kind() {
+            ErrorKind::WouldBlock => LockfileError::FileLocked(path.clone(), e),
+            _ => LockfileError::IoError(path.clone(), e),
+        })?;
         Ok(Self { file, path })
     }
 }
@@ -34,5 +38,66 @@ impl Lockfile {
 impl Drop for Lockfile {
     fn drop(&mut self) {
         let _ = fs::remove_file(&self.path);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use tempdir::TempDir;
+
+    #[cfg(unix)]
+    use std::{fs::Permissions, os::unix::fs::PermissionsExt};
+
+    #[test]
+    fn new_lock() {
+        let temp = TempDir::new("lock_test").unwrap();
+        let path = temp.path().join("lockfile");
+
+        let _lock = Lockfile::new(path.clone()).unwrap();
+        assert!(matches!(
+            Lockfile::new(path).unwrap_err(),
+            LockfileError::FileLocked(..)
+        ));
+    }
+
+    #[test]
+    fn relock_after_drop() {
+        let temp = TempDir::new("lock_test").unwrap();
+        let path = temp.path().join("lockfile");
+
+        let lock1 = Lockfile::new(path.clone()).unwrap();
+        drop(lock1);
+        let lock2 = Lockfile::new(path.clone()).unwrap();
+        drop(lock2);
+
+        assert!(!path.exists());
+    }
+
+    #[test]
+    fn lockfile_exists() {
+        let temp = TempDir::new("lock_test").unwrap();
+        let path = temp.path().join("lockfile");
+
+        let _lockfile = File::create(&path).unwrap();
+
+        let _lock = Lockfile::new(path.clone()).unwrap();
+    }
+
+    #[test]
+    #[cfg(unix)]
+    fn permission_denied_create() {
+        let temp = TempDir::new("lock_test").unwrap();
+        let path = temp.path().join("lockfile");
+
+        let lockfile = File::create(&path).unwrap();
+        lockfile
+            .set_permissions(Permissions::from_mode(0o000))
+            .unwrap();
+
+        assert!(matches!(
+            Lockfile::new(path).unwrap_err(),
+            LockfileError::UnableToOpenFile(..)
+        ));
     }
 }
