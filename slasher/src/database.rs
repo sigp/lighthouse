sigp/lighthouse

commit 0c529b8d52f3fa65257c379b9a4453a3b2b1bc8c

Add slasher broadcast (#2079)
## Issue Addressed

Closes #2048

## Proposed Changes

* Broadcast slashings when the `--slasher-broadcast` flag is provided.
* In the process of implementing this I refactored the slasher service into its own crate so that it could access the network code without creating a circular dependency. I moved the responsibility for putting slashings into the op pool into the service as well, as it makes sense for it to handle the whole slashing lifecycle.

slasher/src/lib.rs
@@ -3,13 +3,13 @@
 mod array;
 mod attestation_queue;
 mod attester_record;
+mod batch_stats;
 mod block_queue;
 pub mod config;
 mod database;
 mod error;
-mod metrics;
+pub mod metrics;
 mod slasher;
-mod slasher_server;
 pub mod test_utils;
 mod utils;
 
@@ -20,7 +20,6 @@ pub use block_queue::BlockQueue;
 pub use config::Config;
 pub use database::SlasherDB;
 pub use error::Error;
-pub use slasher_server::SlasherServer;
 
 use types::{AttesterSlashing, EthSpec, IndexedAttestation, ProposerSlashing};
 

slasher/service/src/lib.rs
@@ -1,0 +1,3 @@
+mod service;
+
+pub use service::SlasherService;

Cargo.toml
@@ -70,6 +70,7 @@ members = [
     "remote_signer/client",
 
     "slasher",
+    "slasher/service",
 
     "testing/ef_tests",
     "testing/eth1_test_rig",

beacon_node/beacon_chain/src/beacon_chain.rs
@@ -39,7 +39,7 @@ use slot_clock::SlotClock;
 use state_processing::{
     common::get_indexed_attestation, per_block_processing,
     per_block_processing::errors::AttestationValidationError, per_slot_processing,
-    BlockSignatureStrategy, SigVerifiedOp, VerifyOperation,
+    BlockSignatureStrategy, SigVerifiedOp,
 };
 use std::borrow::Cow;
 use std::cmp::Ordering;
@@ -1125,63 +1125,6 @@ impl<T: BeaconChainTypes> BeaconChain<T> {
         Ok(signed_aggregate)
     }
 
-    /// Move slashings collected by the slasher into the op pool for block inclusion.
-    fn ingest_slashings_to_op_pool(&self, state: &BeaconState<T::EthSpec>) {
-        if let Some(slasher) = self.slasher.as_ref() {
-            let attester_slashings = slasher.get_attester_slashings();
-            let proposer_slashings = slasher.get_proposer_slashings();
-
-            if !attester_slashings.is_empty() || !proposer_slashings.is_empty() {
-                debug!(
-                    self.log,
-                    "Ingesting slashings";
-                    "num_attester_slashings" => attester_slashings.len(),
-                    "num_proposer_slashings" => proposer_slashings.len(),
-                );
-            }
-
-            for slashing in attester_slashings {
-                let verified_slashing = match slashing.clone().validate(state, &self.spec) {
-                    Ok(verified) => verified,
-                    Err(e) => {
-                        error!(
-                            self.log,
-                            "Attester slashing from slasher failed verification";
-                            "error" => format!("{:?}", e),
-                            "slashing" => format!("{:?}", slashing),
-                        );
-                        continue;
-                    }
-                };
-
-                if let Err(e) = self.import_attester_slashing(verified_slashing) {
-                    error!(
-                        self.log,
-                        "Attester slashing from slasher is invalid";
-                        "error" => format!("{:?}", e),
-                        "slashing" => format!("{:?}", slashing),
-                    );
-                }
-            }
-
-            for slashing in proposer_slashings {
-                let verified_slashing = match slashing.clone().validate(state, &self.spec) {
-                    Ok(verified) => verified,
-                    Err(e) => {
-                        error!(
-                            self.log,
-                            "Proposer slashing from slasher failed verification";
-                            "error" => format!("{:?}", e),
-                            "slashing" => format!("{:?}", slashing),
-                        );
-                        continue;
-                    }
-                };
-                self.import_proposer_slashing(verified_slashing);
-            }
-        }
-    }
-
     /// Check that the shuffling at `block_root` is equal to one of the shufflings of `state`.
     ///
     /// The `target_epoch` argument determines which shuffling to check compatibility with, it
@@ -1876,7 +1819,6 @@ impl<T: BeaconChainTypes> BeaconChain<T> {
             state.latest_block_header.canonical_root()
         };
 
-        self.ingest_slashings_to_op_pool(&state);
         let (proposer_slashings, attester_slashings) =
             self.op_pool.get_slashings(&state, &self.spec);
 
@@ -2093,7 +2035,6 @@ impl<T: BeaconChainTypes> BeaconChain<T> {
         if is_epoch_transition || is_reorg {
             self.persist_head_and_fork_choice()?;
             self.op_pool.prune_attestations(self.epoch()?);
-            self.ingest_slashings_to_op_pool(&new_head.beacon_state);
             self.persist_op_pool()?;
         }
 

beacon_node/client/Cargo.toml
@@ -43,3 +43,4 @@ directory = {path = "../../common/directory"}
 http_api = { path = "../http_api" }
 http_metrics = { path = "../http_metrics" }
 slasher = { path = "../../slasher" }
+slasher_service = { path = "../../slasher/service" }

beacon_node/client/src/builder.rs
@@ -13,7 +13,8 @@ use eth1::{Config as Eth1Config, Service as Eth1Service};
 use eth2_libp2p::NetworkGlobals;
 use genesis::{interop_genesis_state, Eth1GenesisService};
 use network::{NetworkConfig, NetworkMessage, NetworkService};
-use slasher::{Slasher, SlasherServer};
+use slasher::Slasher;
+use slasher_service::SlasherService;
 use slog::{debug, info, warn};
 use ssz::Decode;
 use std::net::TcpListener;
@@ -348,22 +349,21 @@ where
     /// Immediately start the slasher service.
     ///
     /// Error if no slasher is configured.
-    pub fn start_slasher_server(&self) -> Result<(), String> {
+    pub fn start_slasher_service(&self) -> Result<(), String> {
+        let beacon_chain = self
+            .beacon_chain
+            .clone()
+            .ok_or("slasher service requires a beacon chain")?;
+        let network_send = self
+            .network_send
+            .clone()
+            .ok_or("slasher service requires a network sender")?;
         let context = self
             .runtime_context
             .as_ref()
             .ok_or("slasher requires a runtime_context")?
-            .service_context("slasher_server_ctxt".into());
-        let slasher = self
-            .slasher
-            .clone()
-            .ok_or("slasher server requires a slasher")?;
-        let slot_clock = self
-            .slot_clock
-            .clone()
-            .ok_or("slasher server requires a slot clock")?;
-        SlasherServer::run(slasher, slot_clock, &context.executor);
-        Ok(())
+            .service_context("slasher_service_ctxt".into());
+        SlasherService::new(beacon_chain, network_send).run(&context.executor)
     }
 
     /// Immediately starts the service that periodically logs information each slot.
@@ -470,7 +470,7 @@ where
         };
 
         if self.slasher.is_some() {
-            self.start_slasher_server()?;
+            self.start_slasher_service()?;
         }
 
         Ok(Client {

beacon_node/src/cli.rs
@@ -435,6 +435,13 @@ pub fn cli_app<'a, 'b>() -> App<'a, 'b> {
                 .takes_value(true)
         )
         .arg(
+            Arg::with_name("slasher-broadcast")
+                .long("slasher-broadcast")
+                .help("Broadcast slashings found by the slasher to the rest of the network \
+                       [disabled by default].")
+                .requires("slasher")
+        )
+        .arg(
             Arg::with_name("wss-checkpoint")
                 .long("wss-checkpoint")
                 .help(

beacon_node/src/config.rs
@@ -375,6 +375,8 @@ pub fn get_config<E: EthSpec>(
             slasher_config.validator_chunk_size = validator_chunk_size;
         }
 
+        slasher_config.broadcast = cli_args.is_present("slasher-broadcast");
+
         client_config.slasher = Some(slasher_config);
     }
 

commit 43ac3f7209c17bad24356735e5ffa4ce6311d8df

Fix slasher database schema migration to v2 (#2125)
## Issue Addressed

Closes #2119

## Proposed Changes

Update the slasher schema version to v2 for the breaking changes to the config introduced in #2079. Implement a migration from v1 to v2 so that users can seamlessly upgrade from any version of Lighthouse <=1.0.5.

Users who deleted their database for v1.0.5 can upgrade to a release including this patch without any manual intervention. Similarly, any users still on v1.0.4 or earlier can now upgrade without having to drop their database.

slasher/src/database.rs
@@ -4,6 +4,7 @@ use crate::{
 };
 use byteorder::{BigEndian, ByteOrder};
 use lmdb::{Cursor, Database, DatabaseFlags, Environment, RwTransaction, Transaction, WriteFlags};
+use serde::Deserialize;
 use ssz::{Decode, Encode};
 use std::marker::PhantomData;
 use std::sync::Arc;
@@ -12,7 +13,7 @@ use types::{
 };
 
 /// Current database schema version, to check compatibility of on-disk DB with software.
-const CURRENT_SCHEMA_VERSION: u64 = 1;
+pub const CURRENT_SCHEMA_VERSION: u64 = 2;
 
 /// Metadata about the slashing database itself.
 const METADATA_DB: &str = "metadata";
@@ -209,15 +210,7 @@ impl<E: EthSpec> SlasherDB<E> {
 
         let mut txn = db.begin_rw_txn()?;
 
-        if let Some(schema_version) = db.load_schema_version(&mut txn)? {
-            if schema_version != CURRENT_SCHEMA_VERSION {
-                return Err(Error::IncompatibleSchemaVersion {
-                    database_schema_version: schema_version,
-                    software_schema_version: CURRENT_SCHEMA_VERSION,
-                });
-            }
-        }
-        db.store_schema_version(&mut txn)?;
+        db.migrate(&mut txn)?;
 
         if let Some(on_disk_config) = db.load_config(&mut txn)? {
             if !db.config.is_compatible(&on_disk_config) {
@@ -227,7 +220,7 @@ impl<E: EthSpec> SlasherDB<E> {
                 });
             }
         }
-        db.store_config(&mut txn)?;
+        db.store_config(&db.config, &mut txn)?;
         txn.commit()?;
 
         Ok(db)
@@ -263,7 +256,14 @@ impl<E: EthSpec> SlasherDB<E> {
         Ok(())
     }
 
-    pub fn load_config(&self, txn: &mut RwTransaction<'_>) -> Result<Option<Config>, Error> {
+    /// Load a config from disk.
+    ///
+    /// This is generic in order to allow loading of configs for different schema versions.
+    /// Care should be taken to ensure it is only called for `Config`-like `T`.
+    pub fn load_config<'a, T: Deserialize<'a>>(
+        &self,
+        txn: &'a mut RwTransaction<'_>,
+    ) -> Result<Option<T>, Error> {
         Ok(txn
             .get(self.metadata_db, &METADATA_CONFIG_KEY)
             .optional()?
@@ -271,11 +271,11 @@ impl<E: EthSpec> SlasherDB<E> {
             .transpose()?)
     }
 
-    pub fn store_config(&self, txn: &mut RwTransaction<'_>) -> Result<(), Error> {
+    pub fn store_config(&self, config: &Config, txn: &mut RwTransaction<'_>) -> Result<(), Error> {
         txn.put(
             self.metadata_db,
             &METADATA_CONFIG_KEY,
-            &bincode::serialize(self.config.as_ref())?,
+            &bincode::serialize(config)?,
             Self::write_flags(),
         )?;
         Ok(())

slasher/src/error.rs
@@ -25,6 +25,7 @@ pub enum Error {
         on_disk_config: Config,
         config: Config,
     },
+    ConfigMissing,
     DistanceTooLarge,
     DistanceCalculationOverflow,
     /// Missing an attester record that we expected to exist.

slasher/src/lib.rs
@@ -9,6 +9,7 @@ pub mod config;
 mod database;
 mod error;
 pub mod metrics;
+mod migrate;
 mod slasher;
 pub mod test_utils;
 mod utils;

slasher/src/migrate.rs
@@ -1,0 +1,74 @@
+use crate::{database::CURRENT_SCHEMA_VERSION, Config, Error, SlasherDB};
+use lmdb::RwTransaction;
+use serde_derive::{Deserialize, Serialize};
+use std::path::PathBuf;
+use types::EthSpec;
+
+/// Config from schema version 1, for migration to version 2+.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ConfigV1 {
+    database_path: PathBuf,
+    chunk_size: usize,
+    validator_chunk_size: usize,
+    history_length: usize,
+    update_period: u64,
+    max_db_size_mbs: usize,
+}
+
+type ConfigV2 = Config;
+
+impl Into<ConfigV2> for ConfigV1 {
+    fn into(self) -> ConfigV2 {
+        Config {
+            database_path: self.database_path,
+            chunk_size: self.chunk_size,
+            validator_chunk_size: self.validator_chunk_size,
+            history_length: self.history_length,
+            update_period: self.update_period,
+            max_db_size_mbs: self.max_db_size_mbs,
+            broadcast: false,
+        }
+    }
+}
+
+impl<E: EthSpec> SlasherDB<E> {
+    /// If the database exists, and has a schema, attempt to migrate it to the current version.
+    pub fn migrate(&self, txn: &mut RwTransaction<'_>) -> Result<(), Error> {
+        if let Some(schema_version) = self.load_schema_version(txn)? {
+            match (schema_version, CURRENT_SCHEMA_VERSION) {
+                // The migration from v1 to v2 is a bit messy because v1.0.5 silently
+                // changed the schema to v2, so a v1 schema could have either a v1 or v2
+                // config.
+                (1, 2) => {
+                    match self.load_config::<ConfigV1>(txn) {
+                        Ok(Some(config_v1)) => {
+                            // Upgrade to v2 config and store on disk.
+                            let config_v2 = config_v1.into();
+                            self.store_config(&config_v2, txn)?;
+                        }
+                        Ok(None) => {
+                            // Impossible to have schema version and no config.
+                            return Err(Error::ConfigMissing);
+                        }
+                        Err(_) => {
+                            // If loading v1 config failed, ensure loading v2 config succeeds.
+                            // No further action is needed.
+                            let _config_v2 = self.load_config::<ConfigV2>(txn)?;
+                        }
+                    }
+                }
+                (x, y) if x == y => {}
+                (_, _) => {
+                    return Err(Error::IncompatibleSchemaVersion {
+                        database_schema_version: schema_version,
+                        software_schema_version: CURRENT_SCHEMA_VERSION,
+                    });
+                }
+            }
+        }
+
+        // If the migration succeeded, update the schema version on-disk.
+        self.store_schema_version(txn)?;
+        Ok(())
+    }
+}