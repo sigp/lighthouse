28238d97b1f7812626aa9d5594faf5e283e40132
Pawan Dhananjay <pawandhananjay@gmail.com>
2021-01-13 08:09:10 +0000

    Disconnect from peers quicker on internet issues (#2147)
    ## Issue Addressed
    
    Fixes #2146 
    
    ## Proposed Changes
    
    Change ping timeout errors to return `LowToleranceErrors` so that we disconnect faster on internet failures/changes.

--- beacon_node/eth2_libp2p/src/peer_manager/mod.rs
+++ beacon_node/eth2_libp2p/src/peer_manager/mod.rs
@@ -480,7 +480,7 @@ impl<TSpec: EthSpec> PeerManager<TSpec> {
                     Protocol::Status => return,
                 },
             },
-            RPCError::NegotiationTimeout => PeerAction::HighToleranceError,
+            RPCError::NegotiationTimeout => PeerAction::LowToleranceError,
         };
 
         self.report_peer(peer_id, peer_action, ReportSource::RPC);
