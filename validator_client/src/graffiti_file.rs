b87189806268f5af6843f3be33748d2e47ab6b32
pawan <pawandhananjay@gmail.com>
2021-03-01 13:42:10 +0530

    Add test for empty graffiti

--- validator_client/src/graffiti_file.rs
+++ validator_client/src/graffiti_file.rs
@@ -81,9 +81,7 @@ impl GraffitiFile {
 fn read_line(line: &str) -> Result<(Option<PublicKey>, Graffiti), Error> {
     if let Some(i) = line.find(':') {
         let (key, value) = line.split_at(i);
-        if value.is_empty() {
-            return Err(Error::InvalidLine);
-        }
+        // Note: `value.len() >=1` so `value[1..]` is safe
         let graffiti = GraffitiString::from_str(value[1..].trim())
             .map_err(Error::InvalidGraffiti)?
             .into();
@@ -108,14 +106,17 @@ mod tests {
     const DEFAULT_GRAFFITI: &str = "lighthouse";
     const CUSTOM_GRAFFITI1: &str = "custom-graffiti1";
     const CUSTOM_GRAFFITI2: &str = "graffitiwall:720:641:#ffff00";
+    const EMPTY_GRAFFITI: &str = "";
     const PK1: &str = "0x800012708dc03f611751aad7a43a082142832b5c1aceed07ff9b543cf836381861352aa923c70eeb02018b638aa306aa";
     const PK2: &str = "0x80001866ce324de7d80ec73be15e2d064dcf121adf1b34a0d679f2b9ecbab40ce021e03bb877e1a2fe72eaaf475e6e21";
+    const PK3: &str = "0x9035d41a8bc11b08c17d0d93d876087958c9d055afe86fce558e3b988d92434769c8d50b0b463708db80c6aae1160c02";
 
     // Create a graffiti file in the required format and return a path to the file.
     fn create_graffiti_file() -> PathBuf {
         let temp = TempDir::new().unwrap();
         let pk1 = PublicKey::deserialize(&hex::decode(&PK1[2..]).unwrap()).unwrap();
         let pk2 = PublicKey::deserialize(&hex::decode(&PK2[2..]).unwrap()).unwrap();
+        let pk3 = PublicKey::deserialize(&hex::decode(&PK3[2..]).unwrap()).unwrap();
 
         let file_name = temp.into_path().join("graffiti.txt");
 
@@ -130,6 +131,9 @@ mod tests {
         graffiti_file
             .write_all(format!("{}: {}\n", pk2.to_hex_string(), CUSTOM_GRAFFITI2).as_bytes())
             .unwrap();
+        graffiti_file
+            .write_all(format!("{}:{}\n", pk3.to_hex_string(), EMPTY_GRAFFITI).as_bytes())
+            .unwrap();
         graffiti_file.flush().unwrap();
         file_name
     }
@@ -141,6 +145,7 @@ mod tests {
 
         let pk1 = PublicKey::deserialize(&hex::decode(&PK1[2..]).unwrap()).unwrap();
         let pk2 = PublicKey::deserialize(&hex::decode(&PK2[2..]).unwrap()).unwrap();
+        let pk3 = PublicKey::deserialize(&hex::decode(&PK3[2..]).unwrap()).unwrap();
 
         // Read once
         gf.read_graffiti_file().unwrap();
@@ -152,6 +157,11 @@ mod tests {
         assert_eq!(
             gf.load_graffiti(&pk2).unwrap().unwrap(),
             GraffitiString::from_str(CUSTOM_GRAFFITI2).unwrap().into()
+        );
+
+        assert_eq!(
+            gf.load_graffiti(&pk3).unwrap().unwrap(),
+            GraffitiString::from_str(EMPTY_GRAFFITI).unwrap().into()
         );
 
         // Random pk should return the default graffiti
