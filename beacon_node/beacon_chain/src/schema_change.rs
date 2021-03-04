d8e8d972181c4a1f14eb468150fab939746ad7ea
Michael Sproul <michael@sigmaprime.io>
2021-03-04 17:27:51 +1100

    Implement schema migration

--- beacon_node/beacon_chain/src/schema_change.rs
+++ beacon_node/beacon_chain/src/schema_change.rs
@@ -1,12 +1,15 @@
 //! Utilities for managing database schema changes.
 use crate::beacon_chain::BeaconChainTypes;
 use crate::validator_pubkey_cache::ValidatorPubkeyCache;
+use ssz::{Decode, Encode};
+use ssz_derive::{Decode, Encode};
 use std::fs;
 use std::path::Path;
 use std::sync::Arc;
+use store::config::OnDiskStoreConfig;
 use store::hot_cold_store::{HotColdDB, HotColdDBError};
-use store::metadata::{SchemaVersion, CURRENT_SCHEMA_VERSION};
-use store::Error as StoreError;
+use store::metadata::{SchemaVersion, CONFIG_KEY, CURRENT_SCHEMA_VERSION};
+use store::{DBColumn, Error as StoreError, ItemStore, StoreItem};
 
 const PUBKEY_CACHE_FILENAME: &str = "pubkey_cache.ssz";
 
@@ -54,11 +57,48 @@ pub fn migrate_schema<T: BeaconChainTypes>(
 
             Ok(())
         }
+        // Migration for weak subjectivity sync support and clean up of `OnDiskStoreConfig` (#1784).
+        (SchemaVersion(3), SchemaVersion(4)) => {
+            if let Some(OnDiskStoreConfigV3 {
+                slots_per_restore_point,
+                ..
+            }) = db.hot_db.get(&CONFIG_KEY)?
+            {
+                let new_config = OnDiskStoreConfig {
+                    slots_per_restore_point,
+                };
+                db.hot_db.put(&CONFIG_KEY, &new_config)?;
+            }
+
+            db.store_schema_version(to)?;
+            Ok(())
+        }
         // Anything else is an error.
         (_, _) => Err(HotColdDBError::UnsupportedSchemaVersion {
             target_version: to,
             current_version: from,
         }
         .into()),
+    }
+}
+
+// Store config used in v3 schema and earlier.
+#[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
+pub struct OnDiskStoreConfigV3 {
+    pub slots_per_restore_point: u64,
+    pub _block_cache_size: usize,
+}
+
+impl StoreItem for OnDiskStoreConfigV3 {
+    fn db_column() -> DBColumn {
+        DBColumn::BeaconMeta
+    }
+
+    fn as_store_bytes(&self) -> Vec<u8> {
+        self.as_ssz_bytes()
+    }
+
+    fn from_store_bytes(bytes: &[u8]) -> Result<Self, StoreError> {
+        Ok(Self::from_ssz_bytes(bytes)?)
     }
 }
--- beacon_node/store/src/metadata.rs
+++ beacon_node/store/src/metadata.rs
@@ -3,7 +3,7 @@ use ssz::{Decode, Encode};
 use ssz_derive::{Decode, Encode};
 use types::{Checkpoint, Hash256, Slot};
 
-pub const CURRENT_SCHEMA_VERSION: SchemaVersion = SchemaVersion(3);
+pub const CURRENT_SCHEMA_VERSION: SchemaVersion = SchemaVersion(4);
 
 // All the keys that get stored under the `BeaconMeta` column.
 //
