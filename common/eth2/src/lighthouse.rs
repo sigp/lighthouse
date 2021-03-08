97b0ba031fb9662eaccab72a2e4f66484563eb1b
pawan <pawandhananjay@gmail.com>
2021-03-08 18:13:49 +0530

    Add additional system and process metrics

--- common/eth2/src/lighthouse.rs
+++ common/eth2/src/lighthouse.rs
@@ -6,10 +6,13 @@ use crate::{
     BeaconNodeHttpClient, DepositData, Error, Eth1Data, Hash256, StateId, StatusCode,
 };
 use proto_array::core::ProtoArray;
+use psutil::cpu::os::linux::CpuTimesExt;
+use psutil::memory::os::linux::VirtualMemoryExt;
 use reqwest::IntoUrl;
 use serde::{Deserialize, Serialize};
 use ssz::Decode;
 use ssz_derive::{Decode, Encode};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 pub use eth2_libp2p::{types::SyncState, PeerInfo};
 
@@ -81,6 +84,134 @@ use {procinfo::pid, psutil::process::Process};
 /// Reports on the health of the Lighthouse instance.
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Health {
+    pub system: SystemHealth,
+    pub process: ProcessHealth,
+}
+
+/// System related health.
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct SystemHealth {
+    /// Total virtual memory on the system
+    pub sys_virt_mem_total: u64,
+    /// Total virtual memory available for new processes.
+    pub sys_virt_mem_available: u64,
+    /// Total virtual memory used on the system.
+    pub sys_virt_mem_used: u64,
+    /// Total virtual memory not used on the system.
+    pub sys_virt_mem_free: u64,
+    /// Percentage of virtual memory used on the system.
+    pub sys_virt_mem_percent: f32,
+    /// Total cached virtual memory on the system.
+    pub sys_virt_mem_cached: u64,
+    /// Total buffered virtual memory on the system.
+    pub sys_virt_mem_buffers: u64,
+
+    /// System load average over 1 minute.
+    pub sys_loadavg_1: f64,
+    /// System load average over 5 minutes.
+    pub sys_loadavg_5: f64,
+    /// System load average over 15 minutes.
+    pub sys_loadavg_15: f64,
+
+    /// Total cpu cores.
+    pub cpu_cores: u64,
+    /// Total cpu threads.
+    pub cpu_threads: u64,
+
+    /// Total time spent in kernel mode.
+    pub system_seconds_total: u64,
+    /// Total time spent in user mode.
+    pub user_seconds_total: u64,
+    /// Total time spent in waiting for io.
+    pub iowait_seconds_total: u64,
+    /// Total idle cpu time.
+    pub idle_seconds_total: u64,
+
+    /// Total capacity of disk.
+    pub disk_node_bytes_total: u64,
+    /// Free space in disk.
+    pub disk_node_bytes_free: u64,
+    /// Number of disk reads.
+    pub disk_node_reads_total: u64,
+    /// Number of disk writes.
+    pub disk_node_writes_total: u64,
+
+    /// Total bytes received over all network interfaces.
+    pub network_node_bytes_total_received: u64,
+    /// Total bytes sent over all network interfaces.
+    pub network_node_bytes_total_transmit: u64,
+
+    /// Boot time
+    misc_node_boot_ts_seconds: u64,
+    /// OS (would be linux as psutil is not supported in other operating systems).
+    misc_os: String,
+}
+
+impl SystemHealth {
+    #[cfg(not(target_os = "linux"))]
+    pub fn observe() -> Result<Self, String> {
+        Err("Health is only available on Linux".into())
+    }
+
+    #[cfg(target_os = "linux")]
+    pub fn observe() -> Result<Self, String> {
+        let vm = psutil::memory::virtual_memory()
+            .map_err(|e| format!("Unable to get virtual memory: {:?}", e))?;
+        let loadavg =
+            psutil::host::loadavg().map_err(|e| format!("Unable to get loadavg: {:?}", e))?;
+
+        let cpu =
+            psutil::cpu::cpu_times().map_err(|e| format!("Unable to get cpu times: {:?}", e))?;
+
+        let disk_usage = psutil::disk::disk_usage("/")
+            .map_err(|e| format!("Unable to disk usage info: {:?}", e))?;
+
+        let disk = psutil::disk::DiskIoCountersCollector::default()
+            .disk_io_counters()
+            .map_err(|e| format!("Unable to get disk counters: {:?}", e))?;
+
+        let net = psutil::network::NetIoCountersCollector::default()
+            .net_io_counters()
+            .map_err(|e| format!("Unable to get network io counters: {:?}", e))?;
+
+        let boot_time = psutil::host::boot_time()
+            .map_err(|e| format!("Unable to get system boot time: {:?}", e))?
+            .duration_since(UNIX_EPOCH)
+            .map_err(|e| format!("Boot time is lower than unix epoch: {}", e))?
+            .as_secs();
+
+        Ok(Self {
+            sys_virt_mem_total: vm.total(),
+            sys_virt_mem_available: vm.available(),
+            sys_virt_mem_used: vm.used(),
+            sys_virt_mem_free: vm.free(),
+            sys_virt_mem_cached: vm.cached(),
+            sys_virt_mem_buffers: vm.buffers(),
+            sys_virt_mem_percent: vm.percent(),
+            sys_loadavg_1: loadavg.one,
+            sys_loadavg_5: loadavg.five,
+            sys_loadavg_15: loadavg.fifteen,
+            cpu_cores: psutil::cpu::cpu_count_physical(),
+            cpu_threads: psutil::cpu::cpu_count(),
+            system_seconds_total: cpu.total().as_secs(),
+            user_seconds_total: cpu.user().as_secs(),
+            iowait_seconds_total: cpu.iowait().as_secs(),
+            idle_seconds_total: cpu.idle().as_secs(),
+            disk_node_bytes_total: disk_usage.total(),
+            disk_node_bytes_free: disk_usage.free(),
+            disk_node_reads_total: disk.read_count(),
+            disk_node_writes_total: disk.write_count(),
+            network_node_bytes_total_received: net.bytes_recv(),
+            network_node_bytes_total_transmit: net.bytes_sent(),
+            misc_node_boot_ts_seconds: boot_time,
+            misc_os: psutil::host::info().operating_system().as_str().to_string(),
+        })
+    }
+}
+
+/// Process specific health
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct ProcessHealth {
     /// The pid of this process.
     pub pid: u32,
     /// The number of threads used by this pid.
@@ -89,25 +220,11 @@ pub struct Health {
     pub pid_mem_resident_set_size: u64,
     /// The total virtual memory used by this pid.
     pub pid_mem_virtual_memory_size: u64,
-    /// Total virtual memory on the system
-    pub sys_virt_mem_total: u64,
-    /// Total virtual memory available for new processes.
-    pub sys_virt_mem_available: u64,
-    /// Total virtual memory used on the system
-    pub sys_virt_mem_used: u64,
-    /// Total virtual memory not used on the system
-    pub sys_virt_mem_free: u64,
-    /// Percentage of virtual memory used on the system
-    pub sys_virt_mem_percent: f32,
-    /// System load average over 1 minute.
-    pub sys_loadavg_1: f64,
-    /// System load average over 5 minutes.
-    pub sys_loadavg_5: f64,
-    /// System load average over 15 minutes.
-    pub sys_loadavg_15: f64,
+    /// Number of seconds since process started.
+    pub cpu_process_seconds_total: u64,
 }
 
-impl Health {
+impl ProcessHealth {
     #[cfg(not(target_os = "linux"))]
     pub fn observe() -> Result<Self, String> {
         Err("Health is only available on Linux".into())
@@ -124,24 +241,33 @@ impl Health {
 
         let stat = pid::stat_self().map_err(|e| format!("Unable to get stat: {:?}", e))?;
 
-        let vm = psutil::memory::virtual_memory()
-            .map_err(|e| format!("Unable to get virtual memory: {:?}", e))?;
-        let loadavg =
-            psutil::host::loadavg().map_err(|e| format!("Unable to get loadavg: {:?}", e))?;
+        let boot_time = psutil::host::boot_time()
+            .map_err(|e| format!("Unable to get system boot time: {:?}", e))?;
+        let time_since_boot = SystemTime::now()
+            .duration_since(boot_time)
+            .map_err(|e| format!("Boot time is greater than current time: {:?}", e))?;
 
         Ok(Self {
             pid: process.pid(),
             pid_num_threads: stat.num_threads,
             pid_mem_resident_set_size: process_mem.rss(),
             pid_mem_virtual_memory_size: process_mem.vms(),
-            sys_virt_mem_total: vm.total(),
-            sys_virt_mem_available: vm.available(),
-            sys_virt_mem_used: vm.used(),
-            sys_virt_mem_free: vm.free(),
-            sys_virt_mem_percent: vm.percent(),
-            sys_loadavg_1: loadavg.one,
-            sys_loadavg_5: loadavg.five,
-            sys_loadavg_15: loadavg.fifteen,
+            cpu_process_seconds_total: (time_since_boot - process.create_time()).as_secs(),
+        })
+    }
+}
+
+impl Health {
+    #[cfg(not(target_os = "linux"))]
+    pub fn observe() -> Result<Self, String> {
+        Err("Health is only available on Linux".into())
+    }
+
+    #[cfg(target_os = "linux")]
+    pub fn observe() -> Result<Self, String> {
+        Ok(Self {
+            process: ProcessHealth::observe()?,
+            system: SystemHealth::observe()?,
         })
     }
 }
