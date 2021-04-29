9ab104703dbe5c72a2d218be667bd95c10adb222
Paul Hauner <paul@paulhauner.com>
2021-04-29 13:53:15 +1000

    Don't remove zero-padding

--- consensus/serde_utils/src/hex_vec.rs
+++ consensus/serde_utils/src/hex_vec.rs
@@ -10,7 +10,7 @@ where
     S: Serializer,
 {
     let mut hex_string: String = "0x".to_string();
-    hex_string.push_str(hex::encode(&bytes).trim_start_matches('0'));
+    hex_string.push_str(&hex::encode(&bytes));
 
     serializer.serialize_str(&hex_string)
 }
