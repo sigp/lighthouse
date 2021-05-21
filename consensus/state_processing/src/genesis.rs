904fa74d422ef84a5bb3adde36983f0ce8ebff8f
Paul Hauner <paul@paulhauner.com>
2021-05-21 10:26:40 +1000

    Use fork_name_at_slot in genesis

--- consensus/state_processing/src/genesis.rs
+++ consensus/state_processing/src/genesis.rs
@@ -41,7 +41,7 @@ pub fn initialize_beacon_state_from_eth1<T: EthSpec>(
     // To support testnets with Altair enabled from genesis, perform a possible state upgrade here.
     // This must happen *after* deposits and activations are processed or the calculation of sync
     // committees during the upgrade will fail.
-    if spec.altair_fork_slot == Some(spec.genesis_slot) {
+    if spec.fork_name_at_slot(state.slot()) == ForkName::Altair {
         state.upgrade_to_altair(spec)?;
 
         // Reset the sync committees (this seems to be what the tests want)
