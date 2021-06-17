51c9a26f12bda40060aa7eca0942e50ce76d2790
Michael Sproul <michael@sigmaprime.io>
2021-06-17 17:50:33 +1000

    Expand and refactor fork functions

--- consensus/types/src/fork_name.rs
+++ consensus/types/src/fork_name.rs
@@ -26,6 +26,26 @@ impl ForkName {
             }
         }
     }
+
+    /// Return the name of the fork immediately prior to the current one.
+    ///
+    /// If `self` is `ForkName::Base` then `Base` is returned.
+    pub fn previous_fork(self) -> Option<ForkName> {
+        match self {
+            ForkName::Base => None,
+            ForkName::Altair => Some(ForkName::Base),
+        }
+    }
+
+    /// Return the name of the fork immediately after the current one.
+    ///
+    /// If `self` is the last known fork and has no successor, `None` is returned.
+    pub fn next_fork(self) -> Option<ForkName> {
+        match self {
+            ForkName::Base => Some(ForkName::Altair),
+            ForkName::Altair => None,
+        }
+    }
 }
 
 impl std::str::FromStr for ForkName {
@@ -44,4 +64,21 @@ impl std::str::FromStr for ForkName {
 pub struct InconsistentFork {
     pub fork_at_slot: ForkName,
     pub object_fork: ForkName,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use itertools::Itertools;
+
+    #[test]
+    fn previous_and_next_fork_consistent() {
+        assert_eq!(ForkName::Altair.next_fork(), None);
+        assert_eq!(ForkName::Base.previous_fork(), None);
+
+        for (prev_fork, fork) in ForkName::list_all().into_iter().tuple_windows() {
+            assert_eq!(prev_fork.next_fork(), Some(fork));
+            assert_eq!(fork.previous_fork(), Some(prev_fork));
+        }
+    }
 }
