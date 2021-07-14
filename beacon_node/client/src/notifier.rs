796b77d6856f1f3e1a9fae21491e249a520f06a7
Paul Hauner <paul@paulhauner.com>
2021-07-14 11:48:57 +1000

    Remove message

--- beacon_node/client/src/notifier.rs
+++ beacon_node/client/src/notifier.rs
@@ -228,7 +228,6 @@ fn eth1_logging<T: BeaconChainTypes>(beacon_chain: &BeaconChain<T>, log: &Logger
                     warn!(
                         log,
                         "Syncing eth1 block cache";
-                        "tip" => "sync is generally fastest when using a local eth1 node",
                         "est_blocks_remaining" => distance,
                     );
                 }
