23a78395de13ff0ec12a96991b2d3c608307f7fc
Michael Sproul <michael@sigmaprime.io>
2021-08-04 15:40:30 +1000

    Clean up discv5 dev dependencies

--- beacon_node/http_api/tests/common.rs
+++ beacon_node/http_api/tests/common.rs
@@ -2,9 +2,9 @@ use beacon_chain::{
     test_utils::{BeaconChainHarness, EphemeralHarnessType},
     BeaconChain, BeaconChainTypes,
 };
-use discv5::enr::{CombinedKey, EnrBuilder};
 use eth2::{BeaconNodeHttpClient, Timeouts};
 use eth2_libp2p::{
+    discv5::enr::{CombinedKey, EnrBuilder},
     rpc::methods::{MetaData, MetaDataV2},
     types::{EnrAttestationBitfield, EnrSyncCommitteeBitfield, SyncState},
     Enr, NetworkGlobals, PeerId,
