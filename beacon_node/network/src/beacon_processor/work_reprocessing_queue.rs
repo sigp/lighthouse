db8460fc58c60cb2d44a30b19f9f8fb4bcf0f348
Paul Hauner <paul@paulhauner.com>
2021-08-12 14:01:14 +1000

    Set full reprocessing queue error to warn

--- beacon_node/network/src/beacon_processor/work_reprocessing_queue.rs
+++ beacon_node/network/src/beacon_processor/work_reprocessing_queue.rs
@@ -17,7 +17,7 @@ use eth2_libp2p::{MessageId, PeerId};
 use fnv::FnvHashMap;
 use futures::task::Poll;
 use futures::{Stream, StreamExt};
-use slog::{crit, debug, error, Logger};
+use slog::{crit, debug, error, warn, Logger};
 use slot_clock::SlotClock;
 use std::collections::{HashMap, HashSet};
 use std::pin::Pin;
@@ -261,7 +261,7 @@ impl<T: BeaconChainTypes> ReprocessQueue<T> {
                 if let Some(duration_till_slot) = slot_clock.duration_to_slot(block_slot) {
                     // Check to ensure this won't over-fill the queue.
                     if self.queued_block_roots.len() >= MAXIMUM_QUEUED_BLOCKS {
-                        error!(
+                        warn!(
                             log,
                             "Early blocks queue is full";
                             "queue_size" => MAXIMUM_QUEUED_BLOCKS,
