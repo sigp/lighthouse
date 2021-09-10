c255d0e0f64d9edf81e576e1e2f93a334b36ab17
Paul Hauner <paul@paulhauner.com>
2021-09-10 10:33:52 +1000

    Add comments to union selector

--- consensus/ssz/src/union_selector.rs
+++ consensus/ssz/src/union_selector.rs
@@ -1,5 +1,8 @@
 use crate::*;
 
+/// Provides the one-byte "selector" from the SSZ union specification:
+///
+/// https://github.com/ethereum/consensus-specs/blob/v1.1.0-beta.3/ssz/simple-serialize.md#union
 #[derive(Copy, Clone)]
 pub struct UnionSelector(u8);
 
@@ -16,6 +19,7 @@ impl PartialEq<u8> for UnionSelector {
 }
 
 impl UnionSelector {
+    /// Instantiate `self`, returning an error if `selector > MAX_UNION_SELECTOR`.
     pub fn new(selector: u8) -> Result<Self, DecodeError> {
         Some(selector)
             .filter(|_| selector <= MAX_UNION_SELECTOR)
