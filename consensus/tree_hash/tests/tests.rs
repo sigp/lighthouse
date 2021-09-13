d655c663c646bbf5d26ce2c01acf4bf864118a68
Paul Hauner <paul@paulhauner.com>
2021-09-13 15:26:20 +1000

    Remove ssz_types dep from tree_hash

--- consensus/tree_hash/tests/tests.rs
+++ consensus/tree_hash/tests/tests.rs
@@ -1,6 +1,44 @@
-use ssz_types::{typenum::U1, VariableList};
-use tree_hash::{Hash256, TreeHash};
+use ssz_derive::Encode;
+use tree_hash::{Hash256, MerkleHasher, TreeHash, BYTES_PER_CHUNK};
 use tree_hash_derive::TreeHash;
+
+#[derive(Encode)]
+struct HashVec {
+    vec: Vec<u8>,
+}
+
+impl From<Vec<u8>> for HashVec {
+    fn from(vec: Vec<u8>) -> Self {
+        Self { vec }
+    }
+}
+
+impl tree_hash::TreeHash for HashVec {
+    fn tree_hash_type() -> tree_hash::TreeHashType {
+        tree_hash::TreeHashType::List
+    }
+
+    fn tree_hash_packed_encoding(&self) -> Vec<u8> {
+        unreachable!("List should never be packed.")
+    }
+
+    fn tree_hash_packing_factor() -> usize {
+        unreachable!("List should never be packed.")
+    }
+
+    fn tree_hash_root(&self) -> Hash256 {
+        let mut hasher =
+            MerkleHasher::with_leaves((self.vec.len() + BYTES_PER_CHUNK - 1) / BYTES_PER_CHUNK);
+
+        for item in &self.vec {
+            hasher.write(&item.tree_hash_packed_encoding()).unwrap()
+        }
+
+        let root = hasher.finish().unwrap();
+
+        tree_hash::mix_in_length(&root, self.vec.len())
+    }
+}
 
 fn mix_in_selector(a: Hash256, selector: u8) -> Hash256 {
     let mut b = [0; 32];
@@ -54,18 +92,18 @@ fn fixed_union() {
 #[derive(TreeHash)]
 #[tree_hash(enum_behaviour = "transparent")]
 enum VariableTrans {
-    A(VariableList<u8, U1>),
-    B(VariableList<u8, U1>),
+    A(HashVec),
+    B(HashVec),
 }
 
 #[test]
 fn variable_trans() {
     assert_eq!(
-        VariableTrans::A(VariableList::from(vec![2])).tree_hash_root(),
+        VariableTrans::A(HashVec::from(vec![2])).tree_hash_root(),
         u8_hash_concat(2, 1)
     );
     assert_eq!(
-        VariableTrans::B(VariableList::from(vec![2])).tree_hash_root(),
+        VariableTrans::B(HashVec::from(vec![2])).tree_hash_root(),
         u8_hash_concat(2, 1)
     );
 }
@@ -73,18 +111,18 @@ fn variable_trans() {
 #[derive(TreeHash)]
 #[tree_hash(enum_behaviour = "union")]
 enum VariableUnion {
-    A(VariableList<u8, U1>),
-    B(VariableList<u8, U1>),
+    A(HashVec),
+    B(HashVec),
 }
 
 #[test]
 fn variable_union() {
     assert_eq!(
-        VariableUnion::A(VariableList::from(vec![2])).tree_hash_root(),
+        VariableUnion::A(HashVec::from(vec![2])).tree_hash_root(),
         mix_in_selector(u8_hash_concat(2, 1), 0)
     );
     assert_eq!(
-        VariableUnion::B(VariableList::from(vec![2])).tree_hash_root(),
+        VariableUnion::B(HashVec::from(vec![2])).tree_hash_root(),
         mix_in_selector(u8_hash_concat(2, 1), 1)
     );
 }
