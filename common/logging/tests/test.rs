e950c1cde03d294d71d688f2ca84b818e37d73af
Wink Saville <wink@saville.com>
2021-09-19 17:54:27 -0700

    Fix issue with test-release-windows failing
    The problem was the Command needed to be windows specific so I created
    run_cmd to which used `if cfg!(target_os = "windows")` do the right
    thing for windows and other platforms.

--- common/logging/tests/test.rs
+++ common/logging/tests/test.rs
@@ -1,19 +1,32 @@
-#[cfg(debug_assertions)] // Tests fail in release on github actions
 #[cfg(test)]
 mod tests {
+    use std::env;
     use std::process::Command;
+    //use std::io::Error;
+    use std::process::Output;
+
+    fn run_cmd(cmd_line: &str) -> Result<Output, std::io::Error> {
+        let output;
+        if cfg!(target_os = "windows") {
+            output = Command::new(r#"cmd"#).args(["/C", cmd_line]).output();
+        } else {
+            output = Command::new(r#"sh"#).args(["-c", cmd_line]).output();
+        }
+        output
+    }
 
     #[test]
     fn test_test_logger_with_feature_test_logger() {
-        // Test with logging/test_logger feature
-        let output = Command::new(r#"/usr/bin/bash"#)
-            //.args(["-c", r#"ls -al"#])
-            .args(["-c", r#"( cd tests/test-feature-test_logger ; cargo clean ; cargo test --features logging/test_logger)"#])
-            .output();
-        println!(
-            "test_test_logger_with_feature_test_logger output: {:#?}",
-            output
+        let cur_dir = env::current_dir().unwrap();
+        let test_dir = cur_dir.join("tests").join("test-feature-test_logger");
+        let cmd_line = format!(
+            "cd {} && cargo test --features logging/test_logger",
+            test_dir.to_str().unwrap()
         );
+        //println!("cmd_line: {}", cmd_line);
+
+        let output = run_cmd(&cmd_line);
+        //println!("test_test_logger_with_feature_test_logger output: {:#?}", output);
 
         // Assert output data DOES contain "INFO hi, "
         let data = String::from_utf8(output.unwrap().stderr).unwrap();
@@ -24,13 +37,13 @@ mod tests {
     #[test]
     fn test_test_logger_no_features() {
         // Test without features
-        let output = Command::new(r#"/usr/bin/bash"#)
-            .args([
-                "-c",
-                r#"( cd tests/test-feature-test_logger ; cargo test -- --nocapture )"#,
-            ])
-            .output();
-        println!("test_test_logger_no_features output: {:#?}", output);
+        let cur_dir = env::current_dir().unwrap();
+        let test_dir = cur_dir.join("tests").join("test-feature-test_logger");
+        let cmd_line = format!("cd {} && cargo test", test_dir.to_str().unwrap());
+        //println!("cmd_line: {}", cmd_line);
+
+        let output = run_cmd(&cmd_line);
+        //println!("test_test_logger_no_features output: {:#?}", output);
 
         // Assert output data DOES contain "INFO hi, "
         let data = String::from_utf8(output.unwrap().stderr).unwrap();
