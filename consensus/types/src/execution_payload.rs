6da6453a20414f71f7c255e16280591dadcb4bbf
Michael Sproul <michael@sigmaprime.io>
2021-09-24 14:55:21 +1000

    Fix consensus, SSZ, tree hash & run merge EF tests (#2622)
    * Update to v1.1.0-beta.4 (squash of #2548)
    
    * SSZ, cached tree hash, EF tests

--- consensus/types/src/execution_payload.rs
+++ consensus/types/src/execution_payload.rs
@@ -8,8 +8,15 @@ use tree_hash_derive::TreeHash;
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Encode, Decode, TreeHash)]
 #[ssz(enum_behaviour = "union")]
 #[tree_hash(enum_behaviour = "union")]
+#[serde(tag = "selector", content = "value")]
+#[serde(bound = "T: EthSpec")]
 pub enum Transaction<T: EthSpec> {
-    OpaqueTransaction(VariableList<u8, T::MaxBytesPerOpaqueTransaction>),
+    // FIXME(merge): renaming this enum variant to 0 is a bit of a hack...
+    #[serde(rename = "0")]
+    OpaqueTransaction(
+        #[serde(with = "ssz_types::serde_utils::hex_var_list")]
+        VariableList<u8, T::MaxBytesPerOpaqueTransaction>,
+    ),
 }
 
 impl<T: EthSpec, I: SliceIndex<[u8]>> Index<I> for Transaction<T> {
@@ -33,12 +40,13 @@ impl<T: EthSpec> From<VariableList<u8, T::MaxBytesPerOpaqueTransaction>> for Tra
 #[derive(
     Default, Debug, Clone, PartialEq, Serialize, Deserialize, Encode, Decode, TreeHash, TestRandom,
 )]
+#[serde(bound = "T: EthSpec")]
 pub struct ExecutionPayload<T: EthSpec> {
     pub parent_hash: Hash256,
     pub coinbase: Address,
     pub state_root: Hash256,
     pub receipt_root: Hash256,
-    #[serde(with = "serde_logs_bloom")]
+    #[serde(with = "ssz_types::serde_utils::hex_fixed_vec")]
     pub logs_bloom: FixedVector<u8, T::BytesPerLogsBloom>,
     pub random: Hash256,
     #[serde(with = "eth2_serde_utils::quoted_u64")]
@@ -51,7 +59,6 @@ pub struct ExecutionPayload<T: EthSpec> {
     pub timestamp: u64,
     pub base_fee_per_gas: Hash256,
     pub block_hash: Hash256,
-    #[serde(with = "serde_transactions")]
     #[test_random(default)]
     pub transactions: VariableList<Transaction<T>, T::MaxTransactionsPerPayload>,
 }
@@ -74,101 +81,5 @@ impl<T: EthSpec> ExecutionPayload<T> {
             block_hash: Hash256::zero(),
             transactions: VariableList::empty(),
         }
-    }
-}
-
-/// Serializes the `logs_bloom` field.
-pub mod serde_logs_bloom {
-    use super::*;
-    use eth2_serde_utils::hex::PrefixedHexVisitor;
-    use serde::{Deserializer, Serializer};
-
-    pub fn serialize<S, U>(bytes: &FixedVector<u8, U>, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-        U: Unsigned,
-    {
-        let mut hex_string: String = "0x".to_string();
-        hex_string.push_str(&hex::encode(&bytes[..]));
-
-        serializer.serialize_str(&hex_string)
-    }
-
-    pub fn deserialize<'de, D, U>(deserializer: D) -> Result<FixedVector<u8, U>, D::Error>
-    where
-        D: Deserializer<'de>,
-        U: Unsigned,
-    {
-        let vec = deserializer.deserialize_string(PrefixedHexVisitor)?;
-
-        FixedVector::new(vec)
-            .map_err(|e| serde::de::Error::custom(format!("invalid logs bloom: {:?}", e)))
-    }
-}
-
-/// Serializes the `transactions` field.
-pub mod serde_transactions {
-    use super::*;
-    use eth2_serde_utils::hex;
-    use serde::ser::SerializeSeq;
-    use serde::{de, Deserializer, Serializer};
-    use std::marker::PhantomData;
-
-    pub struct ListOfBytesListVisitor<T: EthSpec> {
-        _t: PhantomData<T>,
-    }
-    impl<'a, T> serde::de::Visitor<'a> for ListOfBytesListVisitor<T>
-    where
-        T: EthSpec,
-    {
-        type Value = VariableList<Transaction<T>, T::MaxTransactionsPerPayload>;
-
-        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-            write!(formatter, "a list of 0x-prefixed byte lists")
-        }
-
-        fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-        where
-            A: serde::de::SeqAccess<'a>,
-        {
-            let mut outer = VariableList::default();
-
-            while let Some(val) = seq.next_element::<String>()? {
-                let inner_vec = hex::decode(&val).map_err(de::Error::custom)?;
-                let inner = VariableList::new(inner_vec).map_err(|e| {
-                    serde::de::Error::custom(format!("invalid transaction: {:?}", e))
-                })?;
-                outer.push(inner.into()).map_err(|e| {
-                    serde::de::Error::custom(format!("too many transactions: {:?}", e))
-                })?;
-            }
-
-            Ok(outer)
-        }
-    }
-
-    pub fn serialize<S, T>(
-        value: &VariableList<Transaction<T>, T::MaxTransactionsPerPayload>,
-        serializer: S,
-    ) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-        T: EthSpec,
-    {
-        let mut seq = serializer.serialize_seq(Some(value.len()))?;
-        for val in value {
-            seq.serialize_element(&hex::encode(&val[..]))?;
-        }
-        seq.end()
-    }
-
-    pub fn deserialize<'de, D, T>(
-        deserializer: D,
-    ) -> Result<VariableList<Transaction<T>, T::MaxTransactionsPerPayload>, D::Error>
-    where
-        D: Deserializer<'de>,
-        T: EthSpec,
-    {
-        deserializer.deserialize_any(ListOfBytesListVisitor { _t: PhantomData })
     }
 }
