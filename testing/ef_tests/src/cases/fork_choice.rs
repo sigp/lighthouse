a5f936c03199a7593a1172313efcdba4e061ca67
Paul Hauner <paul@paulhauner.com>
2021-10-19 14:44:07 +1100

    Decode blocks and attestations

--- testing/ef_tests/src/cases/fork_choice.rs
+++ testing/ef_tests/src/cases/fork_choice.rs
@@ -1,7 +1,10 @@
 use super::*;
-use crate::decode::{ssz_decode_file_with, ssz_decode_state, yaml_decode_file};
+use crate::decode::{ssz_decode_file, ssz_decode_file_with, ssz_decode_state, yaml_decode_file};
 use serde_derive::Deserialize;
-use types::{BeaconBlock, BeaconState, Checkpoint, EthSpec, ForkName, Hash256, Slot};
+use types::{
+    Attestation, BeaconBlock, BeaconState, Checkpoint, EthSpec, ForkName, Hash256,
+    SignedBeaconBlock, Slot,
+};
 
 #[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all = "snake_case")]
@@ -22,11 +25,11 @@ pub struct Checks {
 
 #[derive(Debug, Clone, Deserialize)]
 #[serde(untagged, rename_all = "snake_case")]
-pub enum Step {
+pub enum Step<B, A> {
     Tick { tick: u64 },
-    ValidBlock { block: String },
-    MaybeValidBlock { block: String, valid: bool },
-    Attestation { attestation: String },
+    ValidBlock { block: B },
+    MaybeValidBlock { block: B, valid: bool },
+    Attestation { attestation: A },
     Checks(Checks),
 }
 
@@ -36,14 +39,42 @@ pub struct ForkChoiceTest<E: EthSpec> {
     pub description: String,
     pub anchor_state: BeaconState<E>,
     pub anchor_block: BeaconBlock<E>,
-    pub steps: Vec<Step>,
+    pub steps: Vec<Step<SignedBeaconBlock<E>, Attestation<E>>>,
 }
 
 impl<E: EthSpec> LoadCase for ForkChoiceTest<E> {
     fn load_from_dir(path: &Path, fork_name: ForkName) -> Result<Self, Error> {
         let description = format!("{:?}", path);
         let spec = &testing_spec::<E>(fork_name);
-        let steps = yaml_decode_file(&path.join("steps.yaml"))?;
+        let steps: Vec<Step<String, String>> = yaml_decode_file(&path.join("steps.yaml"))?;
+        // This somewhat awkward step resolves the object names in `steps.yaml` into actual decoded
+        // block/attestation objects.
+        let steps = steps
+            .into_iter()
+            .map(|step| match step {
+                Step::Tick { tick } => Ok(Step::Tick { tick }),
+                Step::ValidBlock { block } => {
+                    let block = ssz_decode_file_with(
+                        &path.join(format!("{}.ssz_snappy", block)),
+                        |bytes| SignedBeaconBlock::from_ssz_bytes(bytes, spec),
+                    )?;
+                    Ok(Step::ValidBlock { block })
+                }
+                Step::MaybeValidBlock { block, valid } => {
+                    let block = ssz_decode_file_with(
+                        &path.join(format!("{}.ssz_snappy", block)),
+                        |bytes| SignedBeaconBlock::from_ssz_bytes(bytes, spec),
+                    )?;
+                    Ok(Step::MaybeValidBlock { block, valid })
+                }
+                Step::Attestation { attestation } => {
+                    let attestation =
+                        ssz_decode_file(&path.join(format!("{}.ssz_snappy", attestation)))?;
+                    Ok(Step::Attestation { attestation })
+                }
+                Step::Checks(checks) => Ok(Step::Checks(checks)),
+            })
+            .collect::<Result<_, _>>()?;
         let anchor_state = ssz_decode_state(&path.join("anchor_state.ssz_snappy"), spec)?;
         let anchor_block = ssz_decode_file_with(&path.join("anchor_block.ssz_snappy"), |bytes| {
             BeaconBlock::from_ssz_bytes(bytes, spec)
