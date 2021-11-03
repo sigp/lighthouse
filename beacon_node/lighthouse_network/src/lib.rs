091178b9ae88dfa988739c28fa93b8b134ab2580
Diva M <divma@protonmail.com>
2021-11-03 18:14:15 -0500

    fix http_api tests

--- beacon_node/lighthouse_network/src/lib.rs
+++ beacon_node/lighthouse_network/src/lib.rs
@@ -68,6 +68,7 @@ pub use behaviour::{BehaviourEvent, Gossipsub, PeerRequestId, Request, Response}
 pub use config::Config as NetworkConfig;
 pub use discovery::{CombinedKeyExt, EnrExt, Eth2Enr};
 pub use discv5;
+pub use libp2p;
 pub use libp2p::bandwidth::BandwidthSinks;
 pub use libp2p::gossipsub::{MessageAcceptance, MessageId, Topic, TopicHash};
 pub use libp2p::{core::ConnectedPoint, PeerId, Swarm};
