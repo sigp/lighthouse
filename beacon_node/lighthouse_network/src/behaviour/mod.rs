40fdd1d9dd7702ed75641a00a725507a3fcf329d
Age Manning <Age@AgeManning.com>
2021-11-17 14:17:01 +1100

    Update code for libp2p update

--- beacon_node/lighthouse_network/src/behaviour/mod.rs
+++ beacon_node/lighthouse_network/src/behaviour/mod.rs
@@ -26,7 +26,7 @@ use libp2p::{
     },
     identify::{Identify, IdentifyConfig, IdentifyEvent},
     swarm::{
-        AddressScore, DialPeerCondition, NetworkBehaviour, NetworkBehaviourAction as NBAction,
+        AddressScore, dial_opts::{DialOpts,PeerCondition}, NetworkBehaviour, NetworkBehaviourAction as NBAction,
         NetworkBehaviourEventProcess, PollParameters,
     },
     NetworkBehaviour, PeerId,
@@ -232,6 +232,7 @@ impl<TSpec: EthSpec> Behaviour<TSpec> {
         let mut gossipsub = Gossipsub::new_with_subscription_filter_and_transform(
             MessageAuthenticity::Anonymous,
             config.gs_config.clone(),
+            None, // No metrics for the time being
             filter,
             snappy_transform,
         )
@@ -1071,9 +1072,7 @@ impl<TSpec: EthSpec> Behaviour<TSpec> {
             match event {
                 InternalBehaviourMessage::DialPeer(peer_id) => {
                     let handler = self.new_handler();
-                    return Poll::Ready(NBAction::DialPeer {
-                        peer_id,
-                        condition: DialPeerCondition::Disconnected,
+                    return Poll::Ready(NBAction::Dial { opts: DialOpts::peer_id(peer_id).condition(PeerCondition::Disconnected).build(),
                         handler,
                     });
                 }
