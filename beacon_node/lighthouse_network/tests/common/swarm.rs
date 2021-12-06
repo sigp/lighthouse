f287f489a6d1707bd5dff9e7e45f80f05323d05e
Diva M <divma@protonmail.com>
2021-12-06 16:01:48 -0500

    add swarm pool

--- beacon_node/lighthouse_network/tests/common/swarm.rs
+++ beacon_node/lighthouse_network/tests/common/swarm.rs
@@ -1,7 +1,13 @@
+use std::collections::HashMap;
+use std::pin::Pin;
+
 use super::behaviour::{CallTraceBehaviour, MockBehaviour};
 
-use libp2p::swarm::{NetworkBehaviour, Swarm, SwarmBuilder, SwarmEvent};
-use libp2p::Transport;
+use futures::stream::Stream;
+use futures::task::{Context, Poll};
+use libp2p::swarm::protocols_handler::ProtocolsHandler;
+use libp2p::swarm::{IntoProtocolsHandler, NetworkBehaviour, Swarm, SwarmBuilder, SwarmEvent};
+use libp2p::{PeerId, Transport};
 
 use futures::StreamExt;
 
@@ -36,5 +42,45 @@ pub async fn bind_listener<B: NetworkBehaviour>(
             address,
         } => address,
         _ => panic!("Testing swarm's first event should be a new listener"),
+    }
+}
+
+#[derive(Default)]
+pub struct SwarmPool<B: NetworkBehaviour> {
+    swarms: HashMap<PeerId, Swarm<B>>,
+}
+
+impl<B: NetworkBehaviour> SwarmPool<B> {
+    pub fn insert(&mut self, swarm: Swarm<B>) -> PeerId {
+        let peer_id = *swarm.local_peer_id();
+        self.swarms.insert(peer_id, swarm);
+        peer_id
+    }
+
+    pub fn remove(&mut self, peer_id: &PeerId) {
+        self.swarms.remove(peer_id);
+    }
+
+    pub fn get_mut(&mut self, peer_id: &PeerId) -> Option<&mut Swarm<B>> {
+        self.swarms.get_mut(peer_id)
+    }
+}
+
+impl<B> Stream for SwarmPool<B>
+where
+    B: NetworkBehaviour,
+    <B as NetworkBehaviour>::ProtocolsHandler: ProtocolsHandler,
+{
+    type Item = (PeerId,
+                 SwarmEvent<<B as NetworkBehaviour>::OutEvent, <<<B as NetworkBehaviour>::ProtocolsHandler as IntoProtocolsHandler>::Handler as ProtocolsHandler>::Error>);
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut polls = self
+            .get_mut()
+            .swarms
+            .iter_mut()
+            .map(|(&peer_id, swarm)| swarm.map(move |ev| (peer_id, ev)))
+            .collect::<futures::stream::SelectAll<_>>();
+        polls.poll_next_unpin(cx)
     }
 }
