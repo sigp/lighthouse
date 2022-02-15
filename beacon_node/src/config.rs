02763bad91012381a28c5d8f0a8915fdea7c75dd
Age Manning <Age@AgeManning.com>
2022-02-15 14:08:58 +1100

    Filter non global ips

--- beacon_node/src/config.rs
+++ beacon_node/src/config.rs
@@ -763,6 +763,10 @@ pub fn set_network_config(
         config.metrics_enabled = true;
     }
 
+    if cli_args.is_present("enable-local-discovery") {
+        config.discv5_config.table_filter = |_| true;
+    }
+
     Ok(())
 }
 
