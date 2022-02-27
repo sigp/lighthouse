7e008cb017457e08e522b21621c0ad0ab5b20501
Paul Hauner <paul@paulhauner.com>
2022-02-27 12:39:29 +1100

    Add prepare call to HTTP api

--- beacon_node/http_api/src/lib.rs
+++ beacon_node/http_api/src/lib.rs
@@ -2229,6 +2229,21 @@ pub fn serve<T: BeaconChainTypes>(
                             )
                         })?;
 
+                    execution_layer
+                        .block_on_generic(|_| chain.prepare_beacon_proposer())
+                        .map_err(|e| {
+                            warp_utils::reject::custom_bad_request(format!(
+                                "error spawning update: {:?}",
+                                e
+                            ))
+                        })?
+                        .map_err(|e| {
+                            warp_utils::reject::custom_bad_request(format!(
+                                "error updating proposer preparations: {:?}",
+                                e
+                            ))
+                        })?;
+
                     Ok(())
                 })
             },
