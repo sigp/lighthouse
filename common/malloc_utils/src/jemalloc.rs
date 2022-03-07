e48ab54dcc082cff61fc741ef8f07cd8f4a7541e
Michael Sproul <michael@sigmaprime.io>
2022-03-07 18:47:05 +1100

    Jemalloc tuning via Cargo config

--- common/malloc_utils/src/jemalloc.rs
+++ common/malloc_utils/src/jemalloc.rs
@@ -1,2 +1,12 @@
+//! Set the allocator to `jemalloc`.
+//!
+//! Due to `jemalloc` requiring configuration at compile time or immediately upon runtime
+//! initialisation it is configured via a Cargo config file in `.cargo/config.toml`.
+//!
+//! The `jemalloc` tuning can be overriden by:
+//!
+//! A) `JEMALLOC_SYS_WITH_MALLOC_CONF` at compile-time.
+//! B) `_RJEM_MALLOC_CONF` at runtime.
+
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
