3c675a9dfc0f7391b40c898ef1a27606ab6257d1
Mac L <mjladson@pm.me>
2022-03-24 00:04:48 +0000

    Add Nethermind integration tests (#3100)
    ## Proposed Changes
    
    Extend the current Geth merge integration tests to support Nethermind.

--- testing/execution_engine_integration/src/execution_engine.rs
+++ testing/execution_engine_integration/src/execution_engine.rs
@@ -1,9 +1,7 @@
-use crate::{genesis_json::geth_genesis_json, SUPPRESS_LOGS};
 use execution_layer::DEFAULT_JWT_FILE;
 use sensitive_url::SensitiveUrl;
 use std::path::PathBuf;
-use std::process::{Child, Command, Output, Stdio};
-use std::{env, fs::File};
+use std::process::Child;
 use tempfile::TempDir;
 use unused_port::unused_tcp_port;
 
@@ -64,95 +62,5 @@ impl<E: GenericExecutionEngine> ExecutionEngine<E> {
 
     pub fn datadir(&self) -> PathBuf {
         self.datadir.path().to_path_buf()
-    }
-}
-
-/*
- * Geth-specific Implementation
- */
-
-#[derive(Clone)]
-pub struct Geth;
-
-impl Geth {
-    fn binary_path() -> PathBuf {
-        let manifest_dir: PathBuf = env::var("CARGO_MANIFEST_DIR").unwrap().into();
-        manifest_dir
-            .join("execution_clients")
-            .join("go-ethereum")
-            .join("build")
-            .join("bin")
-            .join("geth")
-    }
-}
-
-impl GenericExecutionEngine for Geth {
-    fn init_datadir() -> TempDir {
-        let datadir = TempDir::new().unwrap();
-
-        let genesis_json_path = datadir.path().join("genesis.json");
-        let mut file = File::create(&genesis_json_path).unwrap();
-        let json = geth_genesis_json();
-        serde_json::to_writer(&mut file, &json).unwrap();
-
-        let output = Command::new(Self::binary_path())
-            .arg("--datadir")
-            .arg(datadir.path().to_str().unwrap())
-            .arg("init")
-            .arg(genesis_json_path.to_str().unwrap())
-            .output()
-            .expect("failed to init geth");
-
-        check_command_output(output, "geth init failed");
-
-        datadir
-    }
-
-    fn start_client(
-        datadir: &TempDir,
-        http_port: u16,
-        http_auth_port: u16,
-        jwt_secret_path: PathBuf,
-    ) -> Child {
-        let network_port = unused_tcp_port().unwrap();
-
-        Command::new(Self::binary_path())
-            .arg("--datadir")
-            .arg(datadir.path().to_str().unwrap())
-            .arg("--http")
-            .arg("--http.api")
-            .arg("engine,eth")
-            .arg("--http.port")
-            .arg(http_port.to_string())
-            .arg("--authrpc.port")
-            .arg(http_auth_port.to_string())
-            .arg("--port")
-            .arg(network_port.to_string())
-            .arg("--authrpc.jwtsecret")
-            .arg(jwt_secret_path.as_path().to_str().unwrap())
-            .stdout(build_stdio())
-            .stderr(build_stdio())
-            .spawn()
-            .expect("failed to start beacon node")
-    }
-}
-
-fn check_command_output(output: Output, failure_msg: &'static str) {
-    if !output.status.success() {
-        let stdout = String::from_utf8_lossy(&output.stdout);
-        let stderr = String::from_utf8_lossy(&output.stderr);
-
-        dbg!(stdout);
-        dbg!(stderr);
-        panic!("{}", failure_msg);
-    }
-}
-
-/// Builds the stdout/stderr handler for commands which might output to the terminal.
-fn build_stdio() -> Stdio {
-    if SUPPRESS_LOGS {
-        Stdio::null()
-    } else {
-        Stdio::inherit()
     }
 }
--- testing/execution_engine_integration/src/main.rs
+++ testing/execution_engine_integration/src/main.rs
@@ -3,26 +3,37 @@
 /// It will first attempt to build any supported integration clients, then it will run tests.
 ///
 /// A return code of `0` indicates the tests succeeded.
-mod build_geth;
+mod build_utils;
 mod execution_engine;
 mod genesis_json;
+mod geth;
+mod nethermind;
 mod test_rig;
 
-use execution_engine::Geth;
+use geth::GethEngine;
+use nethermind::NethermindEngine;
 use test_rig::TestRig;
 
 /// Set to `false` to send logs to the console during tests. Logs are useful when debugging.
-const SUPPRESS_LOGS: bool = false;
+const SUPPRESS_LOGS: bool = true;
 
 fn main() {
     if cfg!(windows) {
         panic!("windows is not supported, only linux");
     }
 
-    test_geth()
+    test_geth();
+    test_nethermind();
 }
 
 fn test_geth() {
-    build_geth::build();
-    TestRig::new(Geth).perform_tests_blocking();
+    let test_dir = build_utils::prepare_dir();
+    geth::build(&test_dir);
+    TestRig::new(GethEngine).perform_tests_blocking();
+}
+
+fn test_nethermind() {
+    let test_dir = build_utils::prepare_dir();
+    nethermind::build(&test_dir);
+    TestRig::new(NethermindEngine).perform_tests_blocking();
 }
