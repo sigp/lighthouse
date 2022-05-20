ab69b7e1c6c7aac672ff648c53c84d92fd1231b1
Paul Hauner <paul@paulhauner.com>
2022-05-21 07:52:36 +1000

    Update beacon_node/beacon_chain/src/beacon_chain.rs

--- beacon_node/beacon_chain/src/beacon_chain.rs
+++ beacon_node/beacon_chain/src/beacon_chain.rs
@@ -4406,7 +4406,7 @@ impl<T: BeaconChainTypes> BeaconChain<T> {
 
     /// Called by the timer on every slot.
     ///
-    /// Note: this function **should** be called from a non-async context since
+    /// Note: this function **MUST** be called from a non-async context since
     /// it contains a call to `fork_choice` which may eventually call
     /// `tokio::runtime::block_on` in certain cases.
     pub fn per_slot_task(self: &Arc<Self>) {
