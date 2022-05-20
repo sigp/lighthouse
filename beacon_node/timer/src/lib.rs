055c2262e95abc6d61d42aee1a5144130c2868b9
Paul Hauner <paul@paulhauner.com>
2022-05-21 07:58:36 +1000

    Use `spawn_blocking_handle`

--- beacon_node/timer/src/lib.rs
+++ beacon_node/timer/src/lib.rs
@@ -3,7 +3,7 @@
 //! This service allows task execution on the beacon node for various functionality.
 
 use beacon_chain::{BeaconChain, BeaconChainTypes};
-use slog::info;
+use slog::{debug, error, info};
 use slot_clock::SlotClock;
 use std::sync::Arc;
 use std::time::Duration;
@@ -26,10 +26,28 @@ pub fn spawn_timer<T: BeaconChainTypes>(
     let mut interval = interval_at(start_instant, Duration::from_secs(seconds_per_slot));
     let per_slot_executor = executor.clone();
     let timer_future = async move {
+        let log = per_slot_executor.log().clone();
         loop {
             interval.tick().await;
             let chain = beacon_chain.clone();
-            per_slot_executor.spawn_blocking(move || chain.per_slot_task(), "timer_per_slot_task");
+            if let Some(handle) = per_slot_executor
+                .spawn_blocking_handle(move || chain.per_slot_task(), "timer_per_slot_task")
+            {
+                if let Err(e) = handle.await {
+                    error!(
+                        log,
+                        "Per slot task failed";
+                        "info" => ?e
+                    );
+                }
+            } else {
+                debug!(
+                    log,
+                    "Per slot task timer stopped";
+                    "info" => "shutting down"
+                );
+                break;
+            }
         }
     };
 
