6dd275dba0f0d162366f64ff69e516e132586e34
Michael Sproul <michael@sigmaprime.io>
2022-06-06 09:36:33 +1000

    Fix per-slot timer in presence of clock changes

--- beacon_node/timer/src/lib.rs
+++ beacon_node/timer/src/lib.rs
@@ -6,29 +6,29 @@ use beacon_chain::{BeaconChain, BeaconChainTypes};
 use slog::{debug, info, warn};
 use slot_clock::SlotClock;
 use std::sync::Arc;
-use std::time::Duration;
-use tokio::time::{interval_at, Instant};
+use tokio::time::sleep;
 
 /// Spawns a timer service which periodically executes tasks for the beacon chain
 pub fn spawn_timer<T: BeaconChainTypes>(
     executor: task_executor::TaskExecutor,
     beacon_chain: Arc<BeaconChain<T>>,
-    seconds_per_slot: u64,
 ) -> Result<(), &'static str> {
     let log = executor.log();
-    let start_instant = Instant::now()
-        + beacon_chain
-            .slot_clock
-            .duration_to_next_slot()
-            .ok_or("slot_notifier unable to determine time to next slot")?;
-
-    // Warning: `interval_at` panics if `seconds_per_slot` = 0.
-    let mut interval = interval_at(start_instant, Duration::from_secs(seconds_per_slot));
     let per_slot_executor = executor.clone();
+
     let timer_future = async move {
         let log = per_slot_executor.log().clone();
         loop {
-            interval.tick().await;
+            let duration_to_next_slot = match beacon_chain.slot_clock.duration_to_next_slot() {
+                Some(duration) => duration,
+                None => {
+                    warn!(log, "Unable to determine duration to next slot");
+                    return;
+                }
+            };
+
+            sleep(duration_to_next_slot).await;
+
             let chain = beacon_chain.clone();
             if let Some(handle) = per_slot_executor
                 .spawn_blocking_handle(move || chain.per_slot_task(), "timer_per_slot_task")
