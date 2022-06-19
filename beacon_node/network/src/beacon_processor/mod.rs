f4287197619b6fcf280ce34e9d778573f641e279
Pawan Dhananjay <pawandhananjay@gmail.com>
2022-06-19 23:13:40 +0000

    Do not penalize peers on execution layer offline errors (#3258)
    ## Issue Addressed
    
    Partly resolves https://github.com/sigp/lighthouse/issues/3032
    
    ## Proposed Changes
    
    Extracts some of the functionality of #3094 into a separate PR as the original PR requires a bit more work.
    Do not unnecessarily penalize peers when we fail to validate received execution payloads because our execution layer is offline.

--- beacon_node/network/src/beacon_processor/mod.rs
+++ beacon_node/network/src/beacon_processor/mod.rs
@@ -75,7 +75,9 @@ mod work_reprocessing_queue;
 mod worker;
 
 use crate::beacon_processor::work_reprocessing_queue::QueuedBlock;
-pub use worker::{ChainSegmentProcessId, GossipAggregatePackage, GossipAttestationPackage};
+pub use worker::{
+    ChainSegmentProcessId, FailureMode, GossipAggregatePackage, GossipAttestationPackage,
+};
 
 /// The maximum size of the channel for work events to the `BeaconProcessor`.
 ///
