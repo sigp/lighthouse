a94c0b91cfcfee6e63e8efefb3988ff311ae1486
realbigsean <sean@sigmaprime.io>
2022-06-29 15:10:53 -0400

    add generic to el in invalid payload rig

--- beacon_node/beacon_chain/tests/payload_invalidation.rs
+++ beacon_node/beacon_chain/tests/payload_invalidation.rs
@@ -64,7 +64,7 @@ impl InvalidPayloadRig {
         self
     }
 
-    fn execution_layer(&self) -> ExecutionLayer {
+    fn execution_layer(&self) -> ExecutionLayer<E> {
         self.harness.chain.execution_layer.clone().unwrap()
     }
 
