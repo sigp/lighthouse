22d5af2332dde66b74637e451fd81703bf2108d5
Paul Hauner <paul@paulhauner.com>
2022-07-21 16:22:45 +1000

    Bump versions

--- common/lighthouse_version/src/lib.rs
+++ common/lighthouse_version/src/lib.rs
@@ -17,8 +17,8 @@ pub const VERSION: &str = git_version!(
         // NOTE: using --match instead of --exclude for compatibility with old Git
         "--match=thiswillnevermatchlol"
     ],
-    prefix = "Lighthouse/v2.3.2-rc.0-",
-    fallback = "Lighthouse/v2.3.2-rc.0"
+    prefix = "Lighthouse/v2.4.0-",
+    fallback = "Lighthouse/v2.4.0"
 );
 
 /// Returns `VERSION`, but with platform information appended to the end.
