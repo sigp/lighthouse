78a1237a4aca3fab81203e3f443d4b111bcd9728
realbigsean <sean@sigmaprime.io>
2022-07-29 08:14:43 -0400

    use  method during block reconstruction

--- beacon_node/http_api/src/publish_blocks.rs
+++ beacon_node/http_api/src/publish_blocks.rs
@@ -5,15 +5,12 @@ use lighthouse_network::PubsubMessage;
 use network::NetworkMessage;
 use slog::{crit, error, info, Logger};
 use slot_clock::SlotClock;
-use std::marker::PhantomData;
 use std::sync::Arc;
 use tokio::sync::mpsc::UnboundedSender;
 use tree_hash::TreeHash;
 use types::{
-    BeaconBlockAltair, BeaconBlockBase, BeaconBlockBodyAltair, BeaconBlockBodyBase,
-    BeaconBlockBodyMerge, BeaconBlockMerge, BlindedPayload, ExecutionBlockHash, ExecutionPayload,
-    ExecutionPayloadHeader, FullPayload, SignedBeaconBlock, SignedBeaconBlockAltair,
-    SignedBeaconBlockBase, SignedBeaconBlockMerge,
+    BlindedPayload, ExecPayload, ExecutionBlockHash, ExecutionPayload, FullPayload,
+    SignedBeaconBlock,
 };
 use warp::Rejection;
 
@@ -121,208 +118,38 @@ async fn reconstruct_block<T: BeaconChainTypes>(
     block: SignedBeaconBlock<T::EthSpec, BlindedPayload<T::EthSpec>>,
     log: Logger,
 ) -> Result<SignedBeaconBlock<T::EthSpec, FullPayload<T::EthSpec>>, Rejection> {
-    let full_block = match block {
-        SignedBeaconBlock::Base(b) => {
-            let SignedBeaconBlockBase { message, signature } = b;
+    let full_payload = if let Ok(payload_header) = block.message().body().execution_payload() {
+        let el = chain.execution_layer.as_ref().ok_or_else(|| {
+            warp_utils::reject::custom_server_error("Missing execution layer".to_string())
+        })?;
 
-            let BeaconBlockBase {
-                slot,
-                proposer_index,
-                parent_root,
-                state_root,
-                body,
-            } = message;
-
-            let BeaconBlockBodyBase {
-                randao_reveal,
-                eth1_data,
-                graffiti,
-                proposer_slashings,
-                attester_slashings,
-                attestations,
-                deposits,
-                voluntary_exits,
-                _phantom,
-            } = body;
-
-            SignedBeaconBlock::Base(SignedBeaconBlockBase {
-                message: BeaconBlockBase {
-                    slot,
-                    proposer_index,
-                    parent_root,
-                    state_root,
-                    body: BeaconBlockBodyBase {
-                        randao_reveal,
-                        eth1_data,
-                        graffiti,
-                        proposer_slashings,
-                        attester_slashings,
-                        attestations,
-                        deposits,
-                        voluntary_exits,
-                        _phantom: PhantomData::default(),
-                    },
-                },
-                signature,
-            })
-        }
-        SignedBeaconBlock::Altair(b) => {
-            let SignedBeaconBlockAltair { message, signature } = b;
-
-            let BeaconBlockAltair {
-                slot,
-                proposer_index,
-                parent_root,
-                state_root,
-                body,
-            } = message;
-
-            let BeaconBlockBodyAltair {
-                randao_reveal,
-                eth1_data,
-                graffiti,
-                proposer_slashings,
-                attester_slashings,
-                attestations,
-                deposits,
-                voluntary_exits,
-                sync_aggregate,
-                _phantom,
-            } = body;
-
-            let full_body = BeaconBlockBodyAltair {
-                randao_reveal,
-                eth1_data,
-                graffiti,
-                proposer_slashings,
-                attester_slashings,
-                attestations,
-                deposits,
-                voluntary_exits,
-                sync_aggregate,
-                _phantom: PhantomData::default(),
-            };
-
-            SignedBeaconBlock::Altair(SignedBeaconBlockAltair {
-                message: BeaconBlockAltair {
-                    slot,
-                    proposer_index,
-                    parent_root,
-                    state_root,
-                    body: full_body,
-                },
-                signature,
-            })
-        }
-        SignedBeaconBlock::Merge(ref b) => {
-            let SignedBeaconBlockMerge { message, signature } = b;
-
-            let BeaconBlockMerge {
-                slot,
-                proposer_index,
-                parent_root,
-                state_root,
-                body,
-            } = message;
-
-            let BeaconBlockBodyMerge {
-                randao_reveal,
-                eth1_data,
-                graffiti,
-                proposer_slashings,
-                attester_slashings,
-                attestations,
-                deposits,
-                voluntary_exits,
-                sync_aggregate,
-                execution_payload,
-            } = body;
-
-            let payload_root = execution_payload.tree_hash_root();
-
-            let BlindedPayload {
-                execution_payload_header,
-            } = execution_payload;
-
-            let ExecutionPayloadHeader {
-                parent_hash,
-                fee_recipient,
-                state_root: payload_state_root,
-                receipts_root,
-                logs_bloom,
-                prev_randao,
-                block_number,
-                gas_limit,
-                gas_used,
-                timestamp,
-                extra_data,
-                base_fee_per_gas,
-                block_hash,
-                transactions_root: _transactions_root,
-            } = execution_payload_header;
-
-            let el = chain.execution_layer.as_ref().ok_or_else(|| {
-                warp_utils::reject::custom_server_error("Missing execution layer".to_string())
-            })?;
-
-            // If the execution block hash is zero, use an empty payload.
-            let full_payload = if *block_hash == ExecutionBlockHash::zero() {
-                ExecutionPayload::default()
+        // If the execution block hash is zero, use an empty payload.
+        let full_payload = if payload_header.block_hash() == ExecutionBlockHash::zero() {
+            ExecutionPayload::default()
             // If we already have an execution payload with this transactions root cached, use it.
-            } else if let Some(cached_payload) = el.get_payload_by_root(&payload_root) {
-                info!(log, "Reconstructing a full block using a local payload"; "block_hash" => ?cached_payload.block_hash);
-                cached_payload
+        } else if let Some(cached_payload) =
+            el.get_payload_by_root(&payload_header.tree_hash_root())
+        {
+            info!(log, "Reconstructing a full block using a local payload"; "block_hash" => ?cached_payload.block_hash);
+            cached_payload
             // Otherwise, this means we are attempting a blind block proposal.
-            } else {
-                let full_payload = el.propose_blinded_beacon_block(&block).await.map_err(|e| {
-                    warp_utils::reject::custom_server_error(format!(
-                        "Blind block proposal failed: {:?}",
-                        e
-                    ))
-                })?;
-                info!(log, "Successfully published a block to the builder network"; "block_hash" => ?full_payload.block_hash);
-                full_payload
-            };
+        } else {
+            let full_payload = el.propose_blinded_beacon_block(&block).await.map_err(|e| {
+                warp_utils::reject::custom_server_error(format!(
+                    "Blind block proposal failed: {:?}",
+                    e
+                ))
+            })?;
+            info!(log, "Successfully published a block to the builder network"; "block_hash" => ?full_payload.block_hash);
+            full_payload
+        };
 
-            SignedBeaconBlock::Merge(SignedBeaconBlockMerge {
-                message: BeaconBlockMerge {
-                    slot: *slot,
-                    proposer_index: *proposer_index,
-                    parent_root: *parent_root,
-                    state_root: *state_root,
-                    body: BeaconBlockBodyMerge {
-                        randao_reveal: randao_reveal.clone(),
-                        eth1_data: eth1_data.clone(),
-                        graffiti: *graffiti,
-                        proposer_slashings: proposer_slashings.clone(),
-                        attester_slashings: attester_slashings.clone(),
-                        attestations: attestations.clone(),
-                        deposits: deposits.clone(),
-                        voluntary_exits: voluntary_exits.clone(),
-                        sync_aggregate: sync_aggregate.clone(),
-                        execution_payload: FullPayload {
-                            execution_payload: ExecutionPayload {
-                                parent_hash: *parent_hash,
-                                fee_recipient: *fee_recipient,
-                                state_root: *payload_state_root,
-                                receipts_root: *receipts_root,
-                                logs_bloom: logs_bloom.clone(),
-                                prev_randao: *prev_randao,
-                                block_number: *block_number,
-                                gas_limit: *gas_limit,
-                                gas_used: *gas_used,
-                                timestamp: *timestamp,
-                                extra_data: extra_data.clone(),
-                                base_fee_per_gas: *base_fee_per_gas,
-                                block_hash: *block_hash,
-                                transactions: full_payload.transactions,
-                            },
-                        },
-                    },
-                },
-                signature: signature.clone(),
-            })
-        }
+        Some(full_payload)
+    } else {
+        None
     };
-    Ok(full_block)
+
+    block.try_into_full_block(full_payload).ok_or_else(|| {
+        warp_utils::reject::custom_server_error("Unable to add payload to block".to_string())
+    })
 }
