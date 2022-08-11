ab51dae1b81fb61c703763416be36480fc400b94
Michael Sproul <michael@sigmaprime.io>
2022-08-11 17:17:58 +1000

    Fix comment typo

--- beacon_node/operation_pool/src/attestation_id.rs
+++ beacon_node/operation_pool/src/attestation_id.rs
@@ -3,7 +3,7 @@ use ssz_derive::{Decode, Encode};
 
 /// Serialized `AttestationData` augmented with a domain to encode the fork info.
 ///
-/// [DEPRECATED] To be removed once all nodes have updated to schema v11.
+/// [DEPRECATED] To be removed once all nodes have updated to schema v12.
 #[derive(
     PartialEq, Eq, Clone, Hash, Debug, PartialOrd, Ord, Encode, Decode, Serialize, Deserialize,
 )]
