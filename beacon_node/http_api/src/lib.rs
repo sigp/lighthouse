a4d918581a9293ef0c48d1f14700cc6f994da92b
Michael Sproul <michael@sigmaprime.io>
2022-10-26 17:44:13 +1100

    Revert "Optimise HTTP validator lookups"
    This reverts commit ca9dc8e0947a0ec83f31830aaabc1ffbd3c14c9c with some modifications.

--- beacon_node/http_api/src/lib.rs
+++ beacon_node/http_api/src/lib.rs
@@ -668,10 +668,9 @@ pub fn serve<T: BeaconChainTypes>(
                 "Invalid validator ID".to_string(),
             ))
         }))
-        .and(log_filter.clone())
         .and(warp::path::end())
         .and_then(
-            |state_id: StateId, chain: Arc<BeaconChain<T>>, validator_id: ValidatorId, log| {
+            |state_id: StateId, chain: Arc<BeaconChain<T>>, validator_id: ValidatorId| {
                 blocking_json_task(move || {
                     let (data, execution_optimistic) = state_id
                         .map_state_and_execution_optimistic(
@@ -679,23 +678,7 @@ pub fn serve<T: BeaconChainTypes>(
                             |state, execution_optimistic| {
                                 let index_opt = match &validator_id {
                                     ValidatorId::PublicKey(pubkey) => {
-                                        // Fast path: use the pubkey cache which is probably
-                                        // initialised at the head.
-                                        match state.get_validator_index_read_only(pubkey) {
-                                            Ok(result) => result,
-                                            Err(e) => {
-                                                // Slow path, fall back to iteration.
-                                                debug!(
-                                                    log,
-                                                    "Validator look-up cache miss";
-                                                    "reason" => ?e,
-                                                );
-                                                state
-                                                    .validators()
-                                                    .iter()
-                                                    .position(|v| v.pubkey == *pubkey)
-                                            }
-                                        }
+                                        state.validators().iter().position(|v| v.pubkey == *pubkey)
                                     }
                                     ValidatorId::Index(index) => Some(*index as usize),
                                 };
