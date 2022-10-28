f2f920dec8533e123991b1c7cfcb3c5bf09e68cc
Giulio rebuffo <giulio.rebuffo@gmail.com>
2022-10-28 03:23:49 +0000

    Added lightclient server side containers (#3655)
    ## Issue Addressed
    
    This PR partially addresses #3651
    
    ## Proposed Changes
    This PR adds the following containers types from [the lightclient specs](https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/light-client/sync-protocol.md): `LightClientUpdate`, `LightClientFinalityUpdate`, `LightClientOptimisticUpdate` and `LightClientBootstrap`. It also implements the creation of each updates as delined by this [document](https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/light-client/full-node.md).
    
    ## Additional Info
    
    Here is a brief description of what each of these container signify:
    
    `LightClientUpdate`: This container is only provided by server (full node) to lightclients when catching up new sync committees beetwen periods and we want possibly one lightclient update ready for each post-altair period the lighthouse node go over. it is needed in the resp/req in method `light_client_update_by_range`.
    
    `LightClientFinalityUpdate/LightClientFinalityUpdate`: Lighthouse will need only the latest of each of this kind of updates, so no need to store them in the database, we can just store the latest one of each one in memory and then just supply them via gossip or respreq, only the latest ones are served by a full node. finality updates marks the transition to a new finalized header, while optimistic updates signify new non-finalized header which are imported optimistically.
    
    `LightClientBootstrap`: This object is retrieved by lightclients during the bootstrap process after a finalized checkpoint is retrieved, ideally we want to store a LightClientBootstrap for each finalized root and then serve each of them by finalized root in respreq protocol id `light_client_bootstrap`.
    
    Little digression to how we implement the creation of each updates: the creation of a optimistic/finality update is just a version of the lightclient_update creation mechanism with less fields being set, there is underlying concept of inheritance, if you look at the specs it becomes very obvious that a lightclient update is just an extension of a finality update and a finality update an extension to an optimistic update.
    
    ## Extra note
    
    `LightClientStore` is not implemented as it is only useful as internal storage design for the lightclient side.

---
+++ consensus/types/src/light_client_optimistic_update.rs
@@ -0,0 +1,59 @@
+use super::{BeaconBlockHeader, EthSpec, Slot, SyncAggregate};
+use crate::{
+    light_client_update::Error, test_utils::TestRandom, BeaconBlock, BeaconState, ChainSpec,
+};
+use serde_derive::{Deserialize, Serialize};
+use ssz_derive::{Decode, Encode};
+use test_random_derive::TestRandom;
+use tree_hash::TreeHash;
+
+/// A LightClientOptimisticUpdate is the update we send on each slot,
+/// it is based off the current unfinalized epoch is verified only against BLS signature.
+#[cfg_attr(feature = "arbitrary-fuzz", derive(arbitrary::Arbitrary))]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Encode, Decode, TestRandom)]
+#[serde(bound = "T: EthSpec")]
+pub struct LightClientOptimisticUpdate<T: EthSpec> {
+    /// The last `BeaconBlockHeader` from the last attested block by the sync committee.
+    pub attested_header: BeaconBlockHeader,
+    /// current sync aggreggate
+    pub sync_aggregate: SyncAggregate<T>,
+    /// Slot of the sync aggregated singature
+    pub signature_slot: Slot,
+}
+
+impl<T: EthSpec> LightClientOptimisticUpdate<T> {
+    pub fn new(
+        chain_spec: ChainSpec,
+        block: BeaconBlock<T>,
+        attested_state: BeaconState<T>,
+    ) -> Result<Self, Error> {
+        let altair_fork_epoch = chain_spec
+            .altair_fork_epoch
+            .ok_or(Error::AltairForkNotActive)?;
+        if attested_state.slot().epoch(T::slots_per_epoch()) < altair_fork_epoch {
+            return Err(Error::AltairForkNotActive);
+        }
+
+        let sync_aggregate = block.body().sync_aggregate()?;
+        if sync_aggregate.num_set_bits() < chain_spec.min_sync_committee_participants as usize {
+            return Err(Error::NotEnoughSyncCommitteeParticipants);
+        }
+
+        // Compute and validate attested header.
+        let mut attested_header = attested_state.latest_block_header().clone();
+        attested_header.state_root = attested_state.tree_hash_root();
+        Ok(Self {
+            attested_header,
+            sync_aggregate: sync_aggregate.clone(),
+            signature_slot: block.slot(),
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::MainnetEthSpec;
+
+    ssz_tests!(LightClientOptimisticUpdate<MainnetEthSpec>);
+}
