e6b2079d2a0d59bc1604dbac79c9aaf8c94d0331
Diva M <divma@protonmail.com>
2022-11-03 19:15:43 -0500

    update test

--- consensus/tree_hash/src/merkle_hasher.rs
+++ consensus/tree_hash/src/merkle_hasher.rs
@@ -368,7 +368,7 @@ mod test {
     fn context_size() {
         assert_eq!(
             mem::size_of::<HalfNode>(),
-            232,
+            216,
             "Halfnode size should be as expected"
         );
     }
