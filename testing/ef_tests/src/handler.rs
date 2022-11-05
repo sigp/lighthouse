496a66d7b63ec48fda538f31e5c92c001443fbba
Giulio Rebuffo <giulio.rebuffo@gmail.com>
2022-11-05 02:11:25 +0100

    added all tests

--- testing/ef_tests/src/handler.rs
+++ testing/ef_tests/src/handler.rs
@@ -639,8 +639,6 @@ impl<E: EthSpec + TypeName> Handler for MerkleProofValidityHandler<E> {
 
     fn is_enabled_for_fork(&self, fork_name: ForkName) -> bool {
         fork_name != ForkName::Base
-            && fork_name != ForkName::Altair
-            && cfg!(not(feature = "fake_crypto"))
     }
 }
 
