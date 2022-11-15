4f4263605ea59a7bcc9122496fba6c4dab1b41e7
Age Manning <Age@AgeManning.com>
2022-11-15 13:14:21 +1100

    fmt

--- beacon_node/http_api/tests/common.rs
+++ beacon_node/http_api/tests/common.rs
@@ -2,8 +2,8 @@ use beacon_chain::{
     test_utils::{BeaconChainHarness, EphemeralHarnessType},
     BeaconChain, BeaconChainTypes,
 };
-use eth2::{BeaconNodeHttpClient, Timeouts};
 use directory::DEFAULT_ROOT_DIR;
+use eth2::{BeaconNodeHttpClient, Timeouts};
 use http_api::{Config, Context};
 use lighthouse_network::{
     discv5::enr::{CombinedKey, EnrBuilder},
@@ -143,7 +143,7 @@ pub async fn create_api_server_on_port<T: BeaconChainTypes>(
             allow_origin: None,
             tls_config: None,
             allow_sync_stalled: false,
-            data_dir: std::path::PathBuf::from(DEFAULT_ROOT_DIR), 
+            data_dir: std::path::PathBuf::from(DEFAULT_ROOT_DIR),
             spec_fork_name: None,
         },
         chain: Some(chain.clone()),
