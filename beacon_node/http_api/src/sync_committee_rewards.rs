c3c4c33cc656c4e163e2580a3f99915394f06d8f
kevinbogner <kevbogner@gmail.com>
2022-12-03 11:01:40 +0100

    get block, get state, convert slot, compute reward

--- beacon_node/http_api/src/sync_committee_rewards.rs
+++ beacon_node/http_api/src/sync_committee_rewards.rs
@@ -9,14 +9,28 @@ pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
     block_id: BlockId,
     validators: Vec<ValidatorId>,
     log: Logger
-) -> Result<SyncCommitteeRewards, Error> {
+) -> Result<SyncCommitteeRewards, Error> Ok({
 
     // Get block_id with full_block()
+    let block = chain
+        .get_block(block_id)?
+        .ok_or(Error::UnknownBlock(block_id))?
+        .full_block()?;
         
     // Get state from chain
+    let state = chain
+        .get_state(&block.state_root(), Some(block.slot()))?
+        .ok_or(Error::UnknownState(block.state_root()))?;
+
+    // Convert a slot into the canonical block root from that slot: block_id.root(&chain).
+    let block_root = block_id.root(&chain)?;
     
     // Call compute_sync_aggregate_reward
+    let rewards = state.compute_sync_aggregate_reward(&block_root, &validators, &log)?;
     
-    // Stuff things into SyncCommitteeRewards
-
-}
+    // Create SyncCommitteeRewards with calculated rewards
+    Ok(SyncCommitteeRewards {
+        rewards,
+    })
+    
+});
