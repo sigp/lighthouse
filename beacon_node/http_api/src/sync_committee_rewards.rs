29d18223ff8c9cbebed7a93285f9768f3f1ba27a
kevinbogner <kevbogner@gmail.com>
2022-12-09 11:08:48 +0100

    fix imports, add comment

--- beacon_node/http_api/src/sync_committee_rewards.rs
+++ beacon_node/http_api/src/sync_committee_rewards.rs
@@ -3,7 +3,7 @@ use beacon_chain::{BeaconChain, BeaconChainTypes};
 use eth2::types::ValidatorId;
 use eth2::lighthouse::SyncCommitteeAttestationRewards;
 use slog::Logger;
-use state_processing::{per_block_processing::altair::sync_committee::compute_sync_aggregate_rewards, BlockReplayer};
+use state_processing::{per_block_processing::altair::sync_committee::compute_sync_aggregate_rewards};
 use crate::BlockId;
 
 pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
@@ -11,7 +11,7 @@ pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
     block_id: BlockId,
     validators: Vec<ValidatorId>,
     log: Logger
-) -> Result<T, E> {
+) -> Result<SyncCommitteeAttestationRewards, E> {
 
     let spec = chain.spec;
 
@@ -21,14 +21,12 @@ pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
 
     let state_root = block.state_root();
 
-    let state = chain.get_state(&state_root, Some(slot))?.unwrap(); // Some comments here to
-                                                                    // indicate this is not the
-                                                                    // exact state but close
-                                                                    // enough
+    // Technically we should use the pre-block state, but it won't matter because
+    // compute_sync_aggregate_rewards() only uses state.get_total_active_balance() which only changes on epoch boundaries.
+    // So, the "wrong" state will still give the same result.
+    let state = chain.get_state(&state_root, Some(slot))?.unwrap();
 
     let (_, rewards) = compute_sync_aggregate_rewards(&state, &spec)?;
-
-
     
     // Create SyncCommitteeRewards with calculated rewards
     Ok(SyncCommitteeAttestationRewards{
