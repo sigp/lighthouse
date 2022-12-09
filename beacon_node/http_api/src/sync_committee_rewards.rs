9cb4fb0e035463f3e45fae9b0cdd342f8b10952e
navie <naviechan@gmail.com>
2022-12-09 19:01:18 +0800

    Fixed conflict

--- beacon_node/http_api/src/sync_committee_rewards.rs
+++ beacon_node/http_api/src/sync_committee_rewards.rs
@@ -4,10 +4,7 @@ use eth2::types::ValidatorId;
 use eth2::lighthouse::SyncCommitteeAttestationRewards;
 use slog::Logger;
 use state_processing::{per_block_processing::altair::sync_committee::compute_sync_aggregate_rewards};
-<<<<<<< HEAD
-=======
 use warp_utils::reject::{beacon_chain_error, custom_bad_request};
->>>>>>> 0f352220 (Changed fields to optional in sync_committee_attestation_rewards)
 use crate::BlockId;
 
 pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
@@ -15,11 +12,7 @@ pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
     block_id: BlockId,
     validators: Vec<ValidatorId>,
     log: Logger
-<<<<<<< HEAD
-) -> Result<SyncCommitteeAttestationRewards, E> {
-=======
 ) -> Result<SyncCommitteeAttestationRewards, warp::Rejection> {
->>>>>>> 0f352220 (Changed fields to optional in sync_committee_attestation_rewards)
 
     let spec = &chain.spec;
 
@@ -29,14 +22,9 @@ pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
 
     let state_root = block.state_root();
 
-<<<<<<< HEAD
     // Technically we should use the pre-block state, but it won't matter because
     // compute_sync_aggregate_rewards() only uses state.get_total_active_balance() which only changes on epoch boundaries.
     // So, the "wrong" state will still give the same result.
-    let state = chain.get_state(&state_root, Some(slot))?.unwrap();
-
-    let (_, rewards) = compute_sync_aggregate_rewards(&state, &spec)?;
-=======
     let state = chain
         .get_state(&state_root, Some(slot))
         .map_err(beacon_chain_error)?
@@ -45,8 +33,6 @@ pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
     let (participant_reward, _) = compute_sync_aggregate_rewards(&state, spec)
         .map_err(|_| custom_bad_request(String::from("Unable to get rewards")))?;
 
-
->>>>>>> 0f352220 (Changed fields to optional in sync_committee_attestation_rewards)
     
     // Create SyncCommitteeRewards with calculated rewards
     Ok(SyncCommitteeAttestationRewards{
