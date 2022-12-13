f396470870ad1eee98027b56743af7145d714eb8
Jimmy Chen <jchen.tc@gmail.com>
2022-12-14 10:04:44 +1100

    add test for a basic struct serde

--- beacon_node/execution_layer/src/lib.rs
+++ beacon_node/execution_layer/src/lib.rs
@@ -739,7 +739,7 @@ impl<T: EthSpec> ExecutionLayer<T> {
                         self.log(),
                         "Requested blinded execution payload";
                         "relay_fee_recipient" => match &relay_result {
-                            Ok(Some(r)) => format!("{:?}", r.data.message.header.fee_recipient()),
+                            Ok(Some(r)) => format!("{:?}", r.data.message.header().fee_recipient()),
                             Ok(None) => "empty response".to_string(),
                             Err(_) => "request failed".to_string(),
                         },
@@ -775,7 +775,7 @@ impl<T: EthSpec> ExecutionLayer<T> {
                             Ok(ProvenancedPayload::Local(local))
                         }
                         (Ok(Some(relay)), Ok(local)) => {
-                            let header = &relay.data.message.header;
+                            let header = &relay.data.message.header();
 
                             info!(
                                 self.log(),
@@ -799,7 +799,7 @@ impl<T: EthSpec> ExecutionLayer<T> {
                                     // NOTE       the comment above was removed in the
                                     //            rebase with unstable.. I think it goes
                                     //            here now?
-                                    BlockProposalContents::Payload(relay.data.message.header),
+                                    BlockProposalContents::Payload(relay.data.message.header().clone()),
                                 )),
                                 Err(reason) if !reason.payload_invalid() => {
                                     info!(
@@ -830,7 +830,7 @@ impl<T: EthSpec> ExecutionLayer<T> {
                             }
                         }
                         (Ok(Some(relay)), Err(local_error)) => {
-                            let header = &relay.data.message.header;
+                            let header = &relay.data.message.header();
 
                             info!(
                                 self.log(),
@@ -854,7 +854,7 @@ impl<T: EthSpec> ExecutionLayer<T> {
                                     // NOTE       the comment above was removed in the
                                     //            rebase with unstable.. I think it goes
                                     //            here now?
-                                    BlockProposalContents::Payload(relay.data.message.header),
+                                    BlockProposalContents::Payload(relay.data.message.header().clone()),
                                 )),
                                 // If the payload is valid then use it. The local EE failed
                                 // to produce a payload so we have no alternative.
@@ -863,7 +863,7 @@ impl<T: EthSpec> ExecutionLayer<T> {
                                     // NOTE       the comment above was removed in the
                                     //            rebase with unstable.. I think it goes
                                     //            here now?
-                                    BlockProposalContents::Payload(relay.data.message.header),
+                                    BlockProposalContents::Payload(relay.data.message.header().clone()),
                                 )),
                                 Err(reason) => {
                                     metrics::inc_counter_vec(
@@ -1832,11 +1832,11 @@ fn verify_builder_bid<T: EthSpec, Payload: AbstractExecPayload<T>>(
     spec: &ChainSpec,
 ) -> Result<(), Box<InvalidBuilderPayload>> {
     let is_signature_valid = bid.data.verify_signature(spec);
-    let header = &bid.data.message.header;
-    let payload_value = bid.data.message.value;
+    let header = &bid.data.message.header();
+    let payload_value = bid.data.message.value().clone();
 
     // Avoid logging values that we can't represent with our Prometheus library.
-    let payload_value_gwei = bid.data.message.value / 1_000_000_000;
+    let payload_value_gwei = bid.data.message.value() / 1_000_000_000;
     if payload_value_gwei <= Uint256::from(i64::max_value()) {
         metrics::set_gauge_vec(
             &metrics::EXECUTION_LAYER_PAYLOAD_BIDS,
@@ -1882,7 +1882,7 @@ fn verify_builder_bid<T: EthSpec, Payload: AbstractExecPayload<T>>(
     } else if !is_signature_valid {
         Err(Box::new(InvalidBuilderPayload::Signature {
             signature: bid.data.signature.clone(),
-            pubkey: bid.data.message.pubkey,
+            pubkey: bid.data.message.pubkey().clone(),
         }))
     } else {
         Ok(())
--- consensus/types/src/builder_bid.rs
+++ consensus/types/src/builder_bid.rs
@@ -1,27 +1,127 @@
 use crate::{
     AbstractExecPayload, ChainSpec, EthSpec, ExecPayload, ExecutionPayloadHeader, SignedRoot,
-    Uint256,
+    Uint256, MainnetEthSpec,
 };
 use bls::PublicKeyBytes;
 use bls::Signature;
 use serde::{Deserialize as De, Deserializer, Serialize as Ser, Serializer};
 use serde_derive::{Deserialize, Serialize};
+use serde_with::As;
 use serde_with::{serde_as, DeserializeAs, SerializeAs};
+use superstruct::superstruct;
 use std::marker::PhantomData;
 use tree_hash_derive::TreeHash;
 
-#[serde_as]
+#[superstruct(
+    variants(Bellatrix, Eip4844),
+    variant_attributes(
+        derive(PartialEq, Debug, Serialize, Deserialize, TreeHash, Clone),
+        serde(bound = "E: EthSpec, Payload: ExecPayload<E>", deny_unknown_fields)
+    )
+)]
 #[derive(PartialEq, Debug, Serialize, Deserialize, TreeHash, Clone)]
-#[serde(bound = "E: EthSpec, Payload: ExecPayload<E>")]
+#[serde(bound = "E: EthSpec, Payload: ExecPayload<E>", deny_unknown_fields)]
+#[tree_hash(enum_behaviour = "transparent")]
 pub struct BuilderBid<E: EthSpec, Payload: AbstractExecPayload<E>> {
-    #[serde_as(as = "BlindedPayloadAsHeader<E>")]
+    #[serde(with = "As::<BlindedPayloadAsHeader<E>>")]
     pub header: Payload,
     #[serde(with = "eth2_serde_utils::quoted_u256")]
     pub value: Uint256,
     pub pubkey: PublicKeyBytes,
+    // #[superstruct(only(Eip4844))]
+    // pub blob_kzg_commitments: VariableList<KzgCommitment, E::MaxBlobsPerBlock>,
     #[serde(skip)]
     #[tree_hash(skip_hashing)]
     _phantom_data: PhantomData<E>,
+}
+
+#[derive(Debug)]
+pub enum SerdeError {
+    Serde(serde_json::Error)
+}
+
+impl From<serde_json::Error> for SerdeError {
+    fn from(err: serde_json::Error) -> Self {
+        SerdeError::Serde(err)
+    }
+}
+
+pub fn deserialize_bid<E: EthSpec, Payload: AbstractExecPayload<E>>(str: &str) -> Result<BuilderBid<E, Payload>, SerdeError> {
+    dbg!(str);
+    let bid = serde_json::from_str(str)?;
+    Ok(bid)
+}
+
+
+#[superstruct(
+    variants(A,B),
+    variant_attributes(derive(Debug, Serialize, Deserialize))
+)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BasicStruct {
+    #[serde(with = "eth2_serde_utils::quoted_u8")]
+    pub field: u8,
+    #[serde(with = "eth2_serde_utils::quoted_u64")]
+    #[superstruct(only(A))]
+    pub fieldA: u64,
+    #[serde(with = "eth2_serde_utils::quoted_u64")]
+    #[superstruct(only(B))]
+    pub fieldB: u64
+}
+
+#[cfg(test)]
+mod tests {
+    use log::debug;
+    use crate::{ExecutionPayload, BlindedPayload, ExecutionPayloadHeaderEip4844};
+    use super::*;
+    
+    #[test]
+    fn test_serialize() {
+        let a = BasicStructA {
+            field: 1,
+            fieldA: 2,
+        };
+        print!("{}", serde_json::to_string_pretty(&a).unwrap());
+    }
+
+    #[test]
+    fn test_deserialize() {
+        let a = r#"{
+            field: 1,
+            fieldA: 2,
+        }"#;
+        dbg!(serde_json::from_str::<BasicStruct>(a));
+    }
+
+    // #[test]
+    fn test_deserialize_bid() {
+        let str = r#"{
+            "header": {
+              "parent_hash": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2",
+              "fee_recipient": "0xabcf8e0d4e9587369b2301d0790347320302cc09",
+              "state_root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2",
+              "receipts_root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2",
+              "logs_bloom": "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+              "prev_randao": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2",
+              "block_number": "1",
+              "gas_limit": "1",
+              "gas_used": "1",
+              "timestamp": "1",
+              "extra_data": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2",
+              "base_fee_per_gas": "1",
+              "excess_data_gas": "1",
+              "block_hash": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2",
+              "transactions_root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2"
+            },
+            "value": "1",
+            "pubkey": "0x93247f2209abcacf57b75a51dafae777f9dd38bc7053d1af526f220a7489a6d3a2753e5f3e8b1cfe39b56f43611df74a",
+            "blob_kzg_commitments": [
+        "0xa94170080872584e54a1cf092d845703b13907f2e6b3b1c0ad573b910530499e3bcd48c6378846b80d2bfa58c81cf3d5"
+            ]
+          }"#;
+        let result = deserialize_bid::<MainnetEthSpec, BlindedPayload<MainnetEthSpec>>(str);
+        dbg!(result);
+    }
 }
 
 impl<E: EthSpec, Payload: AbstractExecPayload<E>> SignedRoot for BuilderBid<E, Payload> {}
@@ -61,7 +161,7 @@ impl<'de, E: EthSpec, Payload: AbstractExecPayload<E>> DeserializeAs<'de, Payloa
 impl<E: EthSpec, Payload: AbstractExecPayload<E>> SignedBuilderBid<E, Payload> {
     pub fn verify_signature(&self, spec: &ChainSpec) -> bool {
         self.message
-            .pubkey
+            .pubkey()
             .decompress()
             .map(|pubkey| {
                 let domain = spec.get_builder_domain();
