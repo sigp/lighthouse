db29cb08a6c73a37f5296d5bceffc182be56d234
Jimmy Chen <jchen.tc@gmail.com>
2022-12-21 00:18:05 +1100

    Add bootnode binary variable in testnet scripts

--- scripts/local_testnet/el_bootnode.sh
+++ scripts/local_testnet/el_bootnode.sh
@@ -1,4 +1,4 @@
 priv_key="02fd74636e96a8ffac8e7b01b0de8dea94d6bcf4989513b38cf59eb32163ff91"
 
 
-/home/sean/CLionProjects/eip4844-interop/geth/go-ethereum/build/bin/bootnode --nodekeyhex $priv_key
+$BOOTNODE_BINARY --nodekeyhex $priv_key
