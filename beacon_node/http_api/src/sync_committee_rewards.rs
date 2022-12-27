3d3f71c8544026dbd6ee2345b470cde092f62ab0
navie <naviechan@gmail.com>
2022-12-27 19:10:16 +0800

    Use post state for sync committee rewards

--- beacon_node/http_api/src/sync_committee_rewards.rs
+++ beacon_node/http_api/src/sync_committee_rewards.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
-use beacon_chain::{BeaconChain, BeaconChainTypes};
+use beacon_chain::{BeaconChain, BeaconChainTypes, BeaconChainError};
 use eth2::types::ValidatorId;
 use eth2::lighthouse::{SyncCommitteeAttestationRewards, SyncCommitteeAttestationReward};
 use slog::{debug, Logger};
-use warp_utils::reject::{beacon_chain_error, custom_bad_request};
+use state_processing::BlockReplayer;
+use types::{BeaconState, SignedBlindedBeaconBlock};
+use warp_utils::reject::beacon_chain_error;
 use crate::BlockId;
 
 pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
@@ -13,19 +15,11 @@ pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
     log: Logger
 ) -> Result<SyncCommitteeAttestationRewards, warp::Rejection> {
 
-    let spec = &chain.spec;
 
-    let (block, execution_optimistic) = block_id.blinded_block(&chain)?;
+    let (block, _) = block_id.blinded_block(&chain)?;
 
-    let slot = block.slot();
-
-    let state_root = block.state_root();
-
-    // TODO: retrieve post state here
-    let mut state = chain
-        .get_state(&state_root, Some(slot))
-        .map_err(beacon_chain_error)?
-        .ok_or_else(|| custom_bad_request(String::from("Unable to get state")))?;
+    let mut state = get_state_before_applying_block(chain.clone(), block.clone())
+        .map_err(beacon_chain_error)?;
 
     let reward_payload = chain
         .compute_sync_committee_rewards(block.message(), &mut state)
@@ -66,4 +60,28 @@ pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
     })
 
     
+}
+
+fn get_state_before_applying_block<T:BeaconChainTypes>(
+    chain: Arc<BeaconChain<T>>,
+    block: SignedBlindedBeaconBlock<T::EthSpec>
+) -> Result<BeaconState<T::EthSpec>, BeaconChainError> {
+
+    let parent_block: SignedBlindedBeaconBlock<T::EthSpec> = chain
+        .get_blinded_block(&block.parent_root())?
+        .ok_or_else(|| BeaconChainError::SyncCommitteeRewardsSyncError)?;
+
+
+    let parent_state = chain
+        .get_state(&parent_block.state_root(), Some(parent_block.slot()))?
+        .ok_or_else(|| BeaconChainError::SyncCommitteeRewardsSyncError)?;
+
+    let replayer = BlockReplayer::new(parent_state, &chain.spec)
+        .no_signature_verification()
+        .state_root_iter([Ok((parent_block.state_root(), parent_block.slot()))].into_iter())
+        .minimal_block_root_verification()
+        .apply_blocks(vec![], Some(block.slot()))
+        .map_err(|_: BeaconChainError| BeaconChainError::SyncCommitteeRewardsSyncError)?;
+
+    Ok(replayer.into_state())
 }
