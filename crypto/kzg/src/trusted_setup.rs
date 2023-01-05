5510c7d5fcea17de1b72f16dda6de724596748b4
Pawan Dhananjay <pawandhananjay@gmail.com>
2023-01-05 15:16:13 +0530

    Fix trusted setup serialize and deserialize

--- crypto/kzg/src/trusted_setup.rs
+++ crypto/kzg/src/trusted_setup.rs
@@ -1,49 +1,124 @@
-use serde::de::Deserializer;
-use serde_derive::Deserialize;
+use c_kzg::{BYTES_PER_G1_POINT, BYTES_PER_G2_POINT};
+use serde::{
+    de::{self, Deserializer, Visitor},
+    Deserialize, Serialize,
+};
 
-const BYTES_PER_G1_POINT: usize = 48;
-const BYTES_PER_G2_POINT: usize = 96;
+/// Wrapper over a BLS G1 point's byte representation.
+#[derive(Debug, Clone, PartialEq)]
+struct G1Point([u8; BYTES_PER_G1_POINT]);
 
-#[derive(Debug, Clone, PartialEq, Deserialize)]
+/// Wrapper over a BLS G2 point's byte representation.
+#[derive(Debug, Clone, PartialEq)]
+struct G2Point([u8; BYTES_PER_G2_POINT]);
+
+/// Contains the trusted setup parameters that are required to instantiate a
+/// `c_kzg::KzgSettings` object.
+///
+/// The serialize/deserialize implementations are written according to
+/// the format specified in the the ethereum consensus specs trusted setup files.
+///
+/// See https://github.com/ethereum/consensus-specs/blob/dev/presets/mainnet/trusted_setups/testing_trusted_setups.json
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct TrustedSetup {
-    #[serde(rename = "SETUP_G1")]
-    #[serde(deserialize_with = "deserialize_g1_array")]
-    pub g1_points: Vec<[u8; BYTES_PER_G1_POINT]>,
-    #[serde(rename = "SETUP_G2")]
-    #[serde(deserialize_with = "deserialize_g2_array")]
-    pub g2_points: Vec<[u8; BYTES_PER_G2_POINT]>,
+    #[serde(rename = "setup_G1")]
+    g1_points: Vec<G1Point>,
+    #[serde(rename = "setup_G2")]
+    g2_points: Vec<G2Point>,
 }
 
-fn deserialize_g1_array<'de, D>(deserializer: D) -> Result<Vec<[u8; BYTES_PER_G1_POINT]>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let decoded: Vec<Vec<u8>> = serde::de::Deserialize::deserialize(deserializer)?;
-    let decoded: Vec<[u8; BYTES_PER_G1_POINT]> = decoded
-        .into_iter()
-        .map(|point| {
-            let mut res = [0; BYTES_PER_G1_POINT];
-            res.copy_from_slice(point.as_ref());
-            res
-        })
-        .collect();
+impl TrustedSetup {
+    pub fn g1_points(&self) -> Vec<[u8; BYTES_PER_G1_POINT]> {
+        self.g1_points.iter().map(|p| p.0).collect()
+    }
 
-    Ok(decoded)
+    pub fn g2_points(&self) -> Vec<[u8; BYTES_PER_G2_POINT]> {
+        self.g2_points.iter().map(|p| p.0).collect()
+    }
 }
 
-fn deserialize_g2_array<'de, D>(deserializer: D) -> Result<Vec<[u8; BYTES_PER_G2_POINT]>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let decoded: Vec<Vec<u8>> = serde::de::Deserialize::deserialize(deserializer)?;
-    let decoded: Vec<[u8; BYTES_PER_G2_POINT]> = decoded
-        .into_iter()
-        .map(|point| {
-            let mut res = [0; BYTES_PER_G2_POINT];
-            res.copy_from_slice(point.as_ref());
-            res
-        })
-        .collect();
+impl Serialize for G1Point {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let point = hex::encode(self.0);
+        serializer.serialize_str(&point)
+    }
+}
 
-    Ok(decoded)
+impl Serialize for G2Point {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let point = hex::encode(self.0);
+        serializer.serialize_str(&point)
+    }
+}
+
+impl<'de> Deserialize<'de> for G1Point {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct G1PointVisitor;
+
+        impl<'de> Visitor<'de> for G1PointVisitor {
+            type Value = G1Point;
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("A 48 byte hex encoded string")
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                let point = hex::decode(strip_prefix(v))
+                    .map_err(|e| de::Error::custom(format!("Failed to decode G1 point: {}", e)))?;
+                let mut res = [0; BYTES_PER_G1_POINT];
+                res.copy_from_slice(&point);
+                Ok(G1Point(res))
+            }
+        }
+
+        deserializer.deserialize_str(G1PointVisitor)
+    }
+}
+
+impl<'de> Deserialize<'de> for G2Point {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct G2PointVisitor;
+
+        impl<'de> Visitor<'de> for G2PointVisitor {
+            type Value = G2Point;
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("A 96 byte hex encoded string")
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                let point = hex::decode(strip_prefix(v))
+                    .map_err(|e| de::Error::custom(format!("Failed to decode G2 point: {}", e)))?;
+                let mut res = [0; BYTES_PER_G2_POINT];
+                res.copy_from_slice(&point);
+                Ok(G2Point(res))
+            }
+        }
+
+        deserializer.deserialize_str(G2PointVisitor)
+    }
+}
+
+fn strip_prefix(s: &str) -> &str {
+    if let Some(stripped) = s.strip_prefix("0x") {
+        stripped
+    } else {
+        s
+    }
 }
