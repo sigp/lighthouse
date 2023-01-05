c7aa610401afad662001f695c7d55c1fc21bf0a2
kevinbogner <kevbogner@gmail.com>
2023-01-05 16:12:10 +0100

    add loops for flag_index and index

--- beacon_node/http_api/src/attestation_rewards.rs
+++ beacon_node/http_api/src/attestation_rewards.rs
@@ -7,6 +7,7 @@ use participation_cache::ParticipationCache;
 use state_processing::{per_epoch_processing::altair::{participation_cache, rewards_and_penalties::get_flag_weight}, common::altair::{BaseRewardPerIncrement, get_base_reward}};
 use types::{Epoch, EthSpec};
 use types::consts::altair::WEIGHT_DENOMINATOR;
+use types::consts::altair::{TIMELY_SOURCE_FLAG_INDEX, TIMELY_TARGET_FLAG_INDEX, TIMELY_HEAD_FLAG_INDEX};
 
 pub fn compute_attestation_rewards<T: BeaconChainTypes>(
     chain: Arc<BeaconChain<T>>,
@@ -29,102 +30,104 @@ pub fn compute_attestation_rewards<T: BeaconChainTypes>(
         Ok(None) => return Err(warp_utils::reject::custom_server_error("Unable to get state root".to_owned())),
         Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get state root".to_owned())),
     };
-
+    
     //Get state from state_root and state_slot
     let state = match chain.get_state(&state_root, Some(state_slot)) {
         Ok(Some(state)) => state,
         Ok(None) => return Err(warp_utils::reject::custom_server_error("State not found".to_owned())),
         Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get state".to_owned())),
     };
-
+    
     //--- Calculate ideal rewards for 33 (0...32) values ---//
-
+    
     //Create ParticipationCache
     let participation_cache = match ParticipationCache::new(&state, spec) {
         Ok(participation_cache) => participation_cache,
         Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get participation cache".to_owned())),
     };
+    
+    //Get previous_epoch through state.previous_epoch()
+    let previous_epoch = state.previous_epoch();
 
     //TODO Get flag_index as usize
     let flag_index = 0;
+    
+    for flag_index in [TIMELY_SOURCE_FLAG_INDEX, TIMELY_TARGET_FLAG_INDEX, TIMELY_HEAD_FLAG_INDEX].iter() {
 
-    //Calculate flag(head, target, source) for the 33 effective_balance values to have a map of (flag, effective_balance) -> ideal_reward
+        //Get unslashed_participating_indices
+        let unslashed_participating_indices = match participation_cache.get_unslashed_participating_indices(*flag_index, previous_epoch) {
+            Ok(unslashed_participating_indices) => unslashed_participating_indices,
+            Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get unslashed participating indices".to_owned())),
+        };    
+        
+        //Get weight as u64
+        let weight = match get_flag_weight(*flag_index) {
+            Ok(weight) => weight,
+            Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get weight".to_owned())),
+        };
+        
+        //Get unslashed_participating_balance
+        let unslashed_participating_balance = match unslashed_participating_indices.total_balance() {
+            Ok(unslashed_participating_balance) => unslashed_participating_balance,
+            Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get unslashed participating balance".to_owned())),
+        };    
+        
+        //Get unslashed_participating_increments
+        let unslashed_participating_increments = match unslashed_participating_balance.safe_div(spec.effective_balance_increment) {
+            Ok(unslashed_participating_increments) => unslashed_participating_increments,
+            Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get unslashed participating increments".to_owned())),
+        };  
+        
+        //Get total_active_balance through current_epoch_total_active_balance
+        let total_active_balance = participation_cache.current_epoch_total_active_balance();
+        
+        //Get active_increments
+        let active_increments = match total_active_balance.safe_div(spec.effective_balance_increment) {
+            Ok(active_increments) => active_increments,
+            Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get active increments".to_owned())),
+        };    
+        
+        //Get base_reward_per_increment through BaseRewardPerIncrement::new
+        let base_reward_per_increment = match BaseRewardPerIncrement::new(total_active_balance, spec) {
+            Ok(base_reward_per_increment) => base_reward_per_increment,
+            Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get base reward per increment".to_owned())),
+        };
+
+        //Get index from participation_cache.eligible_validator_indices
+        let index = participation_cache.eligible_validator_indices();
+        
+        for i in index {
+            let base_reward = get_base_reward(&state, *i, base_reward_per_increment, spec);
+            //Unwrap base_reward to u64
+            let base_reward = match base_reward {
+                Ok(base_reward) => base_reward,
+                Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get base reward".to_owned())),
+            };
+            //Calculate reward_numerator = base_reward * weight * unslashed_participating_increments with Error handling
+            let reward_numerator = match base_reward.safe_mul(weight).and_then(|reward_numerator| {
+                reward_numerator.safe_mul(unslashed_participating_increments)}) {
+                Ok(reward_numerator) => reward_numerator,
+                Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to calculate reward numerator".to_owned())),
+            };
+        
+            //Calculate reward = reward_numerator // (active_increments * WEIGHT_DENOMINATOR)
+            let reward = match reward_numerator.safe_div(active_increments) {
+                Ok(reward) => match reward.safe_div(WEIGHT_DENOMINATOR) {
+                    Ok(reward) => reward,
+                    Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to calculate reward: Division by WEIGHT_DENOMINATOR failed".to_owned())),
+                },
+                Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to calculate reward: Division by active_increments failed".to_owned())),
+            };
+        }
+    }
+   
+    //TODO Calculate flag(head, target, source) for the 33 effective_balance values to have a map of (flag, effective_balance) -> ideal_reward
     let mut ideal_rewards = HashMap::new();
-
+    
     for effective_balance in 0..33 {
         let flag =  0; // calculate_flag();
         ideal_rewards.insert(flag, effective_balance);
     }
-
-    //Get weight as u64
-    let weight = match get_flag_weight(flag_index) {
-        Ok(weight) => weight,
-        Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get weight".to_owned())),
-    };
-    
-    //Get total_active_balance through current_epoch_total_active_balance
-    let total_active_balance = participation_cache.current_epoch_total_active_balance();
-    
-    //Get base_reward_per_increment through BaseRewardPerIncrement::new
-    let base_reward_per_increment = match BaseRewardPerIncrement::new(total_active_balance, spec) {
-        Ok(base_reward_per_increment) => base_reward_per_increment,
-        Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get base reward per increment".to_owned())),
-    };    
-
-    //Get index from participation_cache.eligible_validator_indices
-    let index = participation_cache.eligible_validator_indices();
-
-    //TODO Loop through index
-    let base_reward = get_base_reward(&state, index[0], base_reward_per_increment, spec);
-
-    //Get previous_epoch through state.previous_epoch()
-    let previous_epoch = state.previous_epoch();
-
-    //Get unslashed_participating_indices
-    let unslashed_participating_indices = match participation_cache.get_unslashed_participating_indices(flag_index, previous_epoch) {
-        Ok(unslashed_participating_indices) => unslashed_participating_indices,
-        Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get unslashed participating indices".to_owned())),
-    };    
-
-    //Get unslashed_participating_balance
-    let unslashed_participating_balance = match unslashed_participating_indices.total_balance() {
-        Ok(unslashed_participating_balance) => unslashed_participating_balance,
-        Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get unslashed participating balance".to_owned())),
-    };    
-
-    //Get unslashed_participating_increments
-    let unslashed_participating_increments = match unslashed_participating_balance.safe_div(spec.effective_balance_increment) {
-        Ok(unslashed_participating_increments) => unslashed_participating_increments,
-        Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get unslashed participating increments".to_owned())),
-    };  
-
-    //Unwrap base_reward to u64
-    let base_reward = match base_reward {
-        Ok(base_reward) => base_reward,
-        Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get base reward".to_owned())),
-    };
-    
-    //Calculate reward_numerator = base_reward * weight * unslashed_participating_increments with Error handling
-    let reward_numerator = match base_reward.safe_mul(weight).and_then(|reward_numerator| {
-        reward_numerator.safe_mul(unslashed_participating_increments)}) {
-        Ok(reward_numerator) => reward_numerator,
-        Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to calculate reward numerator".to_owned())),
-    };
-
-    //Get active_increments
-    let active_increments = match total_active_balance.safe_div(spec.effective_balance_increment) {
-        Ok(active_increments) => active_increments,
-        Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to get active increments".to_owned())),
-    };    
-
-    //Calculate reward = reward_numerator // (active_increments * WEIGHT_DENOMINATOR)
-    let reward = match reward_numerator.safe_div(active_increments) {
-        Ok(reward) => match reward.safe_div(WEIGHT_DENOMINATOR) {
-            Ok(reward) => reward,
-            Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to calculate reward: Division by WEIGHT_DENOMINATOR failed".to_owned())),
-        },
-        Err(_) => return Err(warp_utils::reject::custom_server_error("Unable to calculate reward: Division by active_increments failed".to_owned())),
-    };
 
     //TODO Put reward in Vec<IdealAttestationRewards>
 
