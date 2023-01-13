302eaca3448e11ac9cf2c5d64d4b8954ed909c0a
realbigsean <sean@sigmaprime.io>
2023-01-13 16:15:27 -0500

    intentionally skip `LightClientHeader` ssz static tests

--- testing/ef_tests/check_all_files_accessed.py
+++ testing/ef_tests/check_all_files_accessed.py
@@ -39,6 +39,8 @@ excluded_paths = [
     "tests/.*/.*/ssz_static/LightClientOptimistic",
     # LightClientFinalityUpdate
     "tests/.*/.*/ssz_static/LightClientFinalityUpdate",
+    # LightClientHeader
+    "tests/.*/.*/ssz_static/LightClientHeader",
     # Eip4844 tests are disabled for now.
     "tests/.*/eip4844",
     # One of the EF researchers likes to pack the tarballs on a Mac
