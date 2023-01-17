1f894aa4c22cd5237c06c909664aaaf771508d43
navie <naviechan@gmail.com>
2023-01-18 00:26:53 +0800

    To be more specific with http status code it returns

--- beacon_node/http_api/src/sync_committee_rewards.rs
+++ beacon_node/http_api/src/sync_committee_rewards.rs
@@ -5,7 +5,7 @@ use eth2::lighthouse::SyncCommitteeReward;
 use slog::{debug, Logger};
 use state_processing::BlockReplayer;
 use types::{BeaconState, SignedBlindedBeaconBlock};
-use warp_utils::reject::beacon_chain_error;
+use warp_utils::reject::{beacon_chain_error, custom_not_found};
 use crate::{BlockId, ExecutionOptimistic};
 
 pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
@@ -18,8 +18,7 @@ pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
 
     let (block, execution_optimistic) = block_id.blinded_block(&chain)?;
 
-    let mut state = get_state_before_applying_block(chain.clone(), block.clone())
-        .map_err(beacon_chain_error)?;
+    let mut state = get_state_before_applying_block(chain.clone(), block.clone())?;
 
     let reward_payload = chain
         .compute_sync_committee_rewards(block.message(), &mut state)
@@ -62,23 +61,32 @@ pub fn compute_sync_committee_rewards<T: BeaconChainTypes>(
 fn get_state_before_applying_block<T:BeaconChainTypes>(
     chain: Arc<BeaconChain<T>>,
     block: SignedBlindedBeaconBlock<T::EthSpec>
-) -> Result<BeaconState<T::EthSpec>, BeaconChainError> {
+) -> Result<BeaconState<T::EthSpec>, warp::reject::Rejection> {
 
     let parent_block: SignedBlindedBeaconBlock<T::EthSpec> = chain
-        .get_blinded_block(&block.parent_root())?
-        .ok_or_else(|| BeaconChainError::SyncCommitteeRewardsSyncError)?; //TODO
-
+        .get_blinded_block(&block.parent_root())
+        .and_then(|maybe_block| {
+            maybe_block.ok_or(BeaconChainError::MissingBeaconBlock(block.parent_root()))
+        })
+        .map_err(|e| {
+            custom_not_found(format!("Parent block is not available! {:?}", e))
+        })?;
 
     let parent_state = chain
-        .get_state(&parent_block.state_root(), Some(parent_block.slot()))?
-        .ok_or_else(|| BeaconChainError::SyncCommitteeRewardsSyncError)?; //TODO
+        .get_state(&parent_block.state_root(), Some(parent_block.slot()))
+        .and_then(|maybe_state| {
+            maybe_state.ok_or(BeaconChainError::MissingBeaconState(parent_block.state_root()))
+        })
+        .map_err(|e| {
+            custom_not_found(format!("Parent state is not available! {:?}", e))
+        })?;
 
     let replayer = BlockReplayer::new(parent_state, &chain.spec)
         .no_signature_verification()
         .state_root_iter([Ok((parent_block.state_root(), parent_block.slot()))].into_iter())
         .minimal_block_root_verification()
         .apply_blocks(vec![], Some(block.slot()))
-        .map_err(|_: BeaconChainError| BeaconChainError::SyncCommitteeRewardsSyncError)?; //TODO
+        .map_err(beacon_chain_error)?;
 
     Ok(replayer.into_state())
 }
