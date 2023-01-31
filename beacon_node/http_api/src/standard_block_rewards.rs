43c207c40f76c7cb8ff7f14461a66e007c679b59
navie <naviechan@gmail.com>
2023-01-31 21:05:14 +0800

    use pre-state

--- beacon_node/http_api/src/standard_block_rewards.rs
+++ beacon_node/http_api/src/standard_block_rewards.rs
@@ -1,9 +1,10 @@
+use crate::sync_committee_rewards::get_state_before_applying_block;
 use crate::BlockId;
 use crate::ExecutionOptimistic;
-use beacon_chain::{BeaconChain, BeaconChainError, BeaconChainTypes};
+use beacon_chain::{BeaconChain, BeaconChainTypes};
 use eth2::lighthouse::StandardBlockReward;
 use std::sync::Arc;
-use warp_utils::reject::{beacon_chain_error, custom_not_found};
+use warp_utils::reject::beacon_chain_error;
 //// The difference between block_rewards and beacon_block_rewards is the later returns block
 //// reward format that satisfies beacon-api specs
 pub fn compute_beacon_block_rewards<T: BeaconChainTypes>(
@@ -16,14 +17,7 @@ pub fn compute_beacon_block_rewards<T: BeaconChainTypes>(
 
     let block_root = block.canonical_root();
 
-    let slot = block.slot();
-
-    let mut state = chain
-        .get_state(&block.state_root(), Some(slot))
-        .and_then(|maybe_state| {
-            maybe_state.ok_or_else(|| BeaconChainError::MissingBeaconState(block.state_root()))
-        })
-        .map_err(|e| custom_not_found(format!("State is not available! {:?}", e)))?;
+    let mut state = get_state_before_applying_block(chain.clone(), &block)?;
 
     let rewards = chain
         .compute_beacon_block_reward(block_ref, block_root, &mut state)
