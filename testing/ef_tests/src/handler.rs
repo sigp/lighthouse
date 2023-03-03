20be7024e149e41c928fe7f0426724a68e5baff1
Diva M <divma@protonmail.com>
2023-03-03 11:07:26 -0500

    Revert "Use consensus-spec-tests `v1.3.0-rc.3` (#4021)"
    This reverts commit caa6190d4a468ef74d647f668ac04e4accf293d5.

--- testing/ef_tests/src/handler.rs
+++ testing/ef_tests/src/handler.rs
@@ -653,11 +653,6 @@ impl<E: EthSpec + TypeName> Handler for MerkleProofValidityHandler<E> {
 
     fn is_enabled_for_fork(&self, fork_name: ForkName) -> bool {
         fork_name != ForkName::Base
-            // Test is skipped due to some changes in the Capella light client
-            // spec.
-            //
-            // https://github.com/sigp/lighthouse/issues/4022
-            && fork_name != ForkName::Capella
     }
 }
 
