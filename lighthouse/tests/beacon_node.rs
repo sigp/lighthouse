5e176d52453c23cf8c7f6feb49cc7edd7c85dd5a
Diva M <divma@protonmail.com>
2023-03-03 16:20:17 -0500

    more tests

--- lighthouse/tests/beacon_node.rs
+++ lighthouse/tests/beacon_node.rs
@@ -1192,6 +1192,19 @@ fn enr_address_flag_with_ipv4() {
         });
 }
 #[test]
+fn enr_address_flag_with_ipv6() {
+    let addr = "192.167.1.1".parse::<Ipv4Addr>().unwrap();
+    let port = unused_udp4_port().expect("Unable to find unused port.");
+    CommandLineTest::new()
+        .flag("enr-address", Some("192.167.1.1"))
+        .flag("enr-udp-port", Some(port.to_string().as_str()))
+        .run_with_zero_port()
+        .with_config(|config| {
+            assert_eq!(config.network.enr_address, (Some(addr), None));
+            assert_eq!(config.network.enr_udp4_port, Some(port));
+        });
+}
+#[test]
 fn enr_address_dns_flag() {
     let addr = Ipv4Addr::LOCALHOST;
     let ipv6addr = Ipv6Addr::LOCALHOST;
