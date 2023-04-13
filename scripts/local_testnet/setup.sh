df82d6514ac2a7e3c332c5e956e12d0315d8c7db
Pawan Dhananjay <pawandhananjay@gmail.com>
2023-04-13 15:09:38 -0700

    Fix local testnet scripts to use mnemonic based keypairs

--- scripts/local_testnet/setup.sh
+++ scripts/local_testnet/setup.sh
@@ -27,6 +27,7 @@ lcli \
 	--genesis-fork-version $GENESIS_FORK_VERSION \
 	--altair-fork-epoch $ALTAIR_FORK_EPOCH \
 	--bellatrix-fork-epoch $BELLATRIX_FORK_EPOCH \
+	--capella-fork-epoch $CAPELLA_FORK_EPOCH \
 	--ttd $TTD \
 	--eth1-block-hash $ETH1_BLOCK_HASH \
 	--eth1-id $CHAIN_ID \
@@ -35,16 +36,23 @@ lcli \
 	--seconds-per-eth1-block $SECONDS_PER_ETH1_BLOCK \
 	--proposer-score-boost "$PROPOSER_SCORE_BOOST" \
 	--validator-count $GENESIS_VALIDATOR_COUNT \
-	--interop-genesis-state \
+	--derived-genesis-state \
+	--mnemonics-phrase "$MNEMONICS_PHRASE" \
 	--force
 
 echo Specification and genesis.ssz generated at $TESTNET_DIR.
 echo "Generating $VALIDATOR_COUNT validators concurrently... (this may take a while)"
 
 lcli \
-	insecure-validators \
+	mnemonics-validators \
 	--count $VALIDATOR_COUNT \
 	--base-dir $DATADIR \
+	--mnemonics-phrase "$MNEMONICS_PHRASE" \
 	--node-count $BN_COUNT
 
 echo Validators generated with keystore passwords at $DATADIR.
+
+GENESIS_TIME=$(lcli pretty-ssz state_merge ~/.lighthouse/local-testnet/testnet/genesis.ssz  | jq | grep -Po 'genesis_time": "\K.*\d')
+CAPELLA_TIME=$((GENESIS_TIME + (CAPELLA_FORK_EPOCH * 32 * SECONDS_PER_SLOT)))
+
+sed -i 's/"shanghaiTime".*$/"shanghaiTime": '"$CAPELLA_TIME"',/g' genesis.json
--- scripts/local_testnet/start_local_testnet.sh
+++ scripts/local_testnet/start_local_testnet.sh
@@ -125,6 +125,9 @@ done
 
 sleeping 20
 
+# Reset the `genesis.json` config file fork times.
+sed -i 's/"shanghaiTime".*$/"shanghaiTime": 0,/g' genesis.json
+
 for (( bn=1; bn<=$BN_COUNT; bn++ )); do
     secret=$DATADIR/geth_datadir$bn/geth/jwtsecret
     echo $secret
