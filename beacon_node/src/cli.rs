440bc06db7327880eccb3895219a6653ed1ea667
Paul Hauner <paul@paulhauner.com>
2023-04-14 17:10:22 +1000

    Expand help message

--- beacon_node/src/cli.rs
+++ beacon_node/src/cli.rs
@@ -1068,6 +1068,8 @@ pub fn cli_app<'a, 'b>() -> App<'a, 'b> {
             .long("invalid-gossip-verified-blocks-path")
             .value_name("PATH")
             .help("If a block succeeds gossip validation whilst failing full validation, store \
-                    the block SSZ as a file at this path.")
+                    the block SSZ as a file at this path. This feature is only recommended for \
+                    developers. This directory is not pruned, users should be careful to avoid \
+                    filling up their disks.")
         )
 }
