843206d89eac4a4cf7d481fce062335d1666f88a
Michael Sproul <michael@sigmaprime.io>
2023-05-02 14:52:05 +1000

    Expanding `needs` doesn't work, so inspect more

--- scripts/ci/check-success-job.sh
+++ scripts/ci/check-success-job.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
+# Check that $SUCCESS_JOB depends on all other jobs in the given $YAML
 
 set -euf -o pipefail
 
 YAML=$1
-NEEDS=$2
-SUCCESS_JOB=$3
+SUCCESS_JOB=$2
 
-yq '... comments="" | .jobs | map(. | key)' < "$YAML" | grep -v "$SUCCESS_JOB" | sort > all_jobs.txt
-echo "$NEEDS" | jq -r 'keys | join(",")' | tr "," "\n" | sort > dep_jobs.txt
-diff all_jobs.txt dep_jobs.txt
+yq '... comments="" | .jobs | map(. | key)' < "$YAML" | sed "s/^- //"| grep -v "$SUCCESS_JOB" | sort > all_jobs.txt
+yq "... comments=\"\" | .jobs.$SUCCESS_JOB.needs[]" < "$YAML" | grep -v "test-suite-success" | sort > dep_jobs.txt
+diff all_jobs.txt dep_jobs.txt || echo "COMPLETENESS CHECK FAILED" && exit 1
 rm all_jobs.txt dep_jobs.txt
