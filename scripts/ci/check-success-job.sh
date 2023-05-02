3afeadf044b0854dfe852abaddf55e988a9aecce
Michael Sproul <michael@sigmaprime.io>
2023-05-02 15:11:29 +1000

    Use complete list of deps again, fix script
    This reverts commit 19221628bef062534475af544cf31b3a17ca7aa8.

--- scripts/ci/check-success-job.sh
+++ scripts/ci/check-success-job.sh
@@ -8,5 +8,6 @@ SUCCESS_JOB=$2
 
 yq '... comments="" | .jobs | map(. | key) | .[]' < "$YAML" | grep -v "$SUCCESS_JOB" | sort > all_jobs.txt
 yq "... comments=\"\" | .jobs.$SUCCESS_JOB.needs[]" < "$YAML" | grep -v "$SUCCESS_JOB" | sort > dep_jobs.txt
-diff all_jobs.txt dep_jobs.txt || echo "COMPLETENESS CHECK FAILED" && exit 1
+diff all_jobs.txt dep_jobs.txt || (echo "COMPLETENESS CHECK FAILED" && exit 1)
 rm all_jobs.txt dep_jobs.txt
+echo "COMPLETENESS CHECK PASSED"
