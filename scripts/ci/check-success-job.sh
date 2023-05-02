25476a9ef32673aec03cb2c7ef5aea775f43aaa3
Michael Sproul <michael@sigmaprime.io>
2023-05-02 14:54:35 +1000

    Minor cleanups

--- scripts/ci/check-success-job.sh
+++ scripts/ci/check-success-job.sh
@@ -6,7 +6,7 @@ set -euf -o pipefail
 YAML=$1
 SUCCESS_JOB=$2
 
-yq '... comments="" | .jobs | map(. | key)' < "$YAML" | sed "s/^- //"| grep -v "$SUCCESS_JOB" | sort > all_jobs.txt
-yq "... comments=\"\" | .jobs.$SUCCESS_JOB.needs[]" < "$YAML" | grep -v "test-suite-success" | sort > dep_jobs.txt
+yq '... comments="" | .jobs | map(. | key) | .[]' < "$YAML" | grep -v "$SUCCESS_JOB" | sort > all_jobs.txt
+yq "... comments=\"\" | .jobs.$SUCCESS_JOB.needs[]" < "$YAML" | grep -v "$SUCCESS_JOB" | sort > dep_jobs.txt
 diff all_jobs.txt dep_jobs.txt || echo "COMPLETENESS CHECK FAILED" && exit 1
 rm all_jobs.txt dep_jobs.txt
