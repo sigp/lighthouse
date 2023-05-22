1284080df6fa50dfd113865252c6d4739236fe26
jmcph4 <jmcph4.github@gmail.com>
2023-05-22 14:22:41 +1000

    Add test for gossip validity case
    This commit adds an interactive test case for the case where a block is
    only gossip-valid, not consensus-valid nor equivocation-free.

--- beacon_node/http_api/tests/interactive_tests.rs
+++ beacon_node/http_api/tests/interactive_tests.rs
@@ -3,7 +3,7 @@ use beacon_chain::{
     chain_config::{DisallowedReOrgOffsets, ReOrgThreshold},
     test_utils::{AttestationStrategy, BlockStrategy, SyncCommitteeStrategy},
 };
-use eth2::types::DepositContractData;
+use eth2::types::{BeaconBlock, DepositContractData, SignedBeaconBlock};
 use execution_layer::{ForkchoiceState, PayloadAttributes};
 use http_api::test_utils::InteractiveTester;
 use parking_lot::Mutex;
@@ -19,6 +19,8 @@ use types::{
     Address, Epoch, EthSpec, ExecPayload, ExecutionBlockHash, ForkName, FullPayload,
     MainnetEthSpec, ProposerPreparationData, Slot,
 };
+
+use eth2::reqwest::StatusCode;
 
 type E = MainnetEthSpec;
 
@@ -641,6 +643,35 @@ pub async fn proposer_boost_re_org_test(
             payload_attribs.prev_randao(),
         );
     }
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+pub async fn broadcast_validation_block_only_gossip_valid() {
+    // Validator count needs to be at least 32 or proposer boost gets set to 0 when computing
+    // `validator_count // 32`.
+    let validator_count = 64;
+    let num_initial: u64 = 31;
+    let tester = InteractiveTester::<E>::new(None, validator_count).await;
+
+    /* produce a block wih zero parent and state roots in order to reproduce the initial exploit */
+    let block: SignedBeaconBlock<E> = BeaconBlock::empty(&tester.harness.chain.spec).sign(
+        &tester.harness.validator_keypairs[0].sk,
+        &tester
+            .harness
+            .chain
+            .spec
+            .fork_at_epoch(Epoch::new(Slot::new(num_initial).into())),
+        tester.harness.chain.genesis_validators_root,
+        &tester.harness.chain.spec,
+    );
+
+    let response: Result<(), eth2::Error> = tester.client.post_beacon_blocks_v2(&block).await;
+    assert!(response.is_err());
+
+    let error_response: eth2::Error = response.err().unwrap();
+
+    /* mandated by Beacon API spec */
+    assert_eq!(error_response.status(), Some(StatusCode::BAD_REQUEST));
 }
 
 // Test that running fork choice before proposing results in selection of the correct head.
