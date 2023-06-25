fa4426023d2f5894be9ea4f202d3cf0ccd99b133
Eitan Seri-Levi <eserilev@ucsc.edu>
2023-06-25 16:17:37 +0300

    added env struct

--- slasher/src/database/redb_impl.rs
+++ slasher/src/database/redb_impl.rs
@@ -1,7 +1,9 @@
 #![cfg(feature = "redb")]
 
+use crate::config::Config;
+use crate::database::{interface::OpenDatabases, *};
+use crate::Error;
 use std::marker::PhantomData;
-use crate::config::{Config};
 
 pub struct Builder {
     builder: redb::Builder,
@@ -10,7 +12,7 @@ pub struct Builder {
 #[derive(Debug)]
 pub struct Database<'db> {
     db: redb::Database,
-    _phantom: PhantomData<&'db ()>
+    _phantom: PhantomData<&'db ()>,
 }
 
 pub struct WriteTransaction<'db> {
@@ -18,13 +20,55 @@ pub struct WriteTransaction<'db> {
 }
 
 pub struct Table<'db, 'txn, K: redb::RedbKey + 'static, V: redb::RedbValue + 'static> {
-    table: redb::Table<'db, 'txn, K, V>
+    table: redb::Table<'db, 'txn, K, V>,
 }
 
 impl Builder {
     pub fn new(config: &Config) -> Builder {
         let builder = redb::Builder::new();
 
-        Builder{ builder }
+        Builder { builder }
+    }
+}
+
+pub struct Environment {
+    builder: redb::Builder,
+}
+
+impl Environment {
+    pub fn new(config: &Config) -> Result<Environment, Error> {
+        let builder = redb::Builder::new();
+        Ok(Environment { builder })
+    }
+
+    pub fn create_databases(&self) -> Result<OpenDatabases, Error> {
+        let indexed_attestation_db = self.builder.create(INDEXED_ATTESTATION_DB).unwrap();
+        let indexed_attestation_id_db = self.builder.create(INDEXED_ATTESTATION_ID_DB).unwrap();
+        let attesters_db = self.builder.create(ATTESTERS_DB).unwrap();
+        let attesters_max_targets_db = self.builder.create(ATTESTERS_MAX_TARGETS_DB).unwrap();
+        let min_targets_db = self.builder.create(MIN_TARGETS_DB).unwrap();
+        let max_targets_db = self.builder.create(MAX_TARGETS_DB).unwrap();
+        let current_epochs_db = self.builder.create(CURRENT_EPOCHS_DB).unwrap();
+        let proposers_db = self.builder.create(PROPOSERS_DB).unwrap();
+        let metadata_db = self.builder.create(METADATA_DB).unwrap();
+
+        let wrap = |db| {
+            crate::Database::Redb(Database {
+                db,
+                _phantom: PhantomData,
+            })
+        };
+
+        Ok(OpenDatabases {
+            indexed_attestation_db: wrap(indexed_attestation_db),
+            indexed_attestation_id_db: wrap(indexed_attestation_id_db),
+            attesters_db: wrap(attesters_db),
+            attesters_max_targets_db: wrap(attesters_max_targets_db),
+            min_targets_db: wrap(min_targets_db),
+            max_targets_db: wrap(max_targets_db),
+            current_epochs_db: wrap(current_epochs_db),
+            proposers_db: wrap(proposers_db),
+            metadata_db: wrap(metadata_db),
+        })
     }
 }
