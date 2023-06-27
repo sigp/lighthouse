341b07593fce68d0d80dbd3f304e750ac14fe87a
Paul Hauner <paul@paulhauner.com>
2023-06-27 16:25:35 +1000

    Add changes to lighthouse_network

--- beacon_node/lighthouse_network/src/types/mod.rs
+++ beacon_node/lighthouse_network/src/types/mod.rs
@@ -5,7 +5,7 @@ mod subnet;
 mod sync_state;
 mod topics;
 
-use types::{BitVector, EthSpec};
+use types::{BitVector, Epoch, EthSpec, Hash256};
 
 pub type EnrAttestationBitfield<T> = BitVector<<T as EthSpec>::SubnetBitfieldLength>;
 pub type EnrSyncCommitteeBitfield<T> = BitVector<<T as EthSpec>::SyncCommitteeSubnetCount>;
@@ -20,3 +20,16 @@ pub use topics::{
     core_topics_to_subscribe, fork_core_topics, subnet_from_topic_hash, GossipEncoding, GossipKind,
     GossipTopic, LIGHT_CLIENT_GOSSIP_TOPICS,
 };
+
+/// Id associated to a batch processing request, either a sync batch or a parent lookup.
+///
+/// Shared with the `network` and `beacon_processor` crates.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum ChainSegmentProcessId {
+    /// Processing Id of a range syncing batch.
+    RangeBatchId(u64, Epoch),
+    /// Processing ID for a backfill syncing batch.
+    BackSyncBatchId(Epoch),
+    /// Processing Id of the parent lookup of a block.
+    ParentLookup(Hash256),
+}
