372f97fa0812fcaccdb0a66443b6284db6eeac1c
realbigsean <seananderson33@gmail.com>
2023-06-28 16:18:25 -0400

    remove unwrap

--- beacon_node/beacon_chain/src/kzg_utils.rs
+++ beacon_node/beacon_chain/src/kzg_utils.rs
@@ -3,9 +3,10 @@ use types::{Blob, EthSpec, Hash256, KzgCommitment, KzgProof};
 
 /// Converts a blob ssz List object to an array to be used with the kzg
 /// crypto library.
-fn ssz_blob_to_crypto_blob<T: EthSpec>(blob: Blob<T>) -> <<T as EthSpec>::Kzg as KzgPreset>::Blob {
-    //TODO(sean) remove unwrap
-    T::blob_from_bytes(blob.to_vec().as_slice()).unwrap()
+fn ssz_blob_to_crypto_blob<T: EthSpec>(
+    blob: Blob<T>,
+) -> Result<<<T as EthSpec>::Kzg as KzgPreset>::Blob, KzgError> {
+    T::blob_from_bytes(blob.to_vec().as_slice())
 }
 
 /// Validate a single blob-commitment-proof triplet from a `BlobSidecar`.
@@ -16,7 +17,7 @@ pub fn validate_blob<T: EthSpec>(
     kzg_proof: KzgProof,
 ) -> Result<bool, KzgError> {
     kzg.verify_blob_kzg_proof(
-        ssz_blob_to_crypto_blob::<T>(blob),
+        ssz_blob_to_crypto_blob::<T>(blob)?,
         kzg_commitment,
         kzg_proof,
     )
@@ -45,7 +46,7 @@ pub fn validate_blobs<T: EthSpec>(
     let blobs = blobs
         .iter()
         .map(|blob| ssz_blob_to_crypto_blob::<T>(blob.clone())) // Avoid this clone
-        .collect::<Vec<_>>();
+        .collect::<Result<Vec<_>, KzgError>>()?;
 
     kzg.verify_blob_kzg_proof_batch(&blobs, expected_kzg_commitments, kzg_proofs)
 }
@@ -57,7 +58,7 @@ pub fn compute_blob_kzg_proof<T: EthSpec>(
     kzg_commitment: KzgCommitment,
 ) -> Result<KzgProof, KzgError> {
     // Avoid this blob clone
-    kzg.compute_blob_kzg_proof(ssz_blob_to_crypto_blob::<T>(blob.clone()), kzg_commitment)
+    kzg.compute_blob_kzg_proof(ssz_blob_to_crypto_blob::<T>(blob.clone())?, kzg_commitment)
 }
 
 /// Compute the kzg commitment for a given blob.
@@ -65,7 +66,7 @@ pub fn blob_to_kzg_commitment<T: EthSpec>(
     kzg: &Kzg<T::Kzg>,
     blob: Blob<T>,
 ) -> Result<KzgCommitment, KzgError> {
-    kzg.blob_to_kzg_commitment(ssz_blob_to_crypto_blob::<T>(blob))
+    kzg.blob_to_kzg_commitment(ssz_blob_to_crypto_blob::<T>(blob)?)
 }
 
 /// Compute the kzg proof for a given blob and an evaluation point z.
@@ -75,7 +76,7 @@ pub fn compute_kzg_proof<T: EthSpec>(
     z: Hash256,
 ) -> Result<(KzgProof, Hash256), KzgError> {
     let z = z.0.into();
-    kzg.compute_kzg_proof(ssz_blob_to_crypto_blob::<T>(blob), z)
+    kzg.compute_kzg_proof(ssz_blob_to_crypto_blob::<T>(blob)?, z)
         .map(|(proof, z)| (proof, Hash256::from_slice(&z.to_vec())))
 }
 
--- crypto/kzg/src/lib.rs
+++ crypto/kzg/src/lib.rs
@@ -17,6 +17,7 @@ pub enum Error {
     InvalidBytes(CryptoError),
     KzgProofComputationFailed(CryptoError),
     InvalidBlob(CryptoError),
+    InvalidBytesForBlob(CryptoError),
 }
 
 #[derive(Debug)]
