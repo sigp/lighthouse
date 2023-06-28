3469283a589218cf8a01f939394cd144a74b280f
realbigsean <seananderson33@gmail.com>
2023-06-28 16:43:01 -0400

    cleanup tx generator

--- beacon_node/execution_layer/src/lib.rs
+++ beacon_node/execution_layer/src/lib.rs
@@ -1787,7 +1787,7 @@ impl<T: EthSpec> ExecutionLayer<T> {
             VariableList::new(
                 transactions
                     .into_iter()
-                    .map(ethers_tx_to_bytes::<T>)
+                    .map(ethers_tx_to_ssz::<T>)
                     .collect::<Result<Vec<_>, BlobTxConversionError>>()?,
             )
             .map_err(BlobTxConversionError::SszError)
@@ -2172,10 +2172,35 @@ impl From<serde_json::Error> for BlobTxConversionError {
     }
 }
 
-fn ethers_tx_to_bytes<T: EthSpec>(
-    transaction: EthersTransaction,
+fn random_valid_tx<T: EthSpec>(
 ) -> Result<Transaction<T::MaxBytesPerTransaction>, BlobTxConversionError> {
-    VariableList::new(transaction.rlp().to_vec()).map_err(Into::into)
+    // Calculate transaction bytes. We don't care about the contents of the transaction.
+    let transaction: EthersTransaction = serde_json::from_str(
+        r#"{
+            "blockHash":"0x1d59ff54b1eb26b013ce3cb5fc9dab3705b415a67127a003c3e61eb445bb8df2",
+            "blockNumber":"0x5daf3b",
+            "from":"0xa7d9ddbe1f17865597fbd27ec712455208b6b76d",
+            "gas":"0xc350",
+            "gasPrice":"0x4a817c800",
+            "hash":"0x88df016429689c079f3b2f6ad39fa052532c56795b733da78a91ebe6a713944b",
+            "input":"0x68656c6c6f21",
+            "nonce":"0x15",
+            "to":"0xf02c1c8e6114b1dbe8937a39260b5b0a374432bb",
+            "transactionIndex":"0x41",
+            "value":"0xf3dbb76162000",
+            "v":"0x25",
+            "r":"0x1b5e176d927f8e9ab405058b2d2457392da3e20f328b16ddabcebc33eaac5fea",
+            "s":"0x4ba69724e8f69de52f0125ad8b3c5c2cef33019bac3249e2c0a2192766d1721c"
+         }"#,
+    )
+    .unwrap();
+    ethers_tx_to_ssz::<T>(transaction)
+}
+
+fn ethers_tx_to_ssz<T: EthSpec>(
+    tx: EthersTransaction,
+) -> Result<Transaction<T::MaxBytesPerTransaction>, BlobTxConversionError> {
+    VariableList::new(tx.rlp().to_vec()).map_err(Into::into)
 }
 
 fn noop<T: EthSpec>(
--- beacon_node/execution_layer/src/test_utils/execution_block_generator.rs
+++ beacon_node/execution_layer/src/test_utils/execution_block_generator.rs
@@ -6,7 +6,7 @@ use crate::{
         },
         ExecutionBlock, PayloadAttributes, PayloadId, PayloadStatusV1, PayloadStatusV1Status,
     },
-    ethers_tx_to_bytes, BlobsBundleV1, ExecutionBlockWithTransactions,
+    random_valid_tx, BlobsBundleV1, ExecutionBlockWithTransactions,
 };
 use ethers_core::types::Transaction;
 use kzg::{Kzg, KzgPreset};
@@ -653,27 +653,7 @@ pub fn generate_random_blobs<T: EthSpec>(
             .compute_blob_kzg_proof(kzg_blob, commitment)
             .map_err(|e| format!("error computing kzg proof: {:?}", e))?;
 
-        // Calculate transaction bytes. We don't care about the contents of the transaction.
-        let tx: Transaction = serde_json::from_str(
-            r#"{
-    "blockHash":"0x1d59ff54b1eb26b013ce3cb5fc9dab3705b415a67127a003c3e61eb445bb8df2",
-    "blockNumber":"0x5daf3b",
-    "from":"0xa7d9ddbe1f17865597fbd27ec712455208b6b76d",
-    "gas":"0xc350",
-    "gasPrice":"0x4a817c800",
-    "hash":"0x88df016429689c079f3b2f6ad39fa052532c56795b733da78a91ebe6a713944b",
-    "input":"0x68656c6c6f21",
-    "nonce":"0x15",
-    "to":"0xf02c1c8e6114b1dbe8937a39260b5b0a374432bb",
-    "transactionIndex":"0x41",
-    "value":"0xf3dbb76162000",
-    "v":"0x25",
-    "r":"0x1b5e176d927f8e9ab405058b2d2457392da3e20f328b16ddabcebc33eaac5fea",
-    "s":"0x4ba69724e8f69de52f0125ad8b3c5c2cef33019bac3249e2c0a2192766d1721c"
-  }"#,
-        )
-        .unwrap();
-        let tx = ethers_tx_to_bytes::<T>(tx)
+        let tx = random_valid_tx::<T>()
             .map_err(|e| format!("error converting tx bytes to SSZ: {:?}", e))?;
 
         transactions.push(tx);
