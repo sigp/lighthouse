3732333fb0ac4faf288d68688f009290608cbac9
Michael Sproul <michael@sigmaprime.io>
2023-06-29 18:09:10 +1000

    Fix version regex

--- common/lighthouse_version/src/lib.rs
+++ common/lighthouse_version/src/lib.rs
@@ -37,9 +37,8 @@ mod test {
 
     #[test]
     fn version_formatting() {
-        let re =
-            Regex::new(r"^Lighthouse/v[0-9]+\.[0-9]+\.[0-9]+(-tree.[0-9])?(-[[:xdigit:]]{7})?\+?$")
-                .unwrap();
+        let re = Regex::new(r"^Lighthouse/v[0-9]+\.[0-9]+\.[0-9]+(-exp)?(-[[:xdigit:]]{7})?\+?$")
+            .unwrap();
         assert!(
             re.is_match(VERSION),
             "version doesn't match regex: {}",
