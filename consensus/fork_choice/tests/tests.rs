57bb1d931e9dac016153e37aa33714e7b48c8967
realbigsean <seananderson33@gmail.com>
2023-07-11 17:07:52 -0400

    fix progressive balance slashing tests

--- consensus/fork_choice/tests/tests.rs
+++ consensus/fork_choice/tests/tests.rs
@@ -1353,6 +1353,14 @@ async fn progressive_balances_cache_attester_slashing() {
         .apply_blocks_while(|_, state| state.finalized_checkpoint().epoch == 0)
         .await
         .unwrap()
+        // Note: This test may fail if the shuffling used changes, right now it re-runs with
+        // deterministic shuffling. A shuffling change my cause the slashed proposer to propose
+        // again in the next epoch, which results in a block processing failure
+        // (`HeaderInvalid::ProposerSlashed`). The harness should be re-worked to successfully skip
+        // the slot in this scenario rather than panic-ing. The same applies to
+        // `progressive_balances_cache_proposer_slashing`.
+        .apply_blocks(1)
+        .await
         .add_previous_epoch_attester_slashing()
         .await
         // expect fork choice to import blocks successfully after a previous epoch attester is
@@ -1376,6 +1384,14 @@ async fn progressive_balances_cache_proposer_slashing() {
         .apply_blocks_while(|_, state| state.finalized_checkpoint().epoch == 0)
         .await
         .unwrap()
+        // Note: This test may fail if the shuffling used changes, right now it re-runs with
+        // deterministic shuffling. A shuffling change my cause the slashed proposer to propose
+        // again in the next epoch, which results in a block processing failure
+        // (`HeaderInvalid::ProposerSlashed`). The harness should be re-worked to successfully skip
+        // the slot in this scenario rather than panic-ing. The same applies to
+        // `progressive_balances_cache_attester_slashing`.
+        .apply_blocks(1)
+        .await
         .add_previous_epoch_proposer_slashing(MainnetEthSpec::slots_per_epoch())
         .await
         // expect fork choice to import blocks successfully after a previous epoch proposer is
