48c27a020f432b772a2efc187381748b2b66036b
geemo <geemo@tutanota.com>
2023-07-14 14:38:36 -0500

    Added LazyAttestation type

---
+++ consensus/types/src/lazy_attestation.rs
@@ -0,0 +1,29 @@
+use bls::Error;
+use derivative::Derivative;
+use serde_derive::{Deserialize, Serialize};
+use ssz_derive::{Decode, Encode};
+
+use super::{AggregateSignature, Attestation, AttestationData, BitList, EthSpec, SignatureBytes};
+
+/// An attestation type with SSZ bytes for the signature
+#[derive(
+    arbitrary::Arbitrary, Debug, Clone, Serialize, Deserialize, Encode, Decode, Derivative,
+)]
+#[derivative(PartialEq, Hash(bound = "T: EthSpec"))]
+#[serde(bound = "T: EthSpec")]
+#[arbitrary(bound = "T: EthSpec")]
+pub struct LazyAttestation<T: EthSpec> {
+    pub aggregation_bits: BitList<T::MaxValidatorsPerCommittee>,
+    pub data: AttestationData,
+    pub signature: SignatureBytes,
+}
+
+impl<T: EthSpec> LazyAttestation<T> {
+    pub fn to_attestation(self) -> Result<Attestation<T>, Error> {
+        Ok(Attestation {
+            aggregation_bits: self.aggregation_bits,
+            data: self.data,
+            signature: AggregateSignature::from(&self.signature.decompress()?),
+        })
+    }
+}
--- consensus/types/src/lib.rs
+++ consensus/types/src/lib.rs
@@ -49,6 +49,7 @@ pub mod graffiti;
 pub mod historical_batch;
 pub mod historical_summary;
 pub mod indexed_attestation;
+pub mod lazy_attestation;
 pub mod light_client_bootstrap;
 pub mod light_client_finality_update;
 pub mod light_client_optimistic_update;
