7b4b96df47fc176c2329e24f83680c125cc99732
Paul Hauner <paul@paulhauner.com>
2023-07-17 17:55:36 +1000

    Update validator_manager/src/move_validators.rs
    Co-authored-by: Michael Sproul <micsproul@gmail.com>

--- validator_manager/src/move_validators.rs
+++ validator_manager/src/move_validators.rs
@@ -207,7 +207,7 @@ impl MoveConfig {
                     .collect::<Result<Vec<_>, _>>()
                     .map(Validators::Specific)?,
             },
-            (None, None) => Err("Must supply either --{:VALIDATORS_FLAG} or --{:COUNT_FLAG}.")?,
+            (None, None) => Err(format!("Must supply either --{VALIDATORS_FLAG} or --{COUNT_FLAG}."))?,
             (Some(_), Some(_)) => {
                 Err("Cannot supply both --{:VALIDATORS_FLAG} and --{:COUNT_FLAG}.")?
             }
