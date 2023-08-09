c87af3c1b9c29322b45e7c1d08f80aca057d3b26
zhiqiangxu <652732310@qq.com>
2023-08-09 14:22:10 +0800

    move the comment to the right place

--- consensus/fork_choice/src/fork_choice.rs
+++ consensus/fork_choice/src/fork_choice.rs
@@ -466,10 +466,10 @@ where
                 // for lower slots to account for skip slots.
                 .find(|(_, slot)| *slot <= ancestor_slot)
                 .map(|(root, _)| root)),
-            Ordering::Less => Ok(Some(block_root)),
-            Ordering::Equal =>
             // Root is older than queried slot, thus a skip slot. Return most recent root prior
             // to slot.
+            Ordering::Less => Ok(Some(block_root)),
+            Ordering::Equal =>
             {
                 Ok(Some(block_root))
             }
@@ -1192,7 +1192,7 @@ where
         // Update store time.
         store.set_current_slot(time);
 
-        let current_slot = store.get_current_slot();
+        let current_slot = store.get_current_slot();    
 
         // Reset proposer boost if this is a new slot.
         if current_slot > previous_slot {
