3704dd817d75aad9393f397622b827f46c8e55e0
Mac L <mjladson@pm.me>
2023-08-25 17:31:18 +1000

    Enable logs

--- testing/web3signer_tests/src/lib.rs
+++ testing/web3signer_tests/src/lib.rs
@@ -51,11 +51,11 @@ mod tests {
 
     /// If the we are unable to reach the Web3Signer HTTP API within this time out then we will
     /// assume it failed to start.
-    const UPCHECK_TIMEOUT: Duration = Duration::from_secs(60);
+    const UPCHECK_TIMEOUT: Duration = Duration::from_secs(120);
 
     /// Set to `false` to send the Web3Signer logs to the console during tests. Logs are useful when
     /// debugging.
-    const SUPPRESS_WEB3SIGNER_LOGS: bool = true;
+    const SUPPRESS_WEB3SIGNER_LOGS: bool = false;
 
     lazy_static! {
         static ref TEMP_DIR: Arc<Mutex<TempDir>> = Arc::new(Mutex::new(
