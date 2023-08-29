8f875011119a8fc320132cda08526b50aa8ca44b
Paul Hauner <paul@paulhauner.com>
2023-08-29 15:14:24 +1000

    Add detail to test failure

--- common/eth2_network_config/src/lib.rs
+++ common/eth2_network_config/src/lib.rs
@@ -482,7 +482,7 @@ mod tests {
     fn hard_coded_nets_work() {
         for net in HARDCODED_NETS {
             let config = Eth2NetworkConfig::from_hardcoded_net(net)
-                .unwrap_or_else(|_| panic!("{:?}", net.name));
+                .unwrap_or_else(|e| panic!("{:?}: {:?}", net.name, e));
 
             // Ensure we can parse the YAML config to a chain spec.
             if config.config.preset_base == types::GNOSIS {
