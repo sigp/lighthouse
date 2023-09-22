0f05499e304989e1eab0e50ffd8079e1198387e0
João Oliveira <hello@jxs.pt>
2023-09-22 12:00:51 +0000

    Fix cli options (#4772)
    ## Issue Addressed
    
    Fixes breaking change introduced on https://github.com/sigp/lighthouse/pull/4674/  that doesn't allow multiple `http_enabled` `ArgGroup` flags

--- beacon_node/src/cli.rs
+++ beacon_node/src/cli.rs
@@ -1273,5 +1273,5 @@ pub fn cli_app<'a, 'b>() -> App<'a, 'b> {
                 .default_value("64")
                 .takes_value(true)
         )
-        .group(ArgGroup::with_name("enable_http").args(&["http", "gui", "staking"]))
+        .group(ArgGroup::with_name("enable_http").args(&["http", "gui", "staking"]).multiple(true))
 }
