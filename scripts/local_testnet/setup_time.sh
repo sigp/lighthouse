a83744c6adfa2832d2fe35096e96a1dc9a827f96
chonghe <tanck2005@gmail.com>
2023-09-28 16:07:51 +0800

    Fix time

---
+++ scripts/local_testnet/setup_time.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+source ./vars.env
+
+genesis_file=${@:$OPTIND+0:1}
+
+# Update future hardforks time in the EL genesis file based on the CL genesis time
+GENESIS_TIME=$(lcli pretty-ssz --spec $SPEC_PRESET --testnet-dir $TESTNET_DIR BeaconState $TESTNET_DIR/genesis.ssz | jq | grep -Po 'genesis_time": "\K.*\d')
+echo $GENESIS_TIME
+CAPELLA_TIME=$((GENESIS_TIME + (CAPELLA_FORK_EPOCH * 32 * SECONDS_PER_SLOT)))
+echo $CAPELLA_TIME
+sed -i 's/"shanghaiTime".*$/"shanghaiTime": '"$CAPELLA_TIME"',/g' $genesis_file
