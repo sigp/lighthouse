918f7ac72dc10400dc4593c82a23ef506f9b49da
Tan Chee Keong <tanck2005@gmail.com>
2023-10-09 16:50:16 +0800

    Add vm

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -34,6 +34,7 @@ fi
 general_cli=$($CMD --help)
 bn_cli=$($CMD bn --help)
 vc_cli=$($CMD vc --help)
+vm_cli=$($CMD vm --help)
 vm_cli_create=$($CMD vm create --help)
 vm_cli_import=$($CMD vm import --help)
 vm_cli_move=$($CMD vm move --help)
@@ -42,6 +43,7 @@ general=./help_general.md
 bn=./help_bn.md
 vc=./help_vc.md
 am=./help_am.md
+vm=./help_vm.md
 vm_create=./help_vm_create.md
 vm_import=./help_vm_import.md
 vm_move=./help_vm_move.md
@@ -50,13 +52,14 @@ vm_move=./help_vm_move.md
 write_to_file "$general_cli" "$general" "Lighthouse General Commands"
 write_to_file "$bn_cli" "$bn" "Beacon Node"
 write_to_file "$vc_cli" "$vc" "Validator Client"
+write_to_file "$vm" "vm" "Validator Manager"
 write_to_file "$vm_cli_create" "$vm_create" "Validator Manager Create"
 write_to_file "$vm_cli_import" "$vm_import" "Validator Manager Import"
 write_to_file "$vm_cli_move" "$vm_move" "Validator Manager Move"
 
 #input 1 = $1 = files; input 2 = $2 = new files
-files=(./book/src/help_general.md ./book/src/help_bn.md ./book/src/help_vc.md ./book/src/help_vm_create.md ./book/src/help_vm_import.md ./book/src/help_vm_move.md)
-new_files=($general $bn $vc $vm_create $vm_import $vm_move)
+files=(./book/src/help_general.md ./book/src/help_bn.md ./book/src/help_vc.md ./book/src/help_vm.md ./book/src/help_vm_create.md ./book/src/help_vm_import.md ./book/src/help_vm_move.md)
+new_files=($general $bn $vc $vm $vm_create $vm_import $vm_move)
 
 # function to check
 check() {
@@ -89,9 +92,10 @@ check ${files[2]} ${new_files[2]}
 check ${files[3]} ${new_files[3]}
 check ${files[4]} ${new_files[4]}
 check ${files[5]} ${new_files[5]}
+check ${files[6]} ${new_files[6]}
 
 # remove help files
-rm -f help_general.md help_bn.md help_vc.md help_am.md help_vm_create.md help_vm_import.md help_mv_move.md
+rm -f help_general.md help_bn.md help_vc.md help_am.md help_vm.md help_vm_create.md help_vm_import.md help_mv_move.md
 
 # only exit at the very end
 if [[ $changes == true ]]; then
