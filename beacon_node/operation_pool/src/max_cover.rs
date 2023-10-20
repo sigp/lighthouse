06aed57e08f41ab039125a99659aa54439119ae1
Michael Sproul <michael@sigmaprime.io>
2023-10-20 17:19:15 +1100

    Fix bug in max cover and optimise

--- beacon_node/operation_pool/src/max_cover.rs
+++ beacon_node/operation_pool/src/max_cover.rs
@@ -82,42 +82,33 @@ where
 
     for _ in 0..limit {
         // Select the item with the maximum score.
-        match all_items.iter_mut().max_by_key(|(_, vals)| {
-            vals.iter()
-                .filter(|val| val.available && val.item.score() != 0)
-                .map(|val| val.item.score())
-                .max()
-        }) {
-            Some((_, vals)) => {
-                let best = vals
-                    .iter()
-                    .enumerate()
-                    .max_by_key(|(_, val)| val.item.score())
-                    .map(|(ind, _)| ind);
-                if let Some(best_ind) = best {
-                    let best_item = vals[best_ind].item.clone();
-                    if best_item.score() == 0 {
-                        return result;
-                    } else {
-                        // Update the covering sets of the other items, for the inclusion of the selected item.
-                        // Items covered by the selected item can't be re-covered.
-                        vals[best_ind].available = false;
-                        vals.iter_mut()
-                            .filter(|x| x.available && x.item.score() != 0)
-                            .for_each(|x| {
-                                x.item.update_covering_set(
-                                    best_item.intermediate(),
-                                    best_item.covering_set(),
-                                )
-                            });
-                        result.push(best_item)
-                    }
-                } else {
-                    return result;
-                }
+        let best = all_items
+            .iter_mut()
+            .flat_map(|(key, items)| items.iter_mut().map(move |x| (key, x)))
+            .filter(|(_, val)| val.available && val.item.score() != 0)
+            .max_by_key(|(_, val)| val.item.score());
+
+        let (best_key, best_item) = match best {
+            Some((key, val)) => {
+                val.available = false;
+                (key.clone(), val.item.clone())
             }
             None => return result,
         };
+
+        // Update the covering sets of the other items, for the inclusion of the selected item.
+        // Items covered by the selected item can't be re-covered.
+        all_items.get_mut(&best_key).map(|items| {
+            items
+                .iter_mut()
+                .filter(|x| x.available && x.item.score() != 0)
+                .for_each(|x| {
+                    x.item
+                        .update_covering_set(best_item.intermediate(), best_item.covering_set())
+                });
+        });
+
+        result.push(best_item);
     }
 
     result
