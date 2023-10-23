fc76e197e0471652e80319f15d684597876c6d62
Tan Chee Keong <tanck2005@gmail.com>
2023-10-24 07:36:51 +0800

    fix slot per epoch constant

--- scripts/local_testnet/setup_time.sh
+++ scripts/local_testnet/setup_time.sh
@@ -21,7 +21,7 @@ genesis_file=$1
 # Update future hardforks time in the EL genesis file based on the CL genesis time
 GENESIS_TIME=$(lcli pretty-ssz --spec $SPEC_PRESET --testnet-dir $TESTNET_DIR BeaconState $TESTNET_DIR/genesis.ssz | jq | grep -Po 'genesis_time": "\K.*\d')
 echo $GENESIS_TIME
-CAPELLA_TIME=$((GENESIS_TIME + (CAPELLA_FORK_EPOCH * 32 * SECONDS_PER_SLOT)))
+CAPELLA_TIME=$((GENESIS_TIME + (CAPELLA_FORK_EPOCH * $SLOT_PER_EPOCH * SECONDS_PER_SLOT)))
 echo $CAPELLA_TIME
 sed -i 's/"shanghaiTime".*$/"shanghaiTime": '"$CAPELLA_TIME"',/g' $genesis_file
 cat $genesis_file
