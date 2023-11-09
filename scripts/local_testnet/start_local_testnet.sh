1b8c0ed987f493d1ca6d1f93b7c883a51a170691
chonghe <44791194+chong-he@users.noreply.github.com>
2023-11-09 12:06:10 +0800

    Update local testnet doc and parameters (#4749)
    * update
    
    * revise link
    
    * update parameters
    
    * update doc
    
    * Update doc
    
    * add quic port
    
    * remove debug log
    
    * Fix el_bootnode not being killed
    
    * Fix time
    
    * Fix doc in manual creation of testnet
    
    * Update file
    
    * update api doc
    
    * Revert "update api doc"
    
    This reverts commit ed695743deca4818ad074e5285e04764965b1be5.
    
    * add git clone
    
    * Fix path
    
    * Fix path
    
    * Update scripts/local_testnet/setup_time.sh
    
    Co-authored-by: Jimmy Chen <jchen.tc@gmail.com>
    
    * Update scripts/local_testnet/README.md
    
    Co-authored-by: Jimmy Chen <jchen.tc@gmail.com>
    
    * Fix SLOT_PER_EPOCH that changes with mainnet or minimal
    
    * Embedded setup_time.sh in start_local_testnet.sh
    
    * fix slot per epoch constant
    
    * Add comment
    
    * Add CANCUN_TIME
    
    * Fix CANCUN_TIME constant 32 slots
    
    * Correct typo
    
    * chmod +x ./setup_time.sh
    
    ---------
    
    Co-authored-by: Jimmy Chen <jchen.tc@gmail.com>

--- scripts/local_testnet/start_local_testnet.sh
+++ scripts/local_testnet/start_local_testnet.sh
@@ -102,16 +102,8 @@ execute_command_add_PID() {
 echo "executing: ./setup.sh >> $LOG_DIR/setup.log"
 ./setup.sh >> $LOG_DIR/setup.log 2>&1
 
-# Update future hardforks time in the EL genesis file based on the CL genesis time
-GENESIS_TIME=$(lcli pretty-ssz --spec $SPEC_PRESET --testnet-dir $TESTNET_DIR BeaconState $TESTNET_DIR/genesis.ssz | jq | grep -Po 'genesis_time": "\K.*\d')
-echo $GENESIS_TIME
-CAPELLA_TIME=$((GENESIS_TIME + (CAPELLA_FORK_EPOCH * 32 * SECONDS_PER_SLOT)))
-echo $CAPELLA_TIME
-sed -i 's/"shanghaiTime".*$/"shanghaiTime": '"$CAPELLA_TIME"',/g' $genesis_file
-CANCUN_TIME=$((GENESIS_TIME + (DENEB_FORK_EPOCH * 32 * SECONDS_PER_SLOT)))
-echo $CANCUN_TIME
-sed -i 's/"cancunTime".*$/"cancunTime": '"$CANCUN_TIME"',/g' $genesis_file
-cat $genesis_file
+# Call setup_time.sh to update future hardforks time in the EL genesis file based on the CL genesis time
+./setup_time.sh genesis.json
 
 # Delay to let boot_enr.yaml to be created
 execute_command_add_PID bootnode.log ./bootnode.sh
