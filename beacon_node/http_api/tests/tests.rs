f062e774acc412f18e881b61c0d52b688dc545b7
Jimmy Chen <jchen.tc@gmail.com>
2023-11-16 21:52:43 +1100

    Fix test compilation

--- beacon_node/http_api/tests/tests.rs
+++ beacon_node/http_api/tests/tests.rs
@@ -1651,7 +1651,7 @@ impl ApiTester {
             .get_beacon_light_client_optimistic_update::<E>()
             .await
         {
-            Ok(result) => result,
+            Ok(result) => result.map(|res| res.data),
             Err(_) => panic!("query did not fail correctly"),
         };
 
@@ -1667,7 +1667,7 @@ impl ApiTester {
             .get_beacon_light_client_finality_update::<E>()
             .await
         {
-            Ok(result) => result,
+            Ok(result) => result.map(|res| res.data),
             Err(_) => panic!("query did not fail correctly"),
         };
 
