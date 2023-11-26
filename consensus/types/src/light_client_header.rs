65a5770c3345026955625e1431c690bb4ba5b2fe
Eitan Seri-Levi <eserilev@ucsc.edu>
2023-11-26 09:35:08 -0800

    remove unwraps

--- consensus/types/src/light_client_header.rs
+++ consensus/types/src/light_client_header.rs
@@ -1,4 +1,8 @@
-use crate::{test_utils::TestRandom, EthSpec, FixedVector, ExecutionPayloadHeader, Hash256, SignedBeaconBlock};
+use crate::light_client_update::*;
+use crate::{
+    test_utils::TestRandom, EthSpec, ExecutionPayloadHeader, FixedVector, Hash256,
+    SignedBeaconBlock,
+};
 use crate::{BeaconBlockHeader, ExecutionPayload};
 use merkle_proof::{verify_merkle_proof, MerkleTree};
 use serde::{Deserialize, Serialize};
@@ -6,7 +10,6 @@ use ssz::Encode;
 use ssz_derive::{Decode, Encode};
 use test_random_derive::TestRandom;
 use tree_hash::TreeHash;
-use crate::light_client_update::*;
 
 #[derive(
     Debug,
@@ -40,43 +43,38 @@ impl<E: EthSpec> From<BeaconBlockHeader> for LightClientHeader<E> {
     }
 }
 
-impl<E: EthSpec> From<SignedBeaconBlock<E>> for LightClientHeader<E> {
-    fn from(block: SignedBeaconBlock<E>) -> Self {
+impl<E: EthSpec> LightClientHeader<E> {
+    fn new(block: SignedBeaconBlock<E>) -> Result<Self, Error> {
         let epoch = block.message().slot().epoch(E::slots_per_epoch());
 
         // TODO epoch greater than or equal to capella
         let payload: ExecutionPayload<E> = if epoch >= 0 {
             block
                 .message()
-                .execution_payload()
-                .unwrap()
-                .execution_payload_capella()
-                .unwrap()
+                .execution_payload()?
+                .execution_payload_capella()?
                 .to_owned()
                 .into()
         } else if epoch >= 1 {
             block
                 .message()
-                .execution_payload()
-                .unwrap()
-                .execution_payload_deneb()
-                .unwrap()
+                .execution_payload()?
+                .execution_payload_deneb()?
                 .to_owned()
                 .into()
         } else {
-            return LightClientHeader {
+            return Ok(LightClientHeader {
                 beacon: block.message().block_header(),
                 execution: None,
                 execution_branch: None,
-            };
+            });
         };
 
         // TODO fix unwrap
         let header = ExecutionPayloadHeader::from(payload.to_ref());
         let leaves = block
             .message()
-            .body_capella()
-            .unwrap()
+            .body_capella()?
             .as_ssz_bytes()
             .iter()
             .map(|data| data.tree_hash_root())
@@ -84,22 +82,16 @@ impl<E: EthSpec> From<SignedBeaconBlock<E>> for LightClientHeader<E> {
 
         let tree = MerkleTree::create(&leaves, EXECUTION_PAYLOAD_PROOF_LEN);
 
-        let (_, proof) = tree
-            .generate_proof(
-                EXECUTION_PAYLOAD_INDEX,
-                EXECUTION_PAYLOAD_PROOF_LEN,
-            )
-            .unwrap();
+        let (_, proof) =
+            tree.generate_proof(EXECUTION_PAYLOAD_INDEX, EXECUTION_PAYLOAD_PROOF_LEN)?;
 
-        LightClientHeader {
+        Ok(LightClientHeader {
             beacon: block.message().block_header(),
             execution: Some(header),
             execution_branch: Some(proof.into()),
-        }
+        })
     }
-}
 
-impl<E: EthSpec> LightClientHeader<E> {
     fn get_lc_execution_root(&self) -> Option<Hash256> {
         let epoch = self.beacon.slot.epoch(E::slots_per_epoch());
 
@@ -110,46 +102,46 @@ impl<E: EthSpec> LightClientHeader<E> {
             }
         }
 
-        return None;
+        None
     }
 
-    fn is_valid_light_client_header(&self) -> bool {
+    fn is_valid_light_client_header(&self) -> Result<bool, Error> {
         let epoch = self.beacon.slot.epoch(E::slots_per_epoch());
 
         // TODO LESS THAN DENEB
         if epoch < 1 {
             let Some(execution) = &self.execution else {
-                return false;
+                return Ok(false);
             };
 
             // TODO unwrap
-            if execution.blob_gas_used().unwrap().to_owned() != 0 as u64
-                || execution.excess_blob_gas().unwrap().to_owned() != 0 as u64
+            if execution.blob_gas_used()?.to_owned() != 0 as u64
+                || execution.excess_blob_gas()?.to_owned() != 0 as u64
             {
-                return false;
+                return Ok(false);
             }
         }
 
         // TODO LESS THAN DENEB
         if epoch < 0 {
-            return self.execution == None && self.execution_branch == None;
+            return Ok(self.execution == None && self.execution_branch == None);
         }
 
         let Some(execution_root) = self.get_lc_execution_root() else {
-            return false;
+            return Ok(false);
         };
 
         let Some(execution_branch) = &self.execution_branch else {
-            return false;
+            return Ok(false);
         };
 
-        return verify_merkle_proof(
+        Ok(verify_merkle_proof(
             execution_root,
             &execution_branch,
             EXECUTION_PAYLOAD_PROOF_LEN,
             get_subtree_index(EXECUTION_PAYLOAD_INDEX as u32) as usize,
             self.beacon.body_root,
-        );
+        ))
     }
 }
 
