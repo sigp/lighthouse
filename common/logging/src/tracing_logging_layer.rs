d6ff17e72dbc7efe340b65ffee02dc2f78e89940
Eitan Seri-Levi <eserilev@ucsc.edu>
2023-12-06 18:57:46 -0800

    non blocking file writer

--- common/logging/src/tracing_logging_layer.rs
+++ common/logging/src/tracing_logging_layer.rs
@@ -1,9 +1,24 @@
+use std::collections::HashMap;
 use std::fs::{self, File, OpenOptions};
-use std::path::{Path, PathBuf};
-use std::sync::mpsc::{Sender, self};
-use std::thread;
 use std::io::Write;
-use std::time::{SystemTime, Duration};
+use std::path::{Path, PathBuf};
+use std::sync::mpsc::{self, Sender};
+use std::thread;
+
+pub const MAX_FILE_SIZE_IN_BYTES: u64 = 100_000_000;
+
+pub const BASE_FILE_PATH: &str = "";
+
+lazy_static! {
+    pub static ref FILE_WRITER_STREAMS: HashMap<String, Option<NonBlockingFileWriter>> = {
+        let mut m = HashMap::new();
+        let libp2p_logging_thread = NonBlockingFileWriter::new(Path::new("libp2p")).ok();
+        let discv5_logging_thread = NonBlockingFileWriter::new(Path::new("discv5")).ok();
+        m.insert("libp2p".to_string(), libp2p_logging_thread);
+        m.insert("discv5".to_string(), discv5_logging_thread);
+        m
+    };
+}
 
 /// Layer that handles `INFO`, `WARN` and `ERROR` logs emitted per dependency and
 /// writes them to a file. Dependencies are enabled via the `RUST_LOG` env flag.
@@ -22,24 +37,38 @@ impl<S: tracing_core::Subscriber> tracing_subscriber::layer::Layer<S> for Loggin
             None => "unknown",
         };
 
-        let target = &[target];
+        let Some(get_file_writer_stream) = FILE_WRITER_STREAMS.get(target) else {
+            return;
+        };
+
+        let Some(file_writer) = get_file_writer_stream else {
+            return;
+        };
 
         match *meta.level() {
-            tracing_core::Level::INFO => {},
-            tracing_core::Level::WARN => {},
-            tracing_core::Level::ERROR => {},
+            tracing_core::Level::INFO => {
+                let _ = file_writer.write("TEST".to_string());
+                ()
+            }
+            tracing_core::Level::WARN => {
+                let _ = file_writer.write("TEST".to_string());
+                ()
+            }
+            tracing_core::Level::ERROR => {
+                let _ = file_writer.write("TEST".to_string());
+                ()
+            }
             _ => {}
         }
     }
 }
 
-struct NonBlockingFileWriter {
+pub struct NonBlockingFileWriter {
     sender: Sender<String>,
-    path: PathBuf,
 }
 
 impl NonBlockingFileWriter {
-    fn new(path: &std::path::Path) -> Result<Self, std::io::Error> {
+    pub fn new(path: &std::path::Path) -> Result<Self, std::io::Error> {
         let (sender, receiver) = mpsc::channel();
         let path = path.to_path_buf();
 
@@ -52,16 +81,14 @@ impl NonBlockingFileWriter {
                 }
             };
 
-            let mut should_clear_file = false;
-
             for message in receiver {
-                should_clear_file = match NonBlockingFileWriter::get_file_size(&path) {
-                    Ok(file_size) => file_size > 0u64,
+                let should_clear_file = match NonBlockingFileWriter::get_file_size(&path) {
+                    Ok(file_size) => file_size > MAX_FILE_SIZE_IN_BYTES,
                     Err(_) => false,
                 };
 
                 if should_clear_file {
-                    NonBlockingFileWriter::clear_file(&path);
+                    let _ = NonBlockingFileWriter::clear_file(&path);
                 }
 
                 if let Err(e) = writeln!(file, "{}", message) {
@@ -70,19 +97,20 @@ impl NonBlockingFileWriter {
             }
         });
 
-        Ok(NonBlockingFileWriter { sender, path })
-
+        Ok(NonBlockingFileWriter { sender })
     }
 
-    fn write(&self, message: String) -> Result<(), std::io::Error> {
-        self.sender.send(message).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
+    pub fn write(&self, message: String) -> Result<(), std::io::Error> {
+        self.sender
+            .send(message)
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
     }
 
     fn get_file_size(path: &PathBuf) -> std::io::Result<u64> {
         let metadata = fs::metadata(path)?;
         Ok(metadata.len())
     }
-    
+
     fn clear_file(path: &PathBuf) -> std::io::Result<()> {
         File::create(path)?;
         Ok(())
