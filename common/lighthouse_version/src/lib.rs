9cd924366c627431abfb324357ab65acaea5431d
Michael Sproul <michael@sigmaprime.io>
2024-01-12 10:50:34 +1100

    Tree states release v4.6.111-exp

--- common/lighthouse_version/src/lib.rs
+++ common/lighthouse_version/src/lib.rs
@@ -17,8 +17,8 @@ pub const VERSION: &str = git_version!(
         // NOTE: using --match instead of --exclude for compatibility with old Git
         "--match=thiswillnevermatchlol"
     ],
-    prefix = "Lighthouse/v4.5.444-exp-",
-    fallback = "Lighthouse/v4.5.444-exp"
+    prefix = "Lighthouse/v4.6.111-exp-",
+    fallback = "Lighthouse/v4.6.111-exp"
 );
 
 /// Returns `VERSION`, but with platform information appended to the end.
