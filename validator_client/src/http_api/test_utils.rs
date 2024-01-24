f9e36c94edf69c87fafb912f81c124714855a84f
Eitan Seri-Levi <eserilev@ucsc.edu>
2024-01-25 00:09:47 +0200

    Expose additional builder booster related flags in the vc (#5086)
    * expose builder booster flags in vc, enable options in validator endpoints, update tests
    
    * resolve failing test
    
    * fix issues related to CreateConfig and MoveConfig
    
    * remove unneeded val, change how boost factor flag logic in the vc, add some additional documentation
    
    * fix typos
    
    * fix typos
    
    * assume builder-proosals flag if one of other two vc builder flags are present
    
    * fmt
    
    * typo
    
    * typo
    
    * Fix CLI help text
    
    * Prioritise per validator builder boost configurations over CLI flags.
    
    * Add http test for builder boost factor with process defaults.
    
    * Fix issue with PATCH request
    
    * Add prefer builder proposals
    
    * Add more builder boost factor tests.
    
    ---------
    
    Co-authored-by: Mac L <mjladson@pm.me>
    Co-authored-by: Jimmy Chen <jchen.tc@gmail.com>
    Co-authored-by: Paul Hauner <paul@paulhauner.com>

--- validator_client/src/http_api/test_utils.rs
+++ validator_client/src/http_api/test_utils.rs
@@ -315,6 +315,8 @@ impl ApiTester {
                 suggested_fee_recipient: None,
                 gas_limit: None,
                 builder_proposals: None,
+                builder_boost_factor: None,
+                prefer_builder_proposals: None,
                 deposit_gwei: E::default_spec().max_effective_balance,
             })
             .collect::<Vec<_>>();
@@ -447,6 +449,8 @@ impl ApiTester {
                 suggested_fee_recipient: None,
                 gas_limit: None,
                 builder_proposals: None,
+                builder_boost_factor: None,
+                prefer_builder_proposals: None,
             };
 
             self.client
@@ -467,6 +471,8 @@ impl ApiTester {
             suggested_fee_recipient: None,
             gas_limit: None,
             builder_proposals: None,
+            builder_boost_factor: None,
+            prefer_builder_proposals: None,
         };
 
         let response = self
@@ -511,6 +517,8 @@ impl ApiTester {
                     request_timeout_ms: None,
                     client_identity_path: None,
                     client_identity_password: None,
+                    builder_boost_factor: None,
+                    prefer_builder_proposals: None,
                 }
             })
             .collect();
@@ -534,7 +542,15 @@ impl ApiTester {
         let validator = &self.client.get_lighthouse_validators().await.unwrap().data[index];
 
         self.client
-            .patch_lighthouse_validators(&validator.voting_pubkey, Some(enabled), None, None, None)
+            .patch_lighthouse_validators(
+                &validator.voting_pubkey,
+                Some(enabled),
+                None,
+                None,
+                None,
+                None,
+                None,
+            )
             .await
             .unwrap();
 
@@ -582,6 +598,8 @@ impl ApiTester {
                 Some(gas_limit),
                 None,
                 None,
+                None,
+                None,
             )
             .await
             .unwrap();
@@ -609,6 +627,8 @@ impl ApiTester {
                 None,
                 None,
                 Some(builder_proposals),
+                None,
+                None,
                 None,
             )
             .await
