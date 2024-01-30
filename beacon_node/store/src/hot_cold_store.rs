8bd424c6774187a8700380b1b82ad1800ae9d82f
Michael Sproul <michael@sigmaprime.io>
2024-01-30 11:09:28 +1100

    Compact freezer DB after pruning

--- beacon_node/store/src/hot_cold_store.rs
+++ beacon_node/store/src/hot_cold_store.rs
@@ -2376,6 +2376,9 @@ impl<E: EthSpec, Hot: ItemStore<E>, Cold: ItemStore<E>> HotColdDB<E, Hot, Cold> 
             self.cold_db.do_atomically(cold_ops)?;
         }
 
+        // In order to reclaim space, we need to compact the freezer DB as well.
+        self.cold_db.compact()?;
+
         Ok(())
     }
 }
