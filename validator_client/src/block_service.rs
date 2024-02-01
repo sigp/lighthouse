1802080cabe8ec03010f899bd53fececbee88491
Mac L <mjladson@pm.me>
2024-02-01 17:10:25 +1100

    Remove unnecessary pubs

--- validator_client/src/block_service.rs
+++ validator_client/src/block_service.rs
@@ -53,8 +53,8 @@ impl From<Errors<BlockError>> for BlockError {
 pub struct BlockServiceBuilder<T, E: EthSpec> {
     validator_store: Option<Arc<ValidatorStore<T, E>>>,
     slot_clock: Option<Arc<T>>,
-    pub beacon_nodes: Option<Arc<BeaconNodeFallback<T, E>>>,
-    pub proposer_nodes: Option<Arc<BeaconNodeFallback<T, E>>>,
+    beacon_nodes: Option<Arc<BeaconNodeFallback<T, E>>>,
+    proposer_nodes: Option<Arc<BeaconNodeFallback<T, E>>>,
     context: Option<RuntimeContext<E>>,
     graffiti: Option<Graffiti>,
     graffiti_file: Option<GraffitiFile>,
@@ -186,8 +186,8 @@ impl<T: SlotClock, E: EthSpec> ProposerFallback<T, E> {
 pub struct Inner<T, E: EthSpec> {
     validator_store: Arc<ValidatorStore<T, E>>,
     slot_clock: Arc<T>,
-    pub beacon_nodes: Arc<BeaconNodeFallback<T, E>>,
-    pub proposer_nodes: Option<Arc<BeaconNodeFallback<T, E>>>,
+    pub(crate) beacon_nodes: Arc<BeaconNodeFallback<T, E>>,
+    pub(crate) proposer_nodes: Option<Arc<BeaconNodeFallback<T, E>>>,
     context: RuntimeContext<E>,
     graffiti: Option<Graffiti>,
     graffiti_file: Option<GraffitiFile>,
