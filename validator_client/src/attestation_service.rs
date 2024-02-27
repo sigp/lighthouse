e8619b2c0d9edacae9f52b0913d9e8c1f30344ef
kamuik16 <krishang.s@ahduni.edu.in>
2024-02-27 12:16:35 +0530

    fix: issue 5288

--- validator_client/src/attestation_service.rs
+++ validator_client/src/attestation_service.rs
@@ -430,6 +430,11 @@ impl<T: SlotClock + 'static, E: EthSpec> AttestationService<T, E> {
             .flatten()
             .unzip();
 
+        if attestations.len() == 0 {
+            warn!("WARN: No attestations were published");
+            return Ok(None);
+        }
+
         // Post the attestations to the BN.
         match self
             .beacon_nodes
