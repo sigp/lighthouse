fc66452a78394fb2dc46974858254a799928cb78
Tan Chee Keong <tanck2005@gmail.com>
2024-03-13 19:31:00 +0800

    Revert "Adjust width"
    This reverts commit 6fea81b897ce4a346c90902f1c95190875afc2d9.

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -17,9 +17,6 @@ write_to_file() {
 
     # We need to add the header and the backticks to create the code block.
     printf "# %s\n\n\`\`\`\n%s\n\`\`\`" "$program" "$cmd" > "$file"
-
-    # Adjust the width of the help text and append to the end of file
-    sed -i -e '$a\'$'\n''<style> .content main {max-width:88%;} </style>' "$file"
 }
 
 CMD=./target/release/lighthouse
