09849e1332b2c6604bdeee6179d88f54b1fa677a
chonghe <tanck2005@gmail.com>
2024-04-02 08:47:13 +0800

    Update script

--- scripts/markdown.sh
+++ scripts/markdown.sh
@@ -8,4 +8,10 @@ docker run -v ./book:/workdir ghcr.io/igorshubovych/markdownlint-cli:latest '**/
 # exit code
 exit_code=$(echo $?)
 
-exit $exit_code
+if [[ $exit_code == 1 ]]; then
+    echo "Exiting with error to indicate changes required. To fix, run 'make mdlint' and commit the changes."
+    exit $exit_code
+else
+    echo "All markdown files are properly formatted."
+    exit $exit_code
+fi
