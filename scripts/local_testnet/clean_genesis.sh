7a5828b80fe0fc339eba6534bfcc8a1df1ea7700
Daniel Ramirez <hi@danielrachi.com>
2024-04-04 21:18:12 -0500

    Reset prague time

--- scripts/local_testnet/clean_genesis.sh
+++ scripts/local_testnet/clean_genesis.sh
@@ -5,3 +5,4 @@ genesis_file=$1
 # Reset the `genesis.json` config file fork times.
 sed -i 's/"shanghaiTime".*$/"shanghaiTime": 0,/g' $genesis_file
 sed -i 's/"cancunTime".*$/"cancunTime": 0,/g' $genesis_file
+sed -i 's/"pragueTime".*$/"pragueTime": 0,/g' $genesis_file
