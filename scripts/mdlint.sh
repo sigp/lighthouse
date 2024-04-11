2fe777cef663d8956972dc98f3731e398d7dd7d4
chonghe <tanck2005@gmail.com>
2024-04-11 22:09:32 +0800

    Test mdlint.sh

--- scripts/mdlint.sh
+++ scripts/mdlint.sh
@@ -3,15 +3,17 @@
 set -e
 
 # use markdownlint-cli to check for markdown files
-docker run -v ./book:/workdir ghcr.io/igorshubovych/markdownlint-cli:latest '**/*.md' --ignore node_modules --fix
+docker run -v ./book:/workdir ghcr.io/igorshubovych/markdownlint-cli:latest '**/*.md' --ignore node_modules
 
 # exit code
 exit_code=$(echo $?)
+echo $exit_code
 
 if [[ $exit_code == 0 ]]; then
     echo "All markdown files are properly formatted."
     exit $exit_code
 else
-    echo "Exiting with exit code $exit_code. If the exit code is 1, run 'make mdlint' and commit the changes."
+    docker run -v ./book:/workdir ghcr.io/igorshubovych/markdownlint-cli:latest '**/*.md' --ignore node_modules --fix
+    # echo "Exiting with exit code $exit_code. If the exit code is 1, run 'make mdlint' and commit the changes."
     exit $exit_code
 fi
