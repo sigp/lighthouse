12d196d4e614bac519120f0d886db959836b43dc
chonghe <tanck2005@gmail.com>
2024-04-11 16:40:24 +0800

    Set MD033 to false

--- scripts/mdlint.sh
+++ scripts/mdlint.sh
@@ -3,7 +3,7 @@
 #set -e
 
 # use markdownlint-cli docker to check for markdown file s
-docker run -v ./book:/workdir ghcr.io/igorshubovych/markdownlint-cli:latest '**/*.md' --ignore node_modules --fix --disable MD013 MD033
+docker run -v ./book:/workdir ghcr.io/igorshubovych/markdownlint-cli:latest '**/*.md' --ignore node_modules --fix --disable MD013
 
 # exit code
 exit_code=$(echo $?)
