82b131d37fef84b93c4a3477ee8093a0b39f81e7
ethDreamer <37123614+ethDreamer@users.noreply.github.com>
2024-04-22 19:21:21 -0500

    Electra: Add New Containers (#5607)
    * Electra: Add New Containers

---
+++ consensus/types/src/pending_balance_deposit.rs
@@ -0,0 +1,33 @@
+use crate::test_utils::TestRandom;
+use serde::{Deserialize, Serialize};
+use ssz_derive::{Decode, Encode};
+use test_random_derive::TestRandom;
+use tree_hash_derive::TreeHash;
+
+#[derive(
+    arbitrary::Arbitrary,
+    Debug,
+    PartialEq,
+    Eq,
+    Hash,
+    Clone,
+    Serialize,
+    Deserialize,
+    Encode,
+    Decode,
+    TreeHash,
+    TestRandom,
+)]
+pub struct PendingBalanceDeposit {
+    #[serde(with = "serde_utils::quoted_u64")]
+    pub index: u64,
+    #[serde(with = "serde_utils::quoted_u64")]
+    pub amount: u64,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    ssz_and_tree_hash_tests!(PendingBalanceDeposit);
+}
