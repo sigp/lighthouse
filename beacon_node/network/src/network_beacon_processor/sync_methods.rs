cd5c767c22d12977276da7160eee1d0e214ccde5
dapplion <35266934+dapplion@users.noreply.github.com>
2024-05-13 10:48:39 +0300

    Re-add dropped comments

--- beacon_node/network/src/network_beacon_processor/sync_methods.rs
+++ beacon_node/network/src/network_beacon_processor/sync_methods.rs
@@ -115,6 +115,7 @@ impl<T: BeaconChainTypes> NetworkBeaconProcessor<T> {
                 "Rpc block is being processed";
                 "action" => "sending rpc block to reprocessing queue",
                 "block_root" => %block_root,
+                "process_type" => ?process_type,
             );
 
             // Send message to work reprocess queue to retry the block
@@ -147,6 +148,7 @@ impl<T: BeaconChainTypes> NetworkBeaconProcessor<T> {
             "proposer" => block.message().proposer_index(),
             "slot" => block.slot(),
             "commitments" => commitments_formatted,
+            "process_type" => ?process_type,
         );
 
         let result = self
