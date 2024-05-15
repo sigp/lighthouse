2629393ad92bfe005fc1c6bf558c1d7a586fd03e
realbigsean <seananderson33@GMAIL.com>
2024-05-15 02:52:10 -0400

    cargo fmt

--- beacon_node/beacon_chain/src/attestation_verification.rs
+++ beacon_node/beacon_chain/src/attestation_verification.rs
@@ -816,7 +816,7 @@ impl<'a, T: BeaconChainTypes> IndexedUnaggregatedAttestation<'a, T> {
         committees_per_slot: u64,
         subnet_id: Option<SubnetId>,
         chain: &BeaconChain<T>,
-    ) -> Result<(u64, SubnetId), Error> { 
+    ) -> Result<(u64, SubnetId), Error> {
         let expected_subnet_id = SubnetId::compute_subnet_for_attestation::<T::EthSpec>(
             &attestation,
             committees_per_slot,
