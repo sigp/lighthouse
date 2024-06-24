df800b0bbfc90b6daed306ff379e867cb09db1cc
Eitan Seri-Levi <eserilev@ucsc.edu>
2024-06-24 12:19:59 +0200

    re-enable test

--- lighthouse/tests/beacon_node.rs
+++ lighthouse/tests/beacon_node.rs
@@ -2223,7 +2223,7 @@ fn slasher_broadcast_flag_false() {
         });
 }
 
-#[cfg(all(feature = "lmdb", not(any(feature = "lmdb", feature = "mdbx"))))]
+#[cfg(all(feature = "lmdb"))]
 #[test]
 fn slasher_backend_override_to_default() {
     // Hard to test this flag because all but one backend is disabled by default and the backend
