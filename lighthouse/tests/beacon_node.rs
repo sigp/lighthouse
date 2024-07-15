1375779b04f98bd4b990532d5583f5afeddb681e
Tan Chee Keong <tanck2005@gmail.com>
2024-07-15 20:57:12 +0800

    Define dir in execution-jwt

--- lighthouse/tests/beacon_node.rs
+++ lighthouse/tests/beacon_node.rs
@@ -267,6 +267,7 @@ fn always_prepare_payload_default() {
 
 #[test]
 fn always_prepare_payload_override() {
+    let dir = TempDir::new().expect("Unable to create temporary directory");
     CommandLineTest::new()
         .flag("always-prepare-payload", None)
         .flag(
@@ -274,7 +275,10 @@ fn always_prepare_payload_override() {
             Some("0x00000000219ab540356cbb839cbe05303d7705fa"),
         )
         .flag("execution-endpoint", Some("http://localhost:8551/"))
-        .flag("execution-jwt-secret-key", Some(jwt_secret_key))
+        .flag(
+            "execution-jwt",
+            dir.path().join("jwt-file").as_os_str().to_str(),
+        )
         .run_with_zero_port()
         .with_config(|config| assert!(config.chain.always_prepare_payload));
 }
