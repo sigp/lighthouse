6eee7488ed4898e2f792d683c22f0568ab6699cb
Pawan Dhananjay <pawandhananjay@gmail.com>
2024-09-12 17:37:19 -0700

    Change request json types

--- beacon_node/execution_layer/src/test_utils/handle_rpc.rs
+++ beacon_node/execution_layer/src/test_utils/handle_rpc.rs
@@ -247,7 +247,7 @@ pub async fn handle_rpc<E: EthSpec>(
                 Some(
                     ctx.execution_block_generator
                         .write()
-                        .new_payload(request.into()),
+                        .new_payload(request.try_into().unwrap()),
                 )
             } else {
                 None
