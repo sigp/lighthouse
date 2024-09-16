866dccafb106d660ab206ace874de92776631000
João Oliveira <hello@jxs.pt>
2024-09-16 11:03:49 +0100

    update rpc imports to be explicit

--- beacon_node/lighthouse_network/src/service/mod.rs
+++ beacon_node/lighthouse_network/src/service/mod.rs
@@ -10,7 +10,11 @@ use crate::peer_manager::{
 };
 use crate::peer_manager::{MIN_OUTBOUND_ONLY_FACTOR, PEER_EXCESS_FACTOR, PRIORITY_PEER_EXCESS};
 use crate::rpc::methods::MetadataRequest;
-use crate::rpc::*;
+use crate::rpc::{
+    methods, BlocksByRangeRequest, GoodbyeReason, HandlerErr, HandlerEvent, InboundRequest,
+    NetworkParams, OutboundRequest, Protocol, RPCCodedResponse, RPCError, RPCMessage, RPCReceived,
+    RPCResponse, RPCResponseErrorCode, ResponseTermination, RPC,
+};
 use crate::service::behaviour::BehaviourEvent;
 pub use crate::service::behaviour::Gossipsub;
 use crate::types::{
