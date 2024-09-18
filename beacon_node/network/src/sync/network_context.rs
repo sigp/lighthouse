ef7d0dc0b422a8a3f8b81528dbd3129e44dc2356
dapplion <35266934+dapplion@users.noreply.github.com>
2024-09-18 16:38:15 +0200

    Resolve some PeerDAS todos

--- beacon_node/network/src/sync/network_context.rs
+++ beacon_node/network/src/sync/network_context.rs
@@ -764,7 +764,6 @@ impl<T: BeaconChainTypes> SyncNetworkContext<T> {
             self.log.clone(),
         );
 
-        // TODO(das): start request
         // Note that you can only send, but not handle a response here
         match request.continue_requests(self) {
             Ok(_) => {
@@ -774,7 +773,6 @@ impl<T: BeaconChainTypes> SyncNetworkContext<T> {
                 self.custody_by_root_requests.insert(requester, request);
                 Ok(LookupRequestResult::RequestSent(req_id))
             }
-            // TODO(das): handle this error properly
             Err(e) => Err(RpcRequestSendError::CustodyRequestError(e)),
         }
     }
