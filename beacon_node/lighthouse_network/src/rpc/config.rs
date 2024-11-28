29ab3f521c206d8e65aef60c6e53ce4e44d4cb3a
Pawan Dhananjay <pawandhananjay@gmail.com>
2024-11-27 20:34:30 -0800

    Update limits

--- beacon_node/lighthouse_network/src/rpc/config.rs
+++ beacon_node/lighthouse_network/src/rpc/config.rs
@@ -104,14 +104,14 @@ impl RateLimiterConfig {
     pub const DEFAULT_META_DATA_QUOTA: Quota = Quota::n_every(2, 5);
     pub const DEFAULT_STATUS_QUOTA: Quota = Quota::n_every(5, 15);
     pub const DEFAULT_GOODBYE_QUOTA: Quota = Quota::one_every(10);
-    // We request 32 blocks per batch. In a mainnet like network, where we have many peers to choose
-    // from for range sync, we should ideally be spreading out the sync requests over multiple nodes
-    // and request not more than 2 epochs from a single peer every 5 seconds.
-    pub const DEFAULT_BLOCKS_BY_RANGE_QUOTA: Quota = Quota::n_every(160, 5);
+    // The number is chosen to balance between upload bandwidth required to serve
+    // blocks and a decent syncing rate for honest nodes. Malicious nodes would need to
+    // spread out their requests over the time window to max out bandwidth on the server.
+    pub const DEFAULT_BLOCKS_BY_RANGE_QUOTA: Quota = Quota::n_every(128, 10);
     pub const DEFAULT_BLOCKS_BY_ROOT_QUOTA: Quota = Quota::n_every(128, 10);
-    // `DEFAULT_BLOCKS_BY_RANGE_QUOTA` * max_blobs_per_block
-    pub const DEFAULT_BLOBS_BY_RANGE_QUOTA: Quota = Quota::n_every(960, 5);
-    pub const DEFAULT_BLOBS_BY_ROOT_QUOTA: Quota = Quota::n_every(768, 10);
+    // `DEFAULT_BLOCKS_BY_RANGE_QUOTA` * (target + 1) to account for high usage
+    pub const DEFAULT_BLOBS_BY_RANGE_QUOTA: Quota = Quota::n_every(512, 10);
+    pub const DEFAULT_BLOBS_BY_ROOT_QUOTA: Quota = Quota::n_every(512, 10);
     // 320 blocks worth of columns for regular node, or 40 blocks for supernode.
     // Range sync load balances when requesting blocks, and each batch is 32 blocks.
     pub const DEFAULT_DATA_COLUMNS_BY_RANGE_QUOTA: Quota = Quota::n_every(5120, 10);
