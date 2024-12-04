d75d340f2769f647fc6e5083a02887175c110c4e
Pawan Dhananjay <pawandhananjay@gmail.com>
2024-12-03 17:58:51 -0700

    Update builder api for electra

--- beacon_node/execution_layer/src/test_utils/mock_builder.rs
+++ beacon_node/execution_layer/src/test_utils/mock_builder.rs
@@ -1,4 +1,4 @@
-use crate::test_utils::{DEFAULT_BUILDER_PAYLOAD_VALUE_WEI, DEFAULT_JWT_SECRET};
+use crate::test_utils::DEFAULT_JWT_SECRET;
 use crate::{Config, ExecutionLayer, PayloadAttributes, PayloadParameters};
 use eth2::types::{BlobsBundle, BlockId, StateId, ValidatorId};
 use eth2::{BeaconNodeHttpClient, Timeouts, CONSENSUS_VERSION_HEADER};
@@ -584,7 +584,7 @@ pub fn serve<E: EthSpec>(
                 let mut message = match payload_response_type {
                     crate::GetPayloadResponseType::Full(payload_response) => {
                         #[allow(clippy::type_complexity)]
-                        let (payload, _block_value, maybe_blobs_bundle, _maybe_requests): (
+                        let (payload, value, maybe_blobs_bundle, maybe_requests): (
                             ExecutionPayload<E>,
                             Uint256,
                             Option<BlobsBundle<E>>,
@@ -600,8 +600,9 @@ pub fn serve<E: EthSpec>(
                                 blob_kzg_commitments: maybe_blobs_bundle
                                     .map(|b| b.commitments)
                                     .unwrap_or_default(),
-                                value: Uint256::from(DEFAULT_BUILDER_PAYLOAD_VALUE_WEI),
+                                value,
                                 pubkey: builder.builder_sk.public_key().compress(),
+                                execution_requests: maybe_requests.unwrap_or_default(),
                             }),
                             ForkName::Deneb => BuilderBid::Deneb(BuilderBidDeneb {
                                 header: payload
@@ -611,7 +612,7 @@ pub fn serve<E: EthSpec>(
                                 blob_kzg_commitments: maybe_blobs_bundle
                                     .map(|b| b.commitments)
                                     .unwrap_or_default(),
-                                value: Uint256::from(DEFAULT_BUILDER_PAYLOAD_VALUE_WEI),
+                                value,
                                 pubkey: builder.builder_sk.public_key().compress(),
                             }),
                             ForkName::Capella => BuilderBid::Capella(BuilderBidCapella {
@@ -619,7 +620,7 @@ pub fn serve<E: EthSpec>(
                                     .as_capella()
                                     .map_err(|_| reject("incorrect payload variant"))?
                                     .into(),
-                                value: Uint256::from(DEFAULT_BUILDER_PAYLOAD_VALUE_WEI),
+                                value,
                                 pubkey: builder.builder_sk.public_key().compress(),
                             }),
                             ForkName::Bellatrix => BuilderBid::Bellatrix(BuilderBidBellatrix {
@@ -627,7 +628,7 @@ pub fn serve<E: EthSpec>(
                                     .as_bellatrix()
                                     .map_err(|_| reject("incorrect payload variant"))?
                                     .into(),
-                                value: Uint256::from(DEFAULT_BUILDER_PAYLOAD_VALUE_WEI),
+                                value,
                                 pubkey: builder.builder_sk.public_key().compress(),
                             }),
                             ForkName::Base | ForkName::Altair => {
@@ -637,7 +638,7 @@ pub fn serve<E: EthSpec>(
                     }
                     crate::GetPayloadResponseType::Blinded(payload_response) => {
                         #[allow(clippy::type_complexity)]
-                        let (payload, _block_value, maybe_blobs_bundle, _maybe_requests): (
+                        let (payload, value, maybe_blobs_bundle, maybe_requests): (
                             ExecutionPayload<E>,
                             Uint256,
                             Option<BlobsBundle<E>>,
@@ -652,8 +653,9 @@ pub fn serve<E: EthSpec>(
                                 blob_kzg_commitments: maybe_blobs_bundle
                                     .map(|b| b.commitments)
                                     .unwrap_or_default(),
-                                value: Uint256::from(DEFAULT_BUILDER_PAYLOAD_VALUE_WEI),
+                                value,
                                 pubkey: builder.builder_sk.public_key().compress(),
+                                execution_requests: maybe_requests.unwrap_or_default(),
                             }),
                             ForkName::Deneb => BuilderBid::Deneb(BuilderBidDeneb {
                                 header: payload
@@ -663,7 +665,7 @@ pub fn serve<E: EthSpec>(
                                 blob_kzg_commitments: maybe_blobs_bundle
                                     .map(|b| b.commitments)
                                     .unwrap_or_default(),
-                                value: Uint256::from(DEFAULT_BUILDER_PAYLOAD_VALUE_WEI),
+                                value,
                                 pubkey: builder.builder_sk.public_key().compress(),
                             }),
                             ForkName::Capella => BuilderBid::Capella(BuilderBidCapella {
@@ -671,7 +673,7 @@ pub fn serve<E: EthSpec>(
                                     .as_capella()
                                     .map_err(|_| reject("incorrect payload variant"))?
                                     .into(),
-                                value: Uint256::from(DEFAULT_BUILDER_PAYLOAD_VALUE_WEI),
+                                value,
                                 pubkey: builder.builder_sk.public_key().compress(),
                             }),
                             ForkName::Bellatrix => BuilderBid::Bellatrix(BuilderBidBellatrix {
@@ -679,7 +681,7 @@ pub fn serve<E: EthSpec>(
                                     .as_bellatrix()
                                     .map_err(|_| reject("incorrect payload variant"))?
                                     .into(),
-                                value: Uint256::from(DEFAULT_BUILDER_PAYLOAD_VALUE_WEI),
+                                value,
                                 pubkey: builder.builder_sk.public_key().compress(),
                             }),
                             ForkName::Base | ForkName::Altair => {
