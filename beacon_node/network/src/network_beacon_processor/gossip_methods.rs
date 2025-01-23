19d6e329df73803ff5ae82493646c5f09a31c01b
dapplion <35266934+dapplion@users.noreply.github.com>
2025-01-23 17:39:13 +0800

    Resolve sampling request todo

--- beacon_node/network/src/network_beacon_processor/gossip_methods.rs
+++ beacon_node/network/src/network_beacon_processor/gossip_methods.rs
@@ -1447,9 +1447,10 @@ impl<T: BeaconChainTypes> NetworkBeaconProcessor<T> {
         let block = verified_block.block.block_cloned();
         let block_root = verified_block.block_root;
 
-        // TODO(das) Might be too early to issue a request here. We haven't checked that the block
-        // actually includes blob transactions and thus has data. A peer could send a block is
-        // garbage commitments, and make us trigger sampling for a block that does not have data.
+        // LINK PR WITH ATTEMPT TO CHANGE
+        // Note: okay to issue sampling request before the block is execution verified. If the
+        // proposer sends us a block with invalid blob transactions it can trigger us to issue
+        // sampling queries that will never resolve. This attack is equivalent to withholding data.
         if block.num_expected_blobs() > 0 {
             // Trigger sampling for block not yet execution valid. At this point column custodials are
             // unlikely to have received their columns. Triggering sampling so early is only viable with
