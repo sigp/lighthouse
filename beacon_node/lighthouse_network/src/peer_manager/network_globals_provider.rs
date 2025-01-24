acdb7ac5a36e625b43d15b61c798862c84e58af5
diego <diego@sigmaprime.io>
2025-01-24 12:43:30 +0100

    simplify to NetworkGlobalsProvider

---
+++ beacon_node/lighthouse_network/src/peer_manager/network_globals_provider.rs
@@ -0,0 +1,42 @@
+use crate::{NetworkGlobals, PeerId};
+use std::sync::Arc;
+use std::time::Instant;
+use types::EthSpec;
+
+pub trait NetworkGlobalsProvider {
+    /// Returns the number of libp2p connected peers with outbound-only connections.
+    fn connected_outbound_only_peers(&self) -> usize;
+
+    /// Returns the number of libp2p peers that are either connected or being dialed.
+    fn connected_or_dialing_peers(&self) -> usize;
+
+    /// Update min ttl of a peer.
+    fn update_min_ttl(&self, peer_id: &PeerId, min_ttl: Instant);
+
+    /// Returns true if the peer should be dialed. This checks the connection state and the
+    /// score state and determines if the peer manager should dial this peer.
+    fn should_dial(&self, peer_id: &PeerId) -> bool;
+}
+
+impl<E: EthSpec> NetworkGlobalsProvider for Arc<NetworkGlobals<E>> {
+    /// Returns the number of libp2p connected peers with outbound-only connections.
+    fn connected_outbound_only_peers(&self) -> usize {
+        self.as_ref().connected_outbound_only_peers()
+    }
+
+    /// Returns the number of libp2p peers that are either connected or being dialed.
+    fn connected_or_dialing_peers(&self) -> usize {
+        self.as_ref().connected_or_dialing_peers()
+    }
+
+    /// Update min ttl of a peer.
+    fn update_min_ttl(&self, peer_id: &PeerId, min_ttl: Instant) {
+        self.as_ref().peers.write().update_min_ttl(peer_id, min_ttl)
+    }
+
+    /// Returns true if the peer should be dialed. This checks the connection state and the
+    /// score state and determines if the peer manager should dial this peer.
+    fn should_dial(&self, peer_id: &PeerId) -> bool {
+        self.as_ref().peers.read().should_dial(peer_id)
+    }
+}
