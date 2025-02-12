7c921f92b823abc742035ff1440603c2521b6c6f
Maxim Evtush <154841002+maximevtush@users.noreply.github.com>
2025-02-12 11:58:38 +0100

    Update lib.rs

--- testing/eth1_test_rig/src/lib.rs
+++ testing/eth1_test_rig/src/lib.rs
@@ -147,7 +147,7 @@ impl DepositContract {
         self.deposit(deposit).await
     }
 
-    /// Perfoms a blocking deposit.
+    /// Performs a blocking deposit.
     pub async fn deposit(&self, deposit_data: DepositData) -> Result<(), String> {
         self.deposit_async(deposit_data)
             .await
