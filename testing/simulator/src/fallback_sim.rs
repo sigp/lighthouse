d36b7ab5badc4ac4bcf2e5155a9e216dd162fac3
cakevm <cakevm@proton.me>
2025-04-29 14:37:19 +0200

    Increase GENESIS_DELAY from 32 to 40

--- testing/simulator/src/fallback_sim.rs
+++ testing/simulator/src/fallback_sim.rs
@@ -18,7 +18,7 @@ use tracing_subscriber::prelude::*;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use types::{Epoch, EthSpec, MinimalEthSpec};
 const END_EPOCH: u64 = 16;
-const GENESIS_DELAY: u64 = 32;
+const GENESIS_DELAY: u64 = 40;
 const ALTAIR_FORK_EPOCH: u64 = 0;
 const BELLATRIX_FORK_EPOCH: u64 = 0;
 const CAPELLA_FORK_EPOCH: u64 = 1;
