4b8a0fbfa80b174667bf51ebbc8b3ed8f5853093
Eitan Seri-Levi <eserilev@ucsc.edu>
2025-05-15 17:17:40 -0700

    Test cases

--- beacon_node/http_api/tests/interactive_tests.rs
+++ beacon_node/http_api/tests/interactive_tests.rs
@@ -5,7 +5,6 @@ use beacon_chain::{
     ChainConfig,
 };
 use beacon_processor::work_reprocessing_queue::ReprocessQueueMessage;
-use either::Either;
 use eth2::types::ProduceBlockV3Response;
 use eth2::types::{DepositContractData, StateId};
 use execution_layer::{ForkchoiceState, PayloadAttributes};
@@ -894,7 +893,7 @@ async fn queue_attestations_from_http() {
     let fork_name = tester.harness.spec.fork_name_at_slot::<E>(attestation_slot);
 
     // Make attestations to the block and POST them to the beacon node on a background thread.
-    let attestation_future = if fork_name.electra_enabled() {
+    let attestation_future = {
         let single_attestations = harness
             .make_single_attestations(
                 &all_validators,
@@ -907,30 +906,9 @@ async fn queue_attestations_from_http() {
             .flat_map(|attestations| attestations.into_iter().map(|(att, _subnet)| att))
             .collect::<Vec<_>>();
 
-        let attestations = Either::Right(single_attestations);
-
         tokio::spawn(async move {
             client
-                .post_beacon_pool_attestations_v2::<E>(attestations, fork_name)
-                .await
-                .expect("attestations should be processed successfully")
-        })
-    } else {
-        let attestations = harness
-            .make_unaggregated_attestations(
-                &all_validators,
-                &post_state,
-                block.0.state_root(),
-                block_root.into(),
-                attestation_slot,
-            )
-            .into_iter()
-            .flat_map(|attestations| attestations.into_iter().map(|(att, _subnet)| att))
-            .collect::<Vec<_>>();
-
-        tokio::spawn(async move {
-            client
-                .post_beacon_pool_attestations_v1(&attestations)
+                .post_beacon_pool_attestations_v2::<E>(single_attestations, fork_name)
                 .await
                 .expect("attestations should be processed successfully")
         })
--- common/eth2/src/lib.rs
+++ common/eth2/src/lib.rs
@@ -18,7 +18,6 @@ use self::mixin::{RequestAccept, ResponseOptional};
 use self::types::{Error as ResponseError, *};
 use ::types::fork_versioned_response::ExecutionOptimisticFinalizedForkVersionedResponse;
 use derivative::Derivative;
-use either::Either;
 use futures::Stream;
 use futures_util::StreamExt;
 use libp2p_identity::PeerId;
@@ -1367,29 +1366,10 @@ impl BeaconNodeHttpClient {
         self.get_opt(path).await
     }
 
-    /// `POST v1/beacon/pool/attestations`
-    pub async fn post_beacon_pool_attestations_v1<E: EthSpec>(
-        &self,
-        attestations: &[Attestation<E>],
-    ) -> Result<(), Error> {
-        let mut path = self.eth_path(V1)?;
-
-        path.path_segments_mut()
-            .map_err(|()| Error::InvalidUrl(self.server.clone()))?
-            .push("beacon")
-            .push("pool")
-            .push("attestations");
-
-        self.post_with_timeout(path, &attestations, self.timeouts.attestation)
-            .await?;
-
-        Ok(())
-    }
-
     /// `POST v2/beacon/pool/attestations`
     pub async fn post_beacon_pool_attestations_v2<E: EthSpec>(
         &self,
-        attestations: Either<Vec<Attestation<E>>, Vec<SingleAttestation>>,
+        attestations: Vec<SingleAttestation>,
         fork_name: ForkName,
     ) -> Result<(), Error> {
         let mut path = self.eth_path(V2)?;
@@ -1400,26 +1380,13 @@ impl BeaconNodeHttpClient {
             .push("pool")
             .push("attestations");
 
-        match attestations {
-            Either::Right(attestations) => {
-                self.post_with_timeout_and_consensus_header(
-                    path,
-                    &attestations,
-                    self.timeouts.attestation,
-                    fork_name,
-                )
-                .await?;
-            }
-            Either::Left(attestations) => {
-                self.post_with_timeout_and_consensus_header(
-                    path,
-                    &attestations,
-                    self.timeouts.attestation,
-                    fork_name,
-                )
-                .await?;
-            }
-        };
+        self.post_with_timeout_and_consensus_header(
+            path,
+            &attestations,
+            self.timeouts.attestation,
+            fork_name,
+        )
+        .await?;
 
         Ok(())
     }
