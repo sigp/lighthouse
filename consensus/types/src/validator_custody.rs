8ff8d1eec0b222c70d0fd7fb1d895fe70a0bb086
Pawan Dhananjay <pawandhananjay@gmail.com>
2025-05-30 17:54:15 -0700

    Better first version

--- consensus/types/src/validator_custody.rs
+++ consensus/types/src/validator_custody.rs
@@ -1,98 +1,64 @@
-use parking_lot::{lock_api::RwLock, RwLock};
+use std::ops::Deref;
+
+use parking_lot::RwLock;
 
 use crate::{ChainSpec, Epoch};
+use ssz::Decode;
+use ssz_derive::{Decode, Encode};
 
 // TODO(pawan): think more carefully about this number
 pub const EPOCHS_BETWEEN_VALIDATOR_CUSTODY_UPDATES: usize = 10;
 
-/// Specifies the validator custody requirements for the node based
-/// on the number of validators attached to the beacon node.
-#[derive(Debug, Eq, PartialOrd, Ord, Clone)]
-pub enum ValidatorCustody {
-    /// There are sufficient attached validators for the node to have to
-    /// custody all the columns.
-    ///
-    /// Currently, we would need a minimum of 113 validators (32 ETH balance) attached to
-    /// the node to custody all `NUMBER_OF_COLUMNS` columns.
-    ///
-    /// NUMBER_OF_COLUMNS - VALIDATOR_CUSTODY_REQUIREMENT - SAMPLES_PER_SLOT + 1
-    ///  = 128 - 8 - 8 + 1 = 113
-    AllColumns,
-    /// All validator counts < 113.
-    ///
-    /// Note: A validator here refers to a 32 eth unit.
-    NumValidators(usize),
-    NoValidators,
+/// Specifies the number of validators attached to this node.
+#[derive(Debug, Copy, Encode, Decode, Clone)]
+pub struct ValidatorCustodyCount {
+    count: u64,
 }
 
-impl ValidatorCustody {
-    /// Generate the `ValidatorCustody` object based on a persisted value of the
-    /// `cgc`.
-    ///
-    /// This cgc is the value that we get from the persisted metadata/enr.
-    pub fn new_from_persisted_cgc(cgc: usize, spec: &ChainSpec) -> Self {
-        // if cgc >= Self::min_validators_for_full_custody(spec) {
-        //     Self::AllColumns
-        // } else {
-        //     Self::NumValidators(cgc)
-        // }
-        unimplemented!("todo(pawan)")
-    }
-
-    /// The minimum number of validators that need to be attached for
-    /// the node to have to custody all columns.
-    fn min_validators_for_full_custody(spec: &ChainSpec) -> usize {
-        spec.number_of_columns - spec.validator_custody_requirement - spec.samples_per_slot + 1
-    }
-
-    /// Total number of columns to custody based on this validator count
-    pub fn custody_count(&self, spec: &ChainSpec) -> usize {
-        match self {
-            Self::AllColumns => spec.number_of_columns,
-            Self::NumValidators(count) => {
-                if count == 0 {
-                    0
-                } else {
-                    std::cmp::min(
-                        spec.validator_custody_requirement + count - 1,
-                        spec.number_of_columns,
-                    )
-                }
-            }
-        }
+impl ValidatorCustodyCount {
+    pub fn new(count: u64) -> Self {
+        Self { count }
     }
 }
 
-/// Collects the various components for determining the custody count.
-#[derive(Debug, Clone)]
-pub struct CustodyCount {
-    /// Columns to be custodied based on number of validators
-    /// that is attached to this node.
-    pub validator_custody: ValidatorCustody,
-    /// Columns to be custodied based on the cli parameters passed by
-    /// the user on startup.
-    pub cli_custody_count: usize,
+impl Deref for ValidatorCustodyCount {
+    type Target = u64;
+    fn deref(&self) -> &Self::Target {
+        &self.count
+    }
 }
 
-impl CustodyCount {
-    /// The total number of columns that need to be custodied for a node with
-    /// the given params.
-    pub fn custody_columns_count(&self, spec: &ChainSpec) -> usize {
+impl ValidatorCustodyCount {
+    /// Number of columns/custody groups to custody based on the number of validators
+    /// attached.
+    pub fn custody_count(&self, spec: &ChainSpec) -> u64 {
         std::cmp::min(
-            self.cli_custody_count + self.validator_custody.custody_count(spec),
-            spec.number_of_columns,
+            spec.number_of_columns as u64,
+            spec.validator_custody_requirement + self.count - 1,
         )
     }
 }
 
 #[derive(Debug)]
 pub struct CustodyContext {
-    /// This is the `CustodyCount` object we are using to compute the
-    /// cgc value that we advertise to our peers in our enr and metadata.
-    advertised_custody: RwLock<CustodyCount>,
-    /// This is the `CustodyCount` object that we use to perform sampling duties
-    /// at head (while syncing or when receiving gossip).
-    custody_at_head: RwLock<CustodyCount>,
+    /// Columns to be custodied based on number of validators
+    /// that is attached to this node.
+    ///
+    /// This is the number that we use to compute the custody count value that
+    /// we advertise to our peers in the metadata and enr values.
+    advertised_validator_custody: RwLock<ValidatorCustodyCount>,
+    /// This is the validator count that we use to compute the number of columns we need to
+    /// sample at head (while syncing or when receiving gossip).
+    validator_custody_at_head: RwLock<ValidatorCustodyCount>,
+    /// Is the node run as a supernode based on current cli parameters.
+    current_is_supernode: bool,
+    /// The persisted value for `is_supernode` based on the previous run of this node.
+    ///
+    /// Note: We require this value because if a user restarts the node with a higher cli custody
+    /// count value than in the previous run, then we should continue advertising the custody
+    /// count based on the old value than the new one since we haven't backfilled the required
+    /// columns.
+    persisted_is_supernode: bool,
     /// Updates to the number of validators that is attached to this node
     /// over a given time duration.
     /// TODO(pawan): make this a constant sized queue.
@@ -102,38 +68,87 @@ pub struct CustodyContext {
 impl CustodyContext {
     /// Create a new custody default custody context object when no persisted object
     /// exists.
-    pub fn new(cli_custody_count: usize, persisted_cgc: usize, spec: &ChainSpec) -> Self {
-        let advertised_custody = CustodyCount {
-            cli_custody_count,
-            validator_custody: ValidatorCustody::NoValidators,
-        };
-
-        // The advertised custody and the custody object are the same when we
-        // create an entirely new object.
-        let custody_at_head = advertised_custody.clone();
+    ///
+    /// The `is_supernode` value is based on current cli parameters.
+    pub fn new(is_supernode: bool) -> Self {
         Self {
-            advertised_custody: RwLock::new(advertised_custody),
-            custody_at_head: RwLock::new(custody_at_head),
+            advertised_validator_custody: RwLock::new(ValidatorCustodyCount::new(0)),
+            validator_custody_at_head: RwLock::new(ValidatorCustodyCount::new(0)),
+            current_is_supernode: is_supernode,
+            persisted_is_supernode: is_supernode,
             validator_custody_updates: vec![],
         }
     }
 
-    pub fn new_from_persisted_custody_context(bytes: &[u8]) -> Result<Self, String> {}
+    /// Deserialize a `CustodyContext` from SSZ bytes.
+    pub fn new_from_persisted_custody_context(
+        bytes: &[u8],
+        is_supernode: bool,
+    ) -> Result<Self, String> {
+        let ssz_context = CustodyContextSsz::from_ssz_bytes(bytes)
+            .map_err(|e| format!("Failed to decode CustodyContextSsz: {:?}", e))?;
+        Ok(CustodyContext {
+            advertised_validator_custody: RwLock::new(ssz_context.advertised_validator_custody),
+            validator_custody_at_head: RwLock::new(ssz_context.validator_custody_at_head),
+            current_is_supernode: is_supernode,
+            persisted_is_supernode: ssz_context.persisted_is_supernode,
+            validator_custody_updates: ssz_context.validator_custody_updates,
+        })
+    }
 
     /// The custody count that we advertise to our peers in our metadata and
     /// enr values.
-    pub fn advertised_custody_column_count(&self, spec: &ChainSpec) -> usize {
-        self.advertised_custody.read().custody_columns_count(spec)
+    pub fn advertised_custody_column_count(&self, spec: &ChainSpec) -> u64 {
+        if self.persisted_is_supernode {
+            return spec.number_of_columns;
+        }
+        let advertised_validator_custody = self.advertised_validator_custody.read().count;
+        if advertised_validator_custody > 0 {
+            std::cmp::min(
+                spec.validator_custody_requirement + advertised_validator_custody - 1
+                    + spec.custody_requirement,
+                spec.number_of_columns,
+            )
+        } else {
+            spec.custody_requirement
+        }
     }
 
-    /// The number of columns that we sample for the data availability check
-    /// at the head of the chain.
+    /// The custody count that we use to custody columns currently.
     ///
-    /// Use this function to get the custody count number for blocks received
-    /// on gossip/rpc/sync.
-    ///
-    /// This value is essentially the internal `cgc` of the node.
-    pub fn custody_column_count(&self, spec: &ChainSpec) -> usize {
-        self.custody_at_head.read().custody_columns_count(spec)
+    /// This function should be called when figuring out how many columns we
+    /// need to custody when receiving blocks over gossip/rpc or during sync.
+    pub fn head_custody_count(&self, spec: &ChainSpec) -> u64 {
+        if self.current_is_supernode {
+            return spec.number_of_columns;
+        }
+        let custody_at_head = self.validator_custody_at_head.read().count;
+        if custody_at_head > 0 {
+            std::cmp::min(
+                spec.validator_custody_requirement + custody_at_head - 1 + spec.custody_requirement,
+                spec.number_of_columns,
+            )
+        } else {
+            spec.custody_requirement
+        }
+    }
+}
+
+#[derive(Debug, Encode, Decode, Clone)]
+pub struct CustodyContextSsz {
+    advertised_validator_custody: ValidatorCustodyCount,
+    validator_custody_at_head: ValidatorCustodyCount,
+    persisted_is_supernode: bool,
+    validator_custody_updates: Vec<(Epoch, usize)>,
+}
+
+impl From<CustodyContext> for CustodyContextSsz {
+    fn from(context: CustodyContext) -> Self {
+        CustodyContextSsz {
+            advertised_validator_custody: context.advertised_validator_custody.read().clone(),
+            validator_custody_at_head: context.validator_custody_at_head.read().clone(),
+            persisted_is_supernode: context.persisted_is_supernode,
+            validator_custody_updates: context.validator_custody_updates.clone(),
+        }
     }
 }
