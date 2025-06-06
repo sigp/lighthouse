748a65bcf478cb97dc3676d0b6c910571bde45f9
Pawan Dhananjay <pawandhananjay@gmail.com>
2025-06-06 16:31:59 -0700

    Fix a bunch of conditions; change internal api to use atomics

--- beacon_node/beacon_chain/src/validator_custody.rs
+++ beacon_node/beacon_chain/src/validator_custody.rs
@@ -1,4 +1,7 @@
-use std::collections::{BTreeMap, HashMap};
+use std::{
+    collections::{BTreeMap, HashMap},
+    sync::atomic::{AtomicU64, Ordering},
+};
 
 use parking_lot::RwLock;
 
@@ -6,7 +9,13 @@ use ssz_derive::{Decode, Encode};
 use tokio::sync::broadcast::{channel, Receiver, Sender};
 use types::{ChainSpec, Epoch, EthSpec, Slot};
 
-/// Specifies the number of validators attached to this node.
+const CHANNEL_CAPACITY: usize = 10;
+
+/// Specifies the number of validators attached to this node in increments of
+/// `BALANCE_PER_ADDITIONAL_CUSTODY_GROUP`.
+///
+/// Note: The `count` here effectively refers to the sum of all effective
+/// balances of all attached validators divided by `BALANCE_PER_ADDITIONAL_CUSTODY_GROUP`.
 #[derive(Debug, Copy, Encode, Decode, Clone)]
 pub struct ValidatorCustodyCount {
     count: u64,
@@ -25,44 +34,36 @@ impl std::ops::Deref for ValidatorCustodyCount {
     }
 }
 
-impl ValidatorCustodyCount {
-    /// Number of columns/custody groups to custody based on the number of validators
-    /// attached.
-    pub fn custody_count(&self, spec: &ChainSpec) -> u64 {
-        std::cmp::min(
-            spec.number_of_columns as u64,
-            spec.validator_custody_requirement + self.count - 1,
-        )
-    }
-}
-
 /// TODO(pawan): this currently just registers increases in validator count.
 /// Does not handle decreasing validator counts
 #[derive(Default, Debug)]
 struct ValidatorRegistrations {
     /// Set of all validators that is registered to this node along with its effective balance
     /// in increments of `BALANCE_PER_ADDITIONAL_CUSTODY_GROUP`
-    validators: HashMap<usize, u64>,
-    /// Maintains the number of unique validators that hit the subscriptions endpoint each
-    /// epoch where the validator count changed from the previous epoch.
     ///
-    /// If the count is same between subsequent epochs, then the later epoch values aren't stored
-    /// to save space.
-    epoch_validators: BTreeMap<Epoch, u64>,
+    /// Key is validator index and value is effective_balance // BALANCE_PER_ADDITIONAL_CUSTODY_GROUP.
+    validators: HashMap<usize, u64>,
+    /// Maintains the validator custody requirement at a given epoch.
+    ///
+    /// Note: Only stores the epoch value when there's a change in custody requirement.
+    /// So if epoch 10 and 11 has the same custody requirement, only 10 is stored.
+    epoch_validator_custody_requirements: BTreeMap<Epoch, u64>,
 }
 
 impl ValidatorRegistrations {
-    /// Returns the validator count at the latest epoch for the custody requirement.
-    ///
-    /// This should be equivalent to the current `validator_custody_at_head`.
-    pub fn latest_validator_count_for_custody(&self) -> Option<u64> {
-        self.epoch_validators.last_key_value().map(|(_, v)| *v)
+    /// Returns the validator custody requirement at the latest epoch.
+    pub fn latest_validator_custody_requirement(&self) -> Option<u64> {
+        self.epoch_validator_custody_requirements
+            .last_key_value()
+            .map(|(_, v)| *v)
     }
 
     /// Returns the latest epoch at which the validator count changed.
     #[allow(dead_code)]
     pub fn latest_epoch(&self) -> Option<Epoch> {
-        self.epoch_validators.last_key_value().map(|(k, _)| *k)
+        self.epoch_validator_custody_requirements
+            .last_key_value()
+            .map(|(k, _)| *k)
     }
 
     /// Register a new validator index and updates the list of validators if required.
@@ -81,19 +82,22 @@ impl ValidatorRegistrations {
             .insert(validator_index, num_validators_for_effective_balance);
 
         // Each `BALANCE_PER_ADDITIONAL_CUSTODY_GROUP` effectively contributes one unit of "weight".
-        let count_at_epoch = self.validators.values().sum();
+        let validator_count_at_epoch = self.validators.values().sum();
+        let validator_custody_requirement =
+            get_validators_custody_requirement(validator_count_at_epoch, spec);
 
-        // If registering the new validator increased the validator count, then
+        // If registering the new validator increased the total validator "units", then
         // add a new entry for the current epoch
-        if Some(count_at_epoch) != self.latest_validator_count_for_custody() {
-            self.epoch_validators
+        if Some(validator_custody_requirement) != self.latest_validator_custody_requirement() {
+            self.epoch_validator_custody_requirements
                 .entry(epoch)
-                .and_modify(|old_count| *old_count = count_at_epoch)
-                .or_insert(count_at_epoch);
+                .and_modify(|old_custody| *old_custody = validator_custody_requirement)
+                .or_insert(validator_custody_requirement);
         }
         tracing::debug!(
             %epoch,
-            validator_count = count_at_epoch,
+            validator_count = validator_count_at_epoch,
+            validator_custody_requirement,
             "Registered validators"
         );
     }
@@ -123,12 +127,12 @@ pub struct CustodyContext {
     /// Columns to be custodied based on number of validators
     /// that is attached to this node.
     ///
-    /// This is the number that we use to compute the custody count value that
+    /// This is the number that we use to compute the cgc value that
     /// we advertise to our peers in the metadata and enr values.
-    advertised_validator_custody: RwLock<ValidatorCustodyCount>,
-    /// This is the validator count that we use to compute the number of columns we need to
-    /// sample at head (while syncing or when receiving gossip).
-    validator_custody_at_head: RwLock<ValidatorCustodyCount>,
+    advertised_validator_custody_count: AtomicU64,
+    /// This is the validator custody count that we use to compute the number of columns we need to
+    /// custody at head (while syncing or when receiving gossip).
+    validator_custody_count_at_head: AtomicU64,
     /// Is the node run as a supernode based on current cli parameters.
     pub current_is_supernode: bool,
     /// The persisted value for `is_supernode` based on the previous run of this node.
@@ -151,11 +155,11 @@ impl CustodyContext {
     ///
     /// The `is_supernode` value is based on current cli parameters.
     pub fn new(is_supernode: bool) -> Self {
-        let (sender, receiver) = channel(10);
+        let (sender, receiver) = channel(CHANNEL_CAPACITY);
 
         Self {
-            advertised_validator_custody: RwLock::new(ValidatorCustodyCount::new(0)),
-            validator_custody_at_head: RwLock::new(ValidatorCustodyCount::new(0)),
+            advertised_validator_custody_count: AtomicU64::new(0),
+            validator_custody_count_at_head: AtomicU64::new(0),
             current_is_supernode: is_supernode,
             persisted_is_supernode: is_supernode,
             validator_registrations: Default::default(),
@@ -169,10 +173,12 @@ impl CustodyContext {
         ssz_context: CustodyContextSsz,
         is_supernode: bool,
     ) -> Self {
-        let (sender, receiver) = channel(10);
+        let (sender, receiver) = channel(CHANNEL_CAPACITY);
         CustodyContext {
-            advertised_validator_custody: RwLock::new(ssz_context.advertised_validator_custody),
-            validator_custody_at_head: RwLock::new(ssz_context.validator_custody_at_head),
+            advertised_validator_custody_count: AtomicU64::new(
+                ssz_context.advertised_validator_custody,
+            ),
+            validator_custody_count_at_head: AtomicU64::new(ssz_context.validator_custody_at_head),
             current_is_supernode: is_supernode,
             persisted_is_supernode: ssz_context.persisted_is_supernode,
             validator_registrations: Default::default(),
@@ -195,40 +201,47 @@ impl CustodyContext {
         slot: Slot,
         spec: &ChainSpec,
     ) {
-        // // Only do the registrations once per epoch
-        // if slot % E::slots_per_epoch() != 0 {
-        //     return;
-        // }
-
+        // Complete all registrations
         let mut registrations = self.validator_registrations.write();
         for (validator_index, effective_balance) in validators_and_balance {
             registrations.register_validator::<E>(validator_index, effective_balance, slot, spec);
         }
 
-        // Completed registrations, now check if the cgc has changed
-        let mut validator_custody_at_head = self.validator_custody_at_head.write();
-        let Some(new_validator_custody) = registrations.latest_validator_count_for_custody() else {
+        // Completed registrations, now check if the validator custody requirement has changed
+        let Some(new_validator_custody) = registrations.latest_validator_custody_requirement()
+        else {
             return;
         };
 
-        // Update the current validator custody value if the validator registration changes the number of
-        // validators
-        if new_validator_custody != validator_custody_at_head.count {
+        let current_cgc = self.head_custody_count(spec);
+        let validator_custody_count_at_head =
+            self.validator_custody_count_at_head.load(Ordering::Relaxed);
+
+        if new_validator_custody != validator_custody_count_at_head {
             tracing::debug!(
-                old_count = validator_custody_at_head.count,
+                old_count = validator_custody_count_at_head,
                 new_count = new_validator_custody,
                 "Validator count at head updated"
             );
-            *validator_custody_at_head = ValidatorCustodyCount {
-                count: new_validator_custody,
-            };
-            if let Err(e) = self
-                .sender
-                .send(CustodyContextMessage::HeadCustodyCountChanged {
-                    new_custody_count: new_validator_custody,
-                })
-            {
-                tracing::error!(error=?e, "Failed to send custody context message");
+            self.validator_custody_count_at_head
+                .store(new_validator_custody, Ordering::Relaxed);
+
+            let updated_cgc = self.head_custody_count(spec);
+            // Send the message to network only if there are more columns subnets to subscribe to
+            if updated_cgc > current_cgc {
+                tracing::debug!(
+                    old_cgc = current_cgc,
+                    updated_cgc,
+                    "Custody group count updated"
+                );
+                if let Err(e) = self
+                    .sender
+                    .send(CustodyContextMessage::HeadCustodyCountChanged {
+                        new_custody_count: updated_cgc,
+                    })
+                {
+                    tracing::error!(error=?e, "Failed to send custody context message");
+                }
             }
         }
     }
@@ -237,15 +250,15 @@ impl CustodyContext {
     /// enr values.
     pub fn advertised_custody_column_count(&self, spec: &ChainSpec) -> u64 {
         if self.persisted_is_supernode {
-            return spec.number_of_columns;
+            return spec.number_of_custody_groups;
         }
-        let advertised_validator_custody = self.advertised_validator_custody.read().count;
-        if advertised_validator_custody > 0 {
-            get_validators_custody_requirement(advertised_validator_custody, spec)
-                + spec.custody_requirement
-        } else {
-            spec.custody_requirement
-        }
+        let advertised_validator_custody = self
+            .advertised_validator_custody_count
+            .load(Ordering::Relaxed);
+        std::cmp::min(
+            advertised_validator_custody + spec.custody_requirement,
+            spec.number_of_custody_groups,
+        )
     }
 
     /// The custody count that we use to custody columns currently.
@@ -254,14 +267,14 @@ impl CustodyContext {
     /// need to custody when receiving blocks over gossip/rpc or during sync.
     pub fn head_custody_count(&self, spec: &ChainSpec) -> u64 {
         if self.current_is_supernode {
-            return spec.number_of_columns;
+            return spec.number_of_custody_groups;
         }
-        let custody_at_head = self.validator_custody_at_head.read().count;
-        if custody_at_head > 0 {
-            get_validators_custody_requirement(custody_at_head, spec) + spec.custody_requirement
-        } else {
-            spec.custody_requirement
-        }
+        let validator_custody_count_at_head =
+            self.validator_custody_count_at_head.load(Ordering::Relaxed);
+        std::cmp::min(
+            validator_custody_count_at_head + spec.custody_requirement,
+            spec.number_of_custody_groups,
+        )
     }
 }
 
@@ -286,16 +299,22 @@ pub enum CustodyContextMessage {
 /// The custody information that gets persisted across runs.
 #[derive(Debug, Encode, Decode, Clone)]
 pub struct CustodyContextSsz {
-    advertised_validator_custody: ValidatorCustodyCount,
-    validator_custody_at_head: ValidatorCustodyCount,
+    advertised_validator_custody: u64,
+    validator_custody_at_head: u64,
     persisted_is_supernode: bool,
 }
 
 impl From<&CustodyContext> for CustodyContextSsz {
     fn from(context: &CustodyContext) -> Self {
         CustodyContextSsz {
-            advertised_validator_custody: context.advertised_validator_custody.read().clone(),
-            validator_custody_at_head: context.validator_custody_at_head.read().clone(),
+            advertised_validator_custody: context
+                .advertised_validator_custody_count
+                .load(Ordering::Relaxed)
+                .clone(),
+            validator_custody_at_head: context
+                .validator_custody_count_at_head
+                .load(Ordering::Relaxed)
+                .clone(),
             persisted_is_supernode: context.persisted_is_supernode,
         }
     }
@@ -316,11 +335,11 @@ mod tests {
         let spec = E::default_spec();
         assert_eq!(
             custody_context.head_custody_count(&spec),
-            spec.number_of_columns
+            spec.number_of_custody_groups
         );
         assert_eq!(
             custody_context.advertised_custody_column_count(&spec),
-            spec.number_of_columns
+            spec.number_of_custody_groups
         );
     }
 
