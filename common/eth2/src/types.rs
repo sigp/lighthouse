fd89131fa378e4834c34a1b4d3008d1b4ca2e9c9
Tan Chee Keong <tanck@sigmaprime.io>
2025-06-16 13:36:30 +0800

    revise GraffitiPolicy display

--- common/eth2/src/types.rs
+++ common/eth2/src/types.rs
@@ -761,8 +761,8 @@ pub enum GraffitiPolicy {
 impl fmt::Display for GraffitiPolicy {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            GraffitiPolicy::PreserveUserGraffiti => write!(f, "preserve-user-graffiti"),
-            GraffitiPolicy::AppendClientVersions => write!(f, "append-client-versions"),
+            GraffitiPolicy::PreserveUserGraffiti => write!(f, "PreserveUserGraffiti"),
+            GraffitiPolicy::AppendClientVersions => write!(f, "AppendClientVersions"),
         }
     }
 }
