bded50f713da12635f6e16f9df48b56f2311113e
Michael Sproul <michael@sigmaprime.io>
2025-06-30 14:48:55 +1000

    Bump DB schema to v25

--- beacon_node/store/src/metadata.rs
+++ beacon_node/store/src/metadata.rs
@@ -4,7 +4,7 @@ use ssz::{Decode, Encode};
 use ssz_derive::{Decode, Encode};
 use types::{Hash256, Slot};
 
-pub const CURRENT_SCHEMA_VERSION: SchemaVersion = SchemaVersion(24);
+pub const CURRENT_SCHEMA_VERSION: SchemaVersion = SchemaVersion(25);
 
 // All the keys that get stored under the `BeaconMeta` column.
 //
