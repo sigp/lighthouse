445c80f4e37782017ff455ea591e42965c754443
Eitan Seri-Levi <eserilev@ucsc.edu>
2025-07-03 08:45:34 +0300

    fix test'

--- beacon_node/beacon_chain/src/schema_change/migration_schema_v27.rs
+++ beacon_node/beacon_chain/src/schema_change/migration_schema_v27.rs
@@ -1,11 +1,9 @@
-use crate::persisted_custody::{PersistedCustody, CUSTODY_DB_KEY};
-use crate::validator_custody::CustodyContextSsz;
 use crate::BeaconChainTypes;
-use ssz::{Decode, Encode};
-use ssz_derive::{Decode, Encode};
+use ssz::Encode;
 use std::sync::Arc;
+use store::metadata::DataColumnCustodyInfo;
 use store::metadata::DATA_COLUMN_CUSTODY_INFO_KEY;
-use store::{DBColumn, Error, HotColdDB, KeyValueStoreOp, StoreItem};
+use store::{DBColumn, Error, HotColdDB, KeyValueStoreOp};
 use tracing::info;
 
 /// Add `DataColumnCustodyInfo` entry to v27.
@@ -15,18 +13,18 @@ pub fn upgrade_to_v27<T: BeaconChainTypes>(
     let ops = if db.spec.is_peer_das_scheduled() {
         info!("Adding `DataColumnCustodyInfo` to the db");
         let data_column_custody_info = DataColumnCustodyInfo {
-            earliest_available_slot: None,
+            earliest_data_column_slot: None,
         };
         vec![KeyValueStoreOp::PutKeyValue(
-            DBColumn::DataColumnCustodyInfo,
+            DBColumn::BeaconDataColumnCustodyInfo,
             DATA_COLUMN_CUSTODY_INFO_KEY.as_slice().to_vec(),
             data_column_custody_info.as_ssz_bytes(),
         )]
     } else {
         // Delete it from the db if PeerDAS hasn't been scheduled
         vec![KeyValueStoreOp::DeleteKey(
-            DBColumn::DataColumnCustodyInfo,
-            data_column_custody_info.as_ssz_bytes(),
+            DBColumn::BeaconDataColumnCustodyInfo,
+            DATA_COLUMN_CUSTODY_INFO_KEY.as_slice().to_vec(),
         )]
     };
 
@@ -42,8 +40,8 @@ pub fn downgrade_from_v27<T: BeaconChainTypes>(
         ));
     }
     let ops = vec![KeyValueStoreOp::DeleteKey(
-        DBColumn::DataColumnCustodyInfo,
-        data_column_custody_info.as_ssz_bytes(),
+        DBColumn::BeaconDataColumnCustodyInfo,
+        DATA_COLUMN_CUSTODY_INFO_KEY.as_slice().to_vec(),
     )];
     Ok(ops)
 }
