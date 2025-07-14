3e4381e98ab58d0cdf7272907133bdd515e673e4
Tan Chee Keong <tanck@sigmaprime.io>
2025-07-14 09:20:05 +0800

    revert

--- beacon_node/lighthouse_network/tests/common.rs
+++ beacon_node/lighthouse_network/tests/common.rs
@@ -5,7 +5,6 @@ use lighthouse_network::EnrExt;
 use lighthouse_network::Multiaddr;
 use lighthouse_network::{NetworkConfig, NetworkEvent};
 use std::sync::Arc;
-use std::sync::Once;
 use std::sync::Weak;
 use tokio::runtime::Runtime;
 use tracing::{debug, error, info_span, Instrument};
@@ -14,8 +13,6 @@ use types::{
     ChainSpec, EnrForkId, Epoch, EthSpec, FixedBytesExtended, ForkContext, ForkName, Hash256,
     MinimalEthSpec, Slot,
 };
-
-static INIT: Once = Once::new();
 
 type E = MinimalEthSpec;
 
@@ -74,11 +71,10 @@ impl std::ops::DerefMut for Libp2pInstance {
 #[allow(unused)]
 pub fn build_tracing_subscriber(level: &str, enabled: bool) {
     if enabled {
-        INIT.call_once(|| {
-            tracing_subscriber::fmt()
-                .with_env_filter(EnvFilter::try_new(level).unwrap())
-                .init();
-        });
+        tracing_subscriber::fmt()
+            .with_env_filter(EnvFilter::try_new(level).unwrap())
+            .try_init()
+            .unwrap();
     }
 }
 
