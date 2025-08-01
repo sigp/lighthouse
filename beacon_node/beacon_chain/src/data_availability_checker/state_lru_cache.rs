b358a1a108cea403c5d8ed58d645e457798c916a
Jimmy Chen <jchen.tc@gmail.com>
2025-08-02 01:17:13 +1000

    Instrument `PendingComponents`.

--- beacon_node/beacon_chain/src/data_availability_checker/state_lru_cache.rs
+++ beacon_node/beacon_chain/src/data_availability_checker/state_lru_cache.rs
@@ -9,6 +9,7 @@ use parking_lot::RwLock;
 use state_processing::BlockReplayer;
 use std::sync::Arc;
 use store::OnDiskConsensusContext;
+use tracing::{debug_span, instrument, Span};
 use types::beacon_block_body::KzgCommitments;
 use types::{BeaconState, BlindedPayload, ChainSpec, Epoch, EthSpec, Hash256, SignedBeaconBlock};
 
@@ -106,9 +107,11 @@ impl<T: BeaconChainTypes> StateLRUCache<T> {
     /// This method will first check the cache and if the state is not found
     /// it will reconstruct the state by loading the parent state from disk and
     /// replaying the block.
+    #[instrument(skip_all, parent = _span, level = "debug")]
     pub fn recover_pending_executed_block(
         &self,
         diet_executed_block: DietAvailabilityPendingExecutedBlock<T::EthSpec>,
+        _span: &Span,
     ) -> Result<AvailabilityPendingExecutedBlock<T::EthSpec>, AvailabilityCheckError> {
         let state = if let Some(state) = self.states.write().pop(&diet_executed_block.state_root) {
             state
@@ -132,6 +135,7 @@ impl<T: BeaconChainTypes> StateLRUCache<T> {
 
     /// Reconstruct the state by loading the parent state from disk and replaying
     /// the block.
+    #[instrument(skip_all, level = "debug")]
     fn reconstruct_state(
         &self,
         diet_executed_block: &DietAvailabilityPendingExecutedBlock<T::EthSpec>,
@@ -164,8 +168,11 @@ impl<T: BeaconChainTypes> StateLRUCache<T> {
                 .state_root_iter(state_roots.into_iter())
                 .minimal_block_root_verification();
 
+        let block_replayer = debug_span!("reconstruct_state_apply_blocks").in_scope(|| {
+            block_replayer.apply_blocks(vec![diet_executed_block.block.clone_as_blinded()], None)
+        });
+
         block_replayer
-            .apply_blocks(vec![diet_executed_block.block.clone_as_blinded()], None)
             .map(|block_replayer| block_replayer.into_state())
             .and_then(|mut state| {
                 state
