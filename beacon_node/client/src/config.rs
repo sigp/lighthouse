ee9af3defd3edec07e83a2b4b372b70025d7e9f4
Jimmy Chen <jchen.tc@gmail.com>
2025-08-13 13:25:05 +1000

    Fix lint

--- beacon_node/client/src/config.rs
+++ beacon_node/client/src/config.rs
@@ -1,4 +1,3 @@
-use beacon_chain::TrustedSetup;
 use beacon_chain::graffiti_calculator::GraffitiOrigin;
 use beacon_chain::validator_monitor::ValidatorMonitorConfig;
 use beacon_processor::BeaconProcessorConfig;
--- beacon_node/src/config.rs
+++ beacon_node/src/config.rs
@@ -1,5 +1,4 @@
 use account_utils::{STDIN_INPUTS_FLAG, read_input_from_user};
-use beacon_chain::TrustedSetup;
 use beacon_chain::chain_config::{
     DEFAULT_PREPARE_PAYLOAD_LOOKAHEAD_FACTOR, DEFAULT_RE_ORG_HEAD_THRESHOLD,
     DEFAULT_RE_ORG_MAX_EPOCHS_SINCE_FINALIZATION, DEFAULT_RE_ORG_PARENT_THRESHOLD,
