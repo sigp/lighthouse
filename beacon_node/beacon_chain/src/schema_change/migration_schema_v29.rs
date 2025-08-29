e6806cd16aef76913b0fdb34d1bd86b2e90bfd03
Owanikin <oderindeife@gmail.com>
2025-08-29 14:12:11 +0100

    WIP: migration_schema_v29 changes

--- beacon_node/beacon_chain/src/schema_change/migration_schema_v29.rs
+++ beacon_node/beacon_chain/src/schema_change/migration_schema_v29.rs
@@ -4,24 +4,20 @@ use store::{metadata::SchemaVersion, Error, HotColdDB, KeyValueStoreOp};
 
 #[cfg(feature = "redb")]
 use store::config::BeaconNodeBackend;
-// Result<Vec<KeyValueStoreOp>, Error>
+
 pub fn upgrade_to_v29<T: BeaconChainTypes>(
     db: Arc<HotColdDB<T::EthSpec, T::HotStore, T::ColdStore>>
 ) -> Result<Vec<KeyValueStoreOp>, Error> {
     #[cfg(feature = "redb")]
     {
         if let Some(backend) = db.backend() {
-            use store::database::interface::BeaconNodeBackend;
-
             if let BeaconNodeBackend::Redb(redb) = backend {
                 match redb.upgrade() {
                     Ok(did_upgrade) => {
                         if did_upgrade {
-                            info!("Redb file format successfully upgraded to v29");
+                            tracing::info!("Redb file format successfully upgraded to v29");
                         } else {
-                            use tracing::info;
-
-                            info!("Redb file-format already at v29, no upgrade needed");
+                            tracing::info!("Redb file-format already at v29, no upgrade needed");
                         }
                     }
                     Err(e) => {
@@ -43,8 +39,6 @@ pub fn downgrade_from_v29<T: BeaconChainTypes>(
     #[cfg(feature = "redb")]
     {
         if let Some(backend) = db.backend() {
-            use store::database::interface::BeaconNodeBackend;
-
             if let BeaconNodeBackend::Redb(_) = backend {
                 return Err(Error::MigrationError(
                     "Cannot downgrade from v29: Redb file format upgrade is irreversible".to_string()
