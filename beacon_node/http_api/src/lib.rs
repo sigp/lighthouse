0113b007ff69b661443c25c0a39678f1f80373c3
Eitan Seri- Levi <eserilev@gmail.com>
2025-09-02 13:25:41 -0700

    fmt

--- beacon_node/http_api/src/lib.rs
+++ beacon_node/http_api/src/lib.rs
@@ -479,7 +479,9 @@ pub fn serve<T: BeaconChainTypes>(
                                 )))
                             }
                         }
-                        SyncState::SyncTransition | SyncState::BackFillSyncing { .. } | SyncState::CustodyBackFillSyncing { .. } => Ok(()),
+                        SyncState::SyncTransition
+                        | SyncState::BackFillSyncing { .. }
+                        | SyncState::CustodyBackFillSyncing { .. } => Ok(()),
                         SyncState::Synced => Ok(()),
                         SyncState::Stalled => Ok(()),
                     }
