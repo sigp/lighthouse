19688dfbb2274dc447de594c8b958464f8b2b8ee
Eitan Seri- Levi <eserilev@gmail.com>
2025-09-09 23:43:43 -0700

    Prevent silently dropping cell proof chunks

--- consensus/types/src/data_column_sidecar.rs
+++ consensus/types/src/data_column_sidecar.rs
@@ -143,6 +143,10 @@ pub enum DataColumnSidecarError {
     PreDeneb,
     SszError(SszError),
     BuildSidecarFailed(String),
+    InvalidCellProofLength {
+        expected: usize,
+        actual: usize
+    }
 }
 
 impl From<ArithError> for DataColumnSidecarError {
