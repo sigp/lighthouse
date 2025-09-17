6a525cbe7bccbe10b3ce62764dc048a61df5ea64
Eitan Seri- Levi <eserilev@gmail.com>
2025-09-17 09:11:06 -0700

    low prio

--- beacon_node/beacon_processor/src/rayon_manager.rs
+++ beacon_node/beacon_processor/src/rayon_manager.rs
@@ -1,8 +1,7 @@
 use rayon::{ThreadPool, ThreadPoolBuilder};
 use std::sync::Arc;
 
-const DEFAULT_LOW_PRIORITY_DIVISOR: usize = 4;
-const MINIMUM_LOW_PRIORITY_THREAD_COUNT: usize = 2;
+const LOW_PRIORITY_THREAD_COUNT: usize = 1;
 
 pub struct RayonManager {
     /// Smaller rayon thread pool for lower-priority, compute-intensive tasks.
@@ -12,11 +11,9 @@ pub struct RayonManager {
 
 impl Default for RayonManager {
     fn default() -> Self {
-        let low_prio_threads =
-            (num_cpus::get() / DEFAULT_LOW_PRIORITY_DIVISOR).max(MINIMUM_LOW_PRIORITY_THREAD_COUNT);
         let low_priority_threadpool = Arc::new(
             ThreadPoolBuilder::new()
-                .num_threads(low_prio_threads)
+                .num_threads(LOW_PRIORITY_THREAD_COUNT)
                 .build()
                 .expect("failed to build low-priority rayon pool"),
         );
