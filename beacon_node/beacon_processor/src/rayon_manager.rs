c0467f406d25ddbd32268c22836f58fb469b98e6
Eitan Seri- Levi <eserilev@gmail.com>
2025-09-17 16:53:22 -0700

    update minimum thread count to 1

--- beacon_node/beacon_processor/src/rayon_manager.rs
+++ beacon_node/beacon_processor/src/rayon_manager.rs
@@ -1,19 +1,22 @@
 use rayon::{ThreadPool, ThreadPoolBuilder};
 use std::sync::Arc;
 
-const LOW_PRIORITY_THREAD_COUNT: usize = 1;
+const DEFAULT_LOW_PRIORITY_DIVISOR: usize = 4;
+const MINIMUM_LOW_PRIORITY_THREAD_COUNT: usize = 1;
 
 pub struct RayonManager {
     /// Smaller rayon thread pool for lower-priority, compute-intensive tasks.
-    /// By default limited to one thread.
+    /// By default ~25% of CPUs or a minimum of 1 thread.
     pub low_priority_threadpool: Arc<ThreadPool>,
 }
 
 impl Default for RayonManager {
     fn default() -> Self {
+        let low_prio_threads =
+            (num_cpus::get() / DEFAULT_LOW_PRIORITY_DIVISOR).max(MINIMUM_LOW_PRIORITY_THREAD_COUNT);
         let low_priority_threadpool = Arc::new(
             ThreadPoolBuilder::new()
-                .num_threads(LOW_PRIORITY_THREAD_COUNT)
+                .num_threads(low_prio_threads)
                 .build()
                 .expect("failed to build low-priority rayon pool"),
         );
