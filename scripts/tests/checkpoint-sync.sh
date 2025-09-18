c1f0f472ab852ccaaa39ee7e65e8a857c593f027
Eitan Seri- Levi <eserilev@gmail.com>
2025-09-18 15:09:30 -0700

    Fix timeout

--- scripts/tests/checkpoint-sync.sh
+++ scripts/tests/checkpoint-sync.sh
@@ -17,7 +17,7 @@ POLL_INTERVAL_SECS=5
 # Target number of slots to backfill to complete this test.
 TARGET_BACKFILL_SLOTS=1024
 # Timeout for this test, if the node(s) fail to backfill `TARGET_BACKFILL_SLOTS` slots, fail the test.
-TIMEOUT_MINS=10
+TIMEOUT_MINS=12
 TIMEOUT_SECS=$((TIMEOUT_MINS * 60))
 # ------------------------------------------------------
 
