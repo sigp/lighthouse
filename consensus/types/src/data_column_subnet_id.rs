e885a268a6797ad548f086d5a9acb7145e273f28
Javier Chávarri <javier.chavarri@gmail.com>
2025-09-27 19:25:15 +0200

    remove debug transparent as it's not supported by educe

--- consensus/types/src/data_column_subnet_id.rs
+++ consensus/types/src/data_column_subnet_id.rs
@@ -1,17 +1,21 @@
 //! Identifies each data column subnet by an integer identifier.
 use crate::ChainSpec;
 use crate::data_column_sidecar::ColumnIndex;
-use educe::Educe;
 use safe_arith::{ArithError, SafeArith};
 use serde::{Deserialize, Serialize};
 use std::fmt::{self, Display};
 use std::ops::{Deref, DerefMut};
 
 #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
-#[derive(Clone, Copy, Educe, PartialEq, Eq, Hash, Serialize, Deserialize)]
-#[educe(Debug = "transparent")]
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 #[serde(transparent)]
 pub struct DataColumnSubnetId(#[serde(with = "serde_utils::quoted_u64")] u64);
+
+impl fmt::Debug for DataColumnSubnetId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        std::fmt::Debug::fmt(&self.0, f)
+    }
+}
 
 impl DataColumnSubnetId {
     pub fn new(id: u64) -> Self {
