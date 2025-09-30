de241595ba5b2b85c411a8dfc5f0197becc0a8d2
Mac L <mjladson@pm.me>
2025-10-01 02:43:23 +1000

    Migrate deposit_contract to alloy

--- common/deposit_contract/src/lib.rs
+++ common/deposit_contract/src/lib.rs
@@ -1,23 +1,72 @@
-use ethabi::{Contract, Token};
+use alloy_dyn_abi::{DynSolValue, JsonAbiExt};
+use alloy_json_abi::JsonAbi;
+use alloy_primitives::FixedBytes;
 use ssz::{Decode, DecodeError as SszDecodeError, Encode};
 use tree_hash::TreeHash;
 use types::{DepositData, Hash256, PublicKeyBytes, SignatureBytes};
 
-pub use ethabi::Error;
+pub use EncodeError as Error;
+
+#[derive(Debug)]
+pub enum EncodeError {
+    AlloyCoreError(alloy_json_abi::Error),
+    SerdeJsonError(serde_json::Error),
+    DynAbiError(alloy_dyn_abi::Error),
+    FunctionNotFound,
+}
+
+impl From<alloy_json_abi::Error> for EncodeError {
+    fn from(e: alloy_json_abi::Error) -> EncodeError {
+        EncodeError::AlloyCoreError(e)
+    }
+}
+
+impl From<serde_json::Error> for EncodeError {
+    fn from(e: serde_json::Error) -> EncodeError {
+        EncodeError::SerdeJsonError(e)
+    }
+}
+
+impl From<alloy_dyn_abi::Error> for EncodeError {
+    fn from(e: alloy_dyn_abi::Error) -> EncodeError {
+        EncodeError::DynAbiError(e)
+    }
+}
 
 #[derive(Debug)]
 pub enum DecodeError {
-    EthabiError(ethabi::Error),
+    AlloyCoreError(alloy_json_abi::Error),
     SszDecodeError(SszDecodeError),
+    SerdeJsonError(serde_json::Error),
+    DynAbiError(alloy_dyn_abi::Error),
+    FunctionNotFound,
     MissingField,
     UnableToGetBytes,
     MissingToken,
     InadequateBytes,
 }
 
-impl From<ethabi::Error> for DecodeError {
-    fn from(e: ethabi::Error) -> DecodeError {
-        DecodeError::EthabiError(e)
+impl From<alloy_json_abi::Error> for DecodeError {
+    fn from(e: alloy_json_abi::Error) -> DecodeError {
+        DecodeError::AlloyCoreError(e)
+    }
+}
+
+impl From<serde_json::Error> for DecodeError {
+    fn from(e: serde_json::Error) -> DecodeError {
+        DecodeError::SerdeJsonError(e)
+    }
+}
+
+impl From<alloy_dyn_abi::Error> for DecodeError {
+    fn from(e: alloy_dyn_abi::Error) -> DecodeError {
+        DecodeError::DynAbiError(e)
+    }
+}
+
+impl From<SszDecodeError> for DecodeError {
+    fn from(e: SszDecodeError) -> DecodeError {
+        DecodeError::SszDecodeError(e)
     }
 }
 
@@ -34,49 +83,68 @@ pub mod testnet {
         include_bytes!("../contracts/v0.12.1_testnet_validator_registration.bytecode");
 }
 
-pub fn encode_eth1_tx_data(deposit_data: &DepositData) -> Result<Vec<u8>, Error> {
+pub fn encode_eth1_tx_data(deposit_data: &DepositData) -> Result<Vec<u8>, EncodeError> {
     let params = vec![
-        Token::Bytes(deposit_data.pubkey.as_ssz_bytes()),
-        Token::Bytes(deposit_data.withdrawal_credentials.as_ssz_bytes()),
-        Token::Bytes(deposit_data.signature.as_ssz_bytes()),
-        Token::FixedBytes(deposit_data.tree_hash_root().as_ssz_bytes()),
+        DynSolValue::Bytes(deposit_data.pubkey.as_ssz_bytes()),
+        DynSolValue::Bytes(deposit_data.withdrawal_credentials.as_ssz_bytes()),
+        DynSolValue::Bytes(deposit_data.signature.as_ssz_bytes()),
+        DynSolValue::FixedBytes(
+            FixedBytes::<32>::from_slice(&deposit_data.tree_hash_root().as_ssz_bytes()),
+            32,
+        ),
     ];
 
-    // Here we make an assumption that the `crate::testnet::ABI` has a superset of the features of
-    // the crate::ABI`.
-    let abi = Contract::load(ABI)?;
-    let function = abi.function("deposit")?;
-    function.encode_input(&params)
+    let abi: JsonAbi = serde_json::from_slice(ABI)?;
+    let functions = abi
+        .function("deposit")
+        .ok_or(EncodeError::FunctionNotFound)?;
+    let function = &functions[0]; // Get the first (and likely only) deposit function
+
+    Ok(function.abi_encode_input(&params)?)
 }
 
 pub fn decode_eth1_tx_data(
     bytes: &[u8],
     amount: u64,
 ) -> Result<(DepositData, Hash256), DecodeError> {
-    let abi = Contract::load(ABI)?;
-    let function = abi.function("deposit")?;
-    let mut tokens = function.decode_input(bytes.get(4..).ok_or(DecodeError::InadequateBytes)?)?;
+    let abi: JsonAbi = serde_json::from_slice(ABI)?;
+    let functions = abi
+        .function("deposit")
+        .ok_or(DecodeError::FunctionNotFound)?;
+    let function = &functions[0]; // Get the first (and likely only) deposit function
+
+    let input_data = bytes.get(4..).ok_or(DecodeError::InadequateBytes)?;
+    let mut tokens = function.abi_decode_input(input_data)?;
+
+    // Reverse the order since we're popping from the end
+    tokens.reverse();
 
     macro_rules! decode_token {
-        ($type: ty, $to_fn: ident) => {
-            <$type>::from_ssz_bytes(
-                &tokens
-                    .pop()
-                    .ok_or_else(|| DecodeError::MissingToken)?
-                    .$to_fn()
-                    .ok_or_else(|| DecodeError::UnableToGetBytes)?,
-            )
-            .map_err(DecodeError::SszDecodeError)?
-        };
+        ($type: ty) => {{
+            let token = tokens.pop().ok_or(DecodeError::MissingToken)?;
+            let bytes_data = match token {
+                DynSolValue::Bytes(b) => b,
+                DynSolValue::FixedBytes(b, _) => b.to_vec(),
+                _ => return Err(DecodeError::UnableToGetBytes),
+            };
+            <$type>::from_ssz_bytes(&bytes_data)?
+        }};
     }
 
-    let root = decode_token!(Hash256, into_fixed_bytes);
+    let root = {
+        let token = tokens.pop().ok_or(DecodeError::MissingToken)?;
+        let bytes_data = match token {
+            DynSolValue::FixedBytes(b, _) => b.to_vec(),
+            _ => return Err(DecodeError::UnableToGetBytes),
+        };
+        Hash256::from_ssz_bytes(&bytes_data)?
+    };
 
     let deposit_data = DepositData {
         amount,
-        signature: decode_token!(SignatureBytes, into_bytes),
-        withdrawal_credentials: decode_token!(Hash256, into_bytes),
-        pubkey: decode_token!(PublicKeyBytes, into_bytes),
+        signature: decode_token!(SignatureBytes),
+        withdrawal_credentials: decode_token!(Hash256),
+        pubkey: decode_token!(PublicKeyBytes),
     };
 
     Ok((deposit_data, root))
