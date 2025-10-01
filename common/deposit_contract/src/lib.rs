f866bd77ee2e3df84017423c66f33ec30b2a5b7a
Mac L <mjladson@pm.me>
2025-10-01 22:28:44 +1000

    Cleanup

--- common/deposit_contract/src/lib.rs
+++ common/deposit_contract/src/lib.rs
@@ -5,40 +5,12 @@ use ssz::{Decode, DecodeError as SszDecodeError, Encode};
 use tree_hash::TreeHash;
 use types::{DepositData, Hash256, PublicKeyBytes, SignatureBytes};
 
-pub use EncodeError as Error;
-
 #[derive(Debug)]
-pub enum EncodeError {
+pub enum Error {
     AlloyCoreError(alloy_json_abi::Error),
     SerdeJsonError(serde_json::Error),
     DynAbiError(alloy_dyn_abi::Error),
-    FunctionNotFound,
-}
-
-impl From<alloy_json_abi::Error> for EncodeError {
-    fn from(e: alloy_json_abi::Error) -> EncodeError {
-        EncodeError::AlloyCoreError(e)
-    }
-}
-
-impl From<serde_json::Error> for EncodeError {
-    fn from(e: serde_json::Error) -> EncodeError {
-        EncodeError::SerdeJsonError(e)
-    }
-}
-
-impl From<alloy_dyn_abi::Error> for EncodeError {
-    fn from(e: alloy_dyn_abi::Error) -> EncodeError {
-        EncodeError::DynAbiError(e)
-    }
-}
-
-#[derive(Debug)]
-pub enum DecodeError {
-    AlloyCoreError(alloy_json_abi::Error),
     SszDecodeError(SszDecodeError),
-    SerdeJsonError(serde_json::Error),
-    DynAbiError(alloy_dyn_abi::Error),
     FunctionNotFound,
     MissingField,
     UnableToGetBytes,
@@ -46,27 +18,27 @@ pub enum DecodeError {
     InadequateBytes,
 }
 
-impl From<alloy_json_abi::Error> for DecodeError {
-    fn from(e: alloy_json_abi::Error) -> DecodeError {
-        DecodeError::AlloyCoreError(e)
+impl From<alloy_json_abi::Error> for Error {
+    fn from(e: alloy_json_abi::Error) -> Error {
+        Error::AlloyCoreError(e)
     }
 }
 
-impl From<serde_json::Error> for DecodeError {
-    fn from(e: serde_json::Error) -> DecodeError {
-        DecodeError::SerdeJsonError(e)
+impl From<serde_json::Error> for Error {
+    fn from(e: serde_json::Error) -> Error {
+        Error::SerdeJsonError(e)
     }
 }
 
-impl From<alloy_dyn_abi::Error> for DecodeError {
-    fn from(e: alloy_dyn_abi::Error) -> DecodeError {
-        DecodeError::DynAbiError(e)
+impl From<alloy_dyn_abi::Error> for Error {
+    fn from(e: alloy_dyn_abi::Error) -> Error {
+        Error::DynAbiError(e)
     }
 }
 
-impl From<SszDecodeError> for DecodeError {
-    fn from(e: SszDecodeError) -> DecodeError {
-        DecodeError::SszDecodeError(e)
+impl From<SszDecodeError> for Error {
+    fn from(e: SszDecodeError) -> Error {
+        Error::SszDecodeError(e)
     }
 }
 
@@ -83,7 +55,7 @@ pub mod testnet {
         include_bytes!("../contracts/v0.12.1_testnet_validator_registration.bytecode");
 }
 
-pub fn encode_eth1_tx_data(deposit_data: &DepositData) -> Result<Vec<u8>, EncodeError> {
+pub fn encode_eth1_tx_data(deposit_data: &DepositData) -> Result<Vec<u8>, Error> {
     let params = vec![
         DynSolValue::Bytes(deposit_data.pubkey.as_ssz_bytes()),
         DynSolValue::Bytes(deposit_data.withdrawal_credentials.as_ssz_bytes()),
@@ -94,35 +66,34 @@ pub fn encode_eth1_tx_data(deposit_data: &DepositData) -> Result<Vec<u8>, Encode
         ),
     ];
 
+    // Here we make an assumption that the `crate::testnet::ABI` has a superset of the features of
+    // the crate::ABI`.
     let abi: JsonAbi = serde_json::from_slice(ABI)?;
-    let functions = abi
+    let function = abi
         .function("deposit")
-        .ok_or(EncodeError::FunctionNotFound)?;
-    let function = &functions.first().ok_or(EncodeError::FunctionNotFound)?; // Get the first (and likely only) deposit function
+        .and_then(|functions| functions.first())
+        .ok_or(Error::FunctionNotFound)?;
 
-    Ok(function.abi_encode_input(&params)?)
+    function
+        .abi_encode_input(&params)
+        .map_err(Error::DynAbiError)
 }
 
-pub fn decode_eth1_tx_data(
-    bytes: &[u8],
-    amount: u64,
-) -> Result<(DepositData, Hash256), DecodeError> {
+pub fn decode_eth1_tx_data(bytes: &[u8], amount: u64) -> Result<(DepositData, Hash256), Error> {
     let abi: JsonAbi = serde_json::from_slice(ABI)?;
-    let functions = abi
-        .function("deposit")
-        .ok_or(DecodeError::FunctionNotFound)?;
-    let function = &functions.first().ok_or(DecodeError::FunctionNotFound)?;
+    let functions = abi.function("deposit").ok_or(Error::FunctionNotFound)?;
+    let function = &functions.first().ok_or(Error::FunctionNotFound)?;
 
-    let input_data = bytes.get(4..).ok_or(DecodeError::InadequateBytes)?;
+    let input_data = bytes.get(4..).ok_or(Error::InadequateBytes)?;
     let mut tokens = function.abi_decode_input(input_data)?;
 
     macro_rules! decode_token {
         ($type: ty) => {{
-            let token = tokens.pop().ok_or(DecodeError::MissingToken)?;
+            let token = tokens.pop().ok_or(Error::MissingToken)?;
             let bytes_data = match token {
                 DynSolValue::Bytes(b) => b,
                 DynSolValue::FixedBytes(b, _) => b.to_vec(),
-                _ => return Err(DecodeError::UnableToGetBytes),
+                _ => return Err(Error::UnableToGetBytes),
             };
             <$type>::from_ssz_bytes(&bytes_data)?
         }};
