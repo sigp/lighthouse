389c5369eced6d580bedcc763a47459a228a7d78
Eitan Seri- Levi <eserilev@gmail.com>
2025-10-06 12:55:44 -0700

    Fix EF Tests

--- testing/ef_tests/src/cases/fork_choice.rs
+++ testing/ef_tests/src/cases/fork_choice.rs
@@ -472,7 +472,8 @@ impl<E: EthSpec> Tester<E> {
         let since_genesis = tick
             .checked_sub(genesis_time)
             .ok_or_else(|| Error::FailedToParseTest("tick is prior to genesis".into()))?;
-        let slots_since_genesis = since_genesis / self.spec.slot_duration_ms / MS_TO_SEC;
+        let since_genesis_ms = since_genesis * MS_TO_SEC;
+        let slots_since_genesis = since_genesis_ms / self.spec.slot_duration_ms;
         Ok(self.spec.genesis_slot + slots_since_genesis)
     }
 
