884e1171fe60ce2553049a875566260387eac7f0
Tan Chee Keong <tanck@sigmaprime.io>
2025-10-08 14:36:49 +0800

    revise

--- beacon_node/beacon_chain/src/kzg_utils.rs
+++ beacon_node/beacon_chain/src/kzg_utils.rs
@@ -339,24 +339,26 @@ pub fn reconstruct_blobs<E: EthSpec>(
                 cell_ids.push(data_column.index);
             }
 
-            let (all_cells, num_cells_original_blob): (Box<dyn Iterator<Item = _>>, usize) =
-                if data_columns.len() < E::number_of_columns() {
-                    let (cells, _kzg_proofs) = kzg
-                        .recover_cells_and_compute_kzg_proofs(&cell_ids, &cells)
-                        .map_err(|e| {
-                            format!("Failed to recover cells and compute KZG proofs: {e:?}")
-                        })?;
-                    let len = cells.len() / 2;
-                    (Box::new(cells.into_iter()), len)
-                } else {
-                    let len = cells.len() / 2;
-                    (Box::new(cells.into_iter().map(|cell| (*cell).into())), len)
-                };
+            let num_cells_original_blob = E::number_of_columns() / 2;
+            let blob_bytes = if data_columns.len() < E::number_of_columns() {
+                let (recovered_cells, _kzg_proofs) = kzg
+                    .recover_cells_and_compute_kzg_proofs(&cell_ids, &cells)
+                    .map_err(|e| {
+                        format!("Failed to recover cells and compute KZG proofs: {e:?}")
+                    })?;
 
-            let blob_bytes = all_cells
-                .take(num_cells_original_blob)
-                .flat_map(|cell| cell.into_iter())
-                .collect();
+                recovered_cells
+                    .into_iter()
+                    .take(num_cells_original_blob)
+                    .flat_map(|cell| cell.into_iter())
+                    .collect()
+            } else {
+                cells
+                    .into_iter()
+                    .take(num_cells_original_blob)
+                    .flat_map(|cell| (*cell).into_iter())
+                    .collect()
+            };
 
             let blob = Blob::<E>::new(blob_bytes).map_err(|e| format!("{e:?}"))?;
             let kzg_proof = KzgProof::empty();
