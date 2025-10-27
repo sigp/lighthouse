ead5cbf343b7507f734dbcbaf08decb8f4359c52
Michael Sproul <michael@sigmaprime.io>
2025-10-27 13:07:13 +1100

    Handle missing DataColumnCustodyInfo properly

--- beacon_node/http_api/src/custody.rs
+++ beacon_node/http_api/src/custody.rs
@@ -20,20 +20,26 @@ pub fn info<T: BeaconChainTypes>(
         return Err(custom_bad_request("Fulu is not scheduled".to_string()));
     }
 
-    let data_column_custody_info = chain
+    let opt_data_column_custody_info = chain
         .store
         .get_data_column_custody_info()
-        .map_err(|e| custom_server_error(format!("error reading data column custody info: {e:?}")))?
-        .ok_or_else(|| custom_server_error("data column custody info missing".to_string()))?;
+        .map_err(|e| custom_server_error(format!("error reading DataColumnCustodyInfo: {e:?}")))?;
 
     let column_data_availability_boundary = chain
         .column_data_availability_boundary()
         .ok_or_else(|| custom_server_error("unreachable: Fulu should be enabled".to_string()))?;
 
-    let earliest_custodied_data_column_slot = data_column_custody_info
-        .earliest_data_column_slot
+    let earliest_custodied_data_column_slot = opt_data_column_custody_info
+        .and_then(|info| info.earliest_data_column_slot)
         .unwrap_or_else(|| {
-            column_data_availability_boundary.start_slot(T::EthSpec::slots_per_epoch())
+            // If there's no data column custody info/earliest data column slot, it means *column*
+            // backfill is not running. Block backfill could still be running, so our earliest
+            // available column is either the oldest block slot or the DA boundary, whichever is
+            // more recent.
+            let oldest_block_slot = chain.store.get_anchor_info().oldest_block_slot;
+            column_data_availability_boundary
+                .start_slot(T::EthSpec::slots_per_epoch())
+                .max(oldest_block_slot)
         });
     let earliest_custodied_data_column_epoch =
         earliest_custodied_data_column_slot.epoch(T::EthSpec::slots_per_epoch());
