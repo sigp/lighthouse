791c968709aaea1ae8d84f8cd2e6a52c4e048441
Eitan Seri- Levi <eserilev@gmail.com>
2025-11-07 01:57:24 -0800

    Remove duplicate

--- beacon_node/beacon_processor/src/lib.rs
+++ beacon_node/beacon_processor/src/lib.rs
@@ -773,8 +773,6 @@ impl<E: EthSpec> BeaconProcessor<E> {
                             Some(item)
                         } else if let Some(item) = work_queues.rpc_custody_column_queue.pop() {
                             Some(item)
-                        } else if let Some(item) = work_queues.rpc_custody_column_queue.pop() {
-                            Some(item)
                         // Check delayed blocks before gossip blocks, the gossip blocks might rely
                         // on the delayed ones.
                         } else if let Some(item) = work_queues.delayed_block_queue.pop() {
