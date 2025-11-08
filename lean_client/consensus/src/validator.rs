d2b9434b0dda50fe8a9adde4f0227b9530797c68
hopinheimer <knmanas6@gmail.com>
2025-11-07 23:09:22 -0500

    validator impl

--- lean_client/consensus/src/validator.rs
+++ lean_client/consensus/src/validator.rs
@@ -1,4 +1,8 @@
 use ssz_derive::{Decode, Encode};
+use types::{FixedVector, typenum::U52};
+
 use tree_hash_derive::TreeHash;
 #[derive(Clone, PartialEq, Decode, Encode, TreeHash)]
-pub struct Validator {}
+pub struct Validator {
+    pubkey: FixedVector<u8, U52>,
+}
