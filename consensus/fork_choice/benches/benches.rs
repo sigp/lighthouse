635611c15aacd721bb6dd94af7b0dd19cd2111f9
Michael Sproul <michael@sigmaprime.io>
2025-11-10 11:38:41 +1100

    Update benchmark

--- consensus/fork_choice/benches/benches.rs
+++ consensus/fork_choice/benches/benches.rs
@@ -1,4 +1,4 @@
-use criterion::{BenchmarkId, Criterion, black_box, criterion_group, criterion_main};
+use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
 use fork_choice::{QueuedAttestation, dequeue_attestations};
 use std::collections::VecDeque;
 use types::{Epoch, Hash256, Slot};
@@ -6,6 +6,7 @@ use types::{Epoch, Hash256, Slot};
 fn all_benches(c: &mut Criterion) {
     let num_attestations = 1_500_000_usize / 16;
     let unique_slots = 2_usize;
+    let num_iterations = 64;
     let attestations_per_slot = num_attestations / unique_slots;
     let queued_attestations = (0..num_attestations)
         .map(|i| QueuedAttestation {
@@ -16,17 +17,33 @@ fn all_benches(c: &mut Criterion) {
         })
         .collect::<VecDeque<_>>();
 
-    let current_slot = Slot::from(unique_slots) - 1;
-
     c.bench_with_input(
         BenchmarkId::new("dequeue_attestations", num_attestations),
         &queued_attestations,
         |b, attestations| {
             b.iter(|| {
+                // Simulate dequeueing and queuing of attestations over multiple slots.
                 let mut attestations = attestations.clone();
-                let dequeued = dequeue_attestations(current_slot, &mut attestations);
-                assert_eq!(dequeued.len(), num_attestations - attestations_per_slot);
-                black_box(dequeued);
+                let end_slot = unique_slots;
+                for i in 1..=num_iterations {
+                    let dequeued = dequeue_attestations(Slot::from(i), &mut attestations);
+                    assert_eq!(dequeued.len(), attestations_per_slot);
+
+                    let next_slot = end_slot + i - 1;
+                    let new_attestations = std::iter::repeat(QueuedAttestation {
+                        slot: Slot::from(next_slot),
+                        attesting_indices: vec![],
+                        block_root: Hash256::ZERO,
+                        target_epoch: Epoch::new(0),
+                    })
+                    .take(attestations_per_slot);
+
+                    for attestation in new_attestations {
+                        attestations.push(attestation);
+                    }
+
+                    assert_eq!(attestations.len(), num_attestations);
+                }
             })
         },
     );
