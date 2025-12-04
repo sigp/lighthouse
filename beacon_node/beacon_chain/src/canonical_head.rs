6ca472f94de028daa502821df218818a93c28b11
Antoine James <antoine@ethereum.org>
2025-12-04 06:42:10 +0100

    fix(beacon_node): add pruning of observed_column_sidecars

--- beacon_node/beacon_chain/src/canonical_head.rs
+++ beacon_node/beacon_chain/src/canonical_head.rs
@@ -951,6 +951,13 @@ impl<T: BeaconChainTypes> BeaconChain<T> {
                 .start_slot(T::EthSpec::slots_per_epoch()),
         );
 
+        self.observed_column_sidecars.write().prune(
+            new_view
+                .finalized_checkpoint
+                .epoch
+                .start_slot(T::EthSpec::slots_per_epoch()),
+        );
+
         self.observed_slashable.write().prune(
             new_view
                 .finalized_checkpoint
