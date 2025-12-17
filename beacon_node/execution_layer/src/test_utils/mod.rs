dbb37f90008be75643edc89e8be13a9ebb4e4d9b
ackintosh <sora.akatsuki@gmail.com>
2025-12-17 12:30:38 +0900

    adjusts log message

--- beacon_node/execution_layer/src/test_utils/mod.rs
+++ beacon_node/execution_layer/src/test_utils/mod.rs
@@ -754,7 +754,7 @@ pub fn serve<E: EthSpec>(
 
     info!(
         listen_address = listening_socket.to_string(),
-        "Metrics HTTP server started"
+        "Mock Execution Client started"
     );
 
     Ok((listening_socket, server))
