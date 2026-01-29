72f0a7b9c1990f9514408066aee5ec022b1a9d1c
Eitan Seri- Levi <eserilev@gmail.com>
2026-01-29 12:59:52 -0800

    move commitments to bid

--- consensus/types/src/execution/execution_payload_envelope.rs
+++ consensus/types/src/execution/execution_payload_envelope.rs
@@ -1,7 +1,6 @@
 use crate::test_utils::TestRandom;
 use crate::{
-    EthSpec, ExecutionPayloadGloas, ExecutionRequests, ForkName, Hash256, KzgCommitments,
-    SignedRoot, Slot,
+    EthSpec, ExecutionPayloadGloas, ExecutionRequests, ForkName, Hash256, SignedRoot, Slot,
 };
 use context_deserialize::context_deserialize;
 use educe::Educe;
@@ -21,7 +20,6 @@ pub struct ExecutionPayloadEnvelope<E: EthSpec> {
     pub builder_index: u64,
     pub beacon_block_root: Hash256,
     pub slot: Slot,
-    pub blob_kzg_commitments: KzgCommitments<E>,
     pub state_root: Hash256,
 }
 
