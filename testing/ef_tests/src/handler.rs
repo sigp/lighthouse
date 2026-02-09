0baa3ccec0593349784e26026318d85f5c60e474
Michael Sproul <michael@sigmaprime.io>
2026-02-10 10:59:41 +1100

    Enable deposit request tests

--- testing/ef_tests/src/handler.rs
+++ testing/ef_tests/src/handler.rs
@@ -1137,10 +1137,12 @@ impl<E: EthSpec + TypeName, O: Operation<E>> Handler for OperationsHandler<E, O>
     fn is_enabled_for_fork(&self, fork_name: ForkName) -> bool {
         Self::Case::is_enabled_for_fork(fork_name)
             && (!fork_name.gloas_enabled()
-                || self.handler_name() == "withdrawals"
                 || self.handler_name() == "attestation"
+                || self.handler_name() == "attester_slashing"
+                || self.handler_name() == "deposit_requests"
+                || self.handler_name() == "execution_payload"
                 || self.handler_name() == "proposer_slashing"
-                || self.handler_name() == "execution_payload")
+                || self.handler_name() == "withdrawals")
     }
 }
 
