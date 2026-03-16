be9e32f46b7de08f24c409aa5c09eb924826df89
Eitan Seri- Levi <eserilev@gmail.com>
2026-03-15 23:13:14 -0700

    cleanup

--- beacon_node/beacon_chain/src/canonical_head.rs
+++ beacon_node/beacon_chain/src/canonical_head.rs
@@ -370,6 +370,13 @@ impl<T: BeaconChainTypes> CanonicalHead<T> {
         Ok((head, execution_status))
     }
 
+    // TODO(gloas) just a stub for now, implement this once we have fork choice.
+    /// Returns true if the payload for this block is canonical according to fork choice
+    /// Returns an error if the block root doesn't exist in fork choice.
+    fn block_has_canonical_payload(&self, _root: &Hash256) -> Result<bool, Error> {
+        Ok(true)
+    }
+
     /// Returns a clone of `self.cached_head`.
     ///
     /// Takes a read-lock on `self.cached_head` for a short time (just long enough to clone it).
@@ -426,21 +433,14 @@ pub trait CanonicalHeadReader: Send + Sync {
     fn block_has_canonical_payload(&self, root: &Hash256) -> Result<bool, Error>;
 }
 
-impl<T: BeaconChainTypes> CanonicalHeadReader for CanonicalHead<T> {
-    // TODO(gloas) just a stub for now, implement this once we have fork choice.
-    /// Returns true if the payload for this block is canonical according to fork choice
-    /// Returns an error if the block root doesn't exist in fork choice.
-    fn block_has_canonical_payload(&self, _root: &Hash256) -> Result<bool, Error> {
-        Ok(true)
-    }
-}
-
 impl<T: BeaconChainTypes> CanonicalHeadReader for BeaconChain<T> {
     fn block_has_canonical_payload(&self, root: &Hash256) -> Result<bool, Error> {
         self.canonical_head.block_has_canonical_payload(root)
     }
 }
 
+// TODO(refactor) we can move any canonical head related read functions into the newly introduced trait
+// `CanonicalHeadReader`. This trait is useful for writing unit tests.
 impl<T: BeaconChainTypes> BeaconChain<T> {
     /// Contains the "best block"; the head of the canonical `BeaconChain`.
     ///
