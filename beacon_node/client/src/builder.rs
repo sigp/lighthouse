ef148594f2484930d6dfcdba6c6a16df235ff079
Eitan Seri-Levi <eserilev@ucsc.edu>
2026-05-06 18:33:57 +0300

    Remove wierd comment

--- beacon_node/client/src/builder.rs
+++ beacon_node/client/src/builder.rs
@@ -6,8 +6,6 @@ use crate::config::{ClientGenesis, Config as ClientConfig};
 use crate::notifier::spawn_notifier;
 use beacon_chain::attestation_simulator::start_attestation_simulator_service;
 use beacon_chain::data_availability_checker::start_availability_cache_maintenance_service as start_block_cache_maintenance_service;
-// pending_payload_cache pruning is driven from `BeaconChain::after_finalization`, no separate
-// background service is needed.
 use beacon_chain::graffiti_calculator::start_engine_version_cache_refresh_service;
 use beacon_chain::proposer_prep_service::start_proposer_prep_service;
 use beacon_chain::schema_change::migrate_schema;
