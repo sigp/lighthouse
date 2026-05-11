7e539c7337707d7f1b5933f4efea612d0e083dc8
Eitan Seri-Levi <eserilev@ucsc.edu>
2026-05-11 14:48:45 +0300

    remove rename start_block_cache_maintenance_service

--- beacon_node/client/src/builder.rs
+++ beacon_node/client/src/builder.rs
@@ -5,7 +5,7 @@ use crate::compute_light_client_updates::{
 use crate::config::{ClientGenesis, Config as ClientConfig};
 use crate::notifier::spawn_notifier;
 use beacon_chain::attestation_simulator::start_attestation_simulator_service;
-use beacon_chain::data_availability_checker::start_availability_cache_maintenance_service as start_block_cache_maintenance_service;
+use beacon_chain::data_availability_checker::start_availability_cache_maintenance_service;
 use beacon_chain::graffiti_calculator::start_engine_version_cache_refresh_service;
 use beacon_chain::proposer_prep_service::start_proposer_prep_service;
 use beacon_chain::schema_change::migrate_schema;
@@ -782,7 +782,7 @@ where
             }
 
             start_proposer_prep_service(runtime_context.executor.clone(), beacon_chain.clone());
-            start_block_cache_maintenance_service(
+            start_availability_cache_maintenance_service(
                 runtime_context.executor.clone(),
                 beacon_chain.clone(),
             );
