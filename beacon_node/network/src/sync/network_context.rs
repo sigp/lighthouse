ce02aa5ef14e32c5c6d99af45aaf33702eb878b0
dapplion <35266934+dapplion@users.noreply.github.com>
2026-05-20 06:08:02 +0200

    Allow private_interfaces on BlockComponentCouplingError to unbreak lint

--- beacon_node/network/src/sync/network_context.rs
+++ beacon_node/network/src/sync/network_context.rs
@@ -100,7 +100,7 @@ pub enum RpcResponseError {
     RpcError(#[allow(dead_code)] RPCError),
     VerifyError(LookupVerifyError),
     CustodyRequestError(#[allow(dead_code)] CustodyRequestError),
-    BlockComponentCouplingError(CouplingError),
+    BlockComponentCouplingError(#[allow(private_interfaces)] CouplingError),
 }
 
 #[derive(Debug, PartialEq, Eq)]
