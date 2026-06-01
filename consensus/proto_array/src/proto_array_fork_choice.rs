bb9dce3aa323f6f33eecf6377b16bd789b6a2f31
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2026-06-01 08:02:17 +0200

    Simplify documentation for is_child_full method
    Remove redundant explanation about FULL child requirement.

--- consensus/proto_array/src/proto_array_fork_choice.rs
+++ consensus/proto_array/src/proto_array_fork_choice.rs
@@ -294,8 +294,7 @@ impl Block {
     }
 
     /// Returns `true` if the given (post-Gloas) `child_bid` commits to *this* block's execution
-    /// payload, i.e. the child is "FULL" relative to this parent. A FULL child requires this
-    /// parent's payload to have been imported before the child can be imported.
+    /// payload, i.e. the child is "FULL" relative to this parent.
     pub fn is_child_full<E: EthSpec>(&self, child_bid: &SignedExecutionPayloadBid<E>) -> bool {
         if let Some(execution_payload_block_hash) = self.execution_payload_block_hash {
             execution_payload_block_hash == child_bid.message.parent_block_hash
