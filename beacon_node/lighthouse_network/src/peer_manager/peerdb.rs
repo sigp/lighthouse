c2c7004885e689f4f5c6c2a289cc03ee6c17256c
Pawan Dhananjay <pawandhananjay@gmail.com>
2026-07-10 03:18:40 +0530

    Fix rust 1.97 lints (#9607)
    N/A
    
    
      Fix lints. Everything seems straightforward and no logic change.
    
    
    Co-Authored-By: Pawan Dhananjay <pawandhananjay@gmail.com>

--- beacon_node/lighthouse_network/src/peer_manager/peerdb.rs
+++ beacon_node/lighthouse_network/src/peer_manager/peerdb.rs
@@ -1543,7 +1543,7 @@ mod tests {
         }
         assert_eq!(pdb.disconnected_peers, 0);
 
-        for (_, p) in peer_list.iter() {
+        for p in peer_list.values() {
             pdb.inject_disconnect(p);
             // Allow the timing to update correctly
         }
@@ -1578,7 +1578,7 @@ mod tests {
             peer_list.insert(id, new_peer);
         }
         assert_eq!(pdb.disconnected_peers, pdb.disconnected_peers().count());
-        for (_, p) in peer_list.iter() {
+        for p in peer_list.values() {
             pdb.inject_disconnect(p);
         }
         assert_eq!(pdb.disconnected_peers, pdb.disconnected_peers().count());
