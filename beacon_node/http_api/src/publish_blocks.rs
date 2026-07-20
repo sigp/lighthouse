7312ce92ebf595fecd9e12862ee73d22c9401d87
Shane K Moore <41407272+shane-moore@users.noreply.github.com>
2026-07-20 05:03:46 -0700

    chore: make post envelope beacon api spec compliant (#9276)
    Part of #8828 for the stateful path. Aligns `produceBlockV4` and `POST /eth/v1/beacon/execution_payload_envelopes` with beacon-APIs [#580](https://github.com/ethereum/beacon-APIs/pull/580) and [#624](https://github.com/ethereum/beacon-APIs/pull/624).
    
    
      - Request stateful block production with required `include_payload=false`
    - Submit `SignedExecutionPayloadEnvelope` with required `Eth-Consensus-Version` and `Eth-Blob-Data-Included: false` headers
    - Support `broadcast_validation` (`gossip`, `consensus`, `consensus_and_equivocation`)
    - Return `202` when the envelope is broadcast but not fully imported
    
    
    Co-Authored-By: shane-moore <skm1790@gmail.com>

--- beacon_node/http_api/src/publish_blocks.rs
+++ beacon_node/http_api/src/publish_blocks.rs
@@ -728,7 +728,7 @@ fn late_block_logging<T: BeaconChainTypes, P: AbstractExecPayload<T::EthSpec>>(
 }
 
 /// Check if any of the blobs or the block are slashable. Returns `BlockError::Slashable` if so.
-fn check_slashable<T: BeaconChainTypes>(
+pub(crate) fn check_slashable<T: BeaconChainTypes>(
     chain_clone: &BeaconChain<T>,
     block_root: Hash256,
     block_clone: &SignedBeaconBlock<T::EthSpec, FullPayload<T::EthSpec>>,
