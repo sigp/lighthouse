8f4c278c520e458a33cc5df1ca9a3413a13d66a1
Daniel Knopik <daniel@dknopik.de>
2026-07-24 17:19:00 +0200

    fix botched merge

--- beacon_node/execution_layer/src/engine_api/http.rs
+++ beacon_node/execution_layer/src/engine_api/http.rs
@@ -1517,7 +1517,7 @@ impl HttpJsonRpc {
                 }
             }
         } else if engine_capabilities.forkchoice_updated_v4 {
-            self.forkchoice_updated_v4(forkchoice_state, maybe_payload_attributes)
+            self.forkchoice_updated_v4(forkchoice_state, maybe_payload_attributes, custody_columns)
                 .await
         } else if engine_capabilities.forkchoice_updated_v3 {
             self.forkchoice_updated_v3(forkchoice_state, maybe_payload_attributes)
