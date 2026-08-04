d6217c9bb854df779b400c420258889acfd244bb
conache <C.conache@gmail.com>
2026-08-04 19:36:43 +0300

    Accept requests for pre-Heze epochs

--- beacon_node/http_api/src/inclusion_list_duties.rs
+++ beacon_node/http_api/src/inclusion_list_duties.rs
@@ -21,14 +21,6 @@ pub fn inclusion_list_duties<T: BeaconChainTypes>(
         ));
     }
 
-    // Inclusion list committees only exist from the Heze fork onwards.
-    if !chain.spec.fork_name_at_epoch(request_epoch).heze_enabled() {
-        return Err(warp_utils::reject::custom_bad_request(format!(
-            "request epoch {} is prior to the Heze fork",
-            request_epoch
-        )));
-    }
-
     let current_epoch = chain
         .slot_clock
         .now_or_genesis()
--- beacon_node/http_api/tests/interactive_tests.rs
+++ beacon_node/http_api/tests/interactive_tests.rs
@@ -1430,17 +1430,16 @@ async fn inclusion_list_duties_across_fork_boundary() {
         .await;
     assert_eq!(harness.chain.epoch().unwrap(), heze_fork_epoch);
 
-    // Pre-Heze epochs are rejected.
+    // Pre-Heze epochs are not rejected.
     for epoch in [Epoch::new(0), Epoch::new(1)] {
+        let duties =             client
+            .post_validator_duties_inclusion_list(epoch, &[0])
+            .await
+            .unwrap_or_else(|e| panic!("Failed to get duties on pre-Heze epoch {epoch}: {e:?}"));
         assert_eq!(
-            client
-                .post_validator_duties_inclusion_list(epoch, &[0])
-                .await
-                .unwrap_err()
-                .status()
-                .map(Into::into),
-            Some(400),
-            "pre-Heze epoch {epoch} should be rejected"
+               duties.data.len(),
+            1,
+            "validator 0 should have an IL duty in pre-Heze epoch {epoch}"
         );
     }
 
