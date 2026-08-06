3c9a9b65e3ce5b9545d6dd52ca7c30923ddc0fbe
Eitan Seri-Levi <eserilev@gmail.com>
2026-08-06 09:06:00 +0300

    Gloas alpha spec 12 changes (#9596)
    ## Issue Addressed
     
    All the changes required for gloas alpha spec 12 minus stable containers. Note that EF tests cannot pass without the stable container changes. For example finality tests fail w/ ParentBlockRootMismatch because progressive merkelization isn't implemented.
    
    So I think the plan should be to merge progressive containers, without enabling the new alpha spec 12 tests, and then merge this PR

--- beacon_node/beacon_chain/src/payload_envelope_verification/mod.rs
+++ beacon_node/beacon_chain/src/payload_envelope_verification/mod.rs
@@ -237,6 +237,12 @@ pub enum EnvelopeError {
         payload_slot: Slot,
         latest_finalized_slot: Slot,
     },
+    /// An envelope list exceeds its spec limit
+    OperationListTooLong {
+        kind: &'static str,
+        length: usize,
+        max: usize,
+    },
     /// Some Beacon Chain Error
     BeaconChainError(Box<BeaconChainError>),
     /// Some Beacon State error
@@ -275,6 +281,7 @@ impl EnvelopeError {
             | EnvelopeError::ExecutionRequestsRootMismatch { .. }
             | EnvelopeError::UnknownValidator { .. }
             | EnvelopeError::IncorrectBlockProposer { .. }
+            | EnvelopeError::OperationListTooLong { .. }
             | EnvelopeError::EnvelopeProcessingError(_) => true,
             EnvelopeError::ExecutionPayloadError(e) => e.penalize_peer(),
             EnvelopeError::BlockRootUnknown { .. }
