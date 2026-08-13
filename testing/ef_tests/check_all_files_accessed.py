4446c685bdb7190a35bed6e0fe2d7c0a611bdc15
Nikhil Sharma <nikhilsharma230303@gmail.com>
2026-08-13 08:58:33 +0530

    Enable Gloas FCR ef-tests and use parent-first safe hash
    Signed-off-by: Nikhil Sharma <nikhilsharma230303@gmail.com>

--- testing/ef_tests/check_all_files_accessed.py
+++ testing/ef_tests/check_all_files_accessed.py
@@ -60,8 +60,6 @@ excluded_paths = [
     "tests/.*/gloas/ssz_static/ExecutionPayloadHeader/.*",
     # ForkChoiceNode is internal to fork choice and probably doesn't need SSZ tests.
     "tests/.*/gloas/ssz_static/ForkChoiceNode/.*",
-    # TODO(gloas): the FCR handler disables Gloas until Gloas fast confirmation is supported.
-    "tests/.*/gloas/fast_confirmation/.*",
     # TODO(gloas): new in v1.7.0-alpha.13. Needs a handler and support for the
     # `viable_for_head_roots_and_weights` check, which requires proto array internals.
     "tests/.*/gloas/fork_choice/should_apply_proposer_boost/.*",
