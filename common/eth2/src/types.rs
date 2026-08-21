e93dd6ac16a4ec690f4a4203db51add6f0d4d05f
conache <C.conache@gmail.com>
2026-08-21 18:38:50 +0300

    Remove inclusion_list_committee_root from the il duty type

--- common/eth2/src/types.rs
+++ common/eth2/src/types.rs
@@ -867,7 +867,6 @@ pub struct InclusionListDuty {
     #[serde(with = "serde_utils::quoted_u64")]
     pub validator_index: u64,
     pub slot: Slot,
-    pub inclusion_list_committee_root: Hash256,
 }
 
 #[derive(Clone, Deserialize)]
