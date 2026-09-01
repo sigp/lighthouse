54bd7e4f129af6220ddc883f446cc6acf7c17ee7
Daniel Knopik <daniel@dknopik.de>
2026-09-01 15:35:08 +0200

    fix help text

--- beacon_node/src/cli.rs
+++ beacon_node/src/cli.rs
@@ -395,7 +395,7 @@ pub fn cli_app() -> Command {
                 .num_args(0..=1)
                 .default_value("false")
                 .default_missing_value("true")
-                .help("Enables the mplex multiplexer alongside yamux. Yamux is preferred when both are available. Enabled by default; set to \"false\" to disable.")
+                .help("Enables the mplex multiplexer alongside yamux. Yamux is preferred when both are available. Disabled by default; set to \"true\" to enable.")
                 .display_order(0)
         )
         .arg(
