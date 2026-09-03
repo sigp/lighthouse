841eb74aa464d617207cf1e52ddfdc91aff378d7
Pawan Dhananjay <pawandhananjay@gmail.com>
2026-09-03 14:04:52 -0700

    Ignore test vectors temporarily

--- testing/ef_tests/src/cases/fork_choice.rs
+++ testing/ef_tests/src/cases/fork_choice.rs
@@ -421,10 +421,16 @@ impl<E: EthSpec> Case for ForkChoiceTest<E> {
         // introduced in Fulu makes this consistent with the specification from Fulu onward.
         // See: https://github.com/ethereum/consensus-specs/pull/5547
         const IGNORED_PRE_FULU_CASES: &[&str] = &["epoch_boundary"];
+        // TODO(gloas): Remove once the EF test vectors include the fix from
+        // https://github.com/ethereum/consensus-specs/pull/5594.
+        const IGNORED_STALE_GLOAS_CASES: &[&str] =
+            &["new_validator_deposit_with_multiple_epoch_transitions"];
 
         if IGNORED_FAST_CONFIRMATION_CASES.contains(&self.description.as_str())
             || (!fork_name.fulu_enabled()
                 && IGNORED_PRE_FULU_CASES.contains(&self.description.as_str()))
+            || (fork_name == ForkName::Gloas
+                && IGNORED_STALE_GLOAS_CASES.contains(&self.description.as_str()))
         {
             return Err(Error::SkippedKnownFailure);
         }
