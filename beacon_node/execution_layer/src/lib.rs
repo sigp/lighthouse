b24ddb7560e870e677f7a88d947d97e54fd50768
Daniel Knopik <107140945+dknopik@users.noreply.github.com>
2026-09-09 21:57:14 +0200

    `engine_getBlobsV4` (#9438)
    ## Issue Addressed
    
    Implement EIP-8070's engine API calls: `engine_getBlobsV4`.
    
    ## Additional Info
    
    Claude was heavily used for prototyping. Gloas support will be added after #9325 is merged.

--- beacon_node/execution_layer/src/lib.rs
+++ beacon_node/execution_layer/src/lib.rs
@@ -4,7 +4,9 @@
 //! This crate only provides useful functionality for "The Merge", it does not provide any of the
 //! deposit-contract functionality that the `beacon_node/eth1` crate already provides.
 
-use crate::json_structures::{BlobAndProofV2, BlobAndProofV3};
+use crate::json_structures::{
+    BlobAndProofV2, BlobAndProofV3, CustodyColumnsBitArray, GetBlobsV4List,
+};
 use crate::payload_cache::PayloadCache;
 use arc_swap::ArcSwapOption;
 use auth::{Auth, JwtKey, strip_prefix};
@@ -1771,6 +1773,26 @@ impl<E: EthSpec> ExecutionLayer<E> {
         if capabilities.get_blobs_v3 {
             self.engine()
                 .request(|engine| async move { engine.api.get_blobs_v3(query).await })
+                .await
+                .map_err(Box::new)
+                .map_err(Error::EngineError)
+        } else {
+            Err(Error::GetBlobsNotSupported)
+        }
+    }
+
+    pub async fn get_blobs_v4(
+        &self,
+        query: Vec<Hash256>,
+        custody_columns: CustodyColumnsBitArray,
+    ) -> Result<Option<GetBlobsV4List<E>>, Error> {
+        let capabilities = self.get_engine_capabilities(None).await?;
+
+        if capabilities.get_blobs_v4 {
+            self.engine()
+                .request(
+                    |engine| async move { engine.api.get_blobs_v4(query, custody_columns).await },
+                )
                 .await
                 .map_err(Box::new)
                 .map_err(Error::EngineError)
