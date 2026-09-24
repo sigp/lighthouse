0f8c9f97d53bed11b788f39c8e5b7a93248a65a0
dapplion <35266934+dapplion@users.noreply.github.com>
2026-09-24 18:59:36 +0200

    Introduce ParentPayloadStatus for the parent payload edge

--- consensus/proto_array/src/fork_choice_test_definition.rs
+++ consensus/proto_array/src/fork_choice_test_definition.rs
@@ -6,7 +6,7 @@ mod votes;
 
 use crate::error::Error;
 use crate::proto_array_fork_choice::{Block, ExecutionStatus, PayloadStatus, ProtoArrayForkChoice};
-use crate::{InvalidationOperation, JustifiedBalances};
+use crate::{InvalidationOperation, JustifiedBalances, ParentPayloadStatus};
 use fixed_bytes::FixedBytesExtended;
 use serde::{Deserialize, Serialize};
 use ssz::BitVector;
@@ -98,7 +98,7 @@ pub enum Operation {
     },
     AssertParentPayloadStatus {
         block_root: Hash256,
-        expected_status: PayloadStatus,
+        expected_status: ParentPayloadStatus,
     },
     SetPayloadTiebreak {
         block_root: Hash256,
