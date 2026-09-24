84df4c93482e133e1d2b3ef085e234e4295647fc
João Oliveira <hello@jxs.pt>
2026-09-24 09:52:27 +0100

    enable smart dial

--- beacon_node/lighthouse_network/src/peer_manager/mod.rs
+++ beacon_node/lighthouse_network/src/peer_manager/mod.rs
@@ -482,7 +482,8 @@ impl<E: EthSpec> PeerManager<E> {
             vec![]
         };
 
-        // Prioritize Quic connections over Tcp ones.
+        // The order doesn't matter: with smart dialing enabled, libp2p's dial
+        // ranker orders the addresses (QUIC > TCP, IPv6 > IPv4) and applies staggered delays.
         [multiaddr_quic, enr.dialable_multiaddrs_tcp()].concat()
     }
 
